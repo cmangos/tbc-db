6a942a5b9895b14c9a203923aaa67f61fe769d4b
Cala Haryon <calaftp@free.fr>
2013-04-14 10:13:44 +0200

    [0097] Applied SD2 updates
    Everything else seems to be in line, except spell_target_position 4801,8195,20449 which seems off to me
    
    Add missing MovementType Change for Polymorph Clone 16479
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/8399d64e154d8afe73612fb15c4aa2411fa6c57a

---
+++ Updates/0898_CDB-0097_mangos_SD2_fixes.sql
@@ -0,0 +1,4 @@
+-- SD2-classic revision s2347
+-- https://github.com/scriptdev2/scriptdev2/commit/ce98ad5a138ee697cd8577108bc09a848f17c0c8
+UPDATE creature_template SET MovementType = 1 WHERE entry = 16479; -- sheep random movement
+
