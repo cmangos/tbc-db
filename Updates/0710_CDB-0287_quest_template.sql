e599eeceb49cf9c6e00e1b0fab7e960892b8a31f
Cala Haryon <calaftp@free.fr>
2013-10-13 11:04:21 +0200

    [CDB][0287] Fixed Required reputation for quest 6701
    Thanks Neotmiren for pointing and fixing
    * Rogue quest "Syndicate Emblems" - wrong maximum Reputation
    restriction, quest is unavailable
    http://www.wowhead.com/quest=6701/syndicate-emblems
    http://www.wowwiki.com/Syndicate?oldid=295307
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/9793ab4e29886c0fb265d149ce6a1748c69e3793

---
+++ Updates/0710_CDB-0287_quest_template.sql
@@ -0,0 +1,6 @@
+-- Thanks Neotmiren for pointing and fixing
+-- Rogue quest "Syndicate Emblems" - wrong maximum Reputation restriction, quest is unavailable
+-- http://www.wowhead.com/quest=6701/syndicate-emblems
+-- http://www.wowwiki.com/Syndicate?oldid=295307
+UPDATE `quest_template` SET `RequiredMaxRepValue` = 2999 WHERE `entry` = 6701;
+
