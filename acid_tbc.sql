c3c2d8a06bd34c3357ecc59ae209483aed4ba16e
X-Savior <xsavior@acid.com>
2013-11-30 23:07:56 -0700

    Commander Sarannis in Botanica OOC Yell is part of Waypoint Movement - Removed

--- acid_tbc.sql
+++ acid_tbc.sql
@@ -27165,17 +27165,16 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- ==================================================================
 -- High Botanist Freywinn (17975) - boss_high_botanist_freywinn
 -- Commander Sarannis (4.3.4 Official Data - Normal/Heroic) - (Adds Summon Spell: 34803 / Casts - 34810 (NPC: 20083 5 Yards Behind) and 34817 / 34818 / 34819 (NPC: 20078 5 Yards Sides and Front)
-('1797601','17976','1','0','100','7','30000','60000','160000','190000','1','-344','0','0','0','0','0','0','0','0','0','0','Commander Sarannis - Yell OOC'),
-('1797602','17976','4','0','100','6','0','0','0','0','1','-34','0','0','0','0','0','0','0','0','0','0','Commander Sarannis - Yell on Aggro'),
-('1797603','17976','9','0','100','3','0','10','3800','15300','11','34794','1','0','1','-37','0','0','0','0','0','0','Commander Sarannis (Normal) - Cast Arcane Resonance and Yell'),
-('1797604','17976','9','0','100','5','0','10','2800','12900','11','34794','1','0','1','-37','0','0','0','0','0','0','Commander Sarannis (Heroic) - Cast Arcane Resonance and Yell'),
-('1797605','17976','0','0','100','7','20400','20400','13300','37400','11','34799','1','1','1','-38','0','0','0','0','0','0','Commander Sarannis - Cast Arcane Devastation on 3 Arcane Resonance Stack and Yell'),
-('1797606','17976','2','0','100','2','55','0','0','0','1','-343','0','0','12','20078','4','1800000','12','19633','4','1800000','Commander Sarannis (Normal) - Emote and Summon Bloodwarder Reservist and Bloodwarder Mender at 55% HP'),
-('1797607','17976','2','0','100','2','55','0','0','0','12','20078','4','1800000','12','20078','4','1800000','1','-39','0','0','Commander Sarannis (Normal) - Summon 2 Bloodwarder Reservists and Yell at 55% HP'),
-('1797608','17976','0','0','100','5','30000','30000','60000','60000','1','-343','0','0','12','20078','4','1800000','12','19633','4','1800000','Commander Sarannis (Heroic) - Emote and Summon Bloodwarder Reservist and Bloodwarder Mender'),
-('1797609','17976','0','0','100','5','30000','30000','60000','60000','12','20078','4','1800000','12','20078','4','1800000','1','-39','0','0','Commander Sarannis (Heroic) - Summon 2 Bloodwarder Reservists and Yell'),
-('1797610','17976','5','0','100','7','5000','5000','0','0','1','-21','-35','0','0','0','0','0','0','0','0','0','Commander Sarannis - Yell on Player Kill'),
-('1797611','17976','6','0','100','6','0','0','0','0','1','-36','0','0','0','0','0','0','0','0','0','0','Commander Sarannis - Yell on Death'),
+('1797601','17976','4','0','100','6','0','0','0','0','1','-34','0','0','0','0','0','0','0','0','0','0','Commander Sarannis - Yell on Aggro'),
+('1797602','17976','9','0','100','3','0','10','3800','15300','11','34794','1','0','1','-37','0','0','0','0','0','0','Commander Sarannis (Normal) - Cast Arcane Resonance and Yell'),
+('1797603','17976','9','0','100','5','0','10','2800','12900','11','34794','1','0','1','-37','0','0','0','0','0','0','Commander Sarannis (Heroic) - Cast Arcane Resonance and Yell'),
+('1797604','17976','0','0','100','7','20400','20400','13300','37400','11','34799','1','1','1','-38','0','0','0','0','0','0','Commander Sarannis - Cast Arcane Devastation on 3 Arcane Resonance Stack and Yell'),
+('1797605','17976','2','0','100','2','55','0','0','0','1','-343','0','0','12','20078','4','1800000','12','19633','4','1800000','Commander Sarannis (Normal) - Emote and Summon Bloodwarder Reservist and Bloodwarder Mender at 55% HP'),
+('1797606','17976','2','0','100','2','55','0','0','0','12','20078','4','1800000','12','20078','4','1800000','1','-39','0','0','Commander Sarannis (Normal) - Summon 2 Bloodwarder Reservists and Yell at 55% HP'),
+('1797607','17976','0','0','100','5','30000','30000','60000','60000','1','-343','0','0','12','20078','4','1800000','12','19633','4','1800000','Commander Sarannis (Heroic) - Emote and Summon Bloodwarder Reservist and Bloodwarder Mender'),
+('1797608','17976','0','0','100','5','30000','30000','60000','60000','12','20078','4','1800000','12','20078','4','1800000','1','-39','0','0','Commander Sarannis (Heroic) - Summon 2 Bloodwarder Reservists and Yell'),
+('1797609','17976','5','0','100','7','5000','5000','0','0','1','-21','-35','0','0','0','0','0','0','0','0','0','Commander Sarannis - Yell on Player Kill'),
+('1797610','17976','6','0','100','6','0','0','0','0','1','-36','0','0','0','0','0','0','0','0','0','0','Commander Sarannis - Yell on Death'),
 -- Warp Splinter (17977) - boss_warp_splinter
 -- Thorngrin the Tender (4.3.4 Official Data - Normal/Heroic) - (Enrage After Successful Sacrifice Cast - Current is Workaround with EventAI Core Limitations)
 ('1797801','17978','4','0','100','6','0','0','0','0','1','-9','0','0','23','1','0','0','0','0','0','0','Thorngrin the Tender - Yell and Set Phase 1 on Aggro'),
@@ -32243,7 +32242,6 @@ INSERT INTO `creature_ai_texts` (`entry`,`content_default`,`sound`,`type`,`langu
 ('-341','%s jumps out of the shadows!','0','2','0','636','0'),
 ('-342','%s calls more of his allies out of the shadows.','0','2','0','639','0'),
 ('-343','%s calls for reinforcements!','0','2','0','17976','0'),
-('-344','We must not fail our leader!  Kael\'thas will redeem us!','0','1','0','17976','0'),
 -- PLEASE RE-USE -344
 ('-345','The only justice is death!','0','0','0','18796','0'),
 ('-346','%s begins to summon in reinforcements!','0','2','0','2358','0'),
