ec872aecae5c127cae16d7b3d7b6f0d0f735189c
cala <calaftp@free.fr>
2014-06-10 11:58:30 +0200

    [CDB][0555] Fixed modelid of a creature 1949 spawns in Elwynn Forest
    * Are there where female Servants of Azora (Elwynn Forest) in Classic? Yes there were.
    Thanks Neotmiren for pointing and fixing.
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/176e0cf12ce231bc4ae239a4dddb9c80b777aef4)

---
+++ Updates/0567_CDB-0555_creature.sql
@@ -0,0 +1,4 @@
+-- Are there where female Servants of Azora (Elwynn Forest) in Classic?
+-- Thanks Neotmiren for pointing and fixing.
+UPDATE `creature` SET `modelid` = 0 WHERE `id` = 1949; -- allows randomly male or female Servant of Azora
+
