29efe94f8d2b2cdea7319a9b41eadc7c93608879
X-Savior <xsavior@acid.com>
2014-07-24 13:52:51 -0600

    Fix Typo

--- Updates/000_DB_Version.sql
+++ Updates/000_DB_Version.sql
@@ -1,4 +1,4 @@
 -- ==============
 -- VERSION UPDATE
 -- ==============
-UPDATE db_version SET version = 'TBC-DB 1.4.3 - 000 \'VENGEANCE OF AZEROTH BETA BUILD\' for CMaNGOS-TBC';
+UPDATE db_version SET version = 'TBC-DB 1.4.2 - 000 \'PRE-RELEASE BETA BUILD\' for CMaNGOS-TBC';
