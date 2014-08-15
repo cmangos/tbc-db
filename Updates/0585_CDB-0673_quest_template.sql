b3512cf72a2a5f7ef3d3a38ff5495f88bfd78cf3
Tobschinski <tobschinski@gmail.com>
2014-08-15 14:21:34 +0200

    [CDB][0637] Quest - level requirements
    * fix more requirements for various quests in Orgrimmar which have been forgotten in https://github.com/classicdb/database/commit/ac0beca
    
    Source: Master Guide
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/71f4e820e28992b9f22746eb711c6454fdd5a5b3

---
+++ Updates/0585_CDB-0673_quest_template.sql
@@ -0,0 +1,4 @@
+-- Correct More Quest MinLevel
+UPDATE `quest_template` SET `MinLevel`='25' WHERE `entry`='1434'; -- Befouled by Satyr
+UPDATE `quest_template` SET `MinLevel`='9' WHERE `entry`='5729'; -- Hidden Enemies
+
