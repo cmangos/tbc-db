74e53990f7504d66df8cf16ed4f3cfbd2325e843
Tobschinski <tobschinski@gmail.com>
2014-08-15 13:09:15 +0200

    [CDB][0672] Quest: Betrayed 1/4 - 4/4
    * fixed the minimum level for all quests
    
    Source: Bradygames - Master Guide
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/abc2f3a7b3304cb161c3af5bd6a5a41339f343e9

---
+++ Updates/0584_CDB-0672_quest_template.sql
@@ -0,0 +1,6 @@
+-- Quests: Betrayed starting from http://db.hellfire-tbc.com/?npc=4485
+UPDATE `quest_template` SET `MinLevel`='44' WHERE `entry`='3504';
+UPDATE `quest_template` SET `MinLevel`='44' WHERE `entry`='3505';
+UPDATE `quest_template` SET `MinLevel`='44' WHERE `entry`='3506';
+UPDATE `quest_template` SET `MinLevel`='44' WHERE `entry`='3507';
+
