3ff13545297caabb1b5cac254bca02bd9a432017
Cala <calaftp@free.fr>
2014-10-08 14:14:23 +0200

    [CDB][0762] Add creature_template_addon for Twilight Avenger 11880
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/8e34300720f46d9a3715342b76bfcc40214eb317

---
+++ Updates/0602_CDB-0762_creature_addon.sql
@@ -0,0 +1,4 @@
+-- Add creature_template_addon for Twilight Avenger 11880
+DELETE FROM `creature_template_addon` WHERE `entry` = 11880;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (11880, 0, 0, 1, 16, 0, 0, NULL);
+
