bb2ec128f1a3707abbab15c385d48d71d9334836
Tobschinski <tobschinski@github.com>
2015-02-09 04:42:32 +0100

    [CDB][0961] AI: BRD - Fireguard Destroyer
    * Thrash (3417) on spawn &
    * Fireball Volley (15243) in combat!
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/75e7516e53cf992c2a042b7f2e3378d605814be6

---
+++ Updates/0343_CDB-0961_creature_template.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `ResistanceHoly`='0', `ResistanceFire`='0', `ResistanceNature`='0', `ResistanceFrost`='0', `ResistanceShadow`='0', `ResistanceArcane`='0' WHERE `Entry`='8911';
+
