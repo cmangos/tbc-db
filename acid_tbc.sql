b8844e7bd1942a94083ec87479985d67336878d3
Grz3s <grz3sp@wp.pl>
2015-05-03 01:47:33 +0200

    Update npc 18794 after the latest core fixes
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit ACID WOTLK[Update npc 18794 after the latest core fixes] - 227f9f4)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- acid_tbc.sql
+++ acid_tbc.sql
@@ -22171,7 +22171,7 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Ambassador Hellmaw (18731) - boss_ambassador_hellmaw
 -- Grandmaster Vorpil (18732) - boss_grandmaster_vorpil
 -- Cabal Ritualist (4.3.4 Official Data - Normal/Heroic)
-('1879401','18794','11','0','100','6','0','0','0','0','21','0','0','0','0','0','0','0','30','1','3','5','Cabal Ritualist - Prevent Combat Movement and Random Phase Select on Spawn'),
+('1879401','18794','11','0','100','6','0','0','0','0','0','0','0','0','0','0','0','0','30','1','3','5','Cabal Ritualist - Random Phase Select on Spawn'),
 ('1879402','18794','4','0','10','38','0','0','0','0','1','-664','-665','-666','1','-667','-668','-669','1','-665','-666','-669','Cabal Ritualist - Random Say on Aggro'),
 ('1879403','18794','4','61','100','2','0','0','0','0','11','15497','1','0','0','0','0','0','0','0','0','0','Cabal Ritualist (Normal) - Cast Frostbolt on Aggro (Phase 1)'),
 ('1879404','18794','9','61','100','3','0','40','3600','4800','11','15497','1','0','0','0','0','0','0','0','0','0','Cabal Ritualist (Normal) - Cast Frostbolt (Phase 1)'),
