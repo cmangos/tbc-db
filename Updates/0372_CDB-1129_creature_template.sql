3e2b9f1fa29a9eebf6377c37887f1e8d6a3777a3
Tobschinski <tobschinski@gmail.com>
2015-08-05 01:47:51 +0200

    [CDB][1129] Skarr the Unbreakable Mushgog & The Razza
    stats
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/b9ee9ac5c49cf2595ecec0d8150e9a976576be80

---
+++ Updates/0372_CDB-1129_creature_template.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `DamageMultiplier`='5' WHERE `entry`='11447'; -- Mushgog
+UPDATE `creature_template` SET `DamageMultiplier`='12' WHERE `entry`='11497'; -- The Razza
+UPDATE `creature_template` SET `DamageMultiplier`='10' WHERE `entry`='11498'; -- Skarr the Unbreakable
+
