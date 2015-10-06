8dafa2269b74cb320dee6daf4cf79c3e38429800
X-Savior <xsavior@acid.com>
2015-10-06 02:08:22 -0600

    As Promised I am Back and Will Begin Trickling Commits - This is a Useless Placeholder Commit

---
+++ Updates/004_Useless_Placeholder.sql
@@ -0,0 +1,4 @@
+-- ==============
+-- VERSION UPDATE - This File Maintains File Numbering For Syncing Commits
+-- ==============
+UPDATE db_version SET version = 'TBC-DB 1.4.3 \'PRE-RELEASE BUILD\' for CMaNGOS-TBC';
