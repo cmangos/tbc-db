21918421b59ba8fca5dc4eea053ecb1b2f7c63a5
X-Savior <xsavior@acid.com>
2015-10-06 02:29:29 -0600

    Replace Placeholder with Core Patch Update to Latest SQL - Now Should Be Caught Up to Work with Latest Core

--- Updates/004_Useless_Placeholder.sql
+++
@@ -1,4 +0,0 @@
--- ==============
--- VERSION UPDATE - This File Maintains File Numbering For Syncing Commits
--- ==============
-UPDATE db_version SET version = 'TBC-DB 1.4.3 \'PRE-RELEASE BUILD\' for CMaNGOS-TBC';
