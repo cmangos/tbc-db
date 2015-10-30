21299cd1e4e0a89ae2df3e3c7b68ba1fe7a3c61d
Grz3s <grz3sp@wp.pl>
2015-10-30 20:56:56 +0100

    Update script for creature 17391. Required for quest 9540
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit a2bbefd)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- acid_tbc.sql
+++ acid_tbc.sql
@@ -11138,7 +11138,7 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Stillpine Ancestor Akida
 ('1737901','17379','11','0','100','0','0','0','0','0','11','25035','0','1','3','0','16995','0','0','0','0','0','Stillpine Ancestor Akida - Cast Elemental Spawn-in and set correct modelid on Spawn'),
 -- Stillpine Ancestor Coo
-('1739101','17391','11','0','100','0','0','0','0','0','11','25035','0','1','0','0','0','0','0','0','0','0','Stillpine Ancestor Coo - Cast Elemental Spawn-in on Spawn'),
+('1739101','17391','11','0','100','0','0','0','0','0','11','25035','0','1','3','0','16995','0','0','0','0','0','Stillpine Ancestor Coo - Cast Elemental Spawn-in ans set correct modelid on Spawn'),
 -- Stillpine Ancestor Tikti
 ('1739201','17392','11','0','100','0','0','0','0','0','11','25035','0','1','0','0','0','0','0','0','0','0','Stillpine Ancestor Tikti - Cast Elemental Spawn-in on Spawn'),
 -- Stillpine Ancestor Yor
