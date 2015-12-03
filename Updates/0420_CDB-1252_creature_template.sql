4e3270a752445c7d330e7d6603bf61683469c0f0
Tobschinski <tobschinski@gmail.com>
2015-12-03 21:20:07 +0100

    [CDB][1252] Uldaman Stats
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/a2784846748739c34ebdb0edca0895367176ef9a

---
+++ Updates/0420_CDB-1252_creature_template.sql
@@ -0,0 +1,32 @@
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='4847'; -- Shadowforge Relic Hunter (2D) (0.967699A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='4848'; -- Shadowforge Darkcaster (2.4D) (0.967699A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='4849'; -- Shadowforge Archaeologist (2.4D) (0.960742A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='4850'; -- Stonevault Cave Lurker (1.9D) (0.966062A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='4851'; -- Stonevault Rockchewer (1D) (0.964846A)
+UPDATE `creature_template` SET `MaxLevel`='38' WHERE `entry`='4852'; -- Stonevault Oracle (1.8D) (1A) 37-37
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='4853'; -- Stonevault Geomancer (2.4D) (0.967699A)
+UPDATE `creature_template` SET `ArmorMultiplier`='0.3' WHERE `entry`='4855'; -- Stonevault Brawler (2.4D) (0.31843A)
+UPDATE `creature_template` SET `ArmorMultiplier`='2' WHERE `entry`='4857'; -- Stone Keeper (3D) (1.97454A)
+UPDATE `creature_template` SET `ArmorMultiplier`='2' WHERE `entry`='4860'; -- Stone Steward (3D) (1.97328A)
+UPDATE `creature_template` SET `HealthMultiplier`='1.1' WHERE `entry`='6912'; -- Remains of a Paladin (1D) (1A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='7011'; -- Earthen Rocksmasher (1.15D) (0.966194A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='7022'; -- Venomlash Scorpid (2D) (0.966062A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='7030'; -- Shadowforge Geologist (2.1D) (0.967699A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1.3' WHERE `entry`='7076'; -- Earthen Guardian (1.25D) (1.2914A)
+UPDATE `creature_template` SET `MinLevel`='35' WHERE `entry`='7078'; -- Cleft Scorpid (1D) (1A) 36-36
+UPDATE `creature_template` SET `ArmorMultiplier`='0.3' WHERE `entry`='7175'; -- Stonevault Ambusher (1D) (0.322651A)
+UPDATE `creature_template` SET `ArmorMultiplier`='2' WHERE `entry`='7206'; -- Ancient Stone Keeper (3.5D) (1.97454A)
+UPDATE `creature_template` SET `Faction`='416', `ExtraFlags`=`ExtraFlags`|32768 WHERE `entry`='7228'; -- Ironaya (4.5D) (1A) 415
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='7290'; -- Shadowforge Sharpshooter (2.4D) (0.966194A)
+UPDATE `creature_template` SET `ArmorMultiplier`='0.3' WHERE `entry`='7320'; -- Stonevault Mauler (2.5D) (0.31843A)
+UPDATE `creature_template` SET `ArmorMultiplier`='1' WHERE `entry`='7405'; -- Deadly Cleft Scorpid (1D) (0.966062A)
+UPDATE `creature_template` SET `ArmorMultiplier`='10' WHERE `entry`='10120'; -- Vault Warder (3D) (1A) - 17899 armor bestiary (9.76A?)
+
+-- [NOBESTIARY]
+UPDATE `creature_template` SET `DamageMultiplier`='1', `ArmorMultiplier`='1.3' WHERE `entry`='6906'; -- Baelog (5D) (1.2851A)
+UPDATE `creature_template` SET `DamageMultiplier`='1' WHERE `entry`='6907'; -- Eric "The Swift" (3D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='1' WHERE `entry`='6908'; -- Olaf (4D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='1' WHERE `entry`='7291'; -- Galgann Firehammer (3D) (1A)
+UPDATE `creature_template` SET `ExtraFlags`=`ExtraFlags`|32768, `DamageMultiplier`='1' WHERE `entry`='7172'; -- Lore Keeper of Norgannon (1.6D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='1.25' WHERE `entry`='7309'; -- Earthen Custodian (1D) (1A)
+
