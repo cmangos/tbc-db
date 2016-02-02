ac0dae19add4d3b2b81ed350164baccd7c087d3b
Tobschinski <tobschinski@gmail.com>
2016-02-02 14:29:29 +0100

    [CDB][1281] Dire Maul - West
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/de09e6fecb9f8f91bfc55599fed40bb9cadb2b92

---
+++ Updates/0425_CDB-1281_creature_template.sql
@@ -0,0 +1,27 @@
+UPDATE `creature_template` SET `PowerMultiplier`='1.5', `DamageMultiplier`='6', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='2' WHERE `entry`='11459'; -- Ironbark Protector (10D) (1.9785A)
+UPDATE `creature_template` SET `DamageMultiplier`='1', `MeleeBaseAttackTime`='2000' WHERE `entry`='11466'; -- Highborne Summoner (6D) (1A) [NOBESTIARY]
+UPDATE `creature_template` SET `DamageMultiplier`='8', `MeleeBaseAttackTime`='1600' WHERE `entry`='11467'; -- Tsu'zee (11D) (1A)
+UPDATE `creature_template` SET `NpcFlags`='0', `DamageMultiplier`='5.2', `MeleeBaseAttackTime`='1600', `ArmorMultiplier`='1' WHERE `entry`='11469'; -- Eldreth Seether (7D) (0.992261A)
+UPDATE `creature_template` SET `DamageMultiplier`='3.9', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1' WHERE `entry`='11470'; -- Eldreth Sorcerer (6.7D) (0.992232A)
+UPDATE `creature_template` SET `DamageMultiplier`='3.8', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1' WHERE `entry`='11471'; -- Eldreth Apparition (6.5D) (0.992109A)
+UPDATE `creature_template` SET `PowerMultiplier`='3', `DamageMultiplier`='3', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1' WHERE `entry`='11472'; -- Eldreth Spirit (5D) (0.99214A)
+UPDATE `creature_template` SET `DamageMultiplier`='3.9', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1' WHERE `entry`='11473'; -- Eldreth Spectre (6.6D) (0.992232A)
+UPDATE `creature_template` SET `DamageMultiplier`='4.5', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1' WHERE `entry`='11475'; -- Eldreth Phantasm (7.7D) (0.992261A)
+UPDATE `creature_template` SET `DamageMultiplier`='1', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1' WHERE `entry`='11476'; -- Skeletal Highborne (1.4D) (0.99214A)
+UPDATE `creature_template` SET `DamageMultiplier`='1', `MeleeBaseAttackTime`='2000' WHERE `entry`='11477'; -- Rotting Highborne (1.4D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='4', `MeleeBaseAttackTime`='2000' WHERE `entry`='11480'; -- Arcane Aberration (7D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='4', `MeleeBaseAttackTime`='2000' WHERE `entry`='11483'; -- Mana Remnant (7D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MeleeBaseAttackTime`='2000' WHERE `entry`='11484'; -- Residual Monstrosity (10D) (1A) [NOBESTIARY]
+UPDATE `creature_template` SET `DamageMultiplier`='7', `MeleeBaseAttackTime`='2000' WHERE `entry`='11487'; -- Magister Kalendris (12D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MeleeBaseAttackTime`='2000' WHERE `entry`='11488'; -- Illyanna Ravenoak (10D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='8', `MeleeBaseAttackTime`='2000' WHERE `entry`='11489'; -- Tendris Warpwood (13D) (1A)
+UPDATE `creature_template` SET `PowerMultiplier`='2', `MeleeBaseAttackTime`='2000' WHERE `entry`='11496'; -- Immol'thar (8D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='4', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1.5' WHERE `entry`='14303'; -- Petrified Guardian (7D) (1.48925A)
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MeleeBaseAttackTime`='2000' WHERE `entry`='14308'; -- Ferra (10.4D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='1', `MeleeBaseAttackTime`='1000' WHERE `entry`='14370'; -- Cadaverous Worm (2D) (1A) [NOBESTIARY]
+UPDATE `creature_template` SET `DamageMultiplier`='0.1', `DamageSchool`='6', `MeleeBaseAttackTime`='2000', `MechanicImmuneMask`='1' WHERE `entry`='14397'; -- Mana Burst (0.13D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='3.9', `MeleeBaseAttackTime`='2000', `ArmorMultiplier`='1' WHERE `entry`='14398'; -- Eldreth Darter (6.7D) (0.992232A)
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MeleeBaseAttackTime`='2000' WHERE `entry`='14399'; -- Arcane Torrent (10D) (1A)
+UPDATE `creature_template` SET `DamageMultiplier`='1', `MeleeBaseAttackTime`='2000' WHERE `entry`='14400'; -- Arcane Feedback (1.4D) (1A)
+UPDATE `creature_template` SET `Faction`='35', `ExtraFlags`='66', `DamageMultiplier`='1', `MechanicImmuneMask`='1' WHERE `entry`='14566'; -- Ancient Equine Spirit (1.6D) (1A) [NOBESTIARY]
+
