1c09248e87f29c828afabfac7709e4356a780f36
Tobschinski <tobschinski@gmail.com>
2016-02-05 01:56:07 +0100

    [CDB][1300] Gnomeregan - Stats
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/baf2a63d57642da23e4c7442047f1ee819a25777

---
+++ Updates/0429_CDB-1300_creature_template.sql
@@ -0,0 +1,30 @@
+UPDATE `creature_template` SET `MinLevel`='25', `HealthMultiplier`='3.25', `DamageMultiplier`='1.7' WHERE `entry`='6206'; -- Caverndeep Burrower (3D) (1A) (1P) [NOBESTIARY]26-27
+UPDATE `creature_template` SET `MinLevel`='25' WHERE `entry`='6207'; -- Caverndeep Ambusher (1D) (1A) (1P)26-27
+UPDATE `creature_template` SET `MinLevel`='26', `MaxLevel`='27', `DamageMultiplier`='1.7' WHERE `entry`='6211'; -- Caverndeep Reaver (3D) (1A) (1P) [NOBESTIARY]27-28
+UPDATE `creature_template` SET `MaxLevel`='28' WHERE `entry`='6212'; -- Dark Iron Agent (1.7D) (1A) (1P)27	27
+UPDATE `creature_template` SET `Faction`='59' WHERE `entry`='6215'; -- Chomper (1D) (1A) (1P)26-26
+UPDATE `creature_template` SET `MaxLevel`='27' WHERE `entry`='6218'; -- Irradiated Slime (1.7D) (1A) (1P)26-26
+UPDATE `creature_template` SET `MinLevel`='26', `MaxLevel`='27', `DamageMultiplier`='1.7' WHERE `entry`='6220'; -- Irradiated Horror (4D) (1A) (1P) [NOBESTIARY]28-29
+UPDATE `creature_template` SET `DamageMultiplier`='1.7' WHERE `entry`='6225'; -- Mechano-Tank (3D) (1A) (1P) [NOBESTIARY]29-30
+UPDATE `creature_template` SET `DamageMultiplier`='1.7' WHERE `entry`='6226'; -- Mechano-Flamewalker (4D) (1A) (1P) [NOBESTIARY]30-31
+UPDATE `creature_template` SET `DamageMultiplier`='1.7' WHERE `entry`='6227'; -- Mechano-Frostwalker (5D) (1A) (1P) [NOBESTIARY]31-32
+UPDATE `creature_template` SET `DamageMultiplier`='2.4', `MeleeBaseAttackTime`='2000', `MinLootGold`='286' WHERE `entry`='6228'; -- Dark Iron Ambassador (3.8D) (1A) (2P)28-28
+UPDATE `creature_template` SET `DamageMultiplier`='2.4', `ArmorMultiplier`='1.5' WHERE `entry`='6229'; -- Crowd Pummeler 9-60 (5D) (1A) (1P) [NOBESTIARY]28-28
+UPDATE `creature_template` SET `MaxLevel`='28', `DamageMultiplier`='1.7' WHERE `entry`='6230'; -- Peacekeeper Security Suit (1.75D) (1A) (1P)27-27
+UPDATE `creature_template` SET `MaxLevel`='28' WHERE `entry`='6232'; -- Arcane Nullifier X-21 (2.4D) (1A) (1P)27-27
+UPDATE `creature_template` SET `MinLevel`='26', `MaxLevel`='27', `DamageMultiplier`='1.7' WHERE `entry`='6233'; -- Mechanized Sentry (3D) (1A) (1P) [NOBESTIARY]28-29
+UPDATE `creature_template` SET `MinLevel`='27', `MaxLevel`='28', `DamageMultiplier`='1.7' WHERE `entry`='6234'; -- Mechanized Guardian (4D) (1A) (1P) [NOBESTIARY]31-32
+UPDATE `creature_template` SET `ArmorMultiplier`='1.5' WHERE `entry`='6235'; -- Electrocutioner 6000 (2.4D) (1.48319A) (1P)28-28
+UPDATE `creature_template` SET `DamageMultiplier`='1.7' WHERE `entry`='6329'; -- Irradiated Pillager (4D) (1A) (1P) [NOBESTIARY]25-26
+UPDATE `creature_template` SET `MinLevel`='26', `MaxLevel`='27', `DamageMultiplier`='1.7' WHERE `entry`='6391'; -- Holdout Warrior (3D) (1A) (1P) [NOBESTIARY]29-30
+UPDATE `creature_template` SET `MinLevel`='26', `MaxLevel`='27', `DamageMultiplier`='1.7' WHERE `entry`='6392'; -- Holdout Medic (4D) (1A) (1P) [NOBESTIARY]29-30
+UPDATE `creature_template` SET `MinLevel`='26', `MaxLevel`='27', `DamageMultiplier`='1.7' WHERE `entry`='6407'; -- Holdout Technician (3D) (1A) (1P) [NOBESTIARY]29-30
+UPDATE `creature_template` SET `Faction`='17', `DamageMultiplier`='0.1', `DamageSchool`='2', `MeleeBaseAttackTime`='2000', `SchoolImmuneMask`='4' WHERE `entry`='7266'; -- Ember (1.3D) (1A) (1P) [NOBESTIARY]27-27
+UPDATE `creature_template` SET `Faction`='59' WHERE `entry`='7361'; -- Grubbis (2.4D) (1A) (1P)28-28
+UPDATE `creature_template` SET `Faction`='63', `DamageMultiplier`='0.1', `DamageSchool`='2', `MeleeBaseAttackTime`='2000', `SchoolImmuneMask`='4' WHERE `entry`='7738'; -- Burning Servant (0.2D) (1A) (1P)26-26
+UPDATE `creature_template` SET `ArmorMultiplier`='2' WHERE `entry`='7800'; -- Mekgineer Thermaplugg (2.4D) (1.96726A) (1P)28-28
+UPDATE `creature_template` SET `DamageMultiplier`='1' WHERE `entry`='7849'; -- Mobile Alert System (1.3D) (1A) (1P)25-25
+UPDATE `creature_template` SET `ArmorMultiplier`='2' WHERE `entry`='7915'; -- Walking Bomb (1D) (1.96701A) (1P)26-26
+UPDATE `creature_template` SET `HealthMultiplier`='1.05', `DamageMultiplier`='1' WHERE `entry`='7998'; -- Blastmaster Emi Shortfuse (3D) (1A) (1P) [NOBESTIARY]27-27 -> 840hp
+UPDATE `creature_template` SET `MinLevel`='27', `MaxLevel`='28' WHERE `entry`='8035'; -- Dark Iron Land Mine (1D) (1A) (1P) [NOBESTIARY]32-33 decided by summonerlevel anyways
+
