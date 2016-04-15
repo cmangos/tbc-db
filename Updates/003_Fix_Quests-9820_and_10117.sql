d90fcad8cb0062d51d2728e9b9c386dd3e5b1b8e
X-Savior <xsavior@acid.com>
2016-04-15 11:07:26 -0600

    Fix Quests 9820 and 10117 - Horde Only

---
+++ Updates/003_Fix_Quests-9820_and_10117.sql
@@ -0,0 +1,4 @@
+-- =============================================
+-- Fix Quests 9820 and 10117 - Horde Only Quests
+-- =============================================
+UPDATE quest_template SET RequiredRaces=690 WHERE entry IN (10117,9820);
