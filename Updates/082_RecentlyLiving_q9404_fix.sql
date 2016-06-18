37a1567cce2f55258d9b39312708dce7baea3cf1
killerwife <killerwife@gmail.com>
2016-06-18 19:12:24 +0200

    q.9404 fix
    Make item drop

---
+++ Updates/082_RecentlyLiving_q9404_fix.sql
@@ -0,0 +1,4 @@
+DELETE FROM creature_loot_template WHERE entry=15636 AND item=23553;
+INSERT INTO creature_loot_template VALUES('15636', '23553', '-15', '0', '1', '1', '0', 'Recently Living - Living Branch drop'); -- make item drop during q
+
+
