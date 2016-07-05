cf197553c863853ebf4c1eaf8d4fc3e3a8134146
X-Savior <xsavior@acid.com>
2016-07-05 08:09:31 -0600

    [015] q.6568 'Mistress of Deception' - UDB

---
+++ Updates/018_UDB-015_quest_6568.sql
@@ -0,0 +1,4 @@
+-- Mistress of Deception(q.6568)
+UPDATE quest_template SET OfferRewardText ='Hmph. That one.$B$BHow is Rokaro? As suspicious as ever, sending me a note like this.$B$BYou must be on an important mission if Rokaro has sent you to me.$B$BWell, I owe the Warchief a fovor or two.' WHERE entry = 6568; 
+
+
