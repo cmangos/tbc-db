959d805f885f126bddc6b9b2191495fef28ed6cf
X-Savior <xsavior@acid.com>
2016-07-05 08:37:00 -0600

    [024] Bashana Runetotem - UDB

---
+++ Updates/027_UDB-024_creature.sql
@@ -0,0 +1,8 @@
+-- Bashana Runetotem - update for [018] commit (mistake)
+-- sells Packets of Tharlendris Seeds and Evergreen Pouches once the character has finished the quest 'Morrowgrain Research'.
+UPDATE gossip_menu_option SET condition_id = 974 WHERE menu_id = 1701 AND id = 0;
+
+
+
+
+
