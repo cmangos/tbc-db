b8e48a203901babe84565b79b0a6df9163de3056
killerwife <killerwife@gmail.com>
2016-09-26 22:02:30 +0200

    Warrior q.8417 proper q givers

--- Updates/162_membership_benefits.sql
+++ Updates/162_membership_benefits.sql
@@ -12,3 +12,14 @@ INSERT INTO creature_involvedrelation VALUES
 (18265,9887);
 
 
+DELETE FROM creature_questrelation WHERE quest=8417; -- warrior quest proper questgivers
+INSERT INTO creature_questrelation VALUES
+(3354,8417),
+(4593,8417),
+(5113,8417),
+(5479,8417),
+(7315,8417),
+(17120,8417),
+(3041,8417);
+
+
