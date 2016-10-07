a245c508d328ecd7c29aec0d0e4d6167ef5c1ba3
killerwife <killerwife@gmail.com>
2016-10-07 12:18:32 +0200

    Remove updatepacks and update license/authors

--- Archived_Releases/1.4.0/Updates/1.4.3_Updates/000_DB_Version.sql
+++
@@ -1,4 +0,0 @@
--- ==============
--- VERSION UPDATE
--- ==============
-UPDATE db_version SET version = 'TBC-DB 1.4.3 \'PRE-RELEASE BUILD\' for CMaNGOS-TBC';
--- Archived_Releases/1.4.0/Updates/1.4.3_Updates/013_Creature_Template.sql
+++
@@ -1,2 +0,0 @@
-UPDATE creature_template SET InhabitType=4 WHERE Entry IN (6141,4015,2830,6139);
-UPDATE creature_template SET InhabitType=2 WHERE Entry IN (6145);
