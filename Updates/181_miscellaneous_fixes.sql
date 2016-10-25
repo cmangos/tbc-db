a27ab5c69825fd0008099d2902bcf6ee9077bc69
MantisLord <sabinprosper@gmail.com>
2016-10-25 16:36:32 +0200

    Remove XP gain from Unstable Fel Imps

--- Updates/181_miscellaneous_fixes.sql
+++ Updates/181_miscellaneous_fixes.sql
@@ -52,4 +52,6 @@ UPDATE creature_template SET MechanicImmuneMask = 619397119 WHERE entry = 19389;
 
 
 
+UPDATE creature_template SET ExtraFlags = 64 WHERE entry IN (22474, 22475); -- Unstable Fel imps should not give XP
+
 
