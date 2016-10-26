74e9a7ff6bad0c52aad4cefecce7729176a21a3e
X-Savior <xsavior@acid.com>
2016-10-26 17:28:34 +0200

    UDB-066

---
+++ Updates/106_UDB-066_gameobject.sql
@@ -0,0 +1,4 @@
+-- Bubbly Fissure - Lushwater Oasis -- correct spawn point and rotation
+UPDATE gameobject SET position_x = -1040.198, position_y = -2078.984, position_z = 50.57693, orientation = 3.13287, rotation0 = 0, rotation1 = 0, rotation2 = 0, rotation3 = 1 WHERE guid = 13380;
+
+
