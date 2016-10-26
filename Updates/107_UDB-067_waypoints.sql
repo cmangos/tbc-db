99bd18c6756d851e9aa3b2d55f0559e5a3064f49
X-Savior <xsavior@acid.com>
2016-10-26 17:29:03 +0200

    UDB-67

---
+++ Updates/107_UDB-067_waypoints.sql
@@ -0,0 +1,5 @@
+-- Kyle the Frenzied - Bloodhoof Village - waypoints update
+UPDATE creature_movement_template SET position_x = -2324.3, position_y = -357.078, position_z = -8.96122, orientation = 4.05182 WHERE entry = 23616 AND point = 11;
+UPDATE creature_movement_template SET position_x = -2334.89, position_y = -373.074, position_z = -8.33575, orientation = 4.17356 WHERE entry = 23616 AND point = 12;
+
+
