33e6bd7e5290065c21ecbf513675d9c638a24a02
killerwife <killerwife@gmail.com>
2016-10-30 13:21:15 +0100

    Remove spawn of GO that is spawned by script

--- Updates/181_miscellaneous_fixes.sql
+++ Updates/181_miscellaneous_fixes.sql
@@ -54,4 +54,6 @@ UPDATE creature_template SET MechanicImmuneMask = 619397119 WHERE entry = 19389;
 
 UPDATE creature_template SET ExtraFlags = 64 WHERE entry IN (22474, 22475); -- Unstable Fel imps should not give XP
 
+DELETE FROM gameobject WHERE id IN(103661); -- object spawned by script
+
 
