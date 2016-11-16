133f8481ca98129eb0050654152499662663f932
X-Savior <xsavior@acid.com>
2016-11-16 16:53:05 +0100

    Adjust Instance portals minimum levels

---
+++ Updates/175_Adjust_Instance_Portals_MinLevels.sql
@@ -0,0 +1,148 @@
+-- --------------
+-- Ragefire Chasm
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=8 WHERE `id` IN (2230);
+
+-- --------------
+-- Wailing Caverns
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=10 WHERE `id` IN (228);
+
+-- --------------
+-- The Deadmines
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=10 WHERE `id` IN (78);
+
+-- --------------
+-- Shadowfang Keep
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=10 WHERE `id` IN (145);
+
+-- --------------
+-- Blackfathom Deeps
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=19 WHERE `id` IN (257);
+UPDATE `areatrigger_teleport` SET `name`='Blackfathom Deeps Entrance' WHERE `id` IN (257);
+UPDATE `areatrigger_teleport` SET `name`='Blackfathom Deeps Instance Start' WHERE `id` IN (259);
+
+-- --------------
+-- Stormwind Stockade
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=15 WHERE `id` IN (101);
+UPDATE `areatrigger_teleport` SET `name`='Stormwind Stockades Start' WHERE `id` IN (503);
+
+-- --------------
+-- Razorfen Kraul
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=17 WHERE `id` IN (244);
+
+-- --------------
+-- Gnomeregan
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=20 WHERE `id` IN (324);
+
+-- --------------
+-- The Scarlet Monastery
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=20 WHERE `id` IN (45,610,612,614);
+
+-- --------------
+-- Razorfen Downs
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=25 WHERE `id` IN (442);
+
+-- --------------
+-- Uldaman
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=30 WHERE `id` IN (286);
+
+-- --------------
+-- Zul'Farrak
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=35 WHERE `id` IN (924);
+
+-- --------------
+-- Maraudon
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=30 WHERE `id` IN (3133,3134);
+
+-- --------------
+-- Sunken Temple
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=35 WHERE `id` IN (446);
+
+-- --------------
+-- Blackrock Depths
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=40 WHERE `id` IN (1466);
+UPDATE `areatrigger_teleport` SET `name`='Blackrock Depths Entrance - Blackrock Mountian' WHERE `id` IN (1466);
+
+-- --------------
+-- Dire Maul
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=45 WHERE `id` IN (3183,3184,3185,3186,3187,3189);
+
+-- --------------
+-- Lower Blackrock Spire
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=45 WHERE `id` IN (3728);
+UPDATE `areatrigger_teleport` SET `name`='Lower Blackrock Spire Entrance' WHERE `id` IN (3728);
+
+-- --------------
+-- Stratholme
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=45 WHERE `id` IN (2214,2216,2217);
+UPDATE `areatrigger_teleport` SET `name`='Stratholme Entrance' WHERE `id` IN (2214,2216,2217);
+
+-- --------------
+-- Scholomance
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=45 WHERE `id` IN (2567);
+UPDATE `areatrigger_teleport` SET `name`='Stratholme Start' WHERE `id` IN (2568);
+
+-- --------------
+-- Upper Blackrock Spire
+-- --------------
+-- UPDATE `areatrigger_teleport` SET `required_level`=xx WHERE `id` IN (xxxx);
+
+-- --------------
+-- Zul'Gurub
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=50 WHERE `id` IN (3928);
+
+-- --------------
+-- Onyxia's Lair
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=50 WHERE `id` IN (2848);
+
+-- --------------
+-- Molten Core
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=50 WHERE `id` IN (3528,3529,2886);
+
+-- --------------
+-- Blackwing Lair
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=50 WHERE `id` IN (3726);
+
+-- --------------
+-- Ruins of Ahn'Qiraj
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=50 WHERE `id` IN (4008);
+
+-- --------------
+-- Temple of Ahn'Qiraj
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=50 WHERE `id` IN (4010);
+
+-- --------------
+-- Naxxramas
+-- --------------
+UPDATE `areatrigger_teleport` SET `required_level`=51 WHERE `id` IN (4156);
+
+
+
+
+
+
+
