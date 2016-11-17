9e3df7ae93efe59a4c2d3ddaa33f17b2398011d0
Cala <calaftp@free.fr>
2016-11-17 14:04:31 +0100

    CDB-0869 Fixed equipment of creature 5154 in Ironforge * Removed wrong equipment for creature 5154 (Poranna Snowbraid) in Ironforge as she should wear none

---
+++ Updates/201_ClassicDB-0869_creature_template.sql
@@ -0,0 +1,4 @@
+-- Removed wrong equipment for creature 5154 (Poranna Snowbraid) in Ironforge as she should wear none
+UPDATE `creature_template` SET `EquipmentTemplateId` = 0 WHERE `Entry` = 5154;
+
+
