15ed70cd163f9fb06b82ebdf47c3b87b0a5fea68
Grz3s <grz3sp@wp.pl>
2016-11-17 14:28:46 +0100

    WDB-076 mistake corrected

---
+++ Updates/240_UDB-076_creatures.sql
@@ -0,0 +1,7 @@
+-- Shadowmoon Valley
+-- Scorchshell Pincer -- spawn points corrected
+UPDATE `creature` SET position_x = -2987.780273, position_y = 643.577454, position_z = 19.287279, orientation = 5.067141, MovementType = 1, spawndist = 2 WHERE guid = 76519;
+UPDATE `creature` SET position_x = -3055.746338, position_y = 572.366089, position_z = 4.913166, orientation = 3.622008, MovementType = 1, spawndist = 2 WHERE guid = 76517;
+UPDATE `creature` SET position_x = -3004.114502, position_y = 483.591797, position_z = -15.909133, orientation = 4.808893, MovementType = 1, spawndist = 2 WHERE guid = 76516;
+
+
