afdf670faa277cc19a01339070a2cc3e6ee1ea00
X-Savior <xsavior@acid.com>
2016-11-17 14:34:43 +0100

    Fix Warp Splinter loot

---
+++ Updates/252_Fix_Warp_Splinter_Loot.sql
@@ -0,0 +1,7 @@
+-- ============================
+-- Fix Warp Splinter Loot Drops - Botanica Final Boss
+-- ============================
+UPDATE creature_loot_template SET groupid=2 WHERE entry=17977 AND item=35006;
+
+-- THIS FIX HAS BEEN DEPRECIATED - REPLACED WITH COMPLETE BOSS LOOT REVAMP FOR ALL DUNGEONS
+
