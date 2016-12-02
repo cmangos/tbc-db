9aa30b13e8194eb316117c7c9148cabb98918b97
XSavior <xsavior@acid.com>
2016-12-02 14:01:52 +0100

    Mangos string fixes

---
+++ Updates/330_mangos_string.sql
@@ -0,0 +1,5 @@
+DELETE FROM `mangos_string` WHERE entry BETWEEN 1702 AND 1703;
+INSERT INTO `mangos_string` VALUES ('1702', 'Player |cffff0000%s|r [GUID: %u] has |cffff0000%f|r threat', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+                                   ('1703', 'Showing threat for %s [Entry %u]', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+
