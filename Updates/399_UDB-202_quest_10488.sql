13ab11eb3784cc17fc69913c6893bb3bcef0c53f
Grz3s <grz3sp@wp.pl>
2016-12-02 14:50:03 +0100

    WDB-202 q. 'Protecting Our Own' fix

---
+++ Updates/399_UDB-202_quest_10488.sql
@@ -0,0 +1,7 @@
+-- q.10488 'Protecting Our Own'
+DELETE FROM dbscripts_on_event WHERE id = 13489; 
+INSERT INTO dbscripts_on_event (id, delay, command, datalong, datalong2, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(13489,1,8,21142,0,0,0,0,0,0,0,0,0,0,0,0,'q. credit'),
+(13489,2,16,1018,2,20748,25,7,0,0,0,0,0,0,0,0,'');
+
+
