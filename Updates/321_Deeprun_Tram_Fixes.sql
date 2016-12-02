c366f606b1d23825cef4f3be4a6591559aacc54d
XSavior <xsavior@acid.com>
2016-12-02 13:55:25 +0100

    Deeprun tram fixes

---
+++ Updates/321_Deeprun_Tram_Fixes.sql
@@ -0,0 +1,13 @@
+-- Fix Deeprun Tram Movement
+DELETE FROM gameobject WHERE id BETWEEN 176080 AND 176085;
+insert into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) values('18802','176080','369','1','4.58065','28.2097','7.01107','1.5708','0','0','0','0','120','0','1');
+insert into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) values('18803','176081','369','1','4.52807','8.43529','7.01107','1.5708','0','0','0','0','120','0','1');
+insert into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) values('18804','176082','369','1','-45.4005','2492.79','6.9886','1.5708','0','0','0','0','120','0','1');
+insert into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) values('18805','176083','369','1','-45.4007','2512.15','6.9886','1.5708','0','0','0','0','120','0','1');
+insert into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) values('18806','176084','369','1','-45.3934','2472.93','6.9886','1.5708','0','0','0','0','120','0','1');
+insert into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) values('18807','176085','369','1','4.49883','-11.3475','7.01107','4.71238','0','0','0','0','120','0','1');
+
+
+
+
+
