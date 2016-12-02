d8cbd0f713cbb73e38932c7261c7bc8be5c1b3be
Cala <calaftp@free.fr>
2016-12-02 13:10:40 +0100

    CDB-0967 Fixed rewards of quest 8867

---
+++ Updates/303_ClassicDB-0967_item_loot_template.sql
@@ -0,0 +1,13 @@
+-- Fixed rewards of quest 8867 (Lunar Fireworks)
+-- Author: @trulynullified
+-- Thanks thetrueanimal for pointing. This closes #720 
+DELETE FROM `item_loot_template` WHERE `entry` IN (21740, 21743);
+insert into `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) values
+('21740', '21724', '0', '1', '1', '1', '0'),
+('21740', '21725', '0', '1', '1', '1', '0'),
+('21740', '21726', '0', '1', '1', '1', '0'),
+('21743', '21733', '0', '1', '1', '1', '0'),
+('21743', '21734', '0', '1', '1', '1', '0'),
+('21743', '21735', '0', '1', '1', '1', '0');
+
+
