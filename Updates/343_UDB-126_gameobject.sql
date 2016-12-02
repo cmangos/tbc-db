a1da09821d54ce1076f076886ea3af6a5399d933
Cala <calaftp@free.fr>
2016-12-02 14:19:31 +0100

    CDB-0904 - UDB-126 Fixed Z position of GO 30856

---
+++ Updates/343_UDB-126_gameobject.sql
@@ -0,0 +1,5 @@
+-- Classic [0904] + UDB correct (ClassicDB was to high)
+-- Fixed Z position of GO 30856 (Atal'ai Artifact) in Swamp of Sorrows
+UPDATE gameobject SET position_z = 18.6 WHERE guid = 30561;
+
+
