f32b007c066f791f57e260a29ea44c5c3debe73e
Grz3s <grz3sp@wp.pl>
2016-12-11 20:32:24 +0100

    WDB-213 Lanti'gah spawn and movement

---
+++ Updates/407_UDB-213_creature.sql
@@ -0,0 +1,4 @@
+-- Lanti'gah - Barrens
+UPDATE creature SET position_x = -292.942444, position_y = -1909.355591, position_z = 91.666786, orientation = 2.13, spawndist = 10, MovementType = 1 WHERE guid = 13100;
+
+
