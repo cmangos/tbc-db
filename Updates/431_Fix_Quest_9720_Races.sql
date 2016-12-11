b1b787bc0b15e9abf6702873877811a4e8851870
XSavior <xsavior@acid.com>
2016-12-11 20:41:28 +0100

    Fix q.9720 races

---
+++ Updates/431_Fix_Quest_9720_Races.sql
@@ -0,0 +1,5 @@
+-- =======================================
+-- Quest 9720: "Balance Must Be Preserved" - All Races Should Have Access
+-- =======================================
+UPDATE quest_template SET RequiredRaces=0 WHERE entry IN (9720);
+
