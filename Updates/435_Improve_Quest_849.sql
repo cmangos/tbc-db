47b3a02651646f7e79c20a831f302d66e8a22ab4
XSavior <xsavior@acid.com>
2016-12-11 20:42:43 +0100

    Fix q.849 respawn timer for Flying Machine

---
+++ Updates/435_Improve_Quest_849.sql
@@ -0,0 +1,13 @@
+-- ============================================
+-- Improvements for Quest 849 "Revenge of Gann"
+-- ============================================
+-- Shorten Bael Flying Machine Respawn Time
+UPDATE gameobject SET spawntimesecs=180 WHERE id IN (3644);
+
+
+
+
+
+
+
+
