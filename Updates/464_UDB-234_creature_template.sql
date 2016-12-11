ea3d5ff08cb09b895142629710fbc76e2933e6f3
Grz3s <grz3sp@wp.pl>
2016-12-11 21:42:16 +0100

    WDB-234 Warbringer Construct -- UnitFlags corrected -- Not-selectable 0x2000000 & not_attackable 0x100 - are added only for 4 of them in Black Vault by aura

---
+++ Updates/464_UDB-234_creature_template.sql
@@ -0,0 +1,6 @@
+-- Warbringer Construct 8905
+-- UnitFlags corrected
+-- Not-selectable 0x2000000 & not_attackable 0x100 - are added only for 4 of them in Black Vault by aura 
+UPDATE creature_template SET UnitFlags = 64 WHERE Entry = 8905;
+
+
