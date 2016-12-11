3a84f94bbc7351aeac997c6850f255ab12e075e9
Grz3s <grz3sp@wp.pl>
2016-12-11 20:32:43 +0100

    WDB-214 q.10629 'Shizz Work'

---
+++ Updates/408_UDB-214_gameobject_template.sql
@@ -0,0 +1,9 @@
+-- q.10629 'Shizz Work'
+
+-- source: YTDB
+-- missing object added: TEMP Felhound Poo Trap
+DELETE FROM gameobject_template WHERE entry = 184981;
+INSERT INTO gameobject_template (entry, type, displayId, name, IconName, castBarCaption, faction, flags, size, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, mingold, maxgold, ScriptName) VALUES
+(184981,6,1287,'TEMP Felhound Poo Trap','','',0,0,1,0,0,0,37695,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+
