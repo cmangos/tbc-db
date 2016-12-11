a3dfb84ab13ad34b26a72178ad2d848d85b9970b
Grz3s <grz3sp@wp.pl>
2016-12-11 21:40:06 +0100

    WDB-228 respawntime for some gameobjects - New Avalon Registry - The Book of Ur - Empty Cauldron - Iron Chain - New Avalon Patrol Schedule

---
+++ Updates/461_UDB-228_gameobject.sql
@@ -0,0 +1,14 @@
+-- Some of objects (q. related) should respawn almost instant after ... despawn. 
+
+-- New Avalon Registry
+UPDATE gameobject SET spawntimesecs = 2 WHERE guid = 66384;
+-- The Book of Ur
+UPDATE gameobject SET spawntimesecs = 2 WHERE guid = 40667;
+-- Empty Cauldron
+UPDATE gameobject SET spawntimesecs = 2 WHERE guid = 66377;
+-- Iron Chain
+UPDATE gameobject SET spawntimesecs = 2 WHERE guid = 66378;
+-- New Avalon Patrol Schedule
+UPDATE gameobject SET spawntimesecs = 2 WHERE guid = 66308;
+
+
