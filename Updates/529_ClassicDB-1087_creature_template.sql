d3a128dc767888a1b502ee1a31b0edf38d7516f5
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:13:02 +0100

    CDB-1087 Western Plaguelands - Blighted Zombie

---
+++ Updates/529_ClassicDB-1087_creature_template.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `UnitFlags`= UnitFlags|64, `CreatureTypeFlags`='72' WHERE `Entry`='4475';
+UPDATE `creature_template` SET `UnitFlags`= UnitFlags|64, `CreatureTypeFlags`='72' WHERE `Entry`='4474';
+
+
