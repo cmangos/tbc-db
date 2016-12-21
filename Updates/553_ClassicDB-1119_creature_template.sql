4efbda5e233f0defaeb1c43c09077164b9ac3cab
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:21:25 +0100

    CDB-1119 Bloodroar the Stalker

---
+++ Updates/553_ClassicDB-1119_creature_template.sql
@@ -0,0 +1,7 @@
+
+UPDATE `creature_template` SET `FactionAlliance`='16', `FactionHorde`='16', `UnitFlags`='64', `SpeedWalk`='1', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000', `MovementType`='1' WHERE `Entry`='5346';
+
+UPDATE `creature` SET `modelid`='0', `position_x`='-5188.988', `position_y`='1825.247', `position_z`='115.7616', `orientation`='4.312038', `spawndist`='3', `MovementType`='1' WHERE `guid`='51841';
+UPDATE `creature` SET `modelid`='0', `position_x`='-5168.509', `position_y`='1840.464', `position_z`='114.0250', `orientation`='2.338799' WHERE `guid`='50984';
+
+
