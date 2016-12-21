52df0d3a2aa239902e31b5d2a5bda1c4f65bd742
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:23:26 +0100

    CDB-1123 Humar the Pridelord

---
+++ Updates/557_ClassicDB-1123_creature_template.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `FactionAlliance`='16', `FactionHorde`='16', `UnitFlags`='64', `SpeedWalk`='1', `SpeedRun`='1.428571429', `MeleeBaseAttackTime`='1300', `RangedBaseAttackTime`='1300' WHERE `Entry`='5828';
+
+
