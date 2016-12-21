4735fa21822ba6d8ecb1060445cef893aed88152
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:13:19 +0100

    CDB-1088 Western Plaguelands

---
+++ Updates/530_ClassicDB-1088_creature_template.sql
@@ -0,0 +1,7 @@
+
+UPDATE `creature_template` SET `UnitFlags`= UnitFlags|64, `CreatureTypeFlags`='72' WHERE `Entry`='1794';
+UPDATE `creature_template` SET `UnitFlags`= UnitFlags|64, `CreatureTypeFlags`='72' WHERE `Entry`='1795';
+UPDATE `creature_template` SET `UnitFlags`= UnitFlags|64, `CreatureTypeFlags`='72' WHERE `Entry`='1802';
+UPDATE `creature_template` SET `UnitFlags`= UnitFlags|64, `CreatureTypeFlags`='72' WHERE `Entry`='1804';
+
+
