a80adb7d6a89c85c1105ea7dec7eb46b3db5aeb3
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:14:19 +0100

    CDB-1093 Dire Maul: North - Guard Fengus, Slip'kik and Mol'dar

---
+++ Updates/534_ClassicDB-1093_creature_template.sql
@@ -0,0 +1,6 @@
+
+UPDATE `creature_template` SET `NpcFlags`='3', `UnitFlags`='32832', `CreatureTypeFlags`='72' WHERE `Entry`='14326';
+UPDATE `creature_template` SET `NpcFlags`='3', `UnitFlags`='32832', `CreatureTypeFlags`='72', `MovementType`='1' WHERE `Entry`='14321';
+UPDATE `creature_template` SET `NpcFlags`='3', `UnitFlags`='32832', `CreatureTypeFlags`='72', `MovementType`='1' WHERE `Entry`='14323';
+
+
