472eef3f6277cdec6780ba89e352c441fb3d0063
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:14:46 +0100

    CDB-1095 Deathmaw

---
+++ Updates/536_ClassicDB-1095_skinning_loot_template.sql
@@ -0,0 +1,7 @@
+
+UPDATE `creature_template` SET `SpeedWalk`='1', `MeleeBaseAttackTime`='1200', `RangedBaseAttackTime`='1200', `ResistanceFire`='0', `ResistanceNature`='0', `ResistanceFrost`='0', `ResistanceShadow`='0', `ResistanceArcane`='0', `MovementType`='1' WHERE `Entry`='10077';
+
+
+
+
+
