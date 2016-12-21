93fa8c5413c148841237f342a5c41d0ef7f4af14
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:25:01 +0100

    CDB-1128 Dishu

---
+++ Updates/562_ClassicDB-1128_creature_template.sql
@@ -0,0 +1,6 @@
+
+UPDATE `creature_template` SET `UnitFlags`='64', `MeleeBaseAttackTime`='1500', `RangedBaseAttackTime`='1500', `MovementType`='1' WHERE `Entry`='5865';
+
+UPDATE `creature` SET `modelid`='0', `position_x`='542.3774', `position_y`='-2402.823', `position_z`='91.79166', `orientation`='1.328636', `spawndist`='3', `MovementType`='1' WHERE `guid`='51810';
+
+
