2af71e182dd4fb788e275d164a27c01d3c31363f
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:30:57 +0100

    CDB-1145 Kurmokk

---
+++ Updates/578_ClassicDB-1145_creature_template.sql
@@ -0,0 +1,6 @@
+
+UPDATE `creature_template` SET `ModelId1`='3188', `FactionAlliance`='72', `FactionHorde`='72', `Scale`='0', `UnitFlags`='64', `MeleeBaseAttackTime`='2000', `MovementType`='1' WHERE `Entry`='14491';
+
+
+
+
