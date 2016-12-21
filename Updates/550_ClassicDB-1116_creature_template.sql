114c810d1436a732724c75d2a5444bc091dfd7c8
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:20:45 +0100

    CDB-1116 Olm the Wise

---
+++ Updates/550_ClassicDB-1116_creature_template.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `UnitFlags`='32832', `SpeedWalk`='1', `MeleeBaseAttackTime`='2000', `ResistanceFire`='0', `ResistanceNature`='0', `ResistanceShadow`='0', `ResistanceArcane`='0', `MovementType`='1' WHERE `Entry`='14343';
+
+
