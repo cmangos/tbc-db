03e6ac1ffd44b3e8771a5aa0dfd6a29481f3633e
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:18:50 +0100

    CDB-1115 哈尤克 - @Tobschinski is a japanese spy Fix of stats for NPC 14234

---
+++ Updates/549_ClassicDB-1115_creature_template.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `Scale`='0', `UnitFlags`='64', `MeleeBaseAttackTime`='2000', `ResistanceFire`='0', `ResistanceNature`='0', `ResistanceFrost`='0', `ResistanceShadow`='0', `ResistanceArcane`='0', `MovementType`='1' WHERE `Entry`='14234';
+
+
