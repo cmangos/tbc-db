3877c175f313cb18f8fb80ecd8b8af4ff31f5cc7
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:15:15 +0100

    CDB-1101 Kashoch the Reaver

---
+++ Updates/538_ClassicDB-1101_creature_template.sql
@@ -0,0 +1,6 @@
+
+UPDATE `creature_template` SET `SpeedWalk`='1', `Rank`='2', `MovementType`='1' WHERE `Entry`='10198';
+UPDATE `creature_template` SET `MovementType`='1' WHERE `Entry`='7428';
+
+
+
