6b8195c373d43f4ef8c8be7bedff922af8cfb3f3
Grz3s <grz3sp@wp.pl>
2016-12-21 16:42:21 +0100

    WDB-237 Weegli Blastfuse - Script Update

---
+++ Updates/475_UDB-237_DBScript.sql
@@ -0,0 +1,5 @@
+-- Weegli Blastfuse - Script Update 
+-- Ukorz Sandscalp can yell without any error.
+UPDATE dbscripts_on_creature_movement SET search_radius = 37996, data_flags = 0x10 WHERE id = 760713 and delay = 5;
+
+
