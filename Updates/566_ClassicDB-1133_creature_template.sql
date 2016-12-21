ec730a1213349de31a8b7015b3391cb1a576c54b
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:26:03 +0100

    CDB-1133 Zora

---
+++ Updates/566_ClassicDB-1133_creature_template.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `UnitFlags`='64', `SpeedWalk`='1.4', `SpeedRun`='10' / '7', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000', `MovementType`='1' WHERE `Entry`='14474';
+
+
