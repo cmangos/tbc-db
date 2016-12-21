a8eb1ab211b9e54ebea6c79a4ac9487a5c4a28ee
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:16:57 +0100

    CDB-1111 Anub-Shiah, Gorosh the Dervil, Grizzle

---
+++ Updates/545_ClassicDB-1111_creature_template.sql
@@ -0,0 +1,11 @@
+
+-- Anub-Shiah
+UPDATE `creature_template` SET `UnitFlags`='32832', `CreatureTypeFlags`='72', `SpeedWalk`='1', `MeleeBaseAttackTime`='1800', `RangedBaseAttackTime`='1800' WHERE `Entry`='9031';
+
+-- Gorosh the Dervish
+UPDATE `creature_template` SET `UnitFlags`='32832', `CreatureTypeFlags`='72', `SpeedWalk`='1', `MeleeBaseAttackTime`='1800', `RangedBaseAttackTime`='1800' WHERE `Entry`='9027';
+
+-- Grizzle
+UPDATE `creature_template` SET `UnitFlags`='32832', `CreatureTypeFlags`='72', `SpeedWalk`='0.666668', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='9028';
+
+
