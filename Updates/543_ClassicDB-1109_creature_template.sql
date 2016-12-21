8ee22472ea739d26a3fd375665424ee59decb68f
Tobschinski <tobschinski@gmail.com>
2016-12-21 17:16:30 +0100

    CDB-1109 Eviscerator

---
+++ Updates/543_ClassicDB-1109_creature_template.sql
@@ -0,0 +1,6 @@
+
+-- Eviscerator
+UPDATE `creature_template` SET `UnitFlags`='32832', `CreatureTypeFlags`='72', `SpeedWalk`='1', `MeleeBaseAttackTime`='1300', `RangedBaseAttackTime`='1300' WHERE `Entry`='9029';
+
+
+
