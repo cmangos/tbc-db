e256929b46594a4c7edf8166f2831d2acadfaf2c
Grz3s <grz3sp@wp.pl>
2016-12-21 16:43:16 +0100

    WDB-242 Overlord Mok'Morokk - update - after defeted in quest "Challenge Overlord Mok'Morokk" he should     respawn quicker for other players.

---
+++ Updates/477_UDB-242_creature.sql
@@ -0,0 +1,5 @@
+-- Overlord Mok'Morokk - update
+-- after defeted in quest "Challenge Overlord Mok'Morokk" he should respawn quicker for other players.
+UPDATE creature SET spawntimesecs = 60 WHERE guid = 31442;
+
+
