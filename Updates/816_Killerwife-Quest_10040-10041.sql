2836629ff0b326d64bf1afedd6772a88b3c131c8
killerwife <killerwife@gmail.com>
2016-12-23 18:49:06 +0100

    Fix q.10040/10041 quest completion IDs

---
+++ Updates/816_Killerwife-Quest_10040-10041.sql
@@ -0,0 +1,7 @@
+-- Who are they quest fix fix
+-- quest ids 10040 and 10041
+-- Killerwife
+
+UPDATE quest_template SET ReqCreatureOrGOId1=26464,ReqCreatureOrGOId2=26465,ReqCreatureOrGOId3=26466 WHERE entry in(10040,10041);
+
+
