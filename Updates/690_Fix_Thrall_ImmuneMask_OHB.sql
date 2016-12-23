2c615cd0900d7093a5c2b8a0f2895ccfd5ba167a
XSavior <xsavior@acid.com>
2016-12-23 15:33:42 +0100

    Fix Thrall ImmuneMask in Old Hillsbrad

---
+++ Updates/690_Fix_Thrall_ImmuneMask_OHB.sql
@@ -0,0 +1,4 @@
+-- Fix Thrall in Old Hillsbrad Foothills (SHould not be able to Poly or Sap or other stff like that)
+UPDATE creature_template SET MechanicImmuneMask=537592449 WHERe ENTRY in (17876);
+
+
