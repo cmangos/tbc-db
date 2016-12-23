08a7ab5ffd131ed20b67958bfada8d809a8bd5eb
killerwife <killerwife@gmail.com>
2016-12-23 19:02:50 +0100

    Add missing spell_script_target to Vimgol script

---
+++ Updates/831_Killerwife-Vimgol_Spell_Script.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_target` WHERE `entry`=40547 AND `targetentry`=22911;
+INSERT INTO spell_script_target VALUES(40547,1,22911,0); -- and the spells name is Interrupt Unholy Growth
+
