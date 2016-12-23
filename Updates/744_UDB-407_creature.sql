2410d9e77840eba7e355bde0d58942d25ef68feb
Xfurry <xfurry@scriptdev2.com>
2016-12-23 17:22:11 +0100

    WDB-407 Magwin

---
+++ Updates/744_UDB-407_creature.sql
@@ -0,0 +1,6 @@
+-- Magwin
+DELETE FROM creature_template_addon WHERE entry = 17312;
+INSERT INTO creature_template_addon (entry,mount,bytes1,b2_0_sheath,b2_1_flags,emote,moveflags,auras) VALUES
+(17312, 0, 0, 1, 16, 0, 0, '');
+
+
