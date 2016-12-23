3dfd5f99142fe06ac326a118e72c2610cd6a61ef
Grz3s <grz3sp@wp.pl>
2016-12-23 12:33:16 +0100

    WDB-284 Mottled Worg #17952 spawn point corrected

---
+++ Updates/619_UDB-284_Mottled_Worg_Spawn_Corrected.sql
@@ -0,0 +1,4 @@
+-- UDB_284
+-- Mottled Worg - spawn point corrected
+UPDATE creature SET position_x = 1012.858887, position_y = 1488.146973, position_z = 41.295895, orientation = 0.157890, spawndist = 5, movementType = 1 WHERE guid = 17952;
+
