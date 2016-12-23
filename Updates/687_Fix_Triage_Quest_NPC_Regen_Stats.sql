b9d7427697fa9d490ecb33608267696b899166fd
XSavior <xsavior@acid.com>
2016-12-23 15:32:48 +0100

    Fix Traige Quest NPC regen stats

---
+++ Updates/687_Fix_Triage_Quest_NPC_Regen_Stats.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET RegenerateStats = RegenerateStats & (~1) WHERE entry IN (12938,12936,12937,12923,12924,12925);
+UPDATE creature_template SET RegenerateStats = RegenerateStats & (~2) WHERE entry IN (12938,12936,12937,12923,12924,12925);
+
