1eed098fe385abdacdd52cf61f87b48b37c99b59
Harley Larsen <harley@harleylarsen.com>
2016-12-26 17:38:44 -0800

    fix capitalization issues

--- Updates/692_Fix_Incorrect_Plan_Drop.sql
+++ Updates/692_Fix_Incorrect_Plan_Drop.sql
@@ -1,3 +1,3 @@
 -- Plans: Ragesteel Helm Should Only Drop From Anger Guards
-DELETE FROM creature_Loot_template WHERE item=23612 and entry IN (21060,21061);
+DELETE FROM creature_loot_template WHERE item=23612 and entry IN (21060,21061);
 
