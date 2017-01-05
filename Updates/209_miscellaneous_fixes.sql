d21a085d8581da3ce31945c9c434aa3b0cb7460a
killerwife <killerwife@gmail.com>
2017-01-05 15:22:48 +0100

    Increase drop chance of Schematic on attumen to 1%

--- Updates/209_miscellaneous_fixes.sql
+++ Updates/209_miscellaneous_fixes.sql
@@ -1,3 +1,5 @@
 UPDATE gameobject SET spawntimesecs=90 WHERE id=186266; -- Deserter Propaganda - down from abysmal 900
 
+UPDATE creature_loot_template SET ChanceOrQuestChance=1 WHERE entry=16152 AND item=23809; -- Schematic: Stabilized Eternium Scope up from 0.5%
+
 
