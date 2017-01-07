f24b362d988f88db2cfa45290290e1a2d04bb299
killerwife <killerwife@gmail.com>
2017-01-07 23:49:37 +0100

    Give Searing Infernal proper immunities

--- Updates/209_miscellaneous_fixes.sql
+++ Updates/209_miscellaneous_fixes.sql
@@ -2,4 +2,6 @@ UPDATE gameobject SET spawntimesecs=90 WHERE id=186266; -- Deserter Propaganda -
 
 UPDATE creature_loot_template SET ChanceOrQuestChance=1 WHERE entry=16152 AND item=23809; -- Schematic: Stabilized Eternium Scope up from 0.5%
 
+UPDATE creature_template SET SchoolImmuneMask=4,ResistanceFire=0,MechanicImmuneMask=16 WHERE entry IN(6073); -- searing infernal, immune to fire, immune to fear
+
 
