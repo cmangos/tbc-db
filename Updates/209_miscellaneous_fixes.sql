e316ac6bdb02efc269473b0cd3f3b21697990693
killerwife <killerwife@gmail.com>
2017-01-08 19:22:24 +0100

    Fix data of Sprok and add gossip

--- Updates/209_miscellaneous_fixes.sql
+++ Updates/209_miscellaneous_fixes.sql
@@ -12,3 +12,14 @@ DELETE FROM gameobject WHERE guid=30079 AND id=2024;
 INSERT INTO gameobject VALUES -- fix andorhal sign quaternion
 ('30079', '2024', '0', '1', '452.427', '-629.573', '166.348', '2.90598', '0.576802', '-0.173584', '0.779713', '0.170927', '7200', '100', '1');
 
+-- Sprok data fixes and gossip
+UPDATE creature_template SET FactionAlliance=120,FactionHorde=120,GossipMenuId=12741,NpcFlags=1 WHERE entry=8320;
+
+DELETE FROM gossip_menu WHERE entry=12741;
+INSERT INTO gossip_menu VALUES
+('12741', '17880', '0', '0');
+
+DELETE FROM npc_text WHERE id IN(17880);
+INSERT INTO npc_text VALUES
+('17880', 'This teleporter will transport you to Grom''gol in Northern Stranglethorn.$B$BUsually.$B$BI mean, sometimes it''ll just disintegrate you, or reduce all your soft tissues into a greenish, foul-smelling mist, but mostly it''s safe!$B$B...mostly.', '', '0', '100', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0');
+
