bfa15e3bd0ac9e373bd029ce48756867b56a638c
Saltgurka <salt@gurka.com>
2017-01-14 03:13:26 +0100

    Honor Hold Defenders missing spawn + movement

--- Updates/841_Saltgurka-Honor_Hold_Updates.sql
+++ Updates/841_Saltgurka-Honor_Hold_Updates.sql
@@ -12,8 +12,6 @@ INSERT INTO `creature_ai_scripts` VALUES
 -- Update EquipmentTemplate for all Honor Hold Defenders. Entry 20513 (the two guards next to the destroyed keep) had the correct ones, all others were wrong for some reason.
 UPDATE `creature_template` SET `EquipmentTemplateId`=1195 WHERE `entry`=16842;
 
-
-
 -- Outhouse guard GUID: 57942
 -- Since this guard has a gossip menu it needs to have a separate template from the other guards (as far as I know). 
 -- So what I did was I gave this one the template of the two talking guards outside the destroyed keep, and I gave them the normal guard template.
@@ -156,6 +154,160 @@ INSERT INTO `creature_movement` (`id`,`point`,`position_x`,`position_y`,`positio
 (@GUID,@POINT := @POINT + '1',-724.872,2722.862,95.62754,4.549620,250,0), -- 14:54:32
 (@GUID,@POINT := @POINT + '1',-724.4536,2725.909,95.40195,1.439442,250,0), -- 14:54:32
 (@GUID,@POINT := @POINT + '1',-725.5215,2724.479,95.6583,4.015551,15000,5794204); -- Text
+
+-- Add missing spawn.
+DELETE FROM `creature` WHERE `guid`=120000;
+INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `curhealth`, `MovementType`) VALUES 
+('120000', '16842', '530', '-661.241577', '2764.013184', '89.648140', '0', '300', '6600', '2');
+
+SET @GUID := 57948; -- member 120000
+SET @POINT := 0;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=2,`position_x`=-659.241577,`position_y`=2764.013184,`position_z`=89.648140,`orientation`=0 WHERE `guid`=@GUID;
+DELETE FROM `creature_movement` WHERE `id`=@GUID;
+INSERT INTO `creature_movement` (`id`,`point`,`position_x`,`position_y`,`position_z`,`waittime`,`orientation`) VALUES
+(@GUID,@POINT := @POINT + '1',-659.241577,2764.013184,89.648140,500,5.898452),
+(@GUID,@POINT := @POINT + '1',-676.385315,2769.760254,93.590286,500,3.153486),
+(@GUID,@POINT := @POINT + '1',-693.940857,2767.547607,95.120773,500,3.287004),
+(@GUID,@POINT := @POINT + '1',-676.385315,2769.760254,93.590286,500,6.016262);
+
+DELETE FROM `creature_linking` WHERE `master_guid`= 57948;
+INSERT INTO `creature_linking` (`guid`,`master_guid`,`flag`)VALUES
+(120000,57948,515);
+
+-- Pathing for  Entry: 16842 'TDB FORMAT'
+SET @GUID := 57943; -- member 57944
+UPDATE `creature` SET `spawndist`=0,`MovementType`=2,`position_x`=-726.805420,`position_y`=2658.582275,`position_z`=94.162804,`orientation`=0 WHERE `guid`=@GUID;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=0,`position_x`=-728.805420,`position_y`=2658.582275,`position_z`=94.162804,`orientation`=0 WHERE `guid`=57944;
+DELETE FROM `creature_movement` WHERE `id`=@GUID;
+INSERT INTO `creature_movement` (`id`,`point`,`position_x`,`position_y`,`position_z`) VALUES
+(@GUID,1,-729.6794,2657.958,95.2814), -- 23:40:32
+(@GUID,2,-725.9243,2657.444,94.29301), -- 23:40:36
+(@GUID,3,-712.4608,2637.426,91.97912), -- 23:40:40
+(@GUID,4,-696.0266,2633.286,90.51183), -- 23:40:47
+(@GUID,5,-681.8851,2640.387,89.54575), -- 23:40:54
+(@GUID,6,-678.4524,2642.606,89.3196), -- 23:41:01
+(@GUID,7,-676.4327,2655.729,89.65163), -- 23:41:06
+(@GUID,8,-686.6949,2675.069,91.69751), -- 23:41:11
+(@GUID,9,-695.3953,2678.214,93.53847), -- 23:41:16
+(@GUID,10,-715.9707,2676.224,95.10616), -- 23:41:22
+(@GUID,11,-720.6448,2670.696,94.86513), -- 23:41:29
+(@GUID,12,-724.7706,2663.015,94.51012), -- 23:41:34
+(@GUID,13,-732.2354,2660.108,95.79332), -- 23:41:37
+(@GUID,14,-743.7416,2658.196,99.50706), -- 23:41:39
+(@GUID,15,-753.4407,2652.986,104.7612), -- 23:41:45
+(@GUID,16,-760.7173,2650.15,108.0609), -- 23:41:49
+(@GUID,17,-763.5579,2646.074,108.1835), -- 23:41:54
+(@GUID,18,-774.3285,2638.794,108.1835), -- 23:42:00
+(@GUID,19,-751.5222,2652.918,104.1722), -- 23:42:06
+(@GUID,20,-734.8468,2657.947,96.88486); -- 23:42:12
+
+DELETE FROM `creature_linking` WHERE `master_guid`=57943;
+INSERT INTO `creature_linking` (`guid`,`master_guid`,`flag`)VALUES
+(57944,57943,515);
+
+-- Pathing for  Entry: 16842 'TDB FORMAT' 
+SET @GUID := 57923; -- member 57937
+SET @POINT := 0;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=2,`position_x`=-867.476,`position_y`=2718.552,`position_z`=69.17674,`orientation`=0 WHERE `guid`=@GUID;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=0,`position_x`=-867.476,`position_y`=2720.552,`position_z`=69.17674,`orientation`=0 WHERE `guid`=57937;
+DELETE FROM `creature_movement` WHERE `id`=@GUID;
+INSERT INTO `creature_movement` (`id`,`point`,`position_x`,`position_y`,`position_z`) VALUES
+(@GUID,@POINT := @POINT + '1',-867.476,2718.552,69.17674), -- 00:54:49
+(@GUID,@POINT := @POINT + '1',-867.5676,2699.178,79.64346), -- 00:54:59
+(@GUID,@POINT := @POINT + '1',-858.116,2686.463,88.44569), -- 00:55:08
+(@GUID,@POINT := @POINT + '1',-842.5492,2678.867,95.29387), -- 00:55:15
+(@GUID,@POINT := @POINT + '1',-837.273010,2679.171631,96.956741),
+(@GUID,@POINT := @POINT + '1',-831.252136,2679.731689,99.359558),
+(@GUID,@POINT := @POINT + '1',-822.5157,2681.53,101.6271), -- 00:55:23
+(@GUID,@POINT := @POINT + '1',-817.6292,2683.223,103.6337), -- 00:55:32
+(@GUID,@POINT := @POINT + '1',-817.5452,2683.404,103.4257), -- 00:55:37
+(@GUID,@POINT := @POINT + '1',-824.987244,2681.028320,101.549026),
+(@GUID,@POINT := @POINT + '1',-828.351746,2680.089355,100.353310),
+(@GUID,@POINT := @POINT + '1',-832.202515,2678.725098,98.884773),
+(@GUID,@POINT := @POINT + '1',-838.7524,2678.115,96.79544), -- 00:55:39
+(@GUID,@POINT := @POINT + '1',-856.3284,2684.984,89.34355), -- 00:55:48
+(@GUID,@POINT := @POINT + '1',-867.1521,2697.494,80.28738), -- 00:55:56
+(@GUID,@POINT := @POINT + '1',-867.6529,2713.841,71.40306), -- 00:56:04
+(@GUID,@POINT := @POINT + '1',-866.756165,2721.635986,67.149345),
+(@GUID,@POINT := @POINT + '1',-866.285217,2728.395508,63.300289),
+(@GUID,@POINT := @POINT + '1',-865.961182,2733.046143,60.420986),
+(@GUID,@POINT := @POINT + '1',-865.4053,2736.819,58.58712), -- 00:56:12
+(@GUID,@POINT := @POINT + '1',-866.5761,2765.043,41.56224), -- 00:56:22
+(@GUID,@POINT := @POINT + '1',-872.1741,2769.334,37.38614), -- 00:56:35
+(@GUID,@POINT := @POINT + '1',-869.7395,2768.148,39.13062), -- 00:56:41
+(@GUID,@POINT := @POINT + '1',-865.277,2739.819,56.70834), -- 00:56:44
+(@GUID,@POINT := @POINT + '1',-866.348694,2733.137939,60.319584),
+(@GUID,@POINT := @POINT + '1',-867.077209,2729.624268,62.495125),
+(@GUID,@POINT := @POINT + '1',-867.119324,2726.944092,64.111908),
+(@GUID,@POINT := @POINT + '1',-868.219971,2723.613770,65.848427),
+(@GUID,@POINT := @POINT + '1',-868.143616,2720.661865,67.733315),
+(@GUID,@POINT := @POINT + '1',-869.248535,2719.880615,68.087257);
+-- 0x203CCC42401072800031F9000101574B .go -867.476 2718.552 69.17674
+
+DELETE FROM `creature_linking` WHERE `master_guid`=57923;
+INSERT INTO `creature_linking` (`guid`,`master_guid`,`flag`)VALUES
+(57937,57923,515);
+
+-- Pathing for  Entry: 16842 'TDB FORMAT' 
+SET @GUID := 57951; -- member 57952
+SET @POINT := 0;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=2,`position_x`=-687.996,`position_y`=2675.259,`position_z`=92.00164,`orientation`=0 WHERE `guid`=@GUID;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=0,`position_x`=-689.996,`position_y`=2675.259,`position_z`=92.00164,`orientation`=0 WHERE `guid`=57952;
+DELETE FROM `creature_movement` WHERE `id`=@GUID;
+INSERT INTO `creature_movement` (`id`,`point`,`position_x`,`position_y`,`position_z`) VALUES
+(@GUID,@POINT := @POINT + '1',-687.996,2675.259,92.00164), -- 00:56:54
+(@GUID,@POINT := @POINT + '1',-694.5895,2679.407,93.47656), -- 00:57:00
+(@GUID,@POINT := @POINT + '1',-699.2542,2681.575,94.0282), -- 00:57:04
+(@GUID,@POINT := @POINT + '1',-709.6293,2702.073,94.66191), -- 00:57:07
+(@GUID,@POINT := @POINT + '1',-709.9301,2704.808,94.83853), -- 00:57:13
+(@GUID,@POINT := @POINT + '1',-709.663208,2714.1892,94.7226), -- 00:57:24 --C
+(@GUID,@POINT := @POINT + '1',-700.4745,2712.623,94.98143), -- 00:57:35
+(@GUID,@POINT := @POINT + '1',-703.2124,2713.111,94.98071), -- 00:57:36
+(@GUID,@POINT := @POINT + '1',-710.6580,2712.3549,94.7205), -- 00:57:38 --C
+(@GUID,@POINT := @POINT + '1',-710.7588,2702.815,94.6216), -- 00:57:39
+(@GUID,@POINT := @POINT + '1',-707.4856,2689.286,94.33665), -- 00:57:44
+(@GUID,@POINT := @POINT + '1',-706.4232,2686.263,94.17517), -- 00:57:50
+(@GUID,@POINT := @POINT + '1',-694.9254,2679.296,93.42561), -- 00:57:52
+(@GUID,@POINT := @POINT + '1',-679.5647,2669.552,90.44455), -- 00:57:57
+(@GUID,@POINT := @POINT + '1',-678.5782,2668.9,89.83815), -- 00:58:03
+(@GUID,@POINT := @POINT + '1',-662.3951,2684.631,88.87974), -- 00:58:08
+(@GUID,@POINT := @POINT + '1',-650.7223,2700.149,87.90611), -- 00:58:16
+(@GUID,@POINT := @POINT + '1',-642.0238,2715.182,86.90927), -- 00:58:24
+(@GUID,@POINT := @POINT + '1',-640.7755,2717.258,86.57317), -- 00:58:29
+(@GUID,@POINT := @POINT + '1',-634.3006,2732.866,85.51468), -- 00:58:31
+(@GUID,@POINT := @POINT + '1',-616.3732,2751.199,83.35973), -- 00:58:40
+(@GUID,@POINT := @POINT + '1',-605.2081,2765.297,78.54527), -- 00:58:47
+(@GUID,@POINT := @POINT + '1',-596.5933,2784.522,72.0435), -- 00:58:54
+(@GUID,@POINT := @POINT + '1',-591.5787,2803.707,66.74985), -- 00:59:03
+(@GUID,@POINT := @POINT + '1',-590.4258,2822.456,61.94441), -- 00:59:11
+(@GUID,@POINT := @POINT + '1',-592.53,2835.081,59.69307), -- 00:59:20
+(@GUID,@POINT := @POINT + '1',-596.1635,2847.102,58.09987), -- 00:59:26
+(@GUID,@POINT := @POINT + '1',-597.4973,2850.567,57.33707), -- 00:59:31
+(@GUID,@POINT := @POINT + '1',-592.5145,2859.644,57.5414), -- 00:59:34
+(@GUID,@POINT := @POINT + '1',-610.3804,2871.98,54.83242), -- 00:59:37
+(@GUID,@POINT := @POINT + '1',-639.1476,2874.169,50.74456), -- 00:59:45
+(@GUID,@POINT := @POINT + '1',-647.4047,2872.841,49.71684), -- 00:59:57
+(@GUID,@POINT := @POINT + '1',-657.5587,2869.095,49.19283), -- 01:00:05
+(@GUID,@POINT := @POINT + '1',-629.851,2864.712,51.55709), -- 01:00:06
+(@GUID,@POINT := @POINT + '1',-611.9828,2858.52,55.30186), -- 01:00:18
+(@GUID,@POINT := @POINT + '1',-601.4695,2847.149,57.85207), -- 01:00:25
+(@GUID,@POINT := @POINT + '1',-594.7114,2832.047,60.31672), -- 01:00:31
+(@GUID,@POINT := @POINT + '1',-592.8293,2814.866,64.0257), -- 01:00:38
+(@GUID,@POINT := @POINT + '1',-597.939,2791.476,70.42447), -- 01:00:45
+(@GUID,@POINT := @POINT + '1',-606.717,2767.088,77.95975), -- 01:00:55
+(@GUID,@POINT := @POINT + '1',-614.9818,2755.868,82.00406), -- 01:01:05
+(@GUID,@POINT := @POINT + '1',-623.3367,2746.366,84.40461), -- 01:01:12
+(@GUID,@POINT := @POINT + '1',-635.1954,2732.008,85.72681), -- 01:01:19
+(@GUID,@POINT := @POINT + '1',-642.4907,2714.986,86.78146), -- 01:01:26
+(@GUID,@POINT := @POINT + '1',-649.691,2702.885,87.69963), -- 01:01:32
+(@GUID,@POINT := @POINT + '1',-654.83,2695.69,88.33471), -- 01:01:38
+(@GUID,@POINT := @POINT + '1',-666.5414,2681.097,89.36565), -- 01:01:43
+(@GUID,@POINT := @POINT + '1',-677.8886,2669.907,90.08049); -- 01:01:49
+
+DELETE FROM `creature_linking` WHERE `master_guid`=57951;
+INSERT INTO `creature_linking` (`guid`,`master_guid`,`flag`)VALUES
+(57952,57951,515);
+
 
 -- ----------------------------------------------------------
 -- Magus Filinthus
