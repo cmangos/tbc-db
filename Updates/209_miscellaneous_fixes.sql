485861273d04e7a0736a651714fd2e83f65c1a11
killerwife <killerwife@gmail.com>
2017-01-15 18:48:41 +0100

    Fix Tortured Skeleton rep rewards

--- Updates/209_miscellaneous_fixes.sql
+++ Updates/209_miscellaneous_fixes.sql
@@ -114,3 +114,7 @@ UPDATE areatrigger_teleport SET target_orientation=5.346 WHERE id IN(704);
 -- Set respawn to wowhead comment data - TODO: implement pooling for these entries
 UPDATE gameobject SET spawntimesecs=180 WHERE id IN(179486,179490);
 
+-- give proper rep reward normal 12 up to honored, hc 15
+UPDATE creature_onkill_reputation SET RewOnKillRepValue1=12, MaxStanding1=5 WHERE creature_id=18797;
+UPDATE creature_onkill_reputation SET RewOnKillRepValue1=15 WHERE creature_id=20662;
+
