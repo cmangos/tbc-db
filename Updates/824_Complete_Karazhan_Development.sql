3a291ebe8e741bd28f8cd9d711efd7cba7518afb
Saltgurka <alexander.bertilsson2@gmail.com>
2017-01-24 19:29:36 +0100

    Karazhan-Add missing gossip menu for Bennett

--- Updates/824_Complete_Karazhan_Development.sql
+++ Updates/824_Complete_Karazhan_Development.sql
@@ -1031,7 +1031,14 @@ insert into `creature_movement` (`id`, `point`, `position_x`, `position_y`, `pos
 
 -- Bennett
 UPDATE creature SET position_x = '-11091.975586', position_y = '-1896.073120', position_z = '74.388451', orientation = '3.930908', SpawnDist=0, MovementType=2 WHERE guid = '57036';
-UPDATE creature_template SET MovementType=2 WHERE entry IN (16426);
+UPDATE creature_template SET MovementType=2, GossipMenuId = 8173 WHERE entry IN (16426);
+DELETE FROM gossip_menu WHERE entry = 8173;
+INSERT INTO gossip_menu (entry, text_id, script_id, condition_id) VALUES
+(8173, 10120, 0, 0);
+-- missing npc_text added
+DELETE FROM npc_text WHERE ID = 10120;
+INSERT INTO npc_text (ID, text0_0, text0_1, lang0, prob0, em0_0, em0_1, em0_2, em0_3, em0_4, em0_5, text1_0, text1_1, lang1, prob1, em1_0, em1_1, em1_2, em1_3, em1_4, em1_5, text2_0, text2_1, lang2, prob2, em2_0, em2_1, em2_2, em2_3, em2_4, em2_5, text3_0, text3_1, lang3, prob3, em3_0, em3_1, em3_2, em3_3, em3_4, em3_5, text4_0, text4_1, lang4, prob4, em4_0, em4_1, em4_2, em4_3, em4_4, em4_5, text5_0, text5_1, lang5, prob5, em5_0, em5_1, em5_2, em5_3, em5_4, em5_5, text6_0, text6_1, lang6, prob6, em6_0, em6_1, em6_2, em6_3, em6_4, em6_5, text7_0, text7_1, lang7, prob7, em7_0, em7_1, em7_2, em7_3, em7_4, em7_5) VALUES
+(10120, 'I don\'t have time for idle chitchat. I have men and women to train, and a tower to watch.', '', 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0);
 
 DELETE FROM creature_movement_template WHERE entry IN (16426);
 insert into `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`, `textid1`, `textid2`, `textid3`, `textid4`, `textid5`, `emote`, `spell`, `orientation`, `model1`, `model2`) values('16426','1','-11091.975586','-1896.073120','74.388451','5000','1642601','0','0','0','0','0','0','0','3.930908','0','0');
