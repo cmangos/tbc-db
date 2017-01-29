4ad69e800cd2ce2cb49d69fea50f55b622db45e3
killerwife <killerwife@gmail.com>
2017-01-29 19:22:57 +0100

    Summoning channel visual should target Summoning Stone version too

--- Updates/209_miscellaneous_fixes.sql
+++ Updates/209_miscellaneous_fixes.sql
@@ -142,3 +142,8 @@ UPDATE creature_template SET ExtraFlags=ExtraFlags|256 WHERE entry=15690;
 UPDATE gameobject_template SET CustomData1=5000 WHERE entry IN(36727,179944);
 -- Add visual to Meeting stone summon
 UPDATE gameobject_template SET data2=32929 WHERE entry = 179944;
+
+-- Make visual target Meeting Stone Summoning GO as well
+DELETE FROM spell_script_target WHERE entry=32929 AND targetEntry=179944;
+INSERT INTO spell_script_target VALUES('32929', '0', '179944', '0');
+
