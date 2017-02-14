e71dc40af6f932787c74603b5d853fc0d495d3aa
killerwife <killerwife@gmail.com>
2017-02-14 21:10:14 +0100

    Dont require Eye attunement by default

--- Updates/242_miscellaneous_fixes.sql
+++ Updates/242_miscellaneous_fixes.sql
@@ -1,4 +1,7 @@
 -- Molten Core does not require attunement to enter through portal, only through Lothos Riftwalker
 UPDATE areatrigger_teleport SET required_quest_done=0 WHERE id IN(3528,3529);
 
+-- The Eye - dont require attunement
+UPDATE areatrigger_teleport SET required_item=0 AND required_quest_done=0 WHERE id=4470;
+
 
