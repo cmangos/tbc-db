8889877da7b01caf816c75f49e9169f94f7fc34a
Tobschinski <tobschinski@gmail.com>
2017-02-22 16:36:14 -0500

    Gruul's Lair object fixes

---
+++ Updates/257_gruuls_lair_doors.sql
@@ -0,0 +1,4 @@
+UPDATE `gameobject` SET `position_x`='166.8971', `position_y`='368.2259', `position_z`='16.92091', `orientation`='4.71239', `rotation2`='-0.7071066', `rotation3`='0.7071069' WHERE `guid`='50479';
+UPDATE `gameobject` SET `position_x`='99.07292', `position_y`='249.7036', `position_z`='18.3478', `orientation`='3.141593', `rotation2`='-1' WHERE `guid`='12443';
+
+UPDATE `gameobject_template` SET `flags`='32' WHERE `entry`='183817';
