7c8201a3ee314eb2fafa2cf1a4205c3aec6da1cf
killerwife <killerwife@gmail.com>
2017-03-13 22:44:50 +0100

    Fix Invisible target model

--- Updates/242_miscellaneous_fixes.sql
+++ Updates/242_miscellaneous_fixes.sql
@@ -45,4 +45,7 @@ UPDATE `quest_template` SET `OfferRewardText`='You say that you found it on one 
 -- Dalliah the Doomsayer should not be immune to interrupt and should be immune to taunt
 UPDATE creature_template SET MechanicImmuneMask=MechanicImmuneMask&~33554432,ExtraFlags=ExtraFlags|256 WHERE entry IN(20885,21590);
 
+-- Fix invisible target model - previously human ghost
+UPDATE creature_template SET ModelId1=15880 WHERE entry IN(20654);
+
 
