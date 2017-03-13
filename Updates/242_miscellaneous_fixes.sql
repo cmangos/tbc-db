03757c51934d0f977818204ce895a6e41cb1711a
Sneakynick <crhansen12@apu.edu>
2017-03-13 09:41:32 +0100

    Fix typos in quest text

--- Updates/242_miscellaneous_fixes.sql
+++ Updates/242_miscellaneous_fixes.sql
@@ -38,4 +38,9 @@ UPDATE quest_template SET OfferRewardText = 'With Warlord Sriss''tiz dead, the p
 -- Add proper UnitFlags to Ythyar repair vendor in Karazhan
 UPDATE creature_template SET UnitFlags=768 WHERE entry=17518;
 
+-- Fix typos in texts
+UPDATE `quest_template` SET `Objectives`='Deliver the Sealed Letter to Ag\'tor Bloodfist in Azshara.' WHERE `entry`='3504';
+UPDATE `quest_template` SET `OfferRewardText`='You say that you found it on one of the Scourge at Windrunner Spire and that there\'s an inscription on it? Let me see! Here, take this coin for a job well done. ' WHERE `entry`='9175';
+
+
 
