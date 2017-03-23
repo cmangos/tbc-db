8e258a61e19a959cf4295eb34cf434188cb0325e
MantisLord <sabinprosper@gmail.com>
2017-03-22 23:34:27 -0400

    Fix drop of Eroded Leather Case for quest Missing Missive

--- Updates/293_miscellaneous_fixes.sql
+++ Updates/293_miscellaneous_fixes.sql
@@ -12,4 +12,8 @@ UPDATE creature SET equipment_id=0 WHERE id=21453;
 -- Stormwind Soldier - The Stair of Destiny
 UPDATE creature SET equipment_id=0 WHERE id=18948;
 
+-- Eroded Leather Case
+-- Starts quest: Missing Missive (9373)
+-- does not drop currently as it requires 9373
+UPDATE creature_loot_template SET ChanceOrQuestChance=10 WHERE item=23338;
 
