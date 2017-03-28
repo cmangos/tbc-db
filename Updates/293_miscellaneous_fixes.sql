824ac8d59c49d6a690ac7486050ed0f64f45f4a5
MantisLord <sabinprosper@gmail.com>
2017-03-27 23:29:57 -0400

    Misc fixes
    - delete bad links in Zul'Farrak
    - remove respawn time for objectives of quest 9176
    - fix text for quest 459

--- Updates/293_miscellaneous_fixes.sql
+++ Updates/293_miscellaneous_fixes.sql
@@ -47,4 +47,18 @@ INSERT INTO gameobject (guid, id, map, spawnMask, position_x, position_y, positi
 -- Vampiric Mistbat 16354
 UPDATE creature_template SET FactionAlliance=16, FactionHorde=16 WHERE entry IN (15271,15294,16354);
 
+-- The Woodland Protector quest text fix
+UPDATE `quest_template` SET `OfferRewardText`='Your service to the creatures of Shadowglen is worthy of reward, $N.$b$bYou confirmed my fears, however. If the grells have become tainted by the Fel Moss, one can only imagine what has become of the Gnarlpine tribe of furbolgs who once lived here.$b$bShould you find yourself in Dolanaar, able $C, seek out the knowledgeable druid, Athridas Bearmantle. He shares our concern for the well being of the forest.' WHERE `entry`=459;
+
+-- The Twin Ziggurats (9176)
+-- these chests containing Stone of Flame (22599) and Stone of Light (22598) do not despawn
+UPDATE gameobject SET spawntimesecs=0 WHERE id=181238;
+UPDATE gameobject SET spawntimesecs=0 WHERE id=181239;
+
+-- Fix horrible links in ZF
+DELETE FROM creature_linking WHERE guid=45710;
+DELETE FROM creature_linking WHERE guid=81549;
+DELETE FROM creature_linking WHERE master_guid=81598;
+DELETE FROM creature_linking WHERE master_guid=81601;
+
 
