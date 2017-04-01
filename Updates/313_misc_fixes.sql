9755c559e8b80acbb475361948a90e51218f045b
MantisLord <sabinprosper@gmail.com>
2017-04-01 03:06:57 -0400

    More misc fixes
    - add 1 missing Moonwell spawn from CDB
    - correct spawntimes of quest objectives in STV
    - lower requirement of Mageweave Bandage to level 35
    - fix last quest in chain to get UBRS key

---
+++ Updates/313_misc_fixes.sql
@@ -0,0 +1,23 @@
+-- Add missing Moonwell from Classic DB
+DELETE FROM gameobject WHERE guid=55541;
+INSERT INTO gameobject (guid, id, map, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecsmin, spawntimesecsmax, animprogress, state) VALUES
+(55541, 177278, 1, 9630.89, 2520.89, 1331.58, 3.22902, 0, 0, 0.999045, -0.0436984, 900, 900, 100, 1);
+
+-- Stranglethorn Vale spawntime adjustments
+-- Mastery Bosses
+UPDATE creature SET spawntimesecsmin=420, spawntimesecsmax=600 WHERE id=728;
+UPDATE creature SET spawntimesecsmin=420, spawntimesecsmax=600 WHERE id=729;
+UPDATE creature SET spawntimesecsmin=420, spawntimesecsmax=600 WHERE id=730;
+UPDATE creature SET spawntimesecsmin=420, spawntimesecsmax=600 WHERE id=731;
+-- Colonel Kurzen
+UPDATE creature SET spawntimesecsmin=420, spawntimesecsmax=600 WHERE id=813;
+
+-- Player can learn Heavy Mageweave Bandage from Triage quest giver at level 35 in Patch 2.4.3
+UPDATE conditions SET value1=35 WHERE condition_entry=179;
+
+-- Allow players to receive all required items to finish UBRS key quest
+DELETE FROM item_loot_template WHERE entry=12339 AND item=12300;
+INSERT INTO item_loot_template (entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount, condition_id, comments) VALUES
+(12339,12300,100,0,1,1,0,'Orb of Draconic Energy');
+
+
