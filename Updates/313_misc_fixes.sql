be92e63362b7824e889333759acb2870c31ca96c
killerwife <killerwife@gmail.com>
2017-04-01 22:01:44 +0200

    Add spell focuses for Teleport This quest

--- Updates/313_misc_fixes.sql
+++ Updates/313_misc_fixes.sql
@@ -20,4 +20,11 @@ DELETE FROM item_loot_template WHERE entry=12339 AND item=12300;
 INSERT INTO item_loot_template (entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount, condition_id, comments) VALUES
 (12339,12300,100,0,1,1,0,'Orb of Draconic Energy');
 
+-- Missing Spell focuses for Teleport This quests
+DELETE FROM `gameobject` WHERE `guid` IN (150124,150125,150126);
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecsmin`,`spawntimesecsmax`,`animprogress`,`state`) VALUES
+(150124, 300118, 530, 1, 4697.5, 3298.68, 178.892, 2.02823, 0, 0, 0.849013, 0.528372, 120, 120, 0, 1),
+(150125, 300118, 530, 1, 4734.71, 3193.67, 161.48, 0.386745, 0, 0, 0.19217, 0.981362, 120, 120, 0, 1),
+(150126, 300118, 530, 1, 4673.28, 3126.67, 166.725, 5.42979, 0, 0, 0.413867, -0.910337, 120, 120, 0, 1);
+
 
