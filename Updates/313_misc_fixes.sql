9194a48d3e92c8d577bbb12537e7a28bb36378dd
MantisLord <sabinprosper@gmail.com>
2017-04-11 08:37:12 -0400

    Fix drop rate of Hollow Vulture Bone

--- Updates/313_misc_fixes.sql
+++ Updates/313_misc_fixes.sql
@@ -103,4 +103,8 @@ INSERT INTO dbscripts_on_creature_movement(id, delay, command, datalong, datalon
 -- Short John Mithril - make him active
 UPDATE creature_template SET ExtraFlags=4096 WHERE entry IN(14508);
 
+-- Hollow Vulture Bone for q.1176 'Load Lightning' should always drop
+-- http://www.wowhead.com/quest=1176/load-lightening#comments:id=59611
+UPDATE creature_loot_template SET ChanceOrQuestChance=-100 WHERE item=5848;
+
 
