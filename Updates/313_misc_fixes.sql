364eb7c1ea1f03850df463fe420c158bab59edae
MantisLord <sabinprosper@gmail.com>
2017-04-11 14:30:14 -0400

    Remove addon for Homing Robots and shorten respawn

--- Updates/313_misc_fixes.sql
+++ Updates/313_misc_fixes.sql
@@ -107,4 +107,9 @@ UPDATE creature_template SET ExtraFlags=4096 WHERE entry IN(14508);
 -- http://www.wowhead.com/quest=1176/load-lightening#comments:id=59611
 UPDATE creature_loot_template SET ChanceOrQuestChance=-100 WHERE item=5848;
 
+-- Remove permanent death state and shorten respawn timer for Homing Robots
+DELETE FROM creature_template_addon WHERE entry IN (7784,7806,7807);
+DELETE FROM creature_addon WHERE guid IN (23229,93301,50381);
+UPDATE creature SET spawntimesecsmin=150, spawntimesecsmax=150 WHERE id IN (7784,7806,7807);
+
 
