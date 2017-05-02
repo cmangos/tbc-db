d477b09911991891b58bbb2d20c18876b5ef1254
MantisLord <sabinprosper@gmail.com>
2017-05-02 11:30:11 -0400

    Increase drop rate of Crystalline Key to 100%

--- Updates/313_misc_fixes.sql
+++ Updates/313_misc_fixes.sql
@@ -162,4 +162,7 @@ INSERT INTO creature_template_addon(entry,auras) VALUES
 -- Fix factions for Snake trap snakes
 UPDATE creature_template SET FactionAlliance=1629 WHERE entry IN(19921,19833);
 
+-- Crystalline Key for q.10528 'Demonic Crystal Prisons'
+UPDATE creature_loot_template SET ChanceOrQuestChance=-100 WHERE item=30442 AND entry=21309;
+
 
