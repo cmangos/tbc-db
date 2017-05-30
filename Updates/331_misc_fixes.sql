dbe2050b36c37df6d4b01297c298033c64c25072
MantisLord <sabinprosper@gmail.com>
2017-05-29 22:53:31 -0400

    Torek's guards should follow him

--- Updates/331_misc_fixes.sql
+++ Updates/331_misc_fixes.sql
@@ -1,4 +1,12 @@
 -- give proper unitflags and regenerate
 UPDATE creature_template SET UnitFlags=33554688,RegenerateStats=3 WHERE entry IN(20899);
 
+-- https://jira.vengeancewow.com/browse/TBC-135
+-- Torek's Assault
+-- quest should possibly be ported from SD2 to dbscripts (npc_torek)
+DELETE FROM creature_linking WHERE guid IN (32378,32380,32379);
+INSERT INTO creature_linking (guid, master_guid, flag) VALUES
+(32378, 32373, 8192+512+128+2+1),
+(32379, 32373, 8192+512+128+2+1),
+(32380, 32373, 8192+512+128+2+1);
 
