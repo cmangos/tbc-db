2c0f76a5330400a7494dfa7f0314434e5c19ca0e
Saltgurka <saltgurkasalt@gmail.com>
2017-07-02 17:30:56 +0200

    Set the correct string ID in Aeranas script

--- Updates/SpinOnTop/324_Avruus_Orb.sql
+++ Updates/SpinOnTop/324_Avruus_Orb.sql
@@ -8,7 +8,7 @@ DELETE FROM dbscripts_on_gossip where id in(1021901,1021902);
 INSERT INTO dbscripts_on_gossip(id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 ('1021901', '1', '10', '17085', '180000', '0', '0', '0', '0', '0', '0', '0', '0', '-1323.91', '4042.45', '116.626', '0.296706', 'Summon aeranas'),
 ('1021901', '0', '31', '17085', '20', '0', '0', '0', '8', '0', '0', '0', '0', '0', '0', '0', '0', 'Check for Aeranas'),
-('1021902', '0', '0', '0', '0', '0', '17085', '20', '7', '2000005784', '0', '0', '0', '0', '0', '0', '0', 'Aeranas SAY'),
+('1021902', '0', '0', '0', '0', '0', '17085', '20', '7', '2000005654', '0', '0', '0', '0', '0', '0', '0', 'Aeranas SAY'),
 ('1021902', '6', '22', '14', '3', '0', '17085', '20', '7', '0', '0', '0', '0', '0', '0', '0', '0', 'Aeranas Change Faction');
 
 
