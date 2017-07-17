717da67bd86f7e96b17c01f7ec6ca31180433f4f
bdebaere <brechtdebaere@yahoo.com>
2017-07-17 11:38:15 +0200

    [1757] Fixed Z-Axis of AV Call event
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/0aef8b06848ef5d32c82d53defd90518d7805b6e)

---
+++ Updates/0515_CDB-1757_AV_banners.sql
@@ -0,0 +1,6 @@
+-- Corrections to z-axis
+UPDATE gameobject SET position_z=501.65 WHERE guid=190062;
+UPDATE gameobject SET position_z=502.32 WHERE guid=190061;
+UPDATE gameobject SET position_z=505.3 WHERE guid=190080;
+UPDATE gameobject SET position_z=505.917 WHERE guid=190079;
+UPDATE gameobject SET position_z=37.21 WHERE guid=190310;
