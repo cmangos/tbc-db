7bef58b66c89f6486beb95e8a19b999317197225
Darexon <darexon331@gmail.com>
2017-07-18 13:39:36 +0200

    Fix faction for Thrallmar Grunt and Thrallmar Marksman
    solves: https://github.com/cmangos/issues/issues/1426

---
+++ Updates/392_c16580_c16582_faction.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET FactionAlliance = 1668, FactionHorde = 1668 WHERE entry = 16580; -- Thrallmar Grunt
+UPDATE creature_template SET FactionAlliance = 1668, FactionHorde = 1668 WHERE entry = 16582; -- Thrallmar Marksman
+
