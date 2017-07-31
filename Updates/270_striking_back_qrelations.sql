adc4ec4022d14a954bd8c693b650668ef2ae0ce6
MantisLord <sabinprosper@gmail.com>
2017-07-31 08:13:33 -0400

    Resolve conflict of condition entries
    File 241 uses same ids

--- Updates/270_striking_back_qrelations.sql
+++ Updates/270_striking_back_qrelations.sql
@@ -1,26 +1,26 @@
 -- [Midsummer Fire Festival] Questline "Striking Back"
-DELETE FROM conditions WHERE condition_entry BETWEEN 1126 AND 1141;
+DELETE FROM conditions WHERE condition_entry BETWEEN 1200 AND 1215;
 INSERT INTO conditions (condition_entry, type, value1, value2) VALUES
-(1126, 15, 22, 1),
-(1127, 15, 28, 2),
-(1128, -1, 1127, 1126),
-(1129, 15, 29, 1),
-(1130, 15, 38, 2),
-(1131, -1, 1130, 1129),
-(1132, 15, 39, 1),
-(1133, 15, 48, 2),
-(1134, -1, 1133, 1132),
-(1135, 15, 49, 1),
-(1136, 15, 55, 2),
-(1137, -1, 1136, 1135),
-(1138, 15, 56, 1),
-(1139, 15, 63, 2),
-(1140, -1, 1139, 1138),
-(1141, 15, 64, 1);
+(1200, 15, 22, 1),
+(1201, 15, 28, 2),
+(1202, -1, 1201, 1200),
+(1203, 15, 29, 1),
+(1204, 15, 38, 2),
+(1205, -1, 1204, 1203),
+(1206, 15, 39, 1),
+(1207, 15, 48, 2),
+(1208, -1, 1207, 1206),
+(1209, 15, 49, 1),
+(1210, 15, 55, 2),
+(1211, -1, 1210, 1209),
+(1212, 15, 56, 1),
+(1213, 15, 63, 2),
+(1214, -1, 1213, 1212),
+(1215, 15, 64, 1);
 
-UPDATE quest_template SET RequiredCondition = 1128 WHERE entry = 11917;
-UPDATE quest_template SET RequiredCondition = 1131 WHERE entry = 11947;
-UPDATE quest_template SET RequiredCondition = 1134 WHERE entry = 11948;
-UPDATE quest_template SET RequiredCondition = 1137 WHERE entry = 11952;
-UPDATE quest_template SET RequiredCondition = 1140 WHERE entry = 11953;
-UPDATE quest_template SET RequiredCondition = 1141 WHERE entry = 11954;
+UPDATE quest_template SET RequiredCondition = 1202 WHERE entry = 11917;
+UPDATE quest_template SET RequiredCondition = 1205 WHERE entry = 11947;
+UPDATE quest_template SET RequiredCondition = 1208 WHERE entry = 11948;
+UPDATE quest_template SET RequiredCondition = 1211 WHERE entry = 11952;
+UPDATE quest_template SET RequiredCondition = 1214 WHERE entry = 11953;
+UPDATE quest_template SET RequiredCondition = 1215 WHERE entry = 11954;
