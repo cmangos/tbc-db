08a665b1c2d3de4948d48932583b7b7e1b6e2a57
killerwife <killerwife@gmail.com>
2017-08-10 19:21:36 +0200

    Fix flags for last arcatraz bossfight NPCs

---
+++ Updates/495_flag_changes_fixes.sql
@@ -0,0 +1,5 @@
+UPDATE creature_template SET UnitFlags=33344 WHERE entry IN(20904,21622); -- warden melichar correct flags
+UPDATE creature_template SET UnitFlags=832 WHERE entry IN(20977,21602); -- millhouse manastor correct flags
+UPDATE creature_template SET UnitFlags=33600 WHERE entry IN(20912,21599); -- harbringer skyriss correct flags
+
+
