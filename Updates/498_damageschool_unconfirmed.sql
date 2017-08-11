9b4e8b74895bf5fd744de8b52b68a3cc72273789
anonxs <anonxs@web.de>
2017-08-11 21:45:18 +0200

    Damageschool Update

---
+++ Updates/498_damageschool_unconfirmed.sql
@@ -0,0 +1,42 @@
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 3 WHERE `entry` = 14454;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 4 WHERE `entry` = 14458;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 2 WHERE `entry` = 14461;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 5 WHERE `entry` = 18659;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 6 WHERE `entry` = 18865;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 3 WHERE `entry` = 18881;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 5 WHERE `entry` = 19480;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 3 WHERE `entry` = 20774;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 2 WHERE `entry` = 21061;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 3 WHERE `entry` = 21253;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 4 WHERE `entry` = 21260;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 2 WHERE `entry` = 22286;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 4 WHERE `entry` = 22309;
+
+-- Needs Confirmation
+UPDATE `creature_template` SET `damageschool` = 4 WHERE `entry` = 25040;
+
