2f461b2e388bc5eeb10aea5ac8050cfa04b8b260
killerwife <killerwife@gmail.com>
2017-08-11 14:32:01 +0200

    Fix wrong Cyrukh data causing script malfunction

--- Updates/495_flag_changes_fixes.sql
+++ Updates/495_flag_changes_fixes.sql
@@ -2,4 +2,8 @@ UPDATE creature_template SET UnitFlags=33344 WHERE entry IN(20904,21622); -- war
 UPDATE creature_template SET UnitFlags=832 WHERE entry IN(20977,21602); -- millhouse manastor correct flags
 UPDATE creature_template SET UnitFlags=33600 WHERE entry IN(20912,21599); -- harbringer skyriss correct flags
 
+-- cyrukh fixes
+UPDATE creature_template SET UnitFlags=320,FactionAlliance=16,FactionHorde=16 WHERE entry IN(21181);
+UPDATE creature_model_info SET bounding_radius=5,combat_reach=10 WHERE modelid IN(20135);
+
 
