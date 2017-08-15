de71195390ee80e372beb372e8cab2ee5b277c42
MantisLord <sabinprosper@gmail.com>
2017-08-14 23:09:37 -0400

    Fix Eliza not being attackable

--- Updates/495_flag_changes_fixes.sql
+++ Updates/495_flag_changes_fixes.sql
@@ -19,4 +19,10 @@ UPDATE creature_template SET UnitFlags=32768|512|256 WHERE entry=19726; -- "Cree
 -- add missing 512 flag to Netherspite portals
 UPDATE creature_template SET UnitFlags=33555200 WHERE entry IN(17367,17368,17369);
 
+-- q.253 'Bride of the Embalmer'
+-- q.254 'Digging Through the Dirt'
+DELETE FROM dbscripts_on_quest_end WHERE id=254 AND delay=2;
+INSERT INTO dbscripts_on_quest_end (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(254, 2, 5, 46, 256, 0, 314, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Eliza - Remove UnitFlag - Immune to Player');
+
 
