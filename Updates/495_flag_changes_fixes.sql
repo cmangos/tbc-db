262eccc51e142748fd2032a03863921d58ebabf9
killerwife <killerwife@gmail.com>
2017-08-16 14:53:05 +0200

    Fix wrong Harbinger Skyriss flags

--- Updates/495_flag_changes_fixes.sql
+++ Updates/495_flag_changes_fixes.sql
@@ -25,4 +25,7 @@ DELETE FROM dbscripts_on_quest_end WHERE id=254 AND delay=2;
 INSERT INTO dbscripts_on_quest_end (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 (254, 2, 5, 46, 256, 0, 314, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Eliza - Remove UnitFlag - Immune to Player');
 
+-- all Harbinger Skyriss illusions should have 66 flags
+UPDATE creature_template SET UnitFlags=66 WHERE entry IN(21600,21601);
+
 
