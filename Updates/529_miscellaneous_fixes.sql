80e497dc06368d49bd974dd7b5e1e1fb8a4c5f0e
MantisLord <sabinprosper@gmail.com>
2017-08-26 15:28:05 -0400

    Captain Sanders' Hidden Treasure is available to Horde
    https://jira.vengeancewow.com/browse/TBC-1484

--- Updates/529_miscellaneous_fixes.sql
+++ Updates/529_miscellaneous_fixes.sql
@@ -1,4 +1,11 @@
 -- Void Traveler and Sapling should move much more slowly
 UPDATE creature_template SET SpeedWalk=2/2.5,SpeedRun=2/7 WHERE entry IN(19226,19949);
 
+/*
+Captain Sanders' Hidden Treasure
+By satukie (2,550 – 9·22) on 2007/01/12 (Patch 2.0.4)
+Horde can also get this quest along with every other captain sander's quests.
+*/
+UPDATE quest_template SET RequiredRaces=0 WHERE entry IN (140,139,138,136);
+
 
