f145e9123f2992d82b57686a8999d87dc62f065d
MantisLord <sabinprosper@gmail.com>
2017-09-05 13:49:57 -0400

    Correct objectives for quest 1383

--- Updates/529_miscellaneous_fixes.sql
+++ Updates/529_miscellaneous_fixes.sql
@@ -23,4 +23,13 @@ UPDATE quest_template SET ReqSpellCast1=0,ReqSpellCast2=0,ReqSpellCast3=0 WHERE 
 -- fix murmur unitflags
 UPDATE creature_template SET UnitFlags=32832 WHERE entry IN(18708,20657);
 
+-- Nothing But The Truth (1383) changed requirements in 2.3
+-- no longer need the Deepstrider Tumor (the item was replaced by Green Whelp Blood)
+UPDATE item_template SET name='Green Whelp Blood', displayid=47459 WHERE entry=6082;
+UPDATE creature_loot_template SET comments='Green Whelp Blood' WHERE entry=741 AND item=6082;
+UPDATE quest_template SET
+Objectives='Apothecary Faustin at Beggar\'s Haunt needs 5 Shadow Panther Hearts, Mire Lord Fungus and some Green Whelp Blood.',
+OfferRewardText='That Infiltrator in Stonard will surely.... open up... after he drinks my special serum. Haha!$b$bThe ingredients required are by no means easy to procure. Shadow Panthers are common in the Swamp but collecting enough hearts for our purpose can be daunting. And the Mire Lord can be such a fussy beast.$b$b'
+WHERE entry=1383;
+
 
