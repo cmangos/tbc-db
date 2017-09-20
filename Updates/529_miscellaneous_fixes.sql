b700407a3f032d8264b7ca654ceaf29e4fe10302
MantisLord <sabinprosper@gmail.com>
2017-09-20 00:29:18 -0400

    Fix Death Ravager unattackable

--- Updates/529_miscellaneous_fixes.sql
+++ Updates/529_miscellaneous_fixes.sql
@@ -72,4 +72,12 @@ INSERT INTO dbscripts_on_gossip (id, delay, command, datalong, datalong2, datalo
 -- Lollipop - does not drop from any of these creatures
 DELETE FROM creature_loot_template WHERE item=7806;
 
+-- https://jira.vengeancewow.com/browse/TBC-2851
+-- Ravager Cage (181849) opened by Ravager Cage Key (23925)
+-- quest: Strength of One (9582)
+-- Death Ravager (17556) should also have movement path it follows after cage door opens, but no data on this currently
+DELETE FROM dbscripts_on_go_template_use WHERE id=181849;
+INSERT INTO dbscripts_on_go_template_use (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+('181849', '2', '5', '46', '256', '0', '17556', '30', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Death Ravager - Remove UnitFlag (Immune to Player)');
+
 
