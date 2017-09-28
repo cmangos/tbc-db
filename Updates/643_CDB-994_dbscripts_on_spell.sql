eea0226adecd0612d42a60777597b15a5cdd7f49
MantisLord <sabinprosper@gmail.com>
2017-09-28 14:19:45 -0400

    Fix build

--- Updates/643_CDB-994_dbscripts_on_spell.sql
+++ Updates/643_CDB-994_dbscripts_on_spell.sql
@@ -2,8 +2,6 @@
 -- They will now attack cast and revert to their original form after 5 min
 -- Thanks 249CAAFE40 for reporting. This closes #655
 DELETE FROM `dbscripts_on_spell` WHERE `id` = 23359;
-INSERT INTO `dbscripts_on_spell` VALUES
+INSERT INTO `dbscripts_on_spell` (id, delay, command, datalong, datalong2, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 (23359, 0, 26, 0, 0, 0, 0, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 'Giant/Strider attacks player on Transmogrify Spell Hit');
 
--- Fixed Hit Points of NPC 5360 (Deep Strider) in Feralas that were previously too low
-UPDATE `creature_template` SET `HealthMultiplier` = 3, `MinLevelHealth` = 4776, `MaxLevelHealth` = 5130 WHERE `Entry` = 5360;
