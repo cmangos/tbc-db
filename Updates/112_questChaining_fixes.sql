d9b957f82918d6023e8cfa7c758d0d44270c3425
killerwife <killerwife@gmail.com>
2017-09-30 01:38:10 +0200

    q.8275 8276 8280 chaining fixes

---
+++ Updates/112_questChaining_fixes.sql
@@ -0,0 +1,6 @@
+UPDATE `quest_template` SET `NextQuestId`='8280', `ExclusiveGroup`='8275' WHERE (`entry`='8275');
+UPDATE `quest_template` SET `NextQuestId`='8280', `ExclusiveGroup`='8275' WHERE (`entry`='8276');
+UPDATE `quest_template` SET `PrevQuestId`='0', `ExclusiveGroup`='8275' WHERE (`entry`='8280');
+
+
+
