eb7935e9dc848d8996ee5f60c9656b6d07d9cc03
X-Savior <xsavior@acid.com>
2017-09-30 21:52:35 +0200

    Fix Prince Malchezaar's Axes (17650) UnitFlags and NPCFlags

---
+++ Updates/051_Fix_Princes_Axes.sql
@@ -0,0 +1,4 @@
+-- =================================================================
+-- Fix Prince Malchezaar's Axes (17650) Not Attackable or Selectable
+-- =================================================================
+UPDATE creature_template SET NPCFlags=0, UnitFlags=33554434 WHERE entry IN (17650);
