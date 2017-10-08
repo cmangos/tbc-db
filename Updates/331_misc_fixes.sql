29cf5dd8970c6ccbd97df3200c49b23b1439cc34
MantisLord <sabinprosper@gmail.com>
2017-10-09 00:35:27 +0200

    Fix faction of creatures in Black Morass

--- Updates/331_misc_fixes.sql
+++ Updates/331_misc_fixes.sql
@@ -10,3 +10,8 @@ INSERT INTO creature_linking (guid, master_guid, flag) VALUES
 (32379, 32373, 8192+512+128+2+1),
 (32380, 32373, 8192+512+128+2+1);
 
+-- Time Keeper (1)
+UPDATE creature_template SET FactionHorde=1718, FactionAlliance=1718 WHERE entry=20746;
+-- Infinite Dragonflight
+UPDATE creature_template SET FactionHorde=1720, FactionAlliance=1720 WHERE entry IN (17835,17838,17839,17879,17880,17881,17892,21818,21104,21697,21698,20740,20741,20744,22169,22170);
+
