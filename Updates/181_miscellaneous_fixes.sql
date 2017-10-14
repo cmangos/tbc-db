b211ca4e75d752a552386083575980fed4d2fa75
MantisLord <sabinprosper@gmail.com>
2017-10-14 19:10:21 +0200

    Fix Nightlash spell_script_targets and make Gronn-Priests interruptable

--- Updates/181_miscellaneous_fixes.sql
+++ Updates/181_miscellaneous_fixes.sql
@@ -33,3 +33,23 @@ UPDATE quest_template SET ExclusiveGroup=0 WHERE entry=8280; -- remove exclusive
 UPDATE `creature` SET `position_x`=1924.63, `position_y`= 5574.76, `position_z`=273.122,modelid=0 WHERE  `guid`=74872; -- remove hacked positions when visual wasnt working
 
 
+/*
+http://jira.vengeancewow.com/browse/TBC-1174
+Nightlash hitting players with Spirit Steal
+*/
+
+DELETE FROM spell_script_target WHERE entry = 3477;
+INSERT INTO spell_script_target (entry, `type`, targetEntry, inverseEffectMask) VALUES
+(3477, 1, 1772, 0),
+(3477, 1, 1773, 0);
+
+/*
+http://jira.vengeancewow.com/browse/TBC-1941
+http://www.wowhead.com/npc=21350/gronn-priest#comments:id=39991
+Make Gronn-Priest interruptable
+*/
+UPDATE creature_template SET MechanicImmuneMask = 619397119 WHERE entry = 19389;
+
+
+
+
