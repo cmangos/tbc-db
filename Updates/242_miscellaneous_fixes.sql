ef7c58b57fb3d808f71fc8b5892c909c2ed38a52
killerwife <killerwife@gmail.com>
2017-10-15 18:16:09 +0200

    Netherwing qline fixes

--- Updates/242_miscellaneous_fixes.sql
+++ Updates/242_miscellaneous_fixes.sql
@@ -4,4 +4,8 @@ UPDATE areatrigger_teleport SET required_quest_done=0 WHERE id IN(3528,3529);
 -- The Eye - dont require attunement
 UPDATE areatrigger_teleport SET required_item=0 AND required_quest_done=0 WHERE id=4470;
 
+-- Netherwing qline fixes
+UPDATE gameobject SET spawntimesecs=50 WHERE id = 185156; -- adjust spawntime of chains
+UPDATE gameobject_template SET data5=1 WHERE entry IN(185156); -- on unlocking chains they should disappear
+
 
