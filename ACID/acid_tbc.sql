dac078441ca2a7bfdeebc91216d1e2cde6206c6f
anonxs <anonxs@web.de>
2017-10-21 20:29:21 +0200

    [ACID] Vekhnir Keeneye 19982

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -17741,6 +17741,9 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Deathforge Technician
 ('1997901','19979','0','0','100','1','6000','12000','10000','15000','11','36250','1','0','0','0','0','0','0','0','0','0','Deathforge Technician - Cast Steal Armor'),
 ('1997902','19979','2','0','100','1','30','0','8000','8000','11','36825','1','0','0','0','0','0','0','0','0','0','Deathforge Technician - Cast Drain Life at 30% HP'),
+-- Vekh'nir Keeneye
+('1998201','19982','9','0','75','1','0','5','15000','20000','11','38541','0','0','0','0','0','0','0','0','0','0','Vekh\'nir Keeneye - Cast Evasion'),
+('1998202','19982','0','0','100','1','3500','7000','20000','30000','11','35321','1','0','0','0','0','0','0','0','0','0','Vekh\'nir Keeneye - Cast Gushing Wound'),
 -- Bloodmaul Brute (Missing combat spells!)
 ('1999101','19991','22','0','100','1','3','8','11000','0','54','-1051','0','0','0','0','0','0','0','0','0','0','Bloodmaul Brute - Text Emote on Receive TEXTEMOTE_ANGRY if Player has Completed Quest 11000'),
 ('1999102','19991','22','0','100','1','34','8','11000','0','5','94','0','0','0','0','0','0','0','0','0','0','Bloodmaul Brute - Dance on Receive TEXTEMOTE_DANCE if Player has Completed Quest 11000'),
