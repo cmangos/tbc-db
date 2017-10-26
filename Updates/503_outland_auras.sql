20373b8155950cc1f689cda5b198d04cd11cca92
anonxs <anonxs@web.de>
2017-10-26 16:01:11 +0200

    [ACID] Wyrmcult Blessed 21492

--- Updates/503_outland_auras.sql
+++ Updates/503_outland_auras.sql
@@ -39,3 +39,6 @@ UPDATE creature_template_addon SET auras='19194' WHERE entry IN(18567);
 -- Sporelok - Sporeskin
 UPDATE `creature_template_addon` SET `auras` = '35230' WHERE `entry` = 18912;
 
+-- Wyrmcult Blessed - Resistances
+UPDATE `creature_template_addon` SET `auras` = '37637' WHERE `entry` = 21492;
+
