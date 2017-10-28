0194461e8f58f5fc6e05beed85fa4cbd8d530c17
anonxs <anonxs@web.de>
2017-10-28 20:13:49 +0200

    [ACID] Felspine the Greater 21897

--- Updates/503_outland_auras.sql
+++ Updates/503_outland_auras.sql
@@ -39,3 +39,8 @@ UPDATE creature_template_addon SET auras='19194' WHERE entry IN(18567);
 -- Sporelok - Sporeskin
 UPDATE `creature_template_addon` SET `auras` = '35230' WHERE `entry` = 18912;
 
+-- Felspine the Greater - Fel Fire Aura
+DELETE FROM creature_template_addon WHERE entry=21897;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES
+('21897', '0', '0', '1', '16', '0', '0', '36006');
+
