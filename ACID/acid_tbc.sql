c0db9431421b2d6ed286997eb384c76e30c9b662
anonxs <anonxs@web.de>
2017-10-31 19:17:57 +0100

    [ACID] Nethermine Flayer 23169

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -20957,6 +20957,9 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 ('2285907','22859','3','5','100','0','7','0','0','0','49','0','0','0','22','2','0','0','0','0','0','0','Shadowhoof Summoner - Disable Dynamic Movement and Set Phase 2 when Mana is at 7% (Phase 1)'),
 ('2285908','22859','3','3','100','1','100','15','1000','1000','22','1','0','0','0','0','0','0','0','0','0','0','Shadowhoof Summoner - Set Phase 1 when Mana is above 15% (Phase 2)'),
 ('2285909','22859','7','0','100','0','0','0','0','0','22','0','0','0','0','0','0','0','0','0','0','0','Shadowhoof Summoner - Set Phase to 0 on Evade'),
+-- Nethermine Flayer
+('2316901','23169','9','0','100','1','0','5','15000','20000','11','13443','1','0','0','0','0','0','0','0','0','0','Nethermine Flayer - Cast Rend'),
+('2316902','23169','0','0','100','1','4200','6200','28100','31600','11','40770','1','0','0','0','0','0','0','0','0','0','Nethermine Flayer - Cast Shred Armor'),
 -- Nethermine Burster (23285) - npc_burster_worm
 -- Black Blood of Draenor
 ('2328601','23286','0','0','100','1','8000','14000','18000','25000','11','40818','0','0','0','0','0','0','0','0','0','0','Black Blood of Draenor - Cast Toxic Slime'),
