38965e2f5e73dff3af0ed37e8e6fb927fd860808
anonxs <anonxs@web.de>
2017-10-31 01:35:51 +0100

    [ACID] Corrupted Fire Elemental 21706
    This closes: #325

--- Updates/503_outland_auras.sql
+++ Updates/503_outland_auras.sql
@@ -45,3 +45,9 @@ UPDATE `creature_template_addon` SET `auras` = '35194' WHERE `entry` = 21428;
 -- Wyrmcult Blessed - Resistances
 UPDATE `creature_template_addon` SET `auras` = '37637' WHERE `entry` = 21492;
 
+-- Corrupted Fire Elemental - Shadowform
+DELETE FROM `creature_template_addon` WHERE `entry` = 21706;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES
+('21706', '0', '0', '1', '16', '0', '0', '35194');
+
+
