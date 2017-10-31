3ccd6ea583078d5661797abb5ff1ba9d6b060c3a
anonxs <anonxs@web.de>
2017-10-31 14:26:17 +0100

    [ACID] Felspine the Greater 21897
    This closes: #345

--- Updates/503_outland_auras.sql
+++ Updates/503_outland_auras.sql
@@ -50,4 +50,10 @@ DELETE FROM `creature_template_addon` WHERE `entry` = 21706;
 INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES
 ('21706', '0', '0', '1', '16', '0', '0', '35194');
 
+-- Felspine the Greater - Fel Fire Aura
+DELETE FROM creature_template_addon WHERE entry=21897;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES
+('21897', '0', '0', '1', '16', '0', '0', '36006');
+
+
 
