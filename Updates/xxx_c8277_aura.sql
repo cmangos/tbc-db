bbb42212e0d35a8eeab6323ad62e7aa76d1e39b7
anonxs <anonxs@web.de>
2017-11-01 17:41:02 +0100

    Correct File

--- Updates/xxx_c8277_aura.sql
+++ Updates/xxx_c8277_aura.sql
@@ -1,4 +1,3 @@
--- Remove aura 13787 from Bloodmaul Soothsayer c.22384
--- Now handled in ACID since creature addon is not suitable for auras without infinite duration
-UPDATE creature_template_addon SET auras=NULL WHERE entry=22384;
+-- Rekk'tilac - Deadly Poison
+UPDATE `creature_template_addon` SET `auras` = '10022' WHERE `entry` = 8277;
 
