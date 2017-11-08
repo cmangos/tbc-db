1c74e91e7b39e618307a5c4621dde6af11f73c18
anonxs <anonxs@web.de>
2017-11-08 22:03:41 +0100

    [ACID] Crystal Flayer 21189 NSR

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -18324,6 +18324,7 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Felsworn Daggermaw
 ('2112401','21124','9','0','100','1','8','25','15000','21000','11','35570','1','0','0','0','0','0','0','0','0','0','Felsworn Daggermaw - Cast Charge'),
 ('2112402','21124','0','0','100','1','0','5000','10000','11000','11','7367','1','32','0','0','0','0','0','0','0','0','Felsworn Daggermaw - Cast Infected Bite'),
+-- Crystal Flayer 21189 NSR
 -- Bloodmaul Drudger (Missing combat spells!)
 ('2123801','21238','22','0','100','1','3','8','11000','0','54','-1051','0','0','0','0','0','0','0','0','0','0','Bloodmaul Drudger - Text Emote on Receive TEXTEMOTE_ANGRY if Player has Completed Quest 11000'),
 ('2123802','21238','22','0','100','1','34','8','11000','0','5','94','0','0','0','0','0','0','0','0','0','0','Bloodmaul Drudger - Dance on Receive TEXTEMOTE_DANCE if Player has Completed Quest 11000'),
