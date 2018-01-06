e9f291c6ecc4ffd673a0d71bc34c3903dc4ad716
Grz3s <grz3sp@wp.pl>
2018-01-06 16:33:24 +0000

    [WDB] Update for [1306]
    wrong Fix removed - TARGET_RANDOM_UNIT_CHAIN_IN_AREA
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/fa067aa23342ee0df7d3534e125526d633d92568)

---
+++ Updates/0334_WDB-1306_spell_target.sql
@@ -0,0 +1,9 @@
+/*
+Nightlash hitting players with Spirit Steal
+*/
+
+DELETE FROM spell_script_target WHERE entry = 3477; -- TARGET_RANDOM_UNIT_CHAIN_IN_AREA
+-- INSERT INTO spell_script_target (entry, `type`, targetEntry, inverseEffectMask) VALUES
+-- (3477, 1, 1772, 0),
+-- (3477, 1, 1773, 0);
+
