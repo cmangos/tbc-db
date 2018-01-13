5f3f801a2b5061babbc41637457026a023f39ba9
AnonXS <anonxs@web.de>
2018-01-13 20:44:01 +0100

    [ACID] Infinity Blades 21271 (#553)

---
+++ Updates/849_c21271_aura.sql
@@ -0,0 +1,4 @@
+-- Infinity Blades 21271 - Thrash
+DELETE FROM `creature_template_addon` WHERE `entry` = 21271;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (21271, 0, 0, 1, 16, 0, 0, '12787');
+
