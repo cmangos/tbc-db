6efd76d7828d746f6f584ca24932c61208042f9c
killerwife <killerwife@gmail.com>
2018-01-18 09:58:38 +0100

    Update database with correct Call to arms timers
    This disparity was due to vengeance progression. My bad.

---
+++ Updates/998_progression_adapter_file.sql
@@ -0,0 +1,7 @@
+REPLACE INTO game_event(`entry`, `start_time`, `end_time`, `occurence`, `length`, `holiday`, `description`) VALUES
+('18','2011-01-28 02:00:00','2020-12-31 00:00:00','40320','6240','283','Call to Arms: Alterac Valley!'),
+('19','2011-01-14 02:00:00','2020-12-31 00:00:00','40320','6240','284','Call to Arms: Warsong Gulch!'),
+('20','2011-01-21 02:00:00','2020-12-31 00:00:00','40320','6240','285','Call to Arms: Arathi Basin!'),
+('21','2011-01-07 02:00:00','2020-12-31 00:00:00','40320','6240','353','Call to Arms: Eye of the Storm!');
+
+
