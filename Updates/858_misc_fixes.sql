87aff9dfc140e7857d50e051dbf2c9d78c5ec2e5
killerwife <killerwife@gmail.com>
2018-01-31 13:28:26 +0100

    Fix Improved Fire Totems talent

--- Updates/858_misc_fixes.sql
+++ Updates/858_misc_fixes.sql
@@ -1,4 +1,7 @@
 -- Netherstorm Crystal Target needs to float and be visible to everyone
 UPDATE creature_template SET ExtraFlags=0,InhabitType=4 WHERE entry IN(19421);
 
+-- Remove wrong affect masks in favor of using spell_template ones
+DELETE FROM spell_affect WHERE entry IN(16086,16544);
+
 
