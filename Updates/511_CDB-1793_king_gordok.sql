31aeebe715c71ad68e1d2b9468b7a9533d73812d
Andrew "Levitan" Juicy <juicy@tutanota.com>
2018-02-05 23:50:01 +0700

    Some cleanup related to #586, reference_loot_template and it's unused loot values.

--- Updates/511_CDB-1793_king_gordok.sql
+++ Updates/511_CDB-1793_king_gordok.sql
@@ -1,9 +1,7 @@
+DELETE FROM `reference_loot_template` WHERE `entry`='35019';
 
-DELETE FROM `reference_loot_template` WHERE `entry`='35019' and`item`='18520';
-
-DELETE FROM `creature_loot_template` WHERE `entry`='11501' and`item`='35019';
-
-REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES
+DELETE FROM `creature_loot_template` WHERE `entry`='11501' AND `item`='35019';
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES
 ('11501', '18520', '0', '2', '1', '1', '0'),
 ('11501', '18521', '0', '2', '1', '1', '0'),
 ('11501', '18522', '0', '2', '1', '1', '0'),
