c9780c1bd554f9562b018af7ee7c4014e9b032d1
sneaky <crhansen12@apu.edu>
2018-02-10 18:48:32 +0100

    Export AI for Battle Chicken 8836 and Add commit 518d0888b

--- Updates/862_Battle_Chicken_ai.sql
+++ Updates/862_Battle_Chicken_ai.sql
@@ -1,7 +1,7 @@
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) 
 	VALUES 
-	('883601', '8836', '0', '0', '70', '1', '5000', '10000', '15000', '20000', '11', '13168', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Battle Chicken - Cast Chicken Fury periodically'),
-	('883602', '8836', '0', '2', '18', '1', '8000', '8000', '8000', '8000', '11', '23060', '0', '0', '22', '1', '0', '0', '0', '0', '0', '0', 'Battle Chicken - Cast Battle Squawk, set phase 1');
+	('883601', '8836', '0', '0', '70', '1', '5000', '10000', '15000', '20000', '11', '13168', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Battle Chicken - Cast Chicken Fury'),
+	('883602', '8836', '0', '2', '18', '1', '8000', '8000', '8000', '8000', '11', '23060', '0', '0', '22', '1', '0', '0', '0', '0', '0', '0', 'Battle Chicken - Cast Battle Squawk and Set Phase 1');
 
 UPDATE `creature_template` 
 	SET `AIName`='EventAI' 
