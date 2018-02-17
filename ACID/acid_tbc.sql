034c00a0754b1e76b529d9f54697ccb5120135be
AnonXS <anonxs@web.de>
2018-02-17 22:50:25 +0100

    [ACID] Bladespire Battlemage 19996 (#566)
    * [ACID] Bladespire Battlemage 19996
    
    * Add Entry to Name

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -17942,7 +17942,7 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 ('1999515','19995','22','0','100','1','75','8','11000','0','5','15','0','0','0','0','0','0','0','0','0','0','Bladespire Brute - Roar on Receive TEXTEMOTE_ROAR if Player has Completed Quest 11000'),
 ('1999516','19995','22','0','100','1','101','8','11000','0','5','3','0','0','0','0','0','0','0','0','0','0','Bladespire Brute - Wave on Receive TEXTEMOTE_WAVE if Player has Completed Quest 11000'),
 ('1999517','19995','6','0','100','0','10040','0','0','0','54','0','6','10029','0','0','0','0','0','0','0','0','Bladespire Brute - Say on Death if Killer Player has Completed Quest 11000'),
--- Bladespire Battlemage (Missing combat spells!)
+-- Bladespire Battlemage 19996
 ('1999601','19996','22','0','100','1','3','8','11000','0','54','-1051','0','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Text Emote on Receive TEXTEMOTE_ANGRY if Player has Completed Quest 11000'),
 ('1999602','19996','22','0','100','1','34','8','11000','0','5','94','0','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Dance on Receive TEXTEMOTE_DANCE if Player has Completed Quest 11000'),
 ('1999603','19996','22','0','100','1','39','8','11000','0','54','-1049','6','0','54','-1050','6','0','0','0','0','0','Bladespire Battlemage - Say and Text Emote on Receive TEXTEMOTE_FART if Player has Completed Quest 11000'),
@@ -17951,6 +17951,11 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 ('1999606','19996','22','0','100','1','75','8','11000','0','5','15','0','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Roar on Receive TEXTEMOTE_ROAR if Player has Completed Quest 11000'),
 ('1999607','19996','22','0','100','1','101','8','11000','0','5','3','0','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Wave on Receive TEXTEMOTE_WAVE if Player has Completed Quest 11000'),
 ('1999608','19996','6','0','100','0','10040','0','0','0','54','0','6','10029','0','0','0','0','0','0','0','0','Bladespire Battlemage - Say on Death if Killer Player has Completed Quest 11000'),
+('1999609','19996','4','0','15','0','0','0','0','0','1','-359','-360','-361','0','0','0','0','0','0','0','0','Bladespire Battlemage - Random Say on Aggro'),
+('1999610','19996','4','0','100','0','0','0','0','0','22','1','0','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Set Phase 1 on Aggro'),
+('1999611','19996','16','1','100','1','6742','30','10900','18100','11','6742','6','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Cast Bloodlust on Friendlies (Phase 1)'),
+('1999612','19996','9','1','100','1','0','30','14000','18000','11','16102','1','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Cast Flamestrike (Phase 1)'),
+('1999613','19996','7','0','100','0','0','0','0','0','22','0','0','0','0','0','0','0','0','0','0','0','Bladespire Battlemage - Set Phase 0 on Evade'),
 -- Bladespire Enforcer (Missing combat spells!)
 ('1999701','19997','22','0','100','1','3','8','11000','0','54','-1051','0','0','0','0','0','0','0','0','0','0','Bladespire Enforcer - Text Emote on Receive TEXTEMOTE_ANGRY if Player has Completed Quest 11000'),
 ('1999702','19997','22','0','100','1','34','8','11000','0','5','94','0','0','0','0','0','0','0','0','0','0','Bladespire Enforcer - Dance on Receive TEXTEMOTE_DANCE if Player has Completed Quest 11000'),
