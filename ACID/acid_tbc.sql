d763f7cd98d2843fb18f3d905aec20038e8e5c85
AnonXS <anonxs@web.de>
2018-03-22 20:26:19 +0100

    [ACID] Crazed Murkblood Foreman 23305 (#537)

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -21875,6 +21875,8 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Black Blood of Draenor
 ('2328601','23286','0','0','100','1','8000','14000','18000','25000','11','40818','0','0','0','0','0','0','0','0','0','0','Black Blood of Draenor - Cast Toxic Slime'),
 ('2328602','23286','2','0','100','0','40','0','0','0','11','7279','1','1','25','0','0','0','0','0','0','0','Black Blood of Draenor - Cast Black Sludge and Flee at 40% HP'),
+-- Crazed Murkblood Foreman
+('2330501','23305','2','0','100','0','30','0','0','0','11','40743','0','0','54','-106','0','0','0','0','0','0','Crazed Murkblood Foreman - Cast Enrage at 30% HP'),
 -- Netherwing Ally
 ('2332001','23320','0','0','100','1','1000','2000','6000','8000','11','13901','1','0','0','0','0','0','0','0','0','0','Netherwing Ally - Cast Arcane Bolt'),
 -- Nethermine Ravager 23326
