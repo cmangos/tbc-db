d028281ec2c24fff93ba5f27f6712f1c149ec843
AnonXS <anonxs@web.de>
2018-03-22 17:21:46 +0100

    [ACID] Talonpriest Ishaal 23066 (#383)

--- Updates/503_outland_auras.sql
+++ Updates/503_outland_auras.sql
@@ -67,3 +67,6 @@ UPDATE `creature_template_addon` SET `auras` = '38457' WHERE `entry` = 22137;
 -- Cavern Crawler - Poison Proc
 UPDATE `creature_template_addon` SET `auras` = '3616' WHERE `entry` = 22044;
 
+-- Talonpriest Ishaal - Poison Proc, Skettis Corrupted Ghosts, Ghostly Facade, Shadowform
+UPDATE `creature_template_addon` SET `auras` = '11919 37411 37509 37816' WHERE `entry` = 23066;
+
