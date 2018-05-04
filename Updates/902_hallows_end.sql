11bf271d388e7469f2ec83856b8afd3ce7fcd23f
killerwife <killerwife@gmail.com>
2018-05-04 22:10:17 +0200

    Minor fixes to Hallows End headless horseman daily

---
+++ Updates/902_hallows_end.sql
@@ -0,0 +1,2 @@
+UPDATE quest_template SET OfferRewardText='Will you lay the candle down and call the Headless Horseman from his doomed rest?' WHERE entry IN(11405,11401,11404,11392);
+UPDATE quest_template SET EndText='Return to Loosely Turned Soil in Scarlet Monastery.' WHERE entry IN(11405,11401,11404,11392);
