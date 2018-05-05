059a0d452a9a2d46031bbe375d6bbe022da54140
X-Savior <xsavior@acid.com>
2018-05-05 23:05:31 +0200

    Anzu c.23035 Leashing to Room Impelementation

---
+++ Updates/095_Anzu_Leashing.sql
@@ -0,0 +1,3 @@
+-- 2.1.2: Heroic Mode: Anzu, the Raven God may no longer be pulled far away from the location where he is summoned. 
+UPDATE creature_template SET LeashRange=80 WHERE entry IN (23035);
+
