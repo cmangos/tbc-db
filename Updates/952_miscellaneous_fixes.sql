6307a76348e3093bd8900eb09f5362f5de97272b
anonxs <anonxs@web.de>
2018-05-19 19:28:49 +0200

    UnitFlags for Bruisers and CLS Fix for Gadgetzan Sniper 21448

--- Updates/952_miscellaneous_fixes.sql
+++ Updates/952_miscellaneous_fixes.sql
@@ -9,3 +9,15 @@ INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `t
 -- Doomsaw 21119 summoned by 16944
 UPDATE `creature_template` SET `UnitFlags` = 33554432 WHERE `entry` = 21119;
 
+-- Booty Bay Bruiser 4624
+UPDATE `creature_template` SET `Scale` = 1.05, `UnitFlags` = 32768 WHERE `entry` = 4624;
+
+-- Ratchet Bruiser 3502, Gadgetzan Bruiser 9460, Everlook Bruiser 11190
+UPDATE `creature_template` SET `UnitFlags` = 32768 WHERE `entry` IN (3502, 9460, 11190);
+
+-- Gadgetzan Sniper 21448
+UPDATE `creature_template` SET `UnitFlags` = 32768, `MinLevel` = 67, `MaxLevel` = 67, `MinLevelHealth` = 3914, `MaxLevelHealth` = 3914 WHERE `entry` = 21448;
+
+-- Area 52 Big Bruiser 20484, Area 52 Bruiser 20485
+UPDATE `creature_template` SET `UnitFlags` = `UnitFlags`|32768 WHERE `entry` IN (20484, 20485);
+
