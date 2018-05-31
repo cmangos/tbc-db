0249484356c30c35c4857f93a59918a214129128
AnonXS <anonxs@web.de>
2018-05-31 20:35:24 +0200

    [ACID] Port Auras from ACID to creature_(template)addon & Cleanup (#585)

--- Updates/882_c24777_c25571_aura.sql
+++
@@ -1,6 +0,0 @@
--- Sunblade Sentinel 24777,25571
-DELETE FROM `creature_template_addon` WHERE `entry` IN (24777,25571);
-INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES
-(24777,0,0,1,16,0,0,'44537'),
-(25571,0,0,1,16,0,0,'46048');
-
