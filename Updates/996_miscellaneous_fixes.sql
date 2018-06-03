91a5ac1abf205eb662b3abea7fd320a8a3651191
anonxs <anonxs@web.de>
2018-06-04 00:31:21 +0200

    Correct Path for Emissary of Hate 25003 & Correct Values for Flame Wave II 22228

--- Updates/996_miscellaneous_fixes.sql
+++ Updates/996_miscellaneous_fixes.sql
@@ -124,3 +124,16 @@ INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `posit
 (23679, 11, -3040.596, -4358.319, 8.194913, 100, 0, 0),
 (23679, 12, -3047.94, -4343.792, 8.069913, 100, 0, 0),
 (23679, 13, -3050.214, -4329.639, 8.156482, 100, 0, 0);
+
+-- Emissary of Hate 25003
+UPDATE `creature` SET `position_x` = 12583.32, `position_y` = -6916.28, `position_z` = 4.685531, `orientation` = 6.260564, `MovementType` = 2 WHERE `guid` = 52022 AND `id` = 25003;
+DELETE FROM `creature_movement` WHERE `id` = 52022;
+DELETE FROM `creature_movement_template` WHERE `entry` = 25003;
+INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `script_id`) VALUES
+(25003, 1, 12612.62, -6916.943, 4.725943, 100, 0, 0),
+(25003, 2, 12639.27, -6917.027, 4.725943, 100, 8000, 0),
+(25003, 3, 12612.62, -6916.943, 4.725943, 100, 0, 0),
+(25003, 4, 12589.8, -6916, 4.726335, 6.260564, 10000, 0);
+
+-- Flame Wave II 22228
+UPDATE `creature_template` SET `MinLevel` = 68, `MaxLevel` = 69, `UnitFlags` = 33554432, `SpeedRun` = (16 / 7), `SpeedWalk` = (2.5 / 2.5) WHERE `entry` = 22228;
