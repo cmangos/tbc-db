20851d18af39342bfb9c69e2f0eddbbdbaa43413
Saltgurka <saltgurkasalt@gmail.com>
2018-06-10 19:13:28 +0200

    Fix relay script ID collision in file 894

--- Updates/894_q11028_completescript.sql
+++ Updates/894_q11028_completescript.sql
@@ -9,10 +9,9 @@ INSERT INTO `dbscript_string` (`entry`, `content_default`, `sound`, `type`, `lan
 
 DELETE FROM `dbscripts_on_quest_end` WHERE `id` IN(11028);
 INSERT INTO `dbscripts_on_quest_end` (`id`,`delay`,`command`,`datalong`,`datalong2`,`datalong3`,`dataint`,`dataint2`,`dataint3`,`dataint4`,`buddy_entry`,`search_radius`,`data_flags`,`comments`,`x`,`y`,`z`,`o`) VALUES
-(11028,0,45,10069,0,0,0,0,0,0,23306,10,0,'Sky Commander Adaris - Start relay script with Hazzik as source and Player as target',0,0,0,0),
+(11028,0,45,10089,0,0,0,0,0,0,23306,10,0,'Sky Commander Adaris - Start relay script with Hazzik as source and Player as target',0,0,0,0),
 (11028,4,0,0,0,0,2000001455,0,0,0,0,0,0,'Sky Commander Adaris - Do not... bring Terokk into our world, $n...  That would be... madness...',0,0,0,0);
 
-DELETE FROM `dbscripts_on_relay` WHERE `id` IN(10069);
 INSERT INTO `dbscripts_on_relay` (`id`,`delay`,`command`,`datalong`,`datalong2`,`datalong3`,`dataint`,`dataint2`,`dataint3`,`dataint4`,`buddy_entry`,`search_radius`,`data_flags`,`comments`,`x`,`y`,`z`,`o`) VALUES
-(10069,0,0,0,0,0,2000001454,0,0,0,0,0,0,'Hazzik - You over there!  I must speak with you!',0,0,0,0);
+(10089,0,0,0,0,0,2000001454,0,0,0,0,0,0,'Hazzik - You over there!  I must speak with you!',0,0,0,0);
 
