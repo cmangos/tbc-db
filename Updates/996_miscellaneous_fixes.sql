240e81fbfe601f300b97290952f5ac9a9ca5e21a
anonxs <anonxs@web.de>
2018-06-13 17:58:32 +0200

    Gnomeregan - Supercharge 10732, Rebuild 10345 spell_script_target

--- Updates/996_miscellaneous_fixes.sql
+++ Updates/996_miscellaneous_fixes.sql
@@ -279,3 +279,12 @@ UPDATE game_event SET start_time='2011-10-01 00:00:00', end_time='2020-12-30 23:
 UPDATE game_event SET start_time='2011-11-01 00:00:00', end_time='2020-12-30 23:00:00' WHERE entry=74; -- Brew of the Month - November
 UPDATE game_event SET start_time='2011-12-01 00:00:00', end_time='2020-12-30 23:00:00' WHERE entry=75; -- Brew of the Month - December
 
+-- Gnomeregan - Supercharge 10732, Rebuild 10345 Targets (same as 10348)
+DELETE FROM `spell_script_target` WHERE `entry` IN (10732,10345) AND `targetEntry` IN (6225,6226,6227);
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `inverseEffectMask`) VALUES
+(10732, 1, 6225, 0), -- Mechano-Tank
+(10345, 1, 6225, 0), -- Mechano-Tank
+(10732, 1, 6226, 0), -- Mechano-Flamewalker
+(10345, 1, 6226, 0), -- Mechano-Flamewalker
+(10732, 1, 6227, 0), -- Mechano-Frostwalker
+(10345, 1, 6227, 0); -- Mechano-Frostwalker
