1f856c2856e175d820ae4860aba7a12a6776864f
anonxs <anonxs@web.de>
2018-06-17 01:32:17 +0200

    Add MECHANIC_DISORIENTED & MECHANIC_KNOCKOUT Immunity for Pusillin 14354

--- Updates/996_miscellaneous_fixes.sql
+++ Updates/996_miscellaneous_fixes.sql
@@ -318,3 +318,6 @@ INSERT INTO `creature_template_addon` (`entry`, `bytes1`) VALUES (21952,3);
 
 -- Doomsaw 21119
 UPDATE `creature_template` SET `SpeedWalk` = (7 / 2.5), `SpeedRun` = (10 / 7) WHERE `entry` = 21119;
+
+-- MECHANIC_DISORIENTED & MECHANIC_KNOCKOUT Immunity for Pusillin 14354
+UPDATE creature_template SET MechanicImmuneMask=MechanicImmuneMask|2|8192 WHERE entry IN (14354);
