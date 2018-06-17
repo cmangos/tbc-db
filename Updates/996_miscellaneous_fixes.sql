ae537036bebec864d96299ba924b64003be15bb1
anonxs <anonxs@web.de>
2018-06-17 12:17:05 +0200

    Improvements for The Demoniac Scryer q.10838

--- Updates/996_miscellaneous_fixes.sql
+++ Updates/996_miscellaneous_fixes.sql
@@ -323,3 +323,7 @@ UPDATE `creature_template` SET `SpeedWalk` = (7 / 2.5), `SpeedRun` = (10 / 7) WH
 UPDATE creature_template SET MechanicImmuneMask=MechanicImmuneMask|2|8192 WHERE entry IN (14354);
 
 UPDATE `item_template` SET `maxcount` = 0 WHERE `entry` = 32589; -- Hellfire-Encased Pendant 32589
+
+-- Improvements for The Demoniac Scryer q.10838
+UPDATE `creature_template` SET `RegenerateStats` = 0, `MovementType` = 0 WHERE `entry` = 22258; -- Demoniac Scryer 22258
+UPDATE `creature_template` SET `UnitFlags` = 33554432 WHERE `entry` = 22267; -- Magic Sucker Device Buttress 22267
