a3743e6bb951f9c3fcc935d2f3c9949360f113ff
anonxs <anonxs@web.de>
2018-06-17 23:35:39 +0200

    Adjust UnitFlags, ModelIds for Fel Eye Stalk 23323

--- Updates/996_miscellaneous_fixes.sql
+++ Updates/996_miscellaneous_fixes.sql
@@ -330,3 +330,6 @@ UPDATE `creature_template` SET `UnitFlags` = 33554432 WHERE `entry` = 22267; -- 
 
 -- Dancing Sword 21093
 UPDATE `creature_template` SET `FactionAlliance` = 1811, `FactionHorde` = 1811, `UnitFlags` = 33554432, `ExtraFlags` = 524288, `MeleeBaseAttackTime` = 1700 WHERE `entry` = 21093;
+
+-- Fel Eye Stalk 23323
+UPDATE `creature_template` SET `UnitFlags` = 33554688, `ModelId1` = 16480, `ModelId2` = 19595 WHERE `entry` = 23323;
