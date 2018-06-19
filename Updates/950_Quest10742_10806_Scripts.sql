6f692aca95ebb15e289eb9c1e6b50dd344ad946e
Saltgurka <saltgurkasalt@gmail.com>
2018-06-19 22:58:29 +0200

    Fix content_locX displaying "0" as text
    Thanks @RazzorX for noticing https://github.com/cmangos/wotlk-db/commit/1342d2f9464ff7a33369926409f86d5142e79491#commitcomment-29392652

--- Updates/950_Quest10742_10806_Scripts.sql
+++ Updates/950_Quest10742_10806_Scripts.sql
@@ -171,17 +171,17 @@ INSERT INTO `dbscripts_on_creature_death` (`id`,`delay`,`command`,`datalong`,`da
 
 DELETE FROM `dbscript_string` WHERE `entry` BETWEEN 2000005898 AND 2000005908;
 INSERT INTO `dbscript_string` (`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
-(2000005898,'Who dares rouse Goc from his restful slumber?',0,0,0,0,0,0,0,0,0,1,0,53,'Goc yell 1'),
-(2000005899,'It''s only right that you know the name of the one who will take your life.',0,0,0,0,0,0,0,0,0,1,0,397,'Baron yell 1'),
-(2000005900,'Baron Sablemane. It will be on your lips as you gasp your dying breath.',0,0,0,0,0,0,0,0,0,1,0,153,'Baron yell 2'),
-(2000005901,'Your name is as insignificant to me as the names of the thousands who have died under the might of Goc. I will crush you and $N!',0,0,0,0,0,0,0,0,0,1,0,53,'Goc yell 2'),
-(2000005902,'Let us begin.',0,0,0,0,0,0,0,0,0,1,0,15,'Baron yell 3'),
-(2000005903,'We will speak again, but away from here.',0,0,0,0,0,0,0,0,0,1,0,396,'Baron yell 4'),
+(2000005898,'Who dares rouse Goc from his restful slumber?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,53,'Goc yell 1'),
+(2000005899,'It''s only right that you know the name of the one who will take your life.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,397,'Baron yell 1'),
+(2000005900,'Baron Sablemane. It will be on your lips as you gasp your dying breath.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,153,'Baron yell 2'),
+(2000005901,'Your name is as insignificant to me as the names of the thousands who have died under the might of Goc. I will crush you and $N!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,53,'Goc yell 2'),
+(2000005902,'Let us begin.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,15,'Baron yell 3'),
+(2000005903,'We will speak again, but away from here.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,396,'Baron yell 4'),
 
-(2000005904,'I am Rexxar, son of the Mok''Nathal, champion of the Horde.',0,0,0,0,0,0,0,0,0,1,0,397,'Rexxar yell 1'),
-(2000005905,'And their torment at your hands is at an end. By my name, I shall put an end to your life.',0,0,0,0,0,0,0,0,0,1,0,113,'Rexxar yell 2'),
-(2000005906,'Prepare yourself for the end.',0,0,0,0,0,0,0,0,0,1,0,15,'Rexxar yell 3'),
-(2000005907,'I could not have achieved this victory without you. We will speak back at Thunderlord Stronghold.',0,0,0,0,0,0,0,0,0,1,0,396,'Rexxar yell 4'),
-(2000005908,'Who dares place this affront upon the altar of Goc?',0,0,0,0,0,0,0,0,0,1,0,53,'Goc yell 0');
+(2000005904,'I am Rexxar, son of the Mok''Nathal, champion of the Horde.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,397,'Rexxar yell 1'),
+(2000005905,'And their torment at your hands is at an end. By my name, I shall put an end to your life.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,113,'Rexxar yell 2'),
+(2000005906,'Prepare yourself for the end.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,15,'Rexxar yell 3'),
+(2000005907,'I could not have achieved this victory without you. We will speak back at Thunderlord Stronghold.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,396,'Rexxar yell 4'),
+(2000005908,'Who dares place this affront upon the altar of Goc?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,53,'Goc yell 0');
 
 
