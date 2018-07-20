81d09e93da3f2a7dab22282e6b4cb4b17a168a82
Killerwife <killerwife@gmail.com>
2018-07-21 00:07:12 +0200

    Synchronization of loot tables with vengeance
    Merge of diff that was lost before our time. Gets rid of all loot errors.
    Credit for changes goes to whole tbc-db team old and new, credit for merging goes to @Killerwife and @Saltgurka

---
+++ Updates/9998_ItemLootExport.sql
@@ -0,0 +1,3 @@
+INSERT INTO item_loot_template(entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount, condition_id, comments) VALUES
+(15902, 44000, 100, 0, -44000, 1, 0, 'A Crazy Grab Bag'),
+(20767, 44001, 100, 0, -44001, 1, 0, 'Scum Covered Bag');
