87d7ba9d01adef1c0541fd9f8d94b871f6638a27
Tobschinski <tobschinski@gmail.com>
2018-08-05 18:24:10 -0400

    CDB-2021 Naxxramas - Anub'Rekhan
    Source: https://github.com/cmangos/classic-db/commit/c62560c259a740979b43f488a8c04d32f652aa47

---
+++ Updates/0002_CDB-2021_anub_trash.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_template` SET `speedwalk`=(2.5 / 2.5), `speedrun`=(10.0 / 7.0), `scale`='0' WHERE `entry`='15974';
+UPDATE `creature_template` SET `speedwalk`=(2.5 / 2.5), `speedrun`=(10.0 / 7.0), `scale`='0' WHERE `entry`='15975';
+UPDATE `creature_template` SET `speedwalk`=(2.5 / 2.5), `speedrun`=(10.0 / 7.0), `scale`='0' WHERE `entry`='15976';
+UPDATE `creature_template` SET `speedwalk`=(2.5 / 2.5), `speedrun`=(10.0 / 7.0), `scale`='0' WHERE `entry`='15977';
+
+
