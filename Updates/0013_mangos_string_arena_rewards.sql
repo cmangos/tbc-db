66ba8cd1d0a6811e7017d0bc2a11881f4ba968e5
Killerwife <killerwife@gmail.com>
2018-08-06 01:23:13 +0200

    Add mangos_string entries from core

---
+++ Updates/0013_mangos_string_arena_rewards.sql
@@ -0,0 +1,5 @@
+DELETE FROM mangos_string WHERE entry IN(747,748);
+INSERT INTO mangos_string VALUES
+(747,'Awarding Arena Season rewards for Season %u.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(748,'Awarded Arena Season rewards and wiped old stats.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
