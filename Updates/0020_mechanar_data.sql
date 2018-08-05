62c8699d3f8ffcfdc6993030d06d9201ae3b356a
anonxs <anonxs@web.de>
2018-08-06 01:38:54 +0200

    Fix Speed for Raging Flames 20481,21538

---
+++ Updates/0020_mechanar_data.sql
@@ -0,0 +1,4 @@
+-- Raging Flames 20481,21538
+UPDATE `creature_template` SET `SpeedWalk` = (4 / 2.5), `SpeedRun` = (4 / 7) WHERE `entry` = 20481;
+UPDATE `creature_template` SET `SpeedWalk` = (4 / 2.5), `SpeedRun` = (4.75 / 7) WHERE `entry` = 21538;
+
