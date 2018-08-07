93c53056cbd53862b41877f65573e270a083b7ed
anonxs <anonxs@web.de>
2018-08-07 23:24:55 +0200

    [ZulAman] Amani Healing Ward 23757 & Amani Protective Ward 23822 - TotemAI

---
+++ Updates/0046_c23757,23822_TotemAI.sql
@@ -0,0 +1,6 @@
+-- s.42376 Healing Ward - Amani Healing Ward 23757
+UPDATE `creature_template` SET `AIName` = 'TotemAI' WHERE `entry` = 23757;
+
+-- s.42478 Protective Ward -- Amani Protective Ward 23822
+UPDATE `creature_template` SET `AIName` = 'TotemAI' WHERE `entry` = 23822;
+
