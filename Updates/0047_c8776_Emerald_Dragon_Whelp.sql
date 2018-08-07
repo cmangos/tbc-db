3d89406932356e0c8762aa1e73e541ad8b4f87a9
anonxs <anonxs@web.de>
2018-08-07 23:25:48 +0200

    Correct Stats for Emerald Dragon Whelp 8776

---
+++ Updates/0047_c8776_Emerald_Dragon_Whelp.sql
@@ -0,0 +1,4 @@
+-- Emerald Dragon Whelp 8776
+UPDATE `creature_template` SET `HealthMultiplier` = 1.3, `SpeedWalk` = (2.77778 / 2.5), `SpeedRun` = (8 / 7), `UnitClass` = 8 WHERE `entry` = 8776;
+UPDATE `creature_model_info` SET `bounding_radius` = 0.278, `combat_reach` = 1.25 WHERE `modelid` = 694;
+
