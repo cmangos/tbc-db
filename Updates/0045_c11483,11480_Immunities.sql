83e68b2db3b941f271cfbdb1b9cf5995ce6f9c00
anonxs <anonxs@web.de>
2018-08-07 23:24:00 +0200

    Add Arcane School Immunity for Mana Remnant 11483 & Arcane Aberration 11480

---
+++ Updates/0045_c11483,11480_Immunities.sql
@@ -0,0 +1,6 @@
+-- Arcane Aberration 11480 - http://www.wowhead.com/npc=11480/arcane-aberration#comments
+UPDATE `creature_template` SET `SchoolImmuneMask` = 64 WHERE `entry` = 11480;
+
+-- Mana Remnant 11483 - http://www.wowhead.com/npc=11483/mana-remnant#comments
+UPDATE `creature_template` SET `SchoolImmuneMask` = 64 WHERE `entry` = 11483;
+
