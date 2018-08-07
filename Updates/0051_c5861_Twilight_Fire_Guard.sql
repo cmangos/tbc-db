2e17474ff78829199b5131b868be0d8dc02428ea
anonxs <anonxs@web.de>
2018-08-07 23:39:54 +0200

    Correct UnitClass for Twilight Fire Guard 5861 thx @ZenoX92

---
+++ Updates/0051_c5861_Twilight_Fire_Guard.sql
@@ -0,0 +1,4 @@
+-- Twilight Fire Guard 5861 - http://www.wowhead.com/npc=5861/twilight-fire-guard#comments:id=70451
+-- https://github.com/TrinityCore/TrinityCore/commit/c22764c18f5e6244c465ea49c5a2daa5d1e8e308#diff-8f08cb7d45c296b9c4ea498dce5cbe7aR79 thx @ZenoX92
+UPDATE `creature_template` SET `UnitClass` = 2 WHERE `entry` = 5861;
+
