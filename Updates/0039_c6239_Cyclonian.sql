10f8bfcf46f69b9aff84a0a7e140a1646286d423
anonxs <anonxs@web.de>
2018-08-07 23:11:57 +0200

    Add Stun Immunity and Fix DamageSchool for Cyclonian 6239

---
+++ Updates/0039_c6239_Cyclonian.sql
@@ -0,0 +1,3 @@
+-- Cyclonian 6239 - http://www.wowhead.com/npc=6239/cyclonian#comments:id=350733
+UPDATE `creature_template` SET `DamageSchool` = 3, `MechanicImmuneMask` =  `MechanicImmuneMask`|2048 WHERE `entry` = 6239;
+
