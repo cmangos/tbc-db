df34491b23795df180ffa5bbb80f05f5ba7765df
anonxs <anonxs@web.de>
2018-08-07 23:15:07 +0200

    Add SchoolImmuneMask for Minor Manifestation of Earth 5891, Minor Manifestation of Water 5895, Minor Manifestation of Air 5902

---
+++ Updates/0041_c5891,5893,5895,5902_Immunities.sql
@@ -0,0 +1,12 @@
+-- Minor Manifestation of Earth 5891 - no explicit source but likely
+UPDATE `creature_template` SET `SchoolImmuneMask` = 8 WHERE `entry` = 5891;
+
+-- Minor Manifestation of Fire 5893 - http://www.wowhead.com/npc=5893/minor-manifestation-of-fire#comments:id=387160
+UPDATE `creature_template` SET `SchoolImmuneMask` = 4 WHERE `entry` = 5893;
+
+-- Minor Manifestation of Water 5895 - http://www.wowhead.com/npc=5895/minor-manifestation-of-water#comments:id=1063322
+UPDATE `creature_template` SET `SchoolImmuneMask` = 16 WHERE `entry` = 5895;
+
+-- Minor Manifestation of Air 5902 - no explicit source but likely
+UPDATE `creature_template` SET `SchoolImmuneMask` = 64 WHERE `entry` = 5902;
+
