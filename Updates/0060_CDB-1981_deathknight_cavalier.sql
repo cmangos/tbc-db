f6d2a617fcaa1b572c884569317e7b5115d4ff3d
cala <cala@users.noreply.github.com>
2018-08-09 21:02:04 +0200

    CDB-1981 Added equipment for NPC 16163
    * Added weapon for NPC 16163 (Deathknight Cavalier) in Naxxramas
    Thanks @Purownage for reporting.
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/7da81d37254c78c6e3be19d57d5a9e02715354d5)

---
+++ Updates/0060_CDB-1981_deathknight_cavalier.sql
@@ -0,0 +1,4 @@
+-- Added weapon for Deathknight Cavalier
+DELETE FROM creature_equip_template WHERE entry=16163;
+INSERT INTO creature_equip_template (entry, equipentry1) VALUES (16163, 23328);
+UPDATE creature_template SET EquipmentTemplateId=16163 WHERE Entry=16163;
