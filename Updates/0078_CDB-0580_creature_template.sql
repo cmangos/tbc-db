3b417aa67294a3517edd29193d7b0107d564470c
Efonius <Efonius@users.noreply.github.com>
2018-08-11 22:57:05 +0200

    CDB-0580 Fixed model of creature 16119
    * Fixed modelid of creature 16119 (Bone Minion)
    Source: http://www.wowhead.com/npc=16119/bone-minion#screenshots:id=119558
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/f9fe315a3eae5877747eb59d2e6f04e022f4a852)

---
+++ Updates/0078_CDB-0580_creature_template.sql
@@ -0,0 +1,3 @@
+-- Fixed modelid of creature 16119 (Bone Minion)
+-- Source: http://www.wowhead.com/npc=16119/bone-minion#screenshots:id=119558
+UPDATE `creature_template` SET `ModelId1` = 158 WHERE `Entry` = 16119;
