ec242ca755c5e420e2588e44fcfe76b08556e40b
Saltgurka <saltgurkasalt@gmail.com>
2018-08-26 16:57:09 +0200

    Fix UnitFlags for c.23040 and c.23081
    Vim'gol's Circle Bunny
    Vim'gol's Circle Summon Visual Bunny

---
+++ Updates/0140_c23040_c23081.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET UnitFlags=33554432 WHERE entry=23040; -- Vim'gol's Circle Bunny (Sniffed)
+UPDATE creature_template SET UnitFlags=33554432 WHERE entry=23081; -- Vim'gol's Circle Summon Visual Bunny (Guesswork, trigger does not appear in sniffs so use the same flag as 23040)
