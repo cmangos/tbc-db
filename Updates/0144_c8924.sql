9aa628097f41270b0bbe2d142c3e0fbf16c1e460
anonxs <anonxs@web.de>
2018-08-28 18:58:32 +0200

    Add missing Spawn for The Behemoth 8924

---
+++ Updates/0144_c8924.sql
@@ -0,0 +1,5 @@
+-- Add missing Spawn for The Behemoth 8924
+DELETE FROM `creature` WHERE `guid` = 191108 AND `id` = 8924;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `MovementType`) VALUES
+(191108, 8924, 0, 1, -7404.57, -894.073, 171.873, 4.7112, 172800, 259200, 0, 2);
+
