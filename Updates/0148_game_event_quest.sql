5e7e4b52dfc14eb24ec96639ac23872f05b46a40
MantisLord <sabinprosper@gmail.com>
2018-08-29 00:35:31 -0400

    Add missing game_event_quest data
    Source: Classic-DB

---
+++ Updates/0148_game_event_quest.sql
@@ -0,0 +1,58 @@
+DELETE FROM game_event_quest WHERE quest IN (6961,6962,6963,6964,6983,6984,7021,7022,7023,7024,7025,7042,7043,7045,7061,7062,7063,8149,8150,8744,8746,8762,8763,8767,8768,8769,8788,8795,8799,8803,8827,8828,9319,9322,9323,9324,9325,9326,9330,9331,9332,9339,9365,9368,9386);
+INSERT INTO game_event_quest (quest, event) VALUES
+-- Fire Festival
+(9319, 1), -- A Light in Dark Places
+(9322, 1), -- Wild Fires in Kalimdor
+(9323, 1), -- Wild Fires in the Eastern Kingdoms
+(9324, 1), -- Stealing Orgrimmar's Flame
+(9325, 1), -- Stealing Thunder Bluff's Flame
+(9326, 1), -- Stealing the Undercity's Flame
+(9330, 1), -- Stealing Stormwind's Flame
+(9331, 1), -- Stealing Ironforge's Flame
+(9332, 1), -- Stealing Darnassus's Flame
+(9339, 1), -- A Thief's Reward
+(9365, 1), -- A Thief's Reward
+(9368, 1), -- The Festival of Fire
+(9386, 1), -- A Light in Dark Places
+
+-- Winter Veil
+(6961, 2), -- Great-father Winter is Here!
+(6962, 2), -- Treats for Great-father Winter
+(6963, 2), -- Stolen Winter Veil Treats
+(6964, 2), -- The Reason for the Season
+(6983, 2), -- You're a Mean One...
+(6984, 2), -- A Smokywood Pastures' Thank You!
+(7021, 2), -- Great-father Winter is Here!
+(7022, 2), -- Greatfather Winter is Here!
+(7023, 2), -- Greatfather Winter is Here!
+(7024, 2), -- Great-father Winter is Here!
+(7025, 2), -- Treats for Greatfather Winter
+(7042, 2), -- Stolen Winter Veil Treats
+(7043, 2), -- You're a Mean One...
+(7045, 2), -- A Smokywood Pastures' Thank You!
+(7061, 2), -- The Feast of Winter Veil
+(7062, 2), -- The Reason for the Season
+(7063, 2), -- The Feast of Winter Veil
+(8744, 2), -- A Carefully Wrapped Present
+(8746, 2), -- Metzen the Reindeer
+(8762, 2), -- Metzen the Reindeer
+(8763, 2), -- The Hero of the Day
+(8767, 2), -- A Gently Shaken Gift
+(8768, 2), -- A Gaily Wrapped Present
+(8769, 2), -- A Ticking Present
+(8788, 2), -- A Gently Shaken Gift
+(8799, 2), -- The Hero of the Day
+(8803, 2), -- A Festive Gift
+
+-- Winter Veil - Gifts
+(8827, 9), -- Winter's Presents (Classic-DB event ID 21 = TBC-DB event ID 9)
+(8828, 9), -- Winter's Presents (Classic-DB event ID 21 = TBC-DB event ID 9)
+
+-- Harvest Festival
+(8149, 11), -- Honoring a Hero (Classic-DB event ID 33 = TBC-DB event ID 11)
+(8150, 11), -- Honoring a Hero (Classic-DB event ID 33 = TBC-DB event ID 11)
+
+-- AQ War Effort
+(8795, 22); -- The Alliance Needs Your Help!
+
+
