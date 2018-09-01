0a3350c3261f4245c812b4b41b2780fe7d327eb4
killerwife <killerwife@gmail.com>
2018-09-01 14:21:45 +0200

    Add missing invisibility auras to two mobs
    * c.20619 Ethereum Relay
    * c.18688 Ancient Orc Ancestor

---
+++ Updates/0166_mob_invisibility_fixes.sql
@@ -0,0 +1,5 @@
+-- invisibility auras added
+REPLACE INTO creature_template_addon(entry,b2_0_sheath,auras) VALUES
+(20619,0,'16592 39596'), -- Ethereum Relay
+(18688,1,'32648'); -- Ancient Orc Ancestor
+
