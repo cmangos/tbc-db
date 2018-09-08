5c4e713ae23b8a5b5f2d64822b17533657f36992
anonxs <anonxs@web.de>
2018-09-08 20:51:29 +0200

    Correct Health for Archbishop Benedictus 1284

---
+++ Updates/0186_c1284.sql
@@ -0,0 +1,3 @@
+-- Correct Health for Archbishop Benedictus 1284 - 01/28/2008 15:02:24.000
+UPDATE `creature_template` SET `HealthMultiplier` = 200, `MinLevelHealth` = 1062200, `MaxLevelHealth` = 1062200 WHERE `entry` = 1284;
+
