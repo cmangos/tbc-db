3302494064fcf80b2424c847cb751e08ac0bacad
anonxs <anonxs@web.de>
2018-09-11 20:01:12 +0200

    Correct UnitFlags for Coilfang Engineer 17721,20620, Coilfang Siren 17801,20623, Coilfang Warrior 17802,20626, Coilfang Oracle 17803,20622

---
+++ Updates/0192_c17721_17801_17802_17803.sql
@@ -0,0 +1,16 @@
+-- Coilfang Engineer 17721,20620
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 17721;
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 20620;
+
+-- Coilfang Siren 17801,20623
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 17801;
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 20623;
+
+-- Coilfang Warrior 17802,20626
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 17802;
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 20626;
+
+-- Coilfang Oracle 17803,20622
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 17803;
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 20622;
+
