a8a59317d4a61b780d9cb0a0b9747330e9ed9677
anonxs <anonxs@web.de>
2018-09-15 18:47:09 +0200

    Fix UnitFlags for Defender Corpse 21303,21592 & Warder Corpse 21304,21623

---
+++ Updates/0197_c21303,21592_21304,21623.sql
@@ -0,0 +1,7 @@
+-- Defender Corpse 21303,21592
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 21303;
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 21592;
+-- Warder Corpse 21304,21623
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 21304;
+UPDATE `creature_template` SET `UnitFlags` = 32832 WHERE `entry` = 21623;
+
