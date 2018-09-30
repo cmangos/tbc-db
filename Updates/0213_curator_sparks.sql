42225301352c61f3aa80b345bfa893d935ac0d5b
Killerwife <killerwife@gmail.com>
2018-09-30 10:06:02 +0200

    [ACID] Add data and unify logic for all curator sparks
    The passive is done in C++ as well as melee prevention.

---
+++ Updates/0213_curator_sparks.sql
@@ -0,0 +1,4 @@
+-- data and unification for all curator sparks
+UPDATE creature_template SET UnitFlags=33554432,ExtraFlags=4718848,SpeedWalk=8/2.5,SpeedRun=8/7,MechanicImmuneMask=42090322 WHERE entry IN(17096,19781,19782,19783);
+
+
