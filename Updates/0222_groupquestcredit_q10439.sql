c82f3de17ec538a8c54ec0c10ae6af5a6e20d55d
anonxs <anonxs@web.de>
2018-10-07 15:54:56 +0200

    Set Group Quest Credit on Gossip for Dimensius the All-Devouring 10439 - resolves #615

---
+++ Updates/0222_groupquestcredit_q10439.sql
@@ -0,0 +1,3 @@
+-- Set Group Quest Credit on Gossip for Dimensius the All-Devouring 10439 - [TBC-161] https://github.com/cmangos/tbc-db/pull/615
+UPDATE `dbscripts_on_gossip` SET `datalong2` = 1 WHERE `id` = 822801 AND `command` = 8;
+
