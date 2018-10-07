b62729d0565d9a4d5d806ed91ea9a97d09cf72cb
anonxs <anonxs@web.de>
2018-10-07 17:18:04 +0200

    Set Default InhabitType to Ground for Enslaved Netherwing Drake 21722 - resolves #611

---
+++ Updates/0223_c21722.sql
@@ -0,0 +1,3 @@
+-- Set Default InhabitType to Ground for Enslaved Netherwing Drake 21722 - Flight will be enabled in mob_enslaved_netherwing_drake
+UPDATE `creature_template` SET InhabitType = 1 WHERE  `entry` = 21722;
+
