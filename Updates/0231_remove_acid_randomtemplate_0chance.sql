4c2b1f094b91b92e2cb6ab2935ee17d3b043cc60
anonxs <anonxs@web.de>
2018-10-14 20:35:39 +0200

    Remove ACID used dbscript_random_templates WHERE target_id >= 0

---
+++ Updates/0231_remove_acid_randomtemplate_0chance.sql
@@ -0,0 +1,3 @@
+-- Should be done in ACID if possible - https://github.com/cmangos/tbc-db/commit/96701d06c500ff942f8f897a73f819201f2eb265
+DELETE FROM `dbscript_random_templates` WHERE `id` = 10049 AND `target_id` = 0;
+
