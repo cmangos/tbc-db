e51bf3678c125c9fac5d9a7458925734349e6ee2
anonxs <anonxs@web.de>
2018-10-28 16:50:40 +0100

    Fix Faction and UnitFlags for Avatar of the Martyred 18478,20303

---
+++ Updates/0243_c18478.sql
@@ -0,0 +1,4 @@
+-- Avatar of the Martyred 18478,20303
+UPDATE `creature_template` SET `FactionAlliance` = 16, `FactionHorde` = 16, `UnitFlags` = 32832 WHERE `entry` = 18478;
+UPDATE `creature_template` SET `FactionAlliance` = 16, `FactionHorde` = 16, `UnitFlags` = 32832 WHERE `entry` = 20303;
+
