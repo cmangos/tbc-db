0550eabd0e682ac3ef1996907411e8b29a6445de
anonxs <anonxs@web.de>
2018-10-28 21:34:14 +0100

    Add missing Double Attack 18943 for Scryer 10664 & Gordawg 18943

---
+++ Updates/0245_s.18943.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template_addon` SET `auras` = '8279 18943 34304' WHERE `entry` = 10664; -- Scryer
+UPDATE `creature_template_addon` SET `auras` = '18943' WHERE `entry` = 18099; -- Gordawg
+
