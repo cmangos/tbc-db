6cf0dec5fce6b4a9cb20174c22f3215f27c8db24
anonxs <anonxs@web.de>
2018-10-31 23:07:05 +0100

    [0022] flags_extra -> ExtraFlags

--- Updates/0260_mangos_db_errors_fixes.sql
+++ Updates/0260_mangos_db_errors_fixes.sql
@@ -1,7 +1,7 @@
 -- Add movements, correct waypoints and scripts for NPC 550 (Defias Messenger). This basically fixes
 -- DB errors stating that two DB strings were unused while they are indeed used by these scripts
 -- Making the NPC capable of following waypoints (source TBC-DB)
-UPDATE creature_template SET flags_extra=2050, MovementType=2 WHERE entry=550;
+UPDATE creature_template SET ExtraFlags=2050, MovementType=2 WHERE entry=550;
 
 -- Spawning the NPC at the right place (1st waypoint)
 UPDATE creature SET position_x=-11017.7, position_y=1438.34, position_z=43.0221, orientation=5.30596 WHERE guid=45525;
