b48d23d720a5a161837111a0019af4e19c114aab
MantisLord <sabinprosper@gmail.com>
2018-11-11 13:29:50 -0500

    Add missing spawn of Namdo Bizzfizzle (2683)

---
+++ Updates/0296_c2683.sql
@@ -0,0 +1,6 @@
+-- Namdo Bizzfizzle
+DELETE FROM creature WHERE guid=87022;
+INSERT INTO creature (guid, id, map, spawnMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, spawndist, currentwaypoint, curhealth, curmana, DeathState, MovementType) VALUES
+(87022, 2683, 0, 1, 0, 0, -4923.1, 725.529, 253.1, 6.21499, 300, 300, 0, 0, 664, 0, 0, 0);
+
+
