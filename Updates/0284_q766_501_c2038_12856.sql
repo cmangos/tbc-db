f08e974a47ba15eaf34e31e2a089460b8d6ed1db
anonxs <anonxs@web.de>
2018-11-11 21:19:17 +0100

    Move Lord Melenas (2038) pooling to pool_creature_template

--- Updates/0284_q766_501_c2038_12856.sql
+++ Updates/0284_q766_501_c2038_12856.sql
@@ -33,11 +33,9 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `positi
 DELETE FROM `pool_template` WHERE `entry` = 1003;
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`) VALUES
 (1003, 1, 'Lord Melenas (2038)');
-DELETE FROM `pool_creature` WHERE `pool_entry` = 1003;
-INSERT INTO `pool_creature` (`guid`, `pool_entry`, `description`) VALUES
-(1550, 1003, 'Lord Melenas #1'),
-(1551, 1003, 'Lord Melenas #2'),
-(1552, 1003, 'Lord Melenas #3');
+DELETE FROM `pool_creature_template` WHERE `pool_entry` = 1003;
+INSERT INTO `pool_creature_template` (`id`, `pool_entry`, `chance`, `description`) VALUES
+(2038, 1003, 0, 'Lord Melenas (2038)');
 	
 -- Correct Ashenvale Outrunner 12856 due to aura 20540
 UPDATE `creature_template` SET `SpeedWalk`= (2.5 / 2.5), `SpeedRun` = (8 / 7) WHERE `entry` = 12856;
