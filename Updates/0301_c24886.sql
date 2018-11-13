12f630208c85863a1f580a38ebe3de64e3ba3146
anonxs <anonxs@web.de>
2018-11-13 19:18:45 +0100

    Correct Spawn and UnitFlags for Exorcist Vaisha 24886

---
+++ Updates/0301_c24886.sql
@@ -0,0 +1,4 @@
+-- Exorcist Vaisha 24886
+UPDATE `creature_template` SET `UnitFlags` = 36864 WHERE `entry` = 24886;
+UPDATE `creature` SET `position_x` = -2680.179, `position_y` = 4388.869, `position_z` = 35.58163, `orientation` = 0.7504916 WHERE `guid` = 96759 AND `id` = 24886;
+
