dc0f4989f1e4f419aa067cb89e5be54d6125dd57
MantisLord <sabinprosper@gmail.com>
2018-11-16 01:08:22 -0500

    Fix offer reward text for q.11025 The Crystals
    Change "Remember, $B" to "Remember, $N"

---
+++ Updates/0314_q11025_offer_reward_text_fix.sql
@@ -0,0 +1,5 @@
+-- The Crystals q.11025
+-- "Remember, $B" -> "Remember, $N"
+UPDATE quest_template SET OfferRewardText="<Chu'a'lor's left head begins to speak.>$B$BThese will do fine.$B$BRemember, $N, the value of the shard is not in what it can purchase for you. It is not even in what it can potentially do for you when properly forged, though there is some debate over that.$B$B<His right head finishes,...>$B$BThe true value is in the journey to attain the shard." WHERE entry=11025;
+
+
