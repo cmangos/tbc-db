30a1d5c59f51f586adc579048dd11bae08a10df9
MantisLord <sabinprosper@gmail.com>
2018-11-16 00:43:03 -0500

    Fix typo in text used by Irradiated Worker
    Change "weve" to "we've"

---
+++ Updates/0313_dbscript_string_typo.sql
@@ -0,0 +1,5 @@
+-- Irradiated Worker text
+-- "weve" -> "we've"
+UPDATE dbscript_string SET content_default='Hmm... It seems that the syncotron unit we''ve been using has had no noticeable effect on the gravimetric or gravitronic distortions in this sector.' WHERE entry=2000005259;
+
+
