335fb1c708df54cfa271d5b4253eb6b8441c5b1c
anonxs <anonxs@web.de>
2018-11-17 20:25:36 +0100

    Add more spawns for Adamantite Bound Chest 181802

---
+++ Updates/0317_o181802.sql
@@ -0,0 +1,102 @@
+-- Adamantite Bound Chest 181802
+UPDATE `gameobject_template` SET `data1` = 22342, `mingold` = 1520, `maxgold` = 2924 WHERE `entry` = 181802;
+DELETE FROM `gameobject_loot_template` WHERE `entry` IN (181802); -- shares same loottable as 181800
+
+UPDATE `gameobject` SET `position_x` = -4524.896, `position_y` = 1720.212, `position_z` = 174.7809, `orientation` = -2.094395 WHERE `id` = 181802 AND `guid` = 56108;
+UPDATE `gameobject` SET `position_x` = -4351.023, `position_y` = 1719.601, `position_z` = 156.8811, `orientation` = -2.007128 WHERE `id` = 181802 AND `guid` = 56116;
+UPDATE `gameobject` SET `position_x` = -4190.155, `position_y` = 3167.48, `position_z` = 317.6007, `orientation` = 2.809975 WHERE `id` = 181802 AND `guid` = 14829;
+UPDATE `gameobject` SET `position_x` = -4110.731, `position_y` = 3015.355, `position_z` = 344.9143, `orientation` = -1.483528 WHERE `id` = 181802 AND `guid` = 56111;
+UPDATE `gameobject` SET `position_x` = -4086.649, `position_y` = 2507.345, `position_z` = 140.0176, `orientation` = 0.541052 WHERE `id` = 181802 AND `guid` = 56106;
+UPDATE `gameobject` SET `position_x` = -3913.034, `position_y` = 3100.639, `position_z` = 362.4336, `orientation` = 2.862335 WHERE `id` = 181802 AND `guid` = 14690;
+UPDATE `gameobject` SET `position_x` = -3874.913, `position_y` = 3461.044, `position_z` = 275.4309, `orientation` = 0.6283169 WHERE `id` = 181802 AND `guid` = 56118;
+UPDATE `gameobject` SET `position_x` = -3533.843, `position_y` = 2174.155, `position_z` = 34.60061, `orientation` = -0.8901179 WHERE `id` = 181802 AND `guid` = 56105;
+UPDATE `gameobject` SET `position_x` = -3017.349, `position_y` = 6515.13, `position_z` = 99.1902, `orientation` = 0.7679448 WHERE `id` = 181802 AND `guid` = 56112;
+UPDATE `gameobject` SET `position_x` = -3003.698, `position_y` = 6353.838, `position_z` = 96.33028, `orientation` = 0.9250244 WHERE `id` = 181802 AND `guid` = 56121;
+UPDATE `gameobject` SET `position_x` = -2893.958, `position_y` = 6456.362, `position_z` = 101.5514, `orientation` = 1.48353 WHERE `id` = 181802 AND `guid` = 56114;
+UPDATE `gameobject` SET `position_x` = -2892.909, `position_y` = 1658.848, `position_z` = 57.14655, `orientation` = 4.712389 WHERE `id` = 181802 AND `guid` = 56109;
+UPDATE `gameobject` SET `position_x` = -2781.98, `position_y` = 6405.092, `position_z` = 62.14188, `orientation` = -3.036873 WHERE `id` = 181802 AND `guid` = 30430;
+UPDATE `gameobject` SET `position_x` = -2431.546, `position_y` = 4753.95, `position_z` = 153.1431, `orientation` = 3.054326 WHERE `id` = 181802 AND `guid` = 56110;
+UPDATE `gameobject` SET `position_x` = -2416.781, `position_y` = 4552.81, `position_z` = 160.5373, `orientation` = 1.989675 WHERE `id` = 181802 AND `guid` = 56123;
+UPDATE `gameobject` SET `position_x` = -1517.379, `position_y` = 8595.958, `position_z` = 7.259799, `orientation` = -1.518436 WHERE `id` = 181802 AND `guid` = 56120;
+UPDATE `gameobject` SET `position_x` = -1467.272, `position_y` = 8437.011, `position_z` = -0.029554, `orientation` = 2.216568 WHERE `id` = 181802 AND `guid` = 56115;
+UPDATE `gameobject` SET `position_x` = -1434.152, `position_y` = 8549.072, `position_z` = 13.73264, `orientation` = -0.9773844 WHERE `id` = 181802 AND `guid` = 56107;
+UPDATE `gameobject` SET `position_x` = -1032.31, `position_y` = 9017.965, `position_z` = 93.42046, `orientation` = -0.6981319 WHERE `id` = 181802 AND `guid` = 56119;
+UPDATE `gameobject` SET `position_x` = -892.7885, `position_y` = 8700.732, `position_z` = 251.5986, `orientation` = -0.1919852 WHERE `id` = 181802 AND `guid` = 56117;
+UPDATE `gameobject` SET `position_x` = -854.361, `position_y` = 7695.247, `position_z` = 37.34705, `orientation` = -0.06981307 WHERE `id` = 181802 AND `guid` = 56124;
+UPDATE `gameobject` SET `position_x` = -845.1196, `position_y` = 8853.498, `position_z` = 185.3628, `orientation` = 0.5235988 WHERE `id` = 181802 AND `guid` = 56122;
+UPDATE `gameobject` SET `position_x` = -778.9059, `position_y` = 7602.208, `position_z` = 48.64904, `orientation` = -1.343903 WHERE `id` = 181802 AND `guid` = 56113;
+UPDATE `gameobject` SET `position_x` = 2290.536, `position_y` = 5754.549, `position_z` = 269.5532, `orientation` = -1.204277 WHERE `id` = 181802 AND `guid` = 28297;
+UPDATE `gameobject` SET `position_x` = 2562.751, `position_y` = 6222.837, `position_z` = 11.99759, `orientation` = 1.099556 WHERE `id` = 181802 AND `guid` = 56682;
+UPDATE `gameobject` SET `position_x` = 2955.092, `position_y` = 2271.695, `position_z` = 161.7075, `orientation` = 2.460913 WHERE `id` = 181802 AND `guid` = 56683;
+UPDATE `gameobject` SET `position_x` = 2974.05, `position_y` = 4080.875, `position_z` = 150.3778, `orientation` = -0.3490661 WHERE `id` = 181802 AND `guid` = 32783;
+UPDATE `gameobject` SET `position_x` = 3987.379, `position_y` = 4118.472, `position_z` = 189.005, `orientation` = 2.391101 WHERE `id` = 181802 AND `guid` = 28265;
+UPDATE `gameobject` SET `position_x` = 4730.978, `position_y` = 2602.898, `position_z` = 212.0839, `orientation` = 2.321287 WHERE `id` = 181802 AND `guid` = 32810;
+
+SET @GUID := 190609;
+DELETE FROM `gameobject` WHERE `guid` BETWEEN 190610 AND 190636;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`) VALUES
+(@GUID := @GUID + 1, 181802, 530, 1, -3858.161, 2623.01, 92.00819, -0.977383, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -3732.661, 2567.54, 90.23063, 2.478368, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -3711.518, 2707.878, 108.4539, -2.426008, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -3479.98, 2008.736, 96.85999, -0.03490669, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -3443.476, 2056.348, 15.63352, 0.8726646, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -3336.882, 2116.514, 102.3929, -3.071779, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -3071.204, 1588.129, 56.83473, 1.204277, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -3004.927, 1777.426, 75.27542, -1.029745, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -1586.231, 8452.373, -9.956278, -2.199115, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -768.5071, 7964.643, 60.61557, -0.8377579, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -696.6482, 7737.718, 80.77087, 1.37881, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, -669.5178, 8833.169, 200.0232, -1.43117, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 1641.272, 4583.118, 139.2965, 1.134464, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2276.719, 2851.442, 131.6899, -0.6108654, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2374.116, 2586.361, 135.725, 0.2094395, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2445.496, 5625.654, 266.6024, 1.099557, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2463.238, 2889.267, 128.6967, 2.268925, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2749.61, 3255.476, 148.7259, -2.111848, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2751.288, 3108.247, 156.6682, 0.9599311, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2768.547, 4360.816, 136.3649, -1.047198, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2795.004, 3932.923, 144.1864, 2.216568, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2942.942, 2186.748, 165.3282, -0.4712385, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 2987.843, 4234.66, 160.89, 0.1745329, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 3107.093, 2160.784, 149.2222, 1.483528, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 3413.678, 3463.366, 146.2522, 0.3316126, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 3607.977, 7125.159, 140.4459, -0.03490625, 7200, 7200, 100, 1),
+(@GUID := @GUID + 1, 181802, 530, 1, 3648.2, 6539.607, 155.1136, 0.6981315, 7200, 7200, 100, 1);
+
+SET @GUID := 190609;
+DELETE FROM `pool_gameobject` WHERE `guid` BETWEEN 190610 AND 190636;
+INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VALUES
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39950, 0, 'Shadowmoon Valley - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39947, 0, 'Nagrand - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39947, 0, 'Nagrand - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39947, 0, 'Nagrand - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39947, 0, 'Nagrand - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39948, 0, 'Blade''s Edge Mountains - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39948, 0, 'Blade''s Edge Mountains - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39949, 0, 'Netherstorm - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39948, 0, 'Blade''s Edge Mountains - Adamantite Bound Chest (181802)'),
+(@GUID := @GUID + 1, 39948, 0, 'Blade''s Edge Mountains - Adamantite Bound Chest (181802)');
+
+-- Increase respective spawn limit
+UPDATE `pool_template` SET `max_limit` = 10 WHERE `entry` = 39947; -- Nagrand (TBC - 64-67) - Master Chest Pool
+UPDATE `pool_template` SET `max_limit` = 6 WHERE `entry` = 39948; -- Blade's Edge Mountains (TBC - 65-68) - Master Chest Pool
+UPDATE `pool_template` SET `max_limit` = 5 WHERE `entry` = 39949; -- Netherstorm (TBC - 67-70) - Master Chest Pool
+UPDATE `pool_template` SET `max_limit` = 6 WHERE `entry` = 39950; -- Shadowmoon Valley (TBC - 67-70) - Master Chest Pool
+
