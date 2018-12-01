c01366c1ee8a958333e628e3e85d5e681d34d8e6
MantisLord <sabinprosper@gmail.com>
2018-12-01 18:39:09 -0500

    Remove Zeppelin directions from Bluffwatcher
    The route between Orgrimmar and Thunder Bluff was added in Patch 3.2 so this gossip option and related text should not exist in TBC or Classic
    
    Resolves #1365

---
+++ Updates/0418_bluffwatcher_gossip.sql
@@ -0,0 +1,6 @@
+DELETE FROM gossip_menu_option WHERE menu_id=721 AND id=12;
+DELETE FROM npc_text WHERE id=14658;
+DELETE FROM gossip_menu WHERE entry=10596;
+DELETE FROM points_of_interest WHERE entry=300; -- Thunder Bluff Zeppelin
+
+
