132195f0a94019cdd6d5a6e0488f499c4e828731
anonxs <anonxs@web.de>
2018-12-02 18:03:46 +0100

    Correct Faction, UnitFlags, Speed for Okrek 18685

---
+++ Updates/0423_c18685.sql
@@ -0,0 +1,3 @@
+-- Okrek 18685
+UPDATE `creature_template` SET `FactionAlliance` = 1738, `FactionHorde` = 1738, `UnitFlags` = 32768, `SpeedWalk` = (2.5 / 2.5), `SpeedRun` = (8 / 7) WHERE `entry` = 18685;
+
