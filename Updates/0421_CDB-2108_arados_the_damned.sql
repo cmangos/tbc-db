888dac31e2687064cf037622ee806edd89d3f450
anonxs <anonxs@web.de>
2018-12-02 16:37:42 +0100

    Remove Stat changes except for  Model and Speed for Arados the Damned 7073 thx @Tobschinski * https://github.com/cmangos/tbc-db/commit/9bb89ee0982f1bb70d4cdacdc5b1c6a5a09f9f3f#commitcomment-31527184

--- Updates/0421_CDB-2108_arados_the_damned.sql
+++ Updates/0421_CDB-2108_arados_the_damned.sql
@@ -1,7 +1,5 @@
 -- Add missing creature template for NPC 7073 (Arados the Damned)
 -- This is for exhaustivity purpose as the NPC was removed in patch 1.10
 -- Possibly wrong multiplier as NPC was removed
-UPDATE `creature_template` SET `ModelId1` = 11369, `SpeedWalk` = (2.5/2.5), `SpeedRun` = (15/7), `HealthMultiplier` = 1, `MinLevelHealth` = 1220, `MaxLevelHealth` = 1220, 
-`DamageMultiplier` = 1, `MinMeleeDmg` = 49, `MaxMeleeDmg` = 64, `MinRangedDmg` = 33, `MaxRangedDmg` = 49, `MeleeAttackPower` = 118, `RangedAttackPower` = 11, `MeleeBaseAttackTime` = 2000, `RangedBaseAttackTime` = 2000
-WHERE `entry` = 7073;
+UPDATE `creature_template` SET `ModelId1` = 11369, `SpeedWalk` = (2.5/2.5), `SpeedRun` = (15/7) WHERE `entry` = 7073;
 
