0f861eb170507f62ff69e750da3f87bfc0af0670
anonxs <anonxs@web.de>
2018-12-03 23:53:04 +0100

    [CDB][2124][2125] Correct Emote and Movement for c.5843
    https://github.com/cmangos/classic-db/commit/4632c8bda8e3def9d05378c672dae9142d5f222d
    https://github.com/cmangos/classic-db/commit/cdb730b95f34db6ff0e556370773be8bbc69cacb

---
+++ Updates/0440_CDB-2124-2125_c5843_addon_movement.sql
@@ -0,0 +1,10 @@
+-- Uniformize addon and emote for NPC 5843 (Slave Worker)
+-- Slave Worker 5843
+DELETE FROM creature_addon WHERE guid IN (SELECT guid FROM creature WHERE id=5843);
+DELETE FROM creature_template_addon WHERE entry=5843;
+INSERT INTO creature_template_addon (entry,mount,bytes1,b2_0_sheath,b2_1_flags,emote,moveflags,auras) VALUES
+(5843,0,0,1,16,173,0,NULL);
+
+-- Fix movement for two NPC 5843 (Slave Worker)
+UPDATE creature SET MovementType=0, spawndist=0 WHERE guid IN (5679, 5685);
+
