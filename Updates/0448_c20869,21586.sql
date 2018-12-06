504a1b8e27f45de9b71465db305761e1e52b4f67
anonxs <anonxs@web.de>
2018-12-06 23:54:21 +0100

    Correct Faction & UnitFlags for Arcatraz Sentinel 20869,21586

---
+++ Updates/0448_c20869,21586.sql
@@ -0,0 +1,4 @@
+-- Arcatraz Sentinel 20869,21586
+UPDATE `creature_template` SET `FactionAlliance` = 16, `FactionHorde` = 16, `UnitFlags` = 64 WHERE `entry` = 20869;
+UPDATE `creature_template` SET `FactionAlliance` = 16, `FactionHorde` = 16, `UnitFlags` = 64 WHERE `entry` = 21586;
+
