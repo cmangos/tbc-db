e27dd5b3b8877be50378a5e26e936df635f9b47f
Killerwife <killerwife@gmail.com>
2018-12-07 23:32:02 +0100

    New threat string for npc threat command

---
+++ Updates/0452_threat_string.sql
@@ -0,0 +1,3 @@
+-- has accompanying core update
+UPDATE mangos_string SET content_default='Player |cffff0000%s|r [GUID: %u] has |cffff0000%f|r threat, taunt state %u and hostile state %u.' WHERE entry IN(1702);
+
