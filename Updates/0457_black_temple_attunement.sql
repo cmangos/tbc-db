6d6df4e14d2a58502fd0cdaffe5ca0c63d4d5591
MantisLord <sabinprosper@gmail.com>
2018-12-08 14:03:45 -0500

    Remove pre-2.4 status text for Black Temple entrance

---
+++ Updates/0457_black_temple_attunement.sql
@@ -0,0 +1,4 @@
+-- Remove status text pertaining to attunement for Black Temple (not needed after Patch 2.4)
+UPDATE areatrigger_teleport SET status_failed_text='' WHERE id=4598; -- Black Temple (Entrance)
+
+
