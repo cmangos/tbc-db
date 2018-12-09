6e9c480bca980934a9b5ef9214e9f94142183bb1
MantisLord <sabinprosper@gmail.com>
2018-12-08 22:16:48 -0500

    Fix potential error during db install

--- Updates/0454_i5877_lootcondition.sql
+++ Updates/0454_i5877_lootcondition.sql
@@ -10,6 +10,6 @@ REPLACE INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `comme
 (9005, -1, 3, 9004, '(Horde Player AND (Quest ID 1147 Rewarded OR Quest ID 1147 Taken))'),
 (9006, -1, 9005, 9001, '((Horde Player AND Quest ID 1147 Taken) AND Player Has Less Than 1 of Item ID 5877 in Inventory)');
 
-UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 100, `condition_id` = 9006 WHERE `entry` IN (4130,4131,4132,4133) AND `item` = 5877
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 100, `condition_id` = 9006 WHERE `entry` IN (4130,4131,4132,4133) AND `item` = 5877;
 
 -- Not sure what should happen to the item on quest (1148) turn in (c.3428), probably also be removed
