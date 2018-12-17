b76bdfb72ffa10991323de129432ecbb0a4c8f53
MantisLord <sabinprosper@gmail.com>
2018-12-16 22:00:03 -0500

    Add missing script to allow opening of DM - North door from the inside using Torch object
    Source: Classic-DB

---
+++ Updates/0478_dire_maul_north_door.sql
@@ -0,0 +1,5 @@
+DELETE FROM dbscripts_on_go_use WHERE id=49962;
+INSERT INTO dbscripts_on_go_use (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(49962, 0, 11, 49948, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+
