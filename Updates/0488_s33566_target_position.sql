470e84adfc8646b6c59a0476f4636237874ed112
anonxs <anonxs@web.de>
2018-12-24 01:17:02 +0100

    Correct position of first Void Portal 19224 spawned by s.33566, other 4 are handled in ScriptDev

---
+++ Updates/0488_s33566_target_position.sql
@@ -0,0 +1,3 @@
+-- Correct position of first Void Portal 19224 spawned by s.33566, other 4 are handled in ScriptDev
+UPDATE `spell_target_position` SET `target_position_x` = -208.411, `target_position_y` = -263.652, `target_position_z` = 17.08631, `target_orientation` = 3.12187 WHERE `id` = 33566;
+
