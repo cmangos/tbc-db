ff9fea756eb8424d0cd6aea7886eab89dfa1f136
anonxs <anonxs@web.de>
2018-12-25 20:44:19 +0100

    Add missing go trap Hellfire Hot Spot Spreader 183929

---
+++ Updates/0492_o183929.sql
@@ -0,0 +1,6 @@
+-- linked trap for o.183816 (Hellfire Fire)
+DELETE FROM gameobject_template WHERE entry=183929;
+INSERT INTO gameobject_template (entry, type, displayId, name, IconName, castBarCaption, faction, flags, ExtraFlags, size, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, CustomData1, mingold, maxgold, ScriptName) VALUES
+('183929', '6', '2770', 'Hellfire Hot Spot Spreader', '', '', '0', '0', '0', '1', '0', '0', '0', '34516', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '');
+
+
