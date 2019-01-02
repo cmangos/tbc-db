db4810df58f75b69272bd85028a1becbeef707ab
Killerwife <killerwife@gmail.com>
2019-01-02 09:46:08 +0100

    Add another seal of righteousness revision

--- Updates/0510_seal_of_righteousness_core_sync.sql
+++ Updates/0510_seal_of_righteousness_core_sync.sql
@@ -36,4 +36,10 @@ INSERT INTO `spell_chain` VALUES
 (25713,25735,25742,9,0),
 (27156,25713,25742,10,0);
 
+ALTER TABLE db_version CHANGE COLUMN required_s2383_01_mangos_seal_of_righteousness_proc_restored required_s2384_01_mangos_seal_of_righteousness_trainer_cleanup_final bit;
+
+DELETE `npc_trainer_template` WHERE `spell` IN (20154, 20271, 21084);
+
+DELETE `npc_trainer` WHERE `spell` (20154, 20271, 21084);
+
 
