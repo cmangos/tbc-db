a9e8c33f292c72adeb5e56c069c7b4437e891554
MantisLord <sabinprosper@gmail.com>
2019-01-03 09:09:50 -0500

    Add proper targets for Light of the Naaru spell
    39832 hits players while 39831 only hits NPCs

---
+++ Updates/0514_s39831_targets.sql
@@ -0,0 +1,10 @@
+DELETE FROM spell_script_target WHERE entry=39831;
+INSERT INTO spell_script_target (entry, type, targetEntry) VALUES
+(39831, 1, 21701),
+(39831, 1, 22861),
+(39831, 1, 22862),
+(39831, 1, 22863),
+(39831, 1, 22989),
+(39831, 1, 22990);
+
+
