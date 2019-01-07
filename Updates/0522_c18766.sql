4d8941e5bf59adc8bb9c03569ae8524b041a78dc
anonxs <anonxs@web.de>
2019-01-07 23:41:41 +0100

    Correct Stats for Pool of Souls 18766

---
+++ Updates/0522_c18766.sql
@@ -0,0 +1,3 @@
+-- Pool of Souls 18766
+UPDATE `creature_template` SET `MinLevel` = 70, `MaxLevel` = 70, `InhabitType` = 4, `UnitFlags`  = 33554432, `ExtraFlags` = 0, `SpeedWalk` = (2.5 / 2.5), `SpeedRun` = (8 / 7) WHERE `entry` = 18766;
+
