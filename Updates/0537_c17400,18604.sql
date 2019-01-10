face89c0074ea1f2520b836181c6267258feeae5
anonxs <anonxs@web.de>
2019-01-11 00:48:37 +0100

    Correct ModelId and UnitFlags for Felguard Annihilator 17400,18604

---
+++ Updates/0537_c17400,18604.sql
@@ -0,0 +1,4 @@
+-- Felguard Annihilator 17400,18604
+UPDATE `creature_template` SET `ModelId1` = 18342, `UnitFlags` = 64 WHERE `entry` = 17400;
+UPDATE `creature_template` SET `ModelId1` = 18342, `UnitFlags` = 64 WHERE `entry` = 18604;
+
