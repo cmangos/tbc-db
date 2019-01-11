96d9700bddeea9e2a87aec1902259253ad419bcd
anonxs <anonxs@web.de>
2019-01-11 23:12:30 +0100

    Correct UnitFlags and MeleeBaseAttackTime for Bonechewer Beastmaster 17455,18051

---
+++ Updates/0539_c17455,18051.sql
@@ -0,0 +1,4 @@
+-- Bonechewer Beastmaster 17455,18051
+UPDATE `creature_template` SET `UnitFlags` = 32832, `MeleeBaseAttackTime` = 1200 WHERE `entry` = 17455;
+UPDATE `creature_template` SET `UnitFlags` = 32832, `MeleeBaseAttackTime` = 1200 WHERE `entry` = 18051;
+
