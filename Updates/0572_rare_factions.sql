f66eafac3b1182f310fcfdadafc3ab9a9ed512ef
anonxs <anonxs@web.de>
2019-01-13 14:14:07 +0100

    Fixed many rare creature faction
    based on [0591] Fixed many rare creature faction
    https://github.com/cmangos/classic-db/commit/e6f042c8327611ed7d6f241225230254767e2278
    and https://gist.githubusercontent.com/Kinzcool/e81137dab3bc828631ada717f7da5159/raw/796cd141ede5c02299a55ecd3037cdd460b5944c/creaturedifficulty.sql

---
+++ Updates/0572_rare_factions.sql
@@ -0,0 +1,56 @@
+-- based on https://github.com/cmangos/classic-db/commit/e6f042c8327611ed7d6f241225230254767e2278
+-- https://gist.githubusercontent.com/Kinzcool/e81137dab3bc828631ada717f7da5159/raw/796cd141ede5c02299a55ecd3037cdd460b5944c/creaturedifficulty.sql 
+
+UPDATE creature_template SET Faction=189 WHERE entry=14224; -- 7:XT
+UPDATE creature_template SET Faction=16 WHERE entry=5347;  -- Antilus the Soarer
+UPDATE creature_template SET Faction=16 WHERE entry=6648;  -- Antilos
+UPDATE creature_template SET Faction=152 WHERE entry=7895;  -- Ambassador Bloodrage
+UPDATE creature_template SET Faction=103 WHERE entry=10202; -- Azurous
+UPDATE creature_template SET Faction=16 WHERE entry=5346;  -- Bloodroar the Stalker
+UPDATE creature_template SET Faction=45 WHERE entry=8202;  -- Cyclok the Mad
+UPDATE creature_template SET Faction=7 WHERE entry=14223; -- Cranky Benj
+UPDATE creature_template SET Faction=38 WHERE entry=10077; -- Deathmaw (rare Wolf, Searing Gorge)
+UPDATE creature_template SET Faction=270 WHERE entry=5912;  -- Deviate Faerie Dragon
+UPDATE creature_template SET Faction=24 WHERE entry=507;   -- Fenros (rare Worgen, Duskwood)
+UPDATE creature_template SET Faction=16 WHERE entry=1843;  -- Foreman Jerris
+UPDATE creature_template SET Faction=38 WHERE entry=14228; -- Giggler (rare Hyena, Desolace)
+UPDATE creature_template SET Faction=45 WHERE entry=9604;  -- Gorgon'och
+UPDATE creature_template SET Faction=40 WHERE entry=9718;  -- Ghok Bashguud
+UPDATE creature_template SET Faction=16 WHERE entry=10825; -- Gish the Unmoving
+UPDATE creature_template SET Faction=87 WHERE entry=14221; -- Gravis Slipknot
+UPDATE creature_template SET Faction=18 WHERE entry=14447; -- Gilmorian
+UPDATE creature_template SET Faction=16 WHERE entry=5934;  -- Heartrazor (rare elite wyvern, Thousand Needles)
+UPDATE creature_template SET Faction=45 WHERE entry=9602;  -- Hahk'Zor
+UPDATE creature_template SET Faction=7 WHERE entry=10558; -- Hearthsinger Forresten
+UPDATE creature_template SET Faction=16 WHERE entry=10821; -- Hed'mush the Rotting
+UPDATE creature_template SET Faction=49 WHERE entry=5935;  -- Ironeye the Invincible
+UPDATE creature_template SET Faction=189 WHERE entry=8213;  -- Ironback
+UPDATE creature_template SET Faction=45 WHERE entry=2453;  -- Lo'Grosh (rare Ogre, Alterac Mountains)
+UPDATE creature_template SET Faction=16 WHERE entry=1848;  -- Lord Maldazzar
+UPDATE creature_template SET Faction=129 WHERE entry=14236; -- Lord Angler
+UPDATE creature_template SET Faction=45 WHERE entry=2604;  -- Molok the Crusher (Boulderfist Ogre, Arathi Highlands)
+UPDATE creature_template SET Faction=38 WHERE entry=8208;  -- Murderous Blisterpaw (rare Hyena, Tanaris)
+UPDATE creature_template SET Faction=45 WHERE entry=723;   -- Mosh'Ogg Butcher
+UPDATE creature_template SET Faction=45 WHERE entry=8297;  -- Magronos the Unyielding
+UPDATE creature_template SET Faction=14 WHERE entry=14237; -- Oozeworm
+UPDATE creature_template SET Faction=74 WHERE entry=2779;  -- Prince Nazjak
+UPDATE creature_template SET Faction=39 WHERE entry=947;   -- Rohh the Silent
+UPDATE creature_template SET Faction=16 WHERE entry=1837;  -- Scarlet Judge
+UPDATE creature_template SET Faction=16 WHERE entry=1838;  -- Scarlet Interrogator
+UPDATE creature_template SET Faction=16 WHERE entry=1839;  -- Scarlet High Clerist
+UPDATE creature_template SET Faction=16 WHERE entry=1841;  -- Scarlet Executioner
+UPDATE creature_template SET Faction=14 WHERE entry=3295;  -- Sludge Beast
+UPDATE creature_template SET Faction=111 WHERE entry=5864;  -- Swinegart Spearhide
+UPDATE creature_template SET Faction=124 WHERE entry=5916;  -- Sentinel Amarassan
+UPDATE creature_template SET Faction=14 WHERE entry=3652;  -- Trigore the Lasher
+UPDATE creature_template SET Faction=14 WHERE entry=8212;  -- The Reak
+UPDATE creature_template SET Faction=14 WHERE entry=14345; -- The Ongar
+UPDATE creature_template SET Faction=37 WHERE entry=5399;  -- Veyzhak the Cannibal
+UPDATE creature_template SET Faction=189 WHERE entry=14492; -- Verifonix
+UPDATE creature_template SET Faction=795 WHERE entry=8218;  -- Witherheart the Stalker
+UPDATE creature_template SET Faction=37 WHERE entry=10822; -- Warlord Thresh'jin
+UPDATE creature_template SET Faction=37 WHERE entry=5400;  -- Zekkis
+UPDATE creature_template SET Faction=16 WHERE entry=10823; -- Zul'Brin Warpbranch
+UPDATE creature_template SET Faction=310 WHERE entry=14474; -- Zora
+UPDATE creature_template SET Faction=50 WHERE entry=1063;  -- Jade (rare elite Green Drake, Swamp of Sorrow)
+
