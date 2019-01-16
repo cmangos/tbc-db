9880a44216dbce8f449ef67c899bf23135325993
anonxs <anonxs@web.de>
2019-01-16 22:51:46 +0100

    Correct File Number for [589] and add "ghost spawn at Dun Morogh, Kharanos <- killed in Ironforge for Horde", they spawned in IF at their corpse befor
    Source: classic-db

--- Updates/0590_CDB-0492_game_graveyard_zone.sql
+++ Updates/0590_CDB-0492_game_graveyard_zone.sql
@@ -8,3 +8,11 @@ INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`) VALUES (32, 16
 DELETE FROM `game_graveyard_zone` WHERE `ghost_zone` = 3429;
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`) VALUES (913, 3429, 0); -- ghost spawn at Silithus, Scarab Wall (AQ Only) <- killed in Ruins of Ahn’Qiraj for both factions
 
+-- addition from classic-db
+
+-- Add missing diverging graveyard when dying in Ironforge for horde and alliance, source classic-db
+DELETE FROM `game_graveyard_zone` WHERE `id` = 101 AND `ghost_zone` = 1537;
+INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`) VALUES
+(101, 1537, 67); -- ghost spawn at Dun Morogh, Kharanos <- killed in Ironforge for Horde
+-- Alliance seems to be fine, not 102 Dun Morogh, Ironforge "right infront of the gates" but 852 Dun Morogh, Gates of Ironforge "real graveyard with ghosthealer"
+
