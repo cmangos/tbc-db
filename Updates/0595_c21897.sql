d916528eb3ce9b598da57722f648a5763e31b508
anonxs <anonxs@web.de>
2019-01-19 15:24:56 +0100

    Correct Position and Respawntimer for Felspine the Greater 21897
    Thanks @Tobschinski , resolves #632

---
+++ Updates/0595_c21897.sql
@@ -0,0 +1,3 @@
+-- Felspine the Greater 21897
+UPDATE `creature` SET `position_x` = -3573.289, `position_y` = 1139.58, `position_z` = 46.58854, `orientation` = 3.525565, `spawntimesecsmin` = 60, `spawntimesecsmax` = 60 WHERE `id` = 21897;
+
