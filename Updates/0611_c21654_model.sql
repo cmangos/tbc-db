a0afa318eeabab1250909b824798ec3c84a126d2
anonxs <anonxs@web.de>
2019-01-22 21:46:46 +0100

    [DBError] Use tbc equivalent modelid for Skettis Followers Spawner 21654 - this npc was probably added in wotlk

--- Updates/0611_c21654_model.sql
+++ Updates/0611_c21654_model.sql
@@ -1,3 +1,3 @@
 -- Add additional model for Skettis Followers Spawner 21654
-UPDATE `creature_template` SET `ModelId2` = 23501 WHERE `entry` = 21654;
+UPDATE `creature_template` SET `ModelId2` = 22769 WHERE `entry` = 21654; -- wotlk+ 23501
 
