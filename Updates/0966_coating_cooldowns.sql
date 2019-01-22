6df1d882a37af7901a301cd22a8a143657d0a47e
Killerwife <killerwife@gmail.com>
2019-01-22 20:43:49 +0100

    Add cooldowns to weapon coatings

---
+++ Updates/0966_coating_cooldowns.sql
@@ -0,0 +1,8 @@
+-- has associated core commit
+DELETE FROM spell_proc_event WHERE entry IN(33511);
+INSERT INTO spell_proc_event(entry,Cooldown) VALUES
+(45396,45);
+DELETE FROM spell_proc_event WHERE entry IN(33511);
+INSERT INTO spell_proc_event(entry,Cooldown) VALUES
+(45398,40);
+
