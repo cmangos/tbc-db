520f29755689d4ae65f5be891573b1286efda2ca
Thorbjørn H.H. Sindrup <holsthein@live.dk>
2019-01-24 18:39:00 +0100

    Delete unneeded file
    We already have a drop db sql file for the exact same purpse in the core that is more up to date than this one.
    
    https://github.com/cmangos/mangos-tbc/blob/master/sql/create/db_drop_mysql.sql

--- Tools/Install_Script_Helpers/TBC-DB_Drop_Mysql.sql
+++
@@ -1,9 +0,0 @@
-DROP USER mangos@localhost;
-
-DROP DATABASE IF EXISTS `mangos` ;
-
-DROP DATABASE IF EXISTS `characters` ;
-
-DROP DATABASE IF EXISTS `realmd` ;
-
-DROP DATABASE IF EXISTS `scriptdev2` ;
