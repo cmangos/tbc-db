a2f0003886911968abff21b816dfa9e40a06942f
anonxs <anonxs@web.de>
2019-01-27 14:57:37 +0100

    Correct Stats for [DND]Prophecy Quest Credit 22798,22799,22800,22801

---
+++ Updates/0624_c22798,22799,22800,22801.sql
@@ -0,0 +1,13 @@
+-- [DND]Prophecy 1 Quest Credit 22798
+UPDATE `creature` SET `modelid` = 11686, `position_x` = 3784.439, `position_y` = 6729.338, `position_z` = 170.5275, `orientation` = 0.2094395 WHERE `id` = 22798;
+UPDATE `creature_template` SET `ModelId1` = 16046, `ModelId2` = 0, `UnitFlags` = 33554432, `PetSpellDataId` = 0 WHERE `entry` = 22798;
+-- [DND]Prophecy 2 Quest Credit 22799
+UPDATE `creature` SET `modelid` = 11686, `position_x` = 3625.608, `position_y` = 6541.654, `position_z` = 155.1301, `orientation` = 4.118977 WHERE `id` = 22799;
+UPDATE `creature_template` SET `ModelId1` = 16046, `ModelId2` = 0, `UnitFlags` = 33554432, `PetSpellDataId` = 0 WHERE `entry` = 22799;
+-- [DND]Prophecy 3 Quest Credit 22800
+UPDATE `creature` SET `modelid` = 11686, `position_x` = 3734.708, `position_y` = 6639.487, `position_z` = 133.7359, `orientation` = 6.265732 WHERE `id` = 22800;
+UPDATE `creature_template` SET `ModelId1` = 16046, `ModelId2` = 0, `UnitFlags` = 33554432, `PetSpellDataId` = 0 WHERE `entry` = 22800;
+-- [DND]Prophecy 4 Quest Credit 22801
+UPDATE `creature` SET `modelid` = 11686, `position_x` = 3575.479, `position_y` = 6666.419, `position_z` = 128.5616, `orientation` = 3.298672 WHERE `id` = 22801;
+UPDATE `creature_template` SET `ModelId1` = 16046, `ModelId2` = 0, `UnitFlags` = 33554432, `PetSpellDataId` = 0 WHERE `entry` = 22801;
+
