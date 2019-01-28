b04b5df8fbf530d9e75fc0a7761ca7c4ec5dcef7
anonxs <anonxs@web.de>
2019-01-28 19:43:57 +0100

    [CDB][2286] Add missing auras to creature_template_addon and acid
    based on https://github.com/cmangos/tbc-db/commit/ba6e0aa979ebf2992220c3e1b647613aa4d44aff

--- Updates/0703_CDB-2286_creature_template_addon.sql
+++ Updates/0703_CDB-2286_creature_template_addon.sql
@@ -1,30 +1,36 @@
 -- Add missing auras in creature_template_addon (and sometimes even the template itself)
 
--- temp spells (10secs duration) - recheck how these are applied over time (generic timer?)
-/*
 -- Add missing poison proc aura for NPC 5224 (Murk Slitherer)
 UPDATE creature_template_addon SET auras='13299' WHERE entry=5224;
+
 -- Add missing poison proc auras for NPC 2707 (Shadra)
 DELETE FROM creature_template_addon WHERE entry=2707;
-INSERT INTO creature_template_addon VALUES
-(2707, 0, 0, 1, 16, 0, 0, '8601 13299');
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (2707, 0, 0, 1, 16, 0, 0, '8601 13299');
 
 -- Add missing damage shield aura for NPC 4397 (Mudrock Spikeshell)
-UPDATE creature_template_addon SET auras='8393' WHERE entry=4397;
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` = 4397);
+DELETE FROM creature_template_addon WHERE entry=4397;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (4397, 0, 0, 1, 16, 0, 0, '8393');
 
 -- Add missing poison proc aura for NPC 3398 (Gesharahan)
-UPDATE creature_template_addon SET auras='10022' WHERE entry=3398;
+DELETE FROM creature_template_addon WHERE entry=3398;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (3398, 0, 0, 1, 16, 0, 0, '10022');
+
 -- Add missing poison proc aura for NPC 2541 (Lord Sakrasis)
-UPDATE creature_template_addon SET auras='10022' WHERE entry=2541;
+DELETE FROM creature_template_addon WHERE entry=2541;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (2541, 0, 0, 1, 16, 0, 0, '10022');
 
 -- Add missing poison proc aura for NPC 3383 (Southsea Cutthroat)
-UPDATE creature_template_addon SET auras='7276' WHERE entry=3383;
+DELETE FROM creature_template_addon WHERE entry=3383;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (3383, 0, 0, 1, 16, 0, 0, '7276');
+
 -- Add missing wandering plague proc auras for NPC 877 (Saltscale Forager)
-UPDATE creature_template_addon SET auras='7276' WHERE entry=877;
+DELETE FROM creature_template_addon WHERE entry=877;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (877, 0, 0, 1, 16, 0, 0, '7276');
 
 -- Add missing wandering plague proc auras for NPC 1824 (Plague Lurker)
-UPDATE creature_template_addon SET auras='8247' WHERE entry=1824; -- 45secs
-*/
+DELETE FROM creature_template_addon WHERE entry=1824;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (1824, 0, 0, 1, 16, 0, 0, '8247');
 
 -- confirmed
 -- Add missing thrash and stealth aura for NPC 2522 (Jaguero Stalker)
