32f21e5af45338d11143f9024c8d4b8b3757bef3
anonxs <anonxs@web.de>
2019-02-02 14:18:47 +0100

    Correct Default Faction, NpcFlags=1 (Script) and Speed for Magister Aledis 20159 - npc_magister_aledis

---
+++ Updates/0734_c20159.sql
@@ -0,0 +1,3 @@
+-- Magister Aledis 20159 - npc_magister_aledis
+UPDATE `creature_template` SET  `Faction` = 1604, `NpcFlags` = 0, `SpeedWalk` = (2.5 / 2.5), `SpeedRun` = (9.7 / 7) WHERE `entry` = 20159;
+
