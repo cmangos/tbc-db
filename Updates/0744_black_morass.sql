746612795df2b0e103b81cce2fcf1882156d224b
anonxs <anonxs@web.de>
2019-02-10 15:10:28 +0100

    Correct ModelStats for Temporus 17880,20745 & Aeonus 17881,20737

--- Updates/0744_black_morass.sql
+++ Updates/0744_black_morass.sql
@@ -1,6 +1,7 @@
 -- Temporus 17880,20745
 UPDATE `creature_template` SET `ExtraFlags` = `ExtraFlags`|256, `Leash` = 80 WHERE `entry` = 17880;
 UPDATE `creature_template` SET `MinLevel` = 72, `MaxLevel` = 72, `Faction` = 1720, `ExtraFlags` = `ExtraFlags`|256, `Leash` = 80 WHERE `entry` = 20745;
+UPDATE `creature_model_info` SET `bounding_radius` = 0.525, `combat_reach` = 1.75 WHERE `modelid` = 19066;
 
 -- Chrono Lord Deja 17879,20738
 UPDATE `creature_template` SET `Leash` = 80 WHERE `entry` = 17879;
@@ -13,6 +14,7 @@ UPDATE `creature_template` SET `Faction` = 1720, `Leash` = 80 WHERE `entry` = 21
 -- Aeonus 17881,20737
 -- UPDATE `creature_template` SET  WHERE `entry` = 17881;
 UPDATE `creature_template` SET `Faction` = 1720 WHERE `entry` = 20737;
+UPDATE `creature_model_info` SET `bounding_radius` = 0.3, `combat_reach` = 10 WHERE `modelid` = 20510;
 
 -- Rift Lord 17839, 20744, 21140, 22172 & Rift Keeper 21104, 22170, 21148, 22171
 UPDATE `creature_template` SET `Leash` = 80 WHERE `entry` IN (17839, 20744, 21140, 22172, 21104, 22170, 21148, 22171);
