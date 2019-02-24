e4404499536f171e4e8a96b25905800fe5dff592
anonxs <anonxs@web.de>
2019-02-24 16:24:25 +0100

    Correct Speed for Sightless Eye 21346,21612

---
+++ Updates/0763_c21346,21612.sql
@@ -0,0 +1,4 @@
+-- Sightless Eye 21346,21612
+UPDATE `creature_template` SET `SpeedWalk` = (2.5 / 2.5), `SpeedRun` = (17 / 7) WHERE `entry` = 21346;
+UPDATE `creature_template` SET `SpeedWalk` = (2.5 / 2.5), `SpeedRun` = (17 / 7) WHERE `entry` = 21612;
+
