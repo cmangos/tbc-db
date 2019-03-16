53ea3dc32fc93232d1f5aca126094c7aa598d868
anonxs <anonxs@web.de>
2019-03-16 15:27:30 +0100

    Correct Resistances for "Noxxio" Npcs - Bestiary

---
+++ Updates/0792_Noxxio_Resistance.sql
@@ -0,0 +1,8 @@
+-- Correct Resistances for "Noxxio" Npcs
+UPDATE `creature_template` SET `ResistanceFire` = 0, `ResistanceShadow` = 0, `ResistanceArcane` = 0 WHERE `entry` IN (
+13282, -- Noxxion
+13456, -- Noxxion's Spawn
+13696, -- Noxxious Scion
+13736 -- Noxxious Essence
+);
+
