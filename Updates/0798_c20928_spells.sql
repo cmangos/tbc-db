22c8a2a2111cde39f7658f639ec0dadaf97e3bbb
anonxs <anonxs@web.de>
2019-03-19 11:00:01 +0100

    Use Parentspell for Ironspine Forgelord 20928 creature_template_spells

---
+++ Updates/0798_c20928_spells.sql
@@ -0,0 +1,3 @@
+-- Use Parentspell for Ironspine Forgelord 20928 creature_template_spells
+UPDATE `creature_template_spells` SET `spell1` = 36251 WHERE `spell1` = 37179 AND `entry` = 20928; -- same issue as with "thrash" ability
+
