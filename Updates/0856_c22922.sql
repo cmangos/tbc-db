0b22866d9d3c115f04713a6960e9f15a339519ed
anonxs <anonxs@web.de>
2019-04-28 18:46:28 +0200

    Correct Position and NpcFlags for Innkeeper Aelerya 22922

---
+++ Updates/0856_c22922.sql
@@ -0,0 +1,4 @@
+-- Innkeeper Aelerya 22922
+UPDATE `creature` SET `position_x` = 3022.902, `position_y` = 5435.593, `position_z` = 146.7005, `orientation` = 2.146755 WHERE `id` = 22922;
+UPDATE `creature_template` SET `NpcFlags` = 65537 WHERE `entry` = 22922;
+
