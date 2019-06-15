c5e6a4dba45f750208d2cd98f7cff1a762c7c094
AnonXS <anonxs@web.de>
2019-06-15 20:17:18 +0200

    Update and rename 0889_Fix_AV_boss_duplicates.sql to 0902_Fix_AV_boss_duplicates.sql
    some more info

--- Updates/0889_Fix_AV_boss_duplicates.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `creature` WHERE `guid`='51867';
-DELETE FROM `creature` WHERE `guid`='97135';
-DELETE FROM `creature` WHERE `guid`='97136';
-DELETE FROM `creature` WHERE `guid`='97137';
---
+++ Updates/0902_Fix_AV_boss_duplicates.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM `creature` WHERE `id` IN (11948,11946,11949,11947);
+-- Reworked AV Export Outstanding
+DELETE FROM `creature` WHERE `guid`='51867'; -- duplicate 97134
+DELETE FROM `creature` WHERE `guid`='97135'; -- duplicate 51865
+DELETE FROM `creature` WHERE `guid`='97136'; -- duplicate 51868
+DELETE FROM `creature` WHERE `guid`='97137'; -- duplicate 51866
+
