89e1757c8bd1b0e1bc123b77187a705a1ea4e91d
AnonXS <anonxs@web.de>
2019-06-17 18:43:46 +0200

    Update and rename 0903_Fix_wsg_flag_positions.sql to 0904_Fix_wsg_flag_positions.sql

--- Updates/0903_Fix_wsg_flag_positions.sql
+++
@@ -1,7 +0,0 @@
-# Warsong Gulch
-
-# Silverwing Flag Position (Alliance)
-UPDATE `gameobject` SET `position_x` = '1540.423', `position_y` = '1481.325', `position_z` = '351.8284', `orientation` = '3.089183' WHERE (`guid` = '90000');
-
-# Warsong Flag Position (Horde)
-UPDATE `gameobject` SET `position_x` = '916.5073', `position_y` = '1433.826', `position_z` = '346.3796', `orientation` = '0.01745246' WHERE (`guid` = '90001');
