67e3b649f53d1c9791df07a6376812160f395b4d
Muehe <Muehe@users.noreply.github.com>
2019-06-24 14:22:12 -0400

    The Perfect Stout quest chaining fixes
    Ref: https://github.com/cmangos/issues/issues/1985

---
+++ Updates/0911_the_perfect_stout_quest_chain.sql
@@ -0,0 +1,5 @@
+UPDATE `quest_template` SET `PrevQuestId`='0', `NextQuestInChain`='413' WHERE `entry`='315';
+UPDATE `quest_template` SET `PrevQuestId`='315' WHERE `entry`='413';
+UPDATE `quest_template` SET `PrevQuestId`='315', `NextQuestId`='0' WHERE `entry`='415';
+
+
