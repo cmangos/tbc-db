e74a4597699dc1d091ac120cdc08d1b749a72a78
Sneaky <christopher.ron.hansen@gmail.com>
2019-07-08 15:49:05 -0400

    [ACID] Swamplord Musel'ek improvements
    Some AI and flag updates to improve this fight.
    
    New data compared/found with sniff.

---
+++ Updates/0959_swamplord_muselek_rework.sql
@@ -0,0 +1,7 @@
+-- Claw 17827,20165
+UPDATE `creature_template` SET `UnitFlags` = 512 WHERE `entry` IN (17827,20165);
+
+-- Windcaller Claw 17894
+UPDATE `creature_template` SET `UnitFlags` = 33536, `Faction` = 1660 WHERE `Entry` = 17894;
+
+
