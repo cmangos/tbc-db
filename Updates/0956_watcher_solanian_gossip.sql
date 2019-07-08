ac08aee2761f0ca3f7ab790131fd92370be57800
MantisLord <sabinprosper@gmail.com>
2019-07-08 15:28:55 -0400

    Fix export error

--- Updates/0956_watcher_solanian_gossip.sql
+++ Updates/0956_watcher_solanian_gossip.sql
@@ -3,7 +3,7 @@ DELETE FROM `npc_text` WHERE `ID`=7791;
 INSERT INTO `npc_text` (`ID`, `text0_0`, `prob0`) VALUES
 (7791, 'What are you doing here, $r?', 1);
 
-DELETE FROM `gossip_menu` WHERE `entry`=6569;
+DELETE FROM `gossip_menu` WHERE `entry`=6569 AND `text_id`=7791;
 INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES
 (6569, 7791, 0, 278);
 
