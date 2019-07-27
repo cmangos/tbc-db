150b60ac0bd9435e5349dc5ca64b270cd7e93a11
anonxs <anonxs@web.de>
2019-07-27 21:44:43 +0200

    Align comments for spell_bonus_data table

---
+++ Updates/0994_spell_bonus_data_comments.sql
@@ -0,0 +1,4 @@
+-- Align comments for spell_bonus_data table
+UPDATE `spell_bonus_data` SET `comments` = 'Warlock - Health Funnel' WHERE `entry` = 755; -- Warlock - Health Funnel (Rank 1)
+UPDATE `spell_bonus_data` SET `comments` = 'Shaman - Flametongue Attack' WHERE `entry` = 16368; -- NULL
+
