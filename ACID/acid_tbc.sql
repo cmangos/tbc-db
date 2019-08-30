e16f4a810e15375beb2109fd2c904b66a7bdb536
killerwife <killerwife@gmail.com>
2019-08-30 11:26:45 +0200

    [ACID] Lower change to summon Deepmoss Matriarch

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -13707,7 +13707,7 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Gerenzo Wrenchwhistle 4202
 ('420201','4202','2','0','100','0','15','0','0','0','0','0','25','0','0','0','1','-47','0','0','0','0','0','0','Gerenzo Wrenchwhistle - Flee at 15% HP'),
 -- Deepmoss Hatchling
-('426301','4263','2','0','50','0','30','0','0','0','0','0','11','6536','0','0','1','-551','0','0','0','0','0','0','Deepmoss Hatchling - Cast Summon Deepmoss Matriarch at 30% HP'),
+('426301','4263','2','0','30','0','30','0','0','0','0','0','11','6536','0','0','1','-551','0','0','0','0','0','0','Deepmoss Hatchling - Cast Summon Deepmoss Matriarch at 30% HP'),
 -- Deepmoss Matriarch (4264) - NSR
 -- Piznik (4276) - NSR
 -- Tharm
