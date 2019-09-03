0b1e977571b71b28c74726f82bba69dfad0dcf1f
MantisLord <sabinprosper@gmail.com>
2019-09-03 11:16:18 -0400

    [ACID] Justin 1368
    Fixed and added proper randomization/timing to script for Justin, Brandon, and Roman.
    
    Some texts were duplicated or missing entirely.
    
    TODO: Add missing movement and Justin text

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -5549,7 +5549,8 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Brother Cassius 1351
 -- Adam (1366) - NSR
 -- Billy (1367) - NSR
--- Justin (1368) - NSR
+-- Justin 1368
+('136801','1368','1','0','100','1','30000','60000','30000','60000','0','0','53','4','0','0','0','0','0','0','0','0','0','0','Justin - Start Relay Script OOC'),
 -- Brandon (1370) - NSR
 -- Roman (1371) - NSR
 -- Ol' Beasley
