9c61941b0727fd14613cf707a24c07a2d62bbc65
anonxs <anonxs@web.de>
2019-09-24 16:56:00 +0200

    [1053] Update dbscript_random_templates = 2 used in ACID

--- Updates/1053_update_0219.sql
+++ Updates/1053_update_0219.sql
@@ -1,4 +1,4 @@
 -- Were moved to 1001 and 1002 but never deleted
 -- https://github.com/cmangos/tbc-db/commit/58aa53e81310595eb4b7703cf2672fa04b34270f
-DELETE FROM dbscript_random_templates WHERE id in (1,2);
+DELETE FROM dbscript_random_templates WHERE id in (1); -- 2 reused
 
