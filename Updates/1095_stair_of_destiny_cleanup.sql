f52a7d7c06bf00b5baae2b67d056372fd9811229
MantisLord <sabinprosper@gmail.com>
2019-11-06 23:24:45 -0500

    Remove extra spawns for Stair of Destiny event
    Ref: https://github.com/cmangos/issues/issues/2061

---
+++ Updates/1095_stair_of_destiny_cleanup.sql
@@ -0,0 +1,4 @@
+-- delete extra leftover static spawns related to Stair of Destiny event
+DELETE FROM `creature` WHERE `guid` IN (68312,68314,67975,67977,67991,67992,67993,67994,67995,67996,67997,67998,67999,68000);
+
+
