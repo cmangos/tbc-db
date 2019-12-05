2ffb0882cabab3fcdc9cb28332d7534c3e99d792
cala <cala@users.noreply.github.com>
2019-12-05 18:20:46 +0100

    [CDB][2618] Add missing targets for Magnetic Pull spells
    * Add missing targets for Magnetic Pull spells
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/61dc0dce5cd778771cb4d55213eb0aab7f896a4a

---
+++ Updates/1112_CDB-2618_magnetic_pull_spell_target.sql
@@ -0,0 +1,6 @@
+-- Add missing targets for Magnetic Pull spells
+DELETE FROM spell_script_target WHERE entry IN (28338, 28339);
+INSERT INTO spell_script_target (entry, type, targetEntry, inverseEffectMask) VALUES
+(28338, 1, 15929, 0), -- Magnetic Pull -> Stalagg
+(28339, 1, 15930, 0); -- Magnetic Pull -> Feugen
+
