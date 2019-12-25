09b04938a870e821d3e01a7db669dc88095baa91
Killerwife <killerwife@gmail.com>
2019-12-25 16:31:18 +0100

    AQ40 twins doors should start closed
    (cherry picked from commit 5eb1375b3e2ad5aca6ce79a146b332ca1bb95b7e)

--- Updates/Instances/531_temple_of_ahnqiraj.sql
+++ Updates/Instances/531_temple_of_ahnqiraj.sql
@@ -1544,7 +1544,7 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `positi
 -- ===========
 
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`) VALUES
-(@OGUID+1, 180634, 531, -8929.61, 1344.38, -104.211, 3.84846, 0, 0, -0.938191, 0.346119, 7200, 7200, 100, 0), -- Door
+(@OGUID+1, 180634, 531, -8929.61, 1344.38, -104.211, 3.84846, 0, 0, -0.938191, 0.346119, 7200, 7200, 100, 1), -- Door
 (@OGUID+2, 180635, 531, -9016.36, 1330.68, -104.211, 2.80125, 0, 0, 0.985556, 0.169352, 7200, 7200, 100, 1), -- Door
 (@OGUID+3, 180636, 531, -8429.59, 2048.81, 153.309, 3.51685, 0, 0, -0.982449, 0.186529, 7200, 7200, 100, 1), -- Door
 (@OGUID+4, 180690, 531, -8561.12, 1989.54, -3.09152, 4.18879, 0, 0, -0.866025, 0.500001, 7200, 7200, 100, 1), -- Large Scarab Coffer
