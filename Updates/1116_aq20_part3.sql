0956b8c55107cb3b121ba3f1b8fc56ac221c8330
Killerwife <killerwife@gmail.com>
2019-12-25 23:36:10 +0100

    Adjust indices on ayamiss
    Need to number from 0

--- Updates/1116_aq20_part3.sql
+++ Updates/1116_aq20_part3.sql
@@ -1,8 +1,8 @@
 -- ayamiss landing points
 INSERT INTO `creature_movement_template` (`entry`,`pathId`,`point`,`position_x`,`position_y`,`position_z`,`waittime`,`script_id`,`orientation`) VALUES
-(15369,1,1,-9689.981,1548.296,33.27733,0,0,100),
-(15369,1,2,-9682.716,1554.252,31.41621,0,0,100),
-(15369,1,3,-9677.917,1558.839,27.24953,1000,0,100);
+(15369,1,0,-9689.981,1548.296,33.27733,0,0,100),
+(15369,1,1,-9682.716,1554.252,31.41621,0,0,100),
+(15369,1,2,-9677.917,1558.839,27.24953,1000,0,100);
 
 -- hack - this bypasses despawn prevention due to GO casting a hidden 6 second spell the GO should cast - note will be put down even in core
 UPDATE gameobject_template SET data3=65536*6 WHERE entry IN(180619);
