30b2d354477efc319f5b2ddc7813f230acff1a06
anonxs <anonxs@web.de>
2020-01-02 10:57:17 +0100

    Correct Typo in previous commit

--- Updates/1124_flak_cannon_ai.sql
+++ Updates/1124_flak_cannon_ai.sql
@@ -6,8 +6,3 @@ INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`
 -- Legion Flak Cannon 23082
 UPDATE `creature_template` SET `InhabitType` = 7 WHERE `entry` = 23082;
 
-
-
-INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `comment`) 
-	VALUES 
-    
