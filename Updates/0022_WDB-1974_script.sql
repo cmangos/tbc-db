8088905114ea777aa05bf2922ce1229b9c909996
Grz3s <Grz3s@users.noreply.github.com>
2020-01-11 23:03:37 +0000

    [WDB][1974] Spinebreaker Post - Hellfire Peninsula
    Thrallmar Grunt - script fixed
    Console error fixed
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/da65d97c9f3869fae59125e5430303cbaf3107f1

---
+++ Updates/0022_WDB-1974_script.sql
@@ -0,0 +1,4 @@
+-- Spinebreaker Post - Hellfire Peninsula
+-- Thrallmar Grunt - script fixed
+UPDATE dbscripts_on_creature_movement SET search_radius = 15 WHERE id IN (1658002) AND delay IN (23000);
+
