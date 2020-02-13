c6d5e77909cbdf8fbf89faf8ff7bc139bcd75c03
MantisLord <sabinprosper@gmail.com>
2020-02-13 15:01:30 -0500

    Fixes for Andormu
    Child version is not a vendor
    
    Quest 10285 should be turned into child version of Andormu, not the adult version

---
+++ Updates/0042_andormu.sql
@@ -0,0 +1,11 @@
+-- 20130 Andormu (child)
+-- 19932 Andormu (adult)
+
+UPDATE creature_template SET NpcFlags=2, VendorTemplateId=0 WHERE Entry=20130; -- child version is not a vendor
+
+-- Return to Andormu 10285 should be turned into child Andormu, not adult
+DELETE FROM creature_involvedrelation WHERE id=19932 AND quest=10285;
+INSERT INTO creature_involvedrelation (id, quest) VALUES
+(20130, 10285);
+
+
