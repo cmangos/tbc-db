61c947273485f063b0db3f3d1c78061b3eee953d
Miraco <daaniel743@gmail.com>
2020-02-13 13:55:28 +0100

    more errors

--- Updates/Instances/540_shattered_halls.sql
+++ Updates/Instances/540_shattered_halls.sql
@@ -459,7 +459,6 @@ INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
 -- link "leaders" to bosses
 (@CGUID+74, @CGUID+82, 1024), -- Shattered Hand Legionnaire -> Grand Warlock Nethekurse
 (@CGUID+134, @CGUID+74, 1024), -- creature_spawn_entry -> Shattered Hand Legionnaire
-(@CGUID+134, @CGUID+74, 1024), -- creature_spawn_entry -> Shattered Hand Legionnaire
 (@CGUID+135, @CGUID+74, 1024), -- creature_spawn_entry -> Shattered Hand Legionnaire
 (@CGUID+137, @CGUID+74, 1024), -- creature_spawn_entry -> Shattered Hand Legionnaire
 (@CGUID+138, @CGUID+74, 1024), -- creature_spawn_entry -> Shattered Hand Legionnaire
@@ -468,7 +467,6 @@ INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
 (@CGUID+306, @CGUID+74, 1024), -- Shattered Hand Heathen -> Shattered Hand Legionnaire
 (@CGUID+307, @CGUID+74, 1024), -- Shattered Hand Heathen -> Shattered Hand Legionnaire
 (@CGUID+308, @CGUID+74, 1024), -- Shattered Hand Heathen -> Shattered Hand Legionnaire
-(@CGUID+138, @CGUID+74, 1024), -- Shattered Hand Heathen -> Shattered Hand Legionnaire
 (@CGUID+52, @CGUID+53, 1167), -- Shattered Hand Sentry -> Shattered Hand Sentry
 -- (@CGUID+219, @CGUID+139, 1167), -- Shadowmoon Darkcaster -> Shattered Hand Heathen -- or each side together
 -- (@CGUID+67, @CGUID+142, 1167), -- Shadowmoon Acolyte -> Shattered Hand Heathen
@@ -906,12 +904,12 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `positio
 (@CGUID+300, 17695, 540, 3, 467.415, 57.921, 1.936, 0.0064, 86400, 86400, 2, 0, 0, 1), -- Shattered Hand Assassin
 (@CGUID+301, 17695, 540, 3, 274.497, 317.332, -0.3597, 6.27, 86400, 86400, 2, 0, 0, 1), -- Shattered Hand Assassin
 (@CGUID+302, 17695, 540, 3, 519.4913, 265.4981, 1.943648, 1.67471, 86400, 86400, 2, 0, 0, 1), -- Shattered Hand Assassin
-(@CGUID+303, 17695, 540, 3, 518.3307, 125.028, 1.981626, 4.529583, 86400, 86400, 0, 0, 0, 2); -- Shattered Hand Assassin
+(@CGUID+303, 17695, 540, 3, 518.3307, 125.028, 1.981626, 4.529583, 86400, 86400, 0, 0, 0, 2), -- Shattered Hand Assassin
 (@CGUID+304, 17420, 540, 2, 46.3248, 59.879, -12.9904, 0, 1000, 1000, 0, 0, 0, 2), -- Shattered Hand Heathen
 (@CGUID+305, 17420, 540, 2, 83.755, 57.2887, -13.114, 3.15, 1000, 1000, 0, 0, 0, 2), -- Shattered Hand Heathen
 (@CGUID+306, 17420, 540, 2, 53.5243, 60.0879, -13.0071, 0, 1000, 1000, 0, 0, 0, 2), -- Shattered Hand Heathen
 (@CGUID+307, 17420, 540, 2,  90.0297, 57.4987, -13.1003, 3.261756, 1000, 1000, 0, 0, 0, 2), -- Shattered Hand Heathen
-(@CGUID+308, 17420, 540, 2, 87.9912, 57.5445, -13.1049, 3.2617, 1000, 1000, 0, 0, 0, 2), -- Shattered Hand Heathen
+(@CGUID+308, 17420, 540, 2, 87.9912, 57.5445, -13.1049, 3.2617, 1000, 1000, 0, 0, 0, 2); -- Shattered Hand Heathen
 -- ===========
 -- GAMEOBJECTS
 -- ===========
