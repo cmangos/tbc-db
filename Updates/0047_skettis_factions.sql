4d16bc7de66af9e9b6dd0b812acab3855dd23b4f
MantisLord <sabinprosper@gmail.com>
2020-02-14 22:57:15 -0500

    Correct faction for some creatures in Skettis

---
+++ Updates/0047_skettis_factions.sql
@@ -0,0 +1,6 @@
+UPDATE creature_template SET Faction=1869 WHERE Entry=21804; -- Skettis Kaliri
+UPDATE creature_template SET Faction=1862 WHERE Entry=21644; -- Skettis Wing Guard
+UPDATE creature_template SET Faction=1862 WHERE Entry=21650; -- Skettis Talonite
+UPDATE creature_template SET Faction=1862 WHERE Entry=21649; -- Skettis Windwalker
+
+
