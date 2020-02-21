bff838b14a22d7e418aa467664678c2412f5aeed
anonxs <anonxs@web.de>
2020-02-21 16:14:05 +0100

    Correct Level for Focus Fire 18374,20308 summoned by s.32283 by Shirrak the Dead Watcher (18371) - boss_shirrak
    Resolves https://gitlab.com/lights-vengeance/issues/issues/126

---
+++ Updates/0062_focus_fire18374,20308.sql
@@ -0,0 +1,4 @@
+-- Focus Fire 18374,20308 summoned by s.32283 by Shirrak the Dead Watcher (18371) - boss_shirrak
+UPDATE `creature_template` SET `MinLevel` = 65, `MaxLevel` = 65 WHERE `entry` = 18374;
+UPDATE `creature_template` SET `MinLevel` = 70, `MaxLevel` = 70 WHERE `entry` = 20308;
+
