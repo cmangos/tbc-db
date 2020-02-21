35d3a614f10f17efc6e5d5b65a3338b1219fa2fd
MantisLord <sabinprosper@gmail.com>
2020-02-21 06:37:46 -0500

    Update data for Dark Portal Beam Invisible Stalker 18555

---
+++ Updates/0061_dark_portal_beam_invis_stalker.sql
@@ -0,0 +1,4 @@
+-- Dark Portal Beam Invisible Stalker 18555
+UPDATE creature_template SET MovementType=2, SpeedWalk=(10/2.5), InhabitType=7, ModelId2=11686, ExtraFlags=ExtraFlags|16384 WHERE entry=18555;
+
+
