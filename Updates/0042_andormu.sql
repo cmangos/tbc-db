3ff94dfc7b5dbf15947c82a0ed46a5dbb3f0dff0
MantisLord <sabinprosper@gmail.com>
2020-02-21 03:01:27 -0500

    Fix turn-in NPC for quest 10298 "Hero of the Brood"
    Should be turned in to child Andormu, not adult version

--- Updates/0042_andormu.sql
+++ Updates/0042_andormu.sql
@@ -8,4 +8,10 @@ DELETE FROM creature_involvedrelation WHERE id=19932 AND quest=10285;
 INSERT INTO creature_involvedrelation (id, quest) VALUES
 (20130, 10285);
 
+-- Hero of the Brood should be turned in to child Andormu, not adult
+DELETE FROM creature_involvedrelation WHERE id=19932 AND quest=10298;
+DELETE FROM creature_involvedrelation WHERE id=20130 AND quest=10298;
+INSERT INTO creature_involvedrelation (id, quest) VALUES
+(20130, 10298);
+
 
