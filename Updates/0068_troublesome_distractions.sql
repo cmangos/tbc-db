5b7109379f2bef67ff13f69fb24dc761bc7d2b2b
MantisLord <sabinprosper@gmail.com>
2020-02-26 19:26:38 -0500

    Fix q.10273 'Troublesome Distractions'
    Wind Trader Marid should become attackable

---
+++ Updates/0068_troublesome_distractions.sql
@@ -0,0 +1,5 @@
+DELETE FROM dbscripts_on_creature_movement WHERE id=2007101 AND priority=3;
+INSERT INTO dbscripts_on_creature_movement (id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(2007101, 3600, 3, 5, 46, 256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Wind Trader Marid - Remove Unit Flag IMMUNE_TO_PLAYER');
+
+
