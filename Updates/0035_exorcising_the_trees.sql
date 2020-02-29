c3a43ae1af4db27fefa2b737b10a16d8524a4307
MantisLord <sabinprosper@gmail.com>
2020-02-28 22:46:14 -0500

    Fix spell dummy script used in q.10830 'Exorcising the Trees'
    * This script occurs on cast of Koi-Koi Death, not Exorcism Feather
    
    * Fix source/target such that Raven's Wood Leafbeard is the one saying the text

--- Updates/0035_exorcising_the_trees.sql
+++ Updates/0035_exorcising_the_trees.sql
@@ -5,7 +5,7 @@ INSERT INTO spell_script_target(entry, type, targetEntry, inverseEffectMask) VAL
 
 -- note - I still use spell script in core but this is the easiest way to add delay to something
 INSERT INTO dbscripts_on_spell(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
-('38606', '4000', '0', '0', '0', '0', '0', '0', '0', '0', '2000010178', '0', '0', '0', '0', '0', '0', '0', 'Leafbeard Delayed Emote/Text');
+('38640', '4000', '0', '0', '0', '0', '0', '0', '0', '2', '2000010178', '0', '0', '0', '0', '0', '0', '0', 'Leafbeard Delayed Emote/Text');
 
 INSERT INTO `dbscript_string` (`entry`,`content_default`,`sound`,`type`,`language`,`emote`,`broadcast_text_id`,`comment`) VALUES
 ('2000010178','The %s appears very grateful to be free of the koi-koi spirit''s influence.','0','2','0','34','19943','21326');
