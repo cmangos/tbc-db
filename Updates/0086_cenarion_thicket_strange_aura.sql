32f5bba63d9c562e2aa6b6345be76dc60ea3e3ce
MantisLord <sabinprosper@gmail.com>
2020-03-05 16:04:36 -0500

    Add missing Strange Aura spell area entry for Cenarion Thicket
    wotlk-db already has this data

---
+++ Updates/0086_cenarion_thicket_strange_aura.sql
@@ -0,0 +1,5 @@
+DELETE FROM spell_area WHERE spell=32407;
+INSERT INTO spell_area (spell, area, quest_start, quest_start_active, quest_end, condition_id, aura_spell, racemask, gender, autocast) VALUES
+(32407, 3674, 0, 0, 0, 0, 0, 0, 2, 1);
+
+
