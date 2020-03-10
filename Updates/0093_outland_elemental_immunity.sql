6ff75aad593ffc633c7adb26d1f696f72ee99dab
MantisLord <sabinprosper@gmail.com>
2020-03-10 11:51:45 -0400

    Add some missing Outland elemental immunities

---
+++ Updates/0093_outland_elemental_immunity.sql
@@ -0,0 +1,89 @@
+-- HELLFIRE PENINSULA
+
+-- Aeranas
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17085';
+
+-- ZANGARMARSH
+
+-- Bloodscale Elemental
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='20792';
+-- Bloodscale Sentry
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='20090';
+-- Darkcrest Sentry
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='20079';
+
+-- NAGRAND
+
+-- Rumbling Earth-Heart
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='22313';
+-- Storming Wind-Ripper
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='22310';
+-- Lake Spirit
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='17153';
+-- Enraged Crusher
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='18062';
+-- Storm Rager
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17159';
+-- Living Cyclone
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17160';
+-- Lake Surger
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17155';
+-- Watoosun's Polluted Essence
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='18145';
+-- Muck Spawn
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17154';
+-- Shattered Rumbler
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17157';
+-- Tortured Earth Spirit
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17156';
+-- Dust Howler
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='17158';
+-- Minion of Gurok
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='18181';
+-- Minion of Gurok
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='18182';
+-- Rokdar the Sundered Lord
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='18400';
+
+-- SHADOWMOON VALLEY
+
+-- Arcane Burst
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'64' WHERE `Entry`='22022';
+-- Corrupted Water Elemental
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='21428';
+-- Corrupted Fire Elemental
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'4' WHERE `Entry`='21706';
+-- Corrupted Earth Elemental
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='21708';
+-- Corrupted Air Elemental
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='21707';
+-- Enraged Air Spirit
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='21060';
+-- Enraged Earth Shard
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='22115';
+-- Enraged Water Spirit
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='21059';
+-- Enraged Earth Spirit
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384 WHERE `Entry`='21050';
+-- Rumbling Earth-Heart
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='22313';
+
+-- BLADE'S EDGE MOUNTAINS
+
+-- Vile Fire-Soul
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'4' WHERE `Entry`='22298';
+
+-- NETHERSTORM
+
+-- Glacius
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'16' WHERE `Entry`='19653';
+-- Mana Beast
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384 WHERE `Entry`='21267';
+-- Sundered Rumbler
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='18881';
+-- Sundered Shard
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='20498';
+-- Sundered Thunderer
+UPDATE `creature_template` SET `MechanicImmuneMask`=`MechanicImmuneMask`|16384, `SchoolImmuneMask`=`SchoolImmuneMask`|'8' WHERE `Entry`='18882';
+
+
