2fea8fbfec314bdfb5ebd0d7180f80dc0a2507e2
anonxs <anonxs@web.de>
2020-03-15 18:30:52 +0100

    Corrections for Sentinel Keldara Sunblade 8397

---
+++ Updates/0114_c8397.sql
@@ -0,0 +1,5 @@
+-- Sentinel Keldara Sunblade 8397
+UPDATE `creature_template` SET `UnitFlags` = 4608, `LootId` = 0 WHERE `entry` = 8397;
+UPDATE `creature_template_addon` SET `bytes1` = 0 WHERE `entry` = 8397;
+DELETE FROM `creature_loot_template` WHERE `entry` = 8397;
+
