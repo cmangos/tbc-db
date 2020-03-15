27c25d1dc1a4d4b156054fd2da705f368c410ba3
anonxs <anonxs@web.de>
2020-03-15 23:43:07 +0100

    In Dreams q.5944 is turned in at Lord Tirion Fordring 12126

---
+++ Updates/0121_q5944_turn_in.sql
@@ -0,0 +1,4 @@
+-- In Dreams q.5944 is turned in at Lord Tirion Fordring 12126
+-- https://classic.wowhead.com/quest=5944/in-dreams
+UPDATE `creature_involvedrelation` SET `id` = 12126 WHERE `quest` = 5944;
+
