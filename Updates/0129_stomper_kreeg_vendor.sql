8d21a930003a961e812289812b60d667e7c044b2
MantisLord <sabinprosper@gmail.com>
2020-03-17 22:24:32 -0400

    Stomper Kreeg - add missing vendor data
    Ported classic-db data with some corrections:
    
    Gordok Green Grog 18269 and Kreeg's Stout Beatdown 18284 should not have a limited supply
    
    Increased restock time for Volatile Rum

---
+++ Updates/0129_stomper_kreeg_vendor.sql
@@ -0,0 +1,9 @@
+DELETE FROM npc_vendor WHERE entry=14322;
+INSERT INTO npc_vendor (entry, item, maxcount, incrtime, condition_id) VALUES
+('14322', '9260', '6', '10800', '0'),
+('14322', '18269', '0', '0', '0'),
+('14322', '18284', '0', '0', '0'),
+('14322', '18287', '0', '0', '0'),
+('14322', '18288', '0', '0', '0');
+
+
