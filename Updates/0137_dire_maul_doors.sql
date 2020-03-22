4b88b71071bde01efb7e9178e014fb6eb7af5c47
MantisLord <sabinprosper@gmail.com>
2020-03-22 03:12:32 -0400

    Another missing torch-door opening script

--- Updates/0137_dire_maul_doors.sql
+++ Updates/0137_dire_maul_doors.sql
@@ -1,6 +1,7 @@
-DELETE FROM dbscripts_on_go_use WHERE id IN (49958,49960);
+DELETE FROM dbscripts_on_go_use WHERE id IN (49958,49960,49967);
 INSERT INTO dbscripts_on_go_use (id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 (49958, 0, 0, 11, 49943, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
-(49960, 0, 0, 11, 49945, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+(49960, 0, 0, 11, 49945, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(49967, 0, 0, 11, 49956, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
 
 
