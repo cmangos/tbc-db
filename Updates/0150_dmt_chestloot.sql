d4bdba35684c2baf659cb1bb75816b77dd240194
anonxs <anonxs@web.de>
2020-03-23 01:42:41 +0100

    Remove Addition of i.18655 already included in refloot 35037

--- Updates/0150_dmt_chestloot.sql
+++ Updates/0150_dmt_chestloot.sql
@@ -22,8 +22,5 @@ UPDATE `item_template` SET `SellPrice` = 30986 WHERE `entry` = 18482; -- Ogre To
 UPDATE `item_template` SET `SellPrice` = 51451 WHERE `entry` = 18481; -- Skullcracking Mace
 
 -- Add missing Schematic: Major Recombobulator 18655
--- https://classic.wowhead.com/item=18655/schematic-major-recombobulator#contained-in-object
-DELETE FROM `gameobject_loot_template` WHERE `entry` = 16577 AND `item` = 18655;
-INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `comments`) VALUES
-(16577, 18655, 5, 0, 1, 1, 0, 'Schematic: Major Recombobulator');
+-- Included in refloot 35037
 
