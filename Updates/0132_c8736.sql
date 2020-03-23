ddfd331231407ecf98d585e83f8fdeeea777ee64
anonxs <anonxs@web.de>
2020-03-23 18:19:37 +0100

    Give Buzzek Bracketswing 8736 Engineering (Artisan) Trainer only Classic Spells
    As it is not sure that he had tbc spells in tbc already

--- Updates/0132_c8736.sql
+++ Updates/0132_c8736.sql
@@ -1,4 +1,4 @@
 -- Correct TrainerTemplateId for Buzzek Bracketswing 8736 Engineering (Artisan) Trainer (Fix may vary between expansions)
 -- https://web.archive.org/web/20110717132326/http://www.wowhead.com:80/npc=8736#teaches-recipe:0-16+1+3 seems to always have the highest template
-UPDATE `creature_template` SET `TrainerTemplateId` = 1001 WHERE `entry` = 8736;
+UPDATE `creature_template` SET `TrainerTemplateId` = 1000 WHERE `entry` = 8736;
 
