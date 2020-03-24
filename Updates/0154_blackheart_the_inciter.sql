6860ab0fbcf8ea57188281fb2415f94a4cdafd00
killerwife <killerwife@gmail.com>
2020-03-24 22:30:52 +0100

    Add s.45339 targeting for Blackheart

---
+++ Updates/0154_blackheart_the_inciter.sql
@@ -0,0 +1,8 @@
+DELETE FROM spell_script_target WHERE entry IN(45339);
+INSERT INTO spell_script_target(entry, type, targetEntry, inverseEffectMask) VALUES
+(45339,1,19300,0),
+(45339,1,19301,0),
+(45339,1,19302,0),
+(45339,1,19303,0),
+(45339,1,19304,0);
+
