f256bce9d4a0073394add8925af1ea49d0726fb0
MantisLord <sabinprosper@gmail.com>
2020-03-25 02:29:16 -0400

    Require attunement to enter Molten Core via Window/Lava portals
    Can still enter without completing the attunement quest using BRD "The Molten Bridge" entrance

---
+++ Updates/0156_molten_core_attune.sql
@@ -0,0 +1,5 @@
+UPDATE areatrigger_teleport SET status_failed_text='You must be part of a Raid group and at least level 50 to enter.' WHERE id=2886; -- The Molten Bridge
+UPDATE areatrigger_teleport SET required_quest_done=7848, status_failed_text='You must be attuned and in a raid group to enter.' WHERE id=3529; -- The Molten Core Window(Lava) Entrance
+UPDATE areatrigger_teleport SET required_quest_done=7487 WHERE id=3528; -- The Molten Core Window Entrance
+
+
