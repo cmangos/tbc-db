0408b73a7d8a9d558748b0883965321c41af0a99
killerwife <killerwife@gmail.com>
2020-03-25 23:20:44 +0100

    Adjust belf and draenei starter zone respawn timers

---
+++ Updates/0160_entry_zones_spawn_times.sql
@@ -0,0 +1,6 @@
+UPDATE creature SET spawntimesecsmin=90,spawntimesecsmax=90 WHERE id IN(16520,16516); -- ammen vale
+UPDATE creature SET spawntimesecsmin=90,spawntimesecsmax=90 WHERE id IN(15274,15366); -- sunstrider isle
+
+UPDATE creature SET spawntimesecsmin=150,spawntimesecsmax=150 WHERE id IN(16517,16518,16521,16537); -- ammen vale
+UPDATE creature SET spawntimesecsmin=150,spawntimesecsmax=150 WHERE id IN(15271,15294,15372,15273); -- sunstrider isle
+
