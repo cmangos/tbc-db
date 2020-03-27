19e079b92d2afe2915df77734beb27a22fc3dd98
anonxs <anonxs@web.de>
2020-03-27 13:26:55 +0100

    Remove Gems from creature_loot_template

---
+++ Updates/0165_gems_npc_drops.sql
@@ -0,0 +1,14 @@
+-- Remove Gems from creature_loot_template
+DELETE FROM `creature_loot_template` WHERE `item` IN (21929,
+23077,
+23079,
+23107,
+23112,
+23117,
+23436,
+23437,
+23438,
+23439,
+23440,
+23441
+);
