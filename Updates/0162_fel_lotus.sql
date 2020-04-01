57214a84163f6966d30b24622e82a252fb2a4520
anonxs <anonxs@web.de>
2020-04-01 13:21:25 +0200

    Only one Fel Lotus may drop from Outland Herbs

--- Updates/0162_fel_lotus.sql
+++ Updates/0162_fel_lotus.sql
@@ -1,3 +1,9 @@
 -- no evidence that fel lotus should drop more than once
 UPDATE gameobject_loot_template SET maxcount=1 WHERE item IN(22794);
+UPDATE `reference_loot_template` SET `maxcount` = 1 WHERE `item` = 22794 AND `entry` IN (
+12015, -- Felweed
+12016, -- Dreaming Glory
+12017, -- Ragveil
+39000 -- ??? has Unidentified Plant Parts 24401
+);
 
