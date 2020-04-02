83f41c8f3852b37015e54c06126f436c55e25a82
MantisLord <sabinprosper@gmail.com>
2020-04-02 01:47:42 -0400

    Fix OfferRewardText for q.9147 'The Fallen Courier'
    Last sentence, before:
    "Why don?you talk to him?"
    After:
    "Why don't you talk to him?"

---
+++ Updates/0177_the_fallen_courier.sql
@@ -0,0 +1,3 @@
+UPDATE quest_template SET OfferRewardText='Just in time, $N! I was about ready to start digging a hole for our friend here.$B$BMy potion seems to be working; he''s coming to.$B$BHey, I think he''s trying to get your attention. Why don''t you talk to him?' WHERE entry=9147;
+
+
