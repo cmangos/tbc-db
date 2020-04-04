cf3316fcb06eacee091020c96e0735c4cbaf23f6
MantisLord <sabinprosper@gmail.com>
2020-04-04 14:54:32 -0400

    Add missing Spirit Healer spawn at Lake Jorune in Terokkar Forest

---
+++ Updates/0181_lake_jorune_spirit_healer.sql
@@ -0,0 +1,5 @@
+DELETE FROM creature WHERE guid=102367;
+INSERT INTO creature (guid, id, map, spawnMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, spawndist, currentwaypoint, curhealth, curmana, MovementType) VALUES
+(102367, 6491, 530, 1, 0, 0, -2438.87, 4665.47, 161.979, 4.64829, 300, 300, 0, 0, 0, 0, 0);
+
+
