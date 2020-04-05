88b178d1fdb7d5b15d2a4e91b76ab1db77dd495b
MantisLord <sabinprosper@gmail.com>
2020-04-04 22:28:21 -0400

    Correct a few more respawn timers for quest related NPCs

---
+++ Updates/0185_quest_npc_respawn_time.sql
@@ -0,0 +1,9 @@
+UPDATE creature SET spawntimesecsmin=120, spawntimesecsmax=120 WHERE id=20682; -- Terokkarantula
+UPDATE creature SET spawntimesecsmin=120, spawntimesecsmax=120 WHERE id=18423; -- Cho'war the Pillager
+UPDATE creature SET spawntimesecsmin=180, spawntimesecsmax=180 WHERE id=21207; -- Summoner Skartax
+UPDATE creature SET spawntimesecsmin=300, spawntimesecsmax=300 WHERE id=21778; -- Doctor Gutrick
+UPDATE creature SET spawntimesecsmin=120, spawntimesecsmax=180 WHERE id=18583; -- Lisaile Fireweaver
+UPDATE creature SET spawntimesecsmin=420, spawntimesecsmax=480 WHERE id=20763; -- Captured Protectorate Vanguard
+UPDATE creature SET spawntimesecsmin=60, spawntimesecsmax=120 WHERE id=22357; -- Reth'hedron the Subduer
+
+
