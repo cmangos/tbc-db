78eb2281281536d7b239a01f2a1dd0d3563ad58a
Looting <hoechstn@charter.net>
2020-04-08 13:55:51 -0500

    Create 0027_Cenarion_Thicket_Moonwell.sql
    Add missing Moonwell GO in Cenarion Thicket

---
+++ Updates/0354_Cenarion_Thicket_Moonwell.sql
@@ -0,0 +1,2 @@
+DELETE FROM gameobject WHERE guid = 5300291;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`) VALUES('5300291','177281','530','1','-1848.971680','4645.113770','8.658392','-0.235619','0','0','0','0','900','900','100','1');
