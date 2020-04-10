c8f948e5f44a80b3ed27284c55fa6f5e23d54154
anonxs <anonxs@web.de>
2020-04-10 19:02:06 +0200

    [ACID] Improve Ekkorash the Inquisitor 19493

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -18472,9 +18472,9 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Sagan (19482) - NSR
 -- Custodian Dieworth (19488) - NSR
 -- Lieutenant-Sorcerer Morran (19489) - NSR
--- Ekkorash the Inquisitor
-('1949301','19493','9','0','100','1025','0','30','14000','20000','0','0','11','11980','1','32','0','0','0','0','0','0','0','0','Ekkorash the Inquisitor - Cast Curse of Weakness'),
-('1949302','19493','0','0','100','1025','8000','11000','18000','23000','0','0','11','36040','4','0','0','0','0','0','0','0','0','0','Ekkorash the Inquisitor - Cast Fel Flamestrike'),
+-- Ekkorash the Inquisitor 19493
+('1949301','19493','0','0','100','1025','0','0','14000','20000','0','0','11','11980','1','32','0','0','0','0','0','0','0','0','Ekkorash the Inquisitor - Cast Curse of Weakness'),
+('1949302','19493','0','0','100','1025','8000','11000','18000','23000','0','0','11','36040','4','512','0','0','0','0','0','0','0','0','Ekkorash the Inquisitor - Cast Fel Flamestrike'),
 -- Ar'kelos
 ('1949401','19494','9','0','100','1025','0','8','12000','15000','0','0','11','11975','0','0','0','0','0','0','0','0','0','0','Ar''kelos - Cast Arcane Explosion'),
 -- Stormspire Nexus-Guard 19529
