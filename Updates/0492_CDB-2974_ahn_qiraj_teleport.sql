32070431c587235109cdb4cc2e1baa1295af348b
cala <cala@users.noreply.github.com>
2020-04-29 21:42:43 +0200

    [CDB][2974] Update comments for Temple of Ahn'Qiraj Teleporter
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/ac9237c90d532131dbd80197edfc65b351fb2df4

---
+++ Updates/0492_CDB-2974_ahn_qiraj_teleport.sql
@@ -0,0 +1,4 @@
+-- Update comments for Temple of Ahn'Qiraj Teleporter
+UPDATE `dbscripts_on_spell` SET `comments` = 'Cast Teleport to Lair of the Twin Emperors' WHERE `id` = 29182;
+UPDATE `dbscripts_on_spell` SET `comments` = 'Cast Teleport to Final Chamber' WHERE `id` = 29188;
+
