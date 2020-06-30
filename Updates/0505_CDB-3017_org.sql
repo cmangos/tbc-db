32b9ce501abf6d3d3b21725809f2008c0c9106e3
Tobschinski <tobschinski@gmail.com>
2020-06-30 16:07:54 +0200

    [2017] Orgrimmar - Remove Forge & Anvil at Entrance
    removed a questionable forge and an anvil from the entrance area of org
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/074998c4fc30b291d27d04223f5030700a91719d

---
+++ Updates/0505_CDB-3017_org.sql
@@ -0,0 +1,4 @@
+-- no such object in the proximity of creature Entry: 3313
+DELETE FROM `gameobject` WHERE `guid` = 121915 AND `id` = 1685; -- in OG
+DELETE FROM `gameobject` WHERE `guid` = 121916 AND `id` = 1684; -- in OG
+
