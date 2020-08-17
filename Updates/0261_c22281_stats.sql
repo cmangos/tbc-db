2fae893d574d74e4c9effe8908fa96b9586e3aed
anonxs <anonxs@web.de>
2020-08-17 17:18:30 +0200

    Correct Stats for Galvanoth 22281 (Bestiary)

---
+++ Updates/0261_c22281_stats.sql
@@ -0,0 +1,3 @@
+-- Correct Stats for Galvanoth 22281 (Bestiary)
+UPDATE `creature_template` SET `MinLevel` = 72, `MaxLevel` = 72, `HealthMultiplier` = 20, `PowerMultiplier` = 6, `ArmorMultiplier` = 1, `MinLevelHealth` = 118060, `MaxLevelHealth` = 118060, `MinLevelMana` = 19854, `MaxLevelMana` = 19854, `MovementType` = 0 WHERE `entry` = 22281;
+
