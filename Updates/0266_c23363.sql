c1348c75bc8a213580f1bfaf0daf21e1024f8779
anonxs <anonxs@web.de>
2020-09-11 17:30:46 +0200

    Reinsert Sahaak <Keeper of Scrolls> 23363
    Resolves https://github.com/cmangos/tbc-db/issues/800

---
+++ Updates/0266_c23363.sql
@@ -0,0 +1,5 @@
+-- Reinsert Sahaak <Keeper of Scrolls> 23363 - Part of Skettis WIP
+DELETE FROM `creature` WHERE `id` = 23363;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `MovementType`) VALUES
+(242884, 23363, 530, 1, -3867.538, 3464.76, 275.5156, 3.159046, 300, 300, 0, 0);
+
