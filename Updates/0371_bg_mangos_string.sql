d0c98eda675a3dc2f839c074fb1882f7880f4ef8
killerwife <killerwife@gmail.com>
2020-09-11 10:22:37 +0200

    Add a new mangos_string needed for BG error

---
+++ Updates/0371_bg_mangos_string.sql
@@ -0,0 +1,3 @@
+INSERT INTO mangos_string(entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8) VALUES
+('749', 'Someone in your party is still inside an arena.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
