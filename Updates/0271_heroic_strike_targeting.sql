c0725523042bb7a1cd6bf1544ca14911bfc39ac4
anonxs <anonxs@web.de>
2020-10-03 19:32:25 +0200

    Correct Target Type for Add Script Target used by Ashtongue Defender 23216

--- Updates/0271_heroic_strike_targeting.sql
+++ Updates/0271_heroic_strike_targeting.sql
@@ -1,2 +1,4 @@
-INSERT INTO spell_script_target VALUES
-(41975,0,23191,0);
+-- Add Script Target used by Ashtongue Defender 23216
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `inverseEffectMask`) VALUES
+(41975,1,23191,0);
+
