82902ec528be8817d5ab7708f150a7499ae5be8c
anonxs <anonxs@web.de>
2020-10-18 16:03:42 +0200

    Revert "Head of the Horseman 23775 should be stunable"
    thx @ Trugge for confirming
    
    This reverts commit https://github.com/cmangos/tbc-db/commit/8c15f7249064124c64afcdaf00878331beefb90f

--- Updates/0289_c23682.sql
+++ Updates/0289_c23682.sql
@@ -2,5 +2,5 @@
 UPDATE `creature_template` SET `ExtraFlags` = `ExtraFlags`|4194304, `MechanicImmuneMask` = `MechanicImmuneMask`|1+8+256+512+131072+4194304+33554432+536870912 WHERE `entry` = 23682;
 
 -- Add some Immunities for Head of the Horseman 23775 - boss_head_of_horseman
-UPDATE `creature_template` SET `ExtraFlags` = `ExtraFlags`|4194304, `MechanicImmuneMask` = `MechanicImmuneMask`|1+4+8+256+512+131072+33554432+536870912, `MechanicImmuneMask` = `MechanicImmuneMask`&~2048 WHERE `entry` = 23775;
+UPDATE `creature_template` SET `ExtraFlags` = `ExtraFlags`|4194304, `MechanicImmuneMask` = `MechanicImmuneMask`|1+4+8+256+512+131072+33554432+536870912 WHERE `entry` = 23775;
 
