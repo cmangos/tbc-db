3afe45e926a736c452e966275c65974b82cf1c44
killerwife <killerwife@gmail.com>
2020-10-28 17:12:02 +0100

    [Instances] Add Headless Horseman Shaker bunny spawn
    Corrected ID
    
    (cherry picked from commit 3cdd2a68c35a640c0b4c76103ce94900b0a3f115)

--- Updates/Instances/189_scarlet_monastery.sql
+++ Updates/Instances/189_scarlet_monastery.sql
@@ -498,8 +498,10 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `positi
 (@CGUID+341, 4293, 189, 1805.176, 1167.855, 6.820, 0.162434, 7200, 7200, 0, 0, 0, 0), -- Scarlet Scryer
 (@CGUID+342, 4293, 189, 1782.416, 1118.755, 7.490, 5.97183, 7200, 7200, 0, 0, 0, 0), -- Scarlet Scryer
 (@CGUID+343, 4293, 189, 1759.67, 1146.85, 7.49036, 3.19674, 7200, 7200, 0, 0, 0, 0), -- Scarlet Scryer
-(@CGUID+344, 4293, 189, 1788.68, 1145.46, 7.49084, 6.10664, 7200, 7200, 0, 0, 0, 0); -- Scarlet Scryer
+(@CGUID+344, 4293, 189, 1788.68, 1145.46, 7.49084, 6.10664, 7200, 7200, 0, 0, 0, 0), -- Scarlet Scryer
 
+-- TBC+ only
+(@CGUID+401, 23758, 189, 1767.034, 1348.685, 17.542156, 6.267, 7200, 7200, 0, 0, 0, 0); -- Headless Horseman - Earth Explosion Bunny - extrapolated sniff pos from cata+ SM
 
 -- ===========
 -- GAMEOBJECTS
