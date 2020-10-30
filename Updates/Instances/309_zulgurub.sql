caa1ea7a54338ff4893832da5570d4c34f354007
MantisLord <sabinprosper@gmail.com>
2020-10-29 20:05:25 -0400

    [Instances] Fix typo in previous commit
    flipped delay and command columns

--- Updates/Instances/309_zulgurub.sql
+++ Updates/Instances/309_zulgurub.sql
@@ -1673,13 +1673,13 @@ INSERT INTO `game_event_gameobject` (`guid`, `event`) VALUES
 
 DELETE FROM `dbscripts_on_creature_movement` WHERE `id` IN (1134001,1134002,1135901,1135902,1135903,1135904);
 INSERT INTO `dbscripts_on_creature_movement` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
-(1134001, 1, 0, 28, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - kneel'),
-(1134001, 1, 30000, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - stand up'),
-(1134001, 2, 30000, 1, 66, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - salute Hakkar'),
+(1134001, 0, 1, 28, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - kneel'),
+(1134001, 30000, 1, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - stand up'),
+(1134001, 30000, 2, 1, 66, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - salute Hakkar'),
 
-(1134002, 1, 0, 28, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - kneel'),
-(1134002, 1, 57000, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - stand up'),
-(1134002, 2, 57000, 1, 66, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - salute Hakkar'),
+(1134002, 0, 1, 28, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - kneel'),
+(1134002, 57000, 1, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - stand up'),
+(1134002, 57000, 2, 1, 66, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Hakkari Blood Priest - salute Hakkar'),
 
 (1135901, 1000, 20, 1, 5, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Soulflayer - random movement after path 1'),
 (1135901, 45000, 20, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Soulflayer - continue on path 2'),
