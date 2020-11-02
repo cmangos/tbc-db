6d0cd220c23b94a70051272e53cfe399212cfcee
anonxs <anonxs@web.de>
2020-11-02 18:47:18 +0100

    Correct UnitFlags for Gauntlet Imp Trigger 25848

--- Updates/0372_sunwell_data_bosses.sql
+++ Updates/0372_sunwell_data_bosses.sql
@@ -3,3 +3,6 @@ INSERT INTO spell_script_target VALUES
 
 UPDATE creature_model_info SET bounding_radius=3,combat_reach=4.5 WHERE modelid IN(6686);
 
+-- Gauntlet Imp Trigger 25848
+UPDATE `creature_template` SET `UnitFlags` = 33554432 WHERE `entry` = 25848;
+
