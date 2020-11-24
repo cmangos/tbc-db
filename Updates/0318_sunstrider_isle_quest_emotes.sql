0c790cee07b3be791d217da62589a206a181704b
al3xc1985 <al3xc1985@users.noreply.github.com>
2020-11-24 00:53:41 -0500

    Update some quest detail emotes on Sunstrider Isle
    Closes: https://github.com/cmangos/tbc-db/issues/782

---
+++ Updates/0318_sunstrider_isle_quest_emotes.sql
@@ -0,0 +1,17 @@
+-- q.8347 'Aiding the Outrunners' - Quest Emote
+-- https://www.youtube.com/watch?v=7Eqd_YPG0dM
+UPDATE quest_template SET DetailsEmote1 = 2 WHERE entry = 8347;
+
+-- q.8334 'Aggression' - Quest Emote
+-- https://www.youtube.com/watch?v=7WirrvbhwZA
+UPDATE quest_template SET DetailsEmote1 = 25, DetailsEmote2 = 25 WHERE entry = 8334;
+
+-- q.8335 'Felendren the Banished' - Quest Emote
+-- https://www.youtube.com/watch?v=_Km9mc6gAOo
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1 WHERE entry = 8335;
+
+-- q.8346 'Thirst Unending' - Quest Emote
+-- https://www.youtube.com/watch?v=Ybm1Rm7yp6U
+UPDATE quest_template SET DetailsEmote1 = 25 WHERE entry = 8346;
+
+
