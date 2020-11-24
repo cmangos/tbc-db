10a25958d634a5097e6f54d38391c83cf833b789
NeatElves <neatelves@gmail.com>
2020-11-24 00:13:21 -0500

    Fix text for NPC 21465 David Wayne
    Closes: https://github.com/cmangos/tbc-db/issues/798

---
+++ Updates/0317_c21465_text_fix.sql
@@ -0,0 +1,3 @@
+UPDATE `dbscript_string` SET `content_default` = 'Into this blade, I imbue the power to strike down the strongest of demons! No demon shall stand before the wielder of this weapon!', `broadcast_text_id` = '19603' WHERE `entry` =2000005914;
+
+
