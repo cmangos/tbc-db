6bd946df37d61537c9cb0eaaa0ff7f966b74fb95
killerwife <killerwife@gmail.com>
2020-12-08 12:03:29 +0100

    Add core sync for previous revisions

---
+++ Updates/0333_core_sync.sql
@@ -0,0 +1,15 @@
+ALTER TABLE db_version CHANGE COLUMN required_s2416_01_mangos_command_ahbot required_s2419_01_mangos_power_regen bit;
+
+UPDATE creature_template SET RegenerateStats=RegenerateStats*2 WHERE RegenerateStats>0;
+-- not adding 0x001 because its unused and mobs generally do not regen hp in combat anyway
+UPDATE creature_template SET RegenerateStats=RegenerateStats|0x008 WHERE RegenerateStats & 4;
+
+
+
+ALTER TABLE db_version CHANGE COLUMN required_s2419_01_mangos_power_regen required_s2422_01_mangos_creature_template_spells_extension bit;
+
+ALTER TABLE creature_template_spells ADD COLUMN spell9 MEDIUMINT UNSIGNED NOT NULL DEFAULT '0' AFTER spell8;
+ALTER TABLE creature_template_spells ADD COLUMN spell10 MEDIUMINT UNSIGNED NOT NULL DEFAULT '0' AFTER spell9;
+ALTER TABLE creature_template_spells DROP PRIMARY KEY, ADD PRIMARY KEY(entry, setId);
+
+
