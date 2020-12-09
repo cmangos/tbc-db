3d38a461eb574e7f4fca439910bb6775f456bb6c
killerwife <killerwife@gmail.com>
2020-12-09 21:35:15 +0100

    Add aran and curator mana regen disabling to core sync file

--- Updates/0333_core_sync.sql
+++ Updates/0333_core_sync.sql
@@ -3,6 +3,7 @@ ALTER TABLE db_version CHANGE COLUMN required_s2416_01_mangos_command_ahbot requ
 UPDATE creature_template SET RegenerateStats=RegenerateStats*2 WHERE RegenerateStats>0;
 -- not adding 0x001 because its unused and mobs generally do not regen hp in combat anyway
 UPDATE creature_template SET RegenerateStats=RegenerateStats|0x008 WHERE RegenerateStats & 4;
+UPDATE creature_template SET RegenerateStats=0x008+0x002 WHERE entry IN(15691,16524); -- aran and curator do not regen mana in combat
 
 
 
