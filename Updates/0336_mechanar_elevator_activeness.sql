c59abc4e89c6b51bf649241014b78cb816e4f539
killerwife <killerwife@gmail.com>
2020-12-13 11:46:33 +0100

    Fix activeness commit for transport GOs

--- Updates/0336_mechanar_elevator_activeness.sql
+++ Updates/0336_mechanar_elevator_activeness.sql
@@ -1,3 +1,3 @@
 -- is spawned at entrance, wtf
-UPDATE gameobject_template SET ExtraFlags=0x00040000 WHERE entry IN(183788);
+UPDATE gameobject_template SET ExtraFlags=0x00001000 WHERE entry IN(183788);
 
