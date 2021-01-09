c42596c891e0aee3472ece0d219515e46d6ef054
MantisLord <sabinprosper@gmail.com>
2021-01-09 03:51:00 -0500

    Fix ProcFlags for Shaman Lightning and Water Shield abilities

---
+++ Updates/0397_shaman_lightning_water_shield_procs.sql
@@ -0,0 +1,5 @@
+UPDATE spell_proc_event SET procEx=65536 WHERE entry=324; -- Lightning Shield (Rank 1)
+INSERT INTO spell_proc_event (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, ppmRate, CustomChance, Cooldown) VALUES
+(24398, 0, 0, 0, 0, 0, 0, 65536, 0, 0, 0); -- Water Shield (Rank 1)
+
+
