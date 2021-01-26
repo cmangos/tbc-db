cddfb1109e565aa1e57657ea842e842eb950f0f1
killerwife <killerwife@gmail.com>
2021-01-26 22:23:40 +0100

    Add better XYZ to kalecgos realm teleports

---
+++ Updates/0407_kalecgos_teleport.sql
@@ -0,0 +1,4 @@
+-- guesswork based on sniff behaviour
+UPDATE spell_target_position SET target_position_x=1704.311279,target_position_y=927.975281 WHERE id IN(46019);
+UPDATE spell_target_position SET target_position_x=1704.311279,target_position_y=927.975281 WHERE id IN(46020);
+
