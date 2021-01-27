fda974ac89183abaec3fb3d40068551a9eeadd5d
killerwife <killerwife@gmail.com>
2021-01-27 11:45:01 +0100

    Fix data for MgT scrying orb kalecgos script

---
+++ Updates/0408_mgt_kalecgos_data.sql
@@ -0,0 +1,3 @@
+UPDATE dbscripts_on_event SET x=165.9681,y=-408.1578,z=-11.50428,delay=60000 WHERE id = 16547; -- proper spawn
+UPDATE creature_template SET InhabitType=1 WHERE entry=24844; -- core handles it
+
