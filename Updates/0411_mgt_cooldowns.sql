4e4bebb2e90808e157580bed2a59f96b41b1b5c2
killerwife <killerwife@gmail.com>
2021-01-30 22:06:17 +0100

    Add 25568 spells and cds

---
+++ Updates/0411_mgt_cooldowns.sql
@@ -0,0 +1,6 @@
+INSERT INTO creature_template_spells(entry, setId, spell1, spell2, spell3, spell4, spell5, spell6, spell7, spell8, spell9, spell10) VALUES
+('25568', '0', '2', '44475', '46028', '0', '0', '0', '0', '0', '0', '0');
+INSERT INTO creature_cooldowns VALUES
+(25568,44475,30000,30000);
+
+
