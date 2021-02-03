691f89dc6b61c29c41e56b657b1c21faa6b833b9
insuna <dev.insuna@gmail.com>
2021-02-03 14:29:34 +0100

    Fix too big Trash in Naxx
    Many trash mobs had an incorrect Scale value (0) which caused them to be enormous. Setting Scale to 1 fixes this issue.

---
+++ Updates/0413_naxxramas_model_sizes.sql
@@ -0,0 +1,32 @@
+-- Deathknight Captain
+UPDATE creature_template SET Scale=1 WHERE entry=16145;
+-- Deathknight
+UPDATE creature_template SET Scale=1 WHERE entry=16146;
+-- Necro Knight
+UPDATE creature_template SET Scale=1 WHERE entry=16165;
+-- Shade of Naxxramas
+UPDATE creature_template SET Scale=1 WHERE entry=16164;
+-- Deathknight Cavalier
+UPDATE creature_template SET Scale=1 WHERE entry=16163;
+-- Death Lord
+UPDATE creature_template SET Scale=1 WHERE entry=16861;
+-- Spirit of Naxxramas
+UPDATE creature_template SET Scale=1 WHERE entry=16449;
+-- Necropolis Acolyte
+UPDATE creature_template SET Scale=1 WHERE entry=16368;
+-- Plagued Gargoyle
+UPDATE creature_template SET Scale=1 WHERE entry=16446;
+-- Deathknight Vindicator
+UPDATE creature_template SET Scale=1 WHERE entry=16451;
+-- Patchwork Golem
+UPDATE creature_template SET Scale=1 WHERE entry=16017;
+-- Sludge Belcher
+UPDATE creature_template SET Scale=1 WHERE entry=16029;
+-- Necro Knight Guardian
+UPDATE creature_template SET Scale=1 WHERE entry=16452;
+-- Venom Stalker
+UPDATE creature_template SET Scale=1 WHERE entry=15976;
+-- Necro Stalker
+UPDATE creature_template SET Scale=1 WHERE entry=16453;
+-- Carrion Spinner
+UPDATE creature_template SET Scale=1 WHERE entry=15975;
