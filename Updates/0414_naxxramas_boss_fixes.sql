02f4a608b9105c0c19f6b04e24976c11a2b0f9b6
insuna <dev.insuna@gmail.com>
2021-02-04 12:28:34 +0100

    Naxx Fix various Boss configurations
    Increased the run speed of the 4hm to be similar to the reference client
    Added leashes for all bosses requiring leashes (except maybe Maexxna)
    Corrects incorrect Gluth spawn location and orientation
    Increase detection range to more appropriate levels
    ToDo:
    - [ ] Investigate Detection Range for Noth
    - [ ] Investigate Detection Range for Anub'Rekhan
    - [ ] Investigate Detection Range for Grobbulus

---
+++ Updates/0414_naxxramas_boss_fixes.sql
@@ -0,0 +1,27 @@
+-- Highlord Mograine
+UPDATE creature_template SET Leash=110, SpeedRun=2, Detection=90 WHERE entry=16062;
+-- Sir Zeliek
+UPDATE creature_template SET Leash=110, SpeedRun=2, Detection=90 WHERE entry=16063;
+-- Thane Korth'azz
+UPDATE creature_template SET Leash=110, SpeedRun=2, Detection=90 WHERE entry=16064;
+-- Lady Blaumeux
+UPDATE creature_template SET Leash=110, SpeedRun=2, Detection=90 WHERE entry=16065;
+-- Kel'Thuzad
+UPDATE creature_template SET Leash=130 WHERE entry=15990;
+-- Gothik the Harvester
+UPDATE creature_template SET Leash=150 WHERE entry=16060;
+-- Loatheb
+UPDATE creature_template SET Leash=100 WHERE entry=16011;
+-- Noth the Plaguebringer
+UPDATE creature_template SET Leash=120 WHERE entry=15954;
+-- Anub'Rekhan
+UPDATE creature_template SET Leash=130 WHERE entry=15956;
+-- Grobbulus
+UPDATE creature_template SET Leash=180 WHERE entry=15931;
+-- Gluth
+UPDATE creature_template SET Detection=70 WHERE entry=15932;
+UPDATE creature SET position_x="3300.73", position_y="-3140.42", position_z="296.89", orientation="3.90" WHERE guid=5330146;
+-- Feugen
+UPDATE creature_template SET SpeedRun="1.68", Detection=40 WHERE entry=15930;
+-- Stalagg
+UPDATE creature_template SET SpeedRun="1.68", Detection=40 WHERE entry=15929;
