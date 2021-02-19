1fc0a2009e0ac9dac87680111141102c0e856a65
killerwife <killerwife@gmail.com>
2021-02-19 16:02:47 +0100

    Add one more model to custom changes

--- utilities/cmangos_custom.sql
+++ utilities/cmangos_custom.sql
@@ -108,6 +108,7 @@ UPDATE item_template SET class=0 WHERE entry=5514;
 -- not blizzlike but how it actually should work - Brutallus Death Cloud - should always use 11686 ingame
 UPDATE creature_template SET ModelId1=169,ModelId2=11686 WHERE entry IN(25703);
 UPDATE creature_template SET ModelId1=1126,ModelId2=11686 WHERE entry IN(25267);
+UPDATE creature_template SET ModelId1=1126,ModelId2=11686 WHERE entry IN(25265);
 
 -- -------------------------------
 -- Quest custom changes
