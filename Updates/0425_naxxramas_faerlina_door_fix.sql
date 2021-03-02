71ae1795da5e5d0cbfb1f319db5449f7835c1c6b
insuna <dev.insuna@gmail.com>
2021-03-02 11:55:53 +0100

    Ensure duplicate execution doesn't cause errors

--- Updates/0425_naxxramas_faerlina_door_fix.sql
+++ Updates/0425_naxxramas_faerlina_door_fix.sql
@@ -1,2 +1,3 @@
 -- Adds missing door from Faerlina to Naxxramas outer ring
+DELETE FROM gameobject WHERE guid = '9000116';
 INSERT INTO gameobject VALUES ( 9000116, 181167, 533, 1,3121.51, -3790.91, 273.936, 6.27873, 0, 0, 0.00222603, -0.999997, 25, 25, 100, 1);
