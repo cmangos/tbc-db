65c45851a2bfc04aaea676b1263dfa560c061b8c
insuna <dev.insuna@gmail.com>
2021-03-02 11:50:09 +0100

    Fix Faerlina's missing door
    In conjunction with https://github.com/insunaa/mangos-tbc/commit/b19294134266c724af7be9285e0531bcb2a9c7fd

---
+++ Updates/0425_naxxramas_faerlina_door_fix.sql
@@ -0,0 +1,2 @@
+-- Adds missing door from Faerlina to Naxxramas outer ring
+INSERT INTO gameobject VALUES ( 9000116, 181167, 533, 1,3121.51, -3790.91, 273.936, 6.27873, 0, 0, 0.00222603, -0.999997, 25, 25, 100, 1);
