91192bb5b7b238db8c916134db6b173981477ce8
Grz3s <Grz3s@users.noreply.github.com>
2021-03-09 22:44:37 +0000

    [WDB][3276] q.829 'Neeru Fireblade'
    Resolves: https://github.com/cmangos/tbc-db/issues/831
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/c140ae74ee12a63e7105db52c626cecd38709248

---
+++ Updates/0644_WDB-3276_q.829.sql
@@ -0,0 +1,13 @@
+-- q.829 'Neeru Fireblade'
+-- .go c id 3216
+DELETE FROM dbscripts_on_quest_end WHERE id = 829;
+INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+(829, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2000003236, 0, 0, 0, 0, 0, 0, 0, 0, ''), -- 13:42:06.750
+(829, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 2000003237, 0, 0, 0, 0, 0, 0, 0, 0, ''); -- 13:42:09.594
+
+UPDATE quest_template SET CompleteScript = 829 WHERE entry = 829;
+
+DELETE FROM dbscript_string WHERE entry BETWEEN 2000003236 AND 2000003237;
+INSERT INTO dbscript_string (entry, content_default, sound, type, language, emote, comment, broadcast_text_id) VALUES
+(2000003236,'%s inspects the Searing Collar...',0,2,0,0,NULL, 990),
+(2000003237,'The collar is indeed infused with demonic power, $N.  This bodes ill for our people.',0,0,1,0,NULL, 992);
