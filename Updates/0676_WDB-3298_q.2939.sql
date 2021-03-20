7c7d6aacd4956e25f02b7c7291790afa3649574a
Grz3s <Grz3s@users.noreply.github.com>
2021-03-20 02:25:20 +0000

    [WDB][3298] q.2939 'In Search of Knowledge'
    Resolves: https://github.com/cmangos/tbc-db/issues/896
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/834f75a6eaf30b8b2db402dc65ac6a091fc43442

---
+++ Updates/0676_WDB-3298_q.2939.sql
@@ -0,0 +1,14 @@
+-- q.2939 'In Search of Knowledge'
+-- .go c id 7764
+-- .go c id 7907
+-- then quest 2940 from book
+DELETE FROM dbscripts_on_quest_end WHERE id = 2939;
+INSERT INTO dbscripts_on_quest_end (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(2939,0,0,0,0,0,0,0,0,2000003266,0,0,0,0,0,0,0,'');
+
+UPDATE quest_template SET CompleteScript = 2939 WHERE entry = 2939;
+
+DELETE FROM dbscript_string WHERE entry = 2000003266;
+INSERT INTO dbscript_string (entry, content_default, sound, type, language, emote, comment, broadcast_text_id) VALUES
+(2000003266,'Take a look around, $n. Perhaps you will find something that I have missed.',0,0,0,1,NULL, 4042);
+
