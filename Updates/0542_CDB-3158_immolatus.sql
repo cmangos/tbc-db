d2a3a667397652259c1594deef63c259e1dc1807
Tobschinski <tobschinski@gmail.com>
2021-03-20 13:31:47 +0100

    [CDB] 3158 - Felwood - Immolatus
    update damage range
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/8d28d16babfc6282912ed1725d7395c05d22d1aa

---
+++ Updates/0542_CDB-3158_immolatus.sql
@@ -0,0 +1,2 @@
+-- Immolatus 7137
+UPDATE `creature_template` SET `SpeedWalk` = '1', `MeleeBaseAttackTime` = '3000', `RangedBaseAttackTime` = '3000', `MeleeAttackPower` = '234' WHERE (`Entry` = '7137');
