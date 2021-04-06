441a5466ddf37025c4d38e9c18f621029440c533
killerwife <killerwife@gmail.com>
2021-04-06 08:59:17 +0200

    Add more fixed flags to SWP dummy portal file

--- Updates/0454_shattrath_portal_drummy_flags.sql
+++ Updates/0454_shattrath_portal_drummy_flags.sql
@@ -1,3 +1,4 @@
-UPDATE creature_template SET UnitFlags=33555200 WHERE entry IN(26251);
+UPDATE creature_template SET UnitFlags=33555200,InhabitType=7 WHERE entry IN(26251); -- Shattrath Portal Dummy
+UPDATE creature_template SET UnitFlags=33555200 WHERE entry IN(26057); -- Anveena marker
 
 
