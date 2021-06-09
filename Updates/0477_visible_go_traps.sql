bfeeffb4f26c209fa2cb1a5fd350d20a85d64fb6
shagu <ericmauser@googlemail.com>
2021-06-09 19:20:20 +0200

    Resolve more visible gotrap cases
    based on https://github.com/cmangos/tbc-db/pull/938

--- Updates/0477_visible_go_traps.sql
+++ Updates/0477_visible_go_traps.sql
@@ -1,17 +1,22 @@
 -- Fix some Gameobject Traps being visible to players
 UPDATE `gameobject_template` SET `data9` = 1 WHERE `entry` IN (
+-- wotlk/classic-db
 103575, -- Containment Coffer TRAP
 128972, -- Shallow Grave TRAP
-
 176592, -- Shellfish Trap
 176750, -- Kodo Bones
 177493, -- Fire of Elune (Trap
 177529, -- Altar of Elune (Trap
 178124, -- Resonite Crystal (Trap
 178248, -- Naga Brazier (trap
-
+181214, -- Necropolis critter spawner
+-- vmangos
 179324, -- Frostwolf Landmine
 179325, -- Stormpike Landmine
-
-181214); -- Necropolis critter spawner
+-- thx @shagu
+171941, -- Blackrock Keg Trap
+180391, -- Heart of Hakkar Spell Emitter
+184718, -- Cauldron Summoner
+184722 -- Cauldron Bug Summoner
+);
 
