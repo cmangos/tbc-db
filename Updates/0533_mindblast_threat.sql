3dd50e59366d796a585b69d41d3b650420329468
anonxs <anonxs@web.de>
2021-07-04 23:37:04 +0200

    Remove Double Threat for Priest Mind Blast in TBC+
    https://wowwiki.fandom.com/wiki/Mind_Blast?oldid=1588360

---
+++ Updates/0533_mindblast_threat.sql
@@ -0,0 +1,4 @@
+-- Prior to Patch 2.0, it caused 100% extra threat, reducing its PvE viability.
+-- https://wowwiki.fandom.com/wiki/Mind_Blast?oldid=1588360
+DELETE FROM `spell_threat` WHERE `entry` = 8092; -- Mind Blast	8092	0	2	0
+
