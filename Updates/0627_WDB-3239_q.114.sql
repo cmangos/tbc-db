a0f4b859a242ebffe05ea46d3c9e2e59d268107f
anonxs <anonxs@web.de>
2021-07-14 00:59:49 +0200

    Update 0627_WDB-3239_q.114 - dbscript_string changed

--- Updates/0627_WDB-3239_q.114.sql
+++ Updates/0627_WDB-3239_q.114.sql
@@ -2,7 +2,7 @@
 -- seems her orientation is sometimes bugged
 DELETE FROM dbscripts_on_quest_end WHERE id = 114;
 INSERT INTO dbscripts_on_quest_end (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
-(114,2000,0,0,0,0,0,0,0,2000000167,0,0,0,0,0,0,0,'Maybell Maclure - First Say'),
+(114,2000,0,0,0,0,0,0,0,2000000135,0,0,0,0,0,0,0,'Maybell Maclure - First Say'),
 (114,3000,42,0,0,0,0,0,0x04,3699,0,0,0,0,0,0,0,'Maybell Maclure - equip'),
 (114,4000,1,92,0,0,0,0,0,0,0,0,0,0,0,0,0,'Maybell Maclure - Emote OneShotEatNoSheathe'),
 (114,7950,42,0,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'Maybell Maclure - reset equip'),
