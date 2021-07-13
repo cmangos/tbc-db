8dc319499b0cb7739a3548526f27a4f7fe073bf7
anonxs <anonxs@web.de>
2021-07-14 01:00:24 +0200

    Update 0628_delete_hunter_pets - delete not needed creature_addon

--- Updates/0628_delete_hunter_pets.sql
+++ Updates/0628_delete_hunter_pets.sql
@@ -31,3 +31,26 @@ UPDATE `creature_template_addon` SET `bytes1` = 0, `b2_1_flags` = 16, `auras` = 
 
 UPDATE `creature_template_addon` SET `auras` = '34429' WHERE `entry` = 19668; -- Shadowform on Shadowfiend is correct
 
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` IN (
+157, -- Goretusk
+454, -- Young Goretusk
+728, -- Bhag'thera
+731, -- King Bangalash
+3100, -- Elder Mottled Boar
+4277, -- Eye of Kilrogg
+7384, -- Cornish Rex
+7456, -- Winterspring Screecher
+7550, -- Wood Frog
+7560, -- Snowshoe Rabbit
+8960, -- Felpaw Scavenger
+10200, -- Rak'shiri
+11361, -- Zulian Tiger
+14832, -- Kerri Hicks
+15358, -- Lurky
+15489, -- Mana Spring Totem V
+16117, -- Plagued Swine
+19668, -- Shadowfiend
+21071, -- Totem of Spirits
+22484, -- Zeppit
+24981 -- Converted Sentry
+));
