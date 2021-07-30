d9c7b688a76f9f4d4751c01e4d4a31e4568e9504
MantisLord <sabinprosper@gmail.com>
2021-07-29 22:18:03 -0400

    Correction for update 667
    After reviewing TBCC data, I've confirmed this NPC actually doesn't have any gossip at all

--- Updates/0667_c24974_gossip.sql
+++ Updates/0667_c24974_gossip.sql
@@ -1,4 +1,4 @@
-UPDATE gossip_menu SET condition_id=15 WHERE entry=9821 AND text_id=13557;
-UPDATE creature_template SET GossipMenuId=9821 WHERE Entry=24974;
+UPDATE creature_template SET NpcFlags=NpcFlags&~1 WHERE Entry=24974;
+
 
 
