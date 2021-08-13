6c988edcfbb66cea888b3caa77d37ff81c9ec320
anonxs <anonxs@web.de>
2021-08-13 02:31:07 +0200

    Further Increase Aggro Radius for Underbog Lord 17734,20187

---
+++ Updates/0742_Underbog_Lord_Detection.sql
@@ -0,0 +1,4 @@
+-- Further Increase Aggro Radius for Underbog Lord 17734,20187
+-- https://youtu.be/V0SyEKPzDCg?t=360
+UPDATE `creature_template` SET `Detection` = 40 WHERE `entry` IN (17734,20187); -- 30
+
