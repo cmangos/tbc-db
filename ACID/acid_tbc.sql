c60bd3eccc245bb9aa53166bab86f9e44c41db84
NeatElves <neatelves@gmail.com>
2021-08-16 00:10:45 +0200

    Improve Script for Quest 2608
    Based on https://github.com/cmangos/tbc-db/issues/941
    
    Co-authored-by: AnonXS

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -5748,6 +5748,7 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 ('674001','6740','22','0','100','1','41','548','0','0','0','0','5','23','0','0','15','8356','6','0','1','-1470','0','0','Innkeeper Allison - Emote and Gives Quest Credit on Received Emote'),
 -- Renzik "The Shiv" 6946
 -- Doc Mixilpixil 7207
+('720701','7207','22','0','100','1','61','9011','0','0','0','0','53','9003','0','0','15','2608','6','0','0','0','0','0','Doc Mixilpixil - Start Relay Script and Quest Complete (2608) on Receive Emote'),
 -- Noarm (7208) - NSR
 -- Borgus Steelhand 7232
 -- Shailiea 7295
