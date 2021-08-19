4bbdb937d71c83809b8d6cf7e48b1470397d04d9
anonxs <anonxs@web.de>
2021-08-19 03:15:04 +0200

    Add creature_cooldowns for Sunblade Mage Guard (Heroic) - Glaive Throw 46028

---
+++ Updates/0768_creature_cooldown_c.25568.sql
@@ -0,0 +1,5 @@
+-- Add missing creature_cooldowns
+
+-- MgT
+REPLACE INTO `creature_cooldowns` (`entry`, `spellid`, `cooldownmin`, `cooldownmax`) VALUES (25568, 46028, 12000, 20000); -- Sunblade Mage Guard (Heroic) - Glaive Throw
+
