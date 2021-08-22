49985a995e09db347d9085a3234c607f785726af
MantisLord <sabinprosper@gmail.com>
2021-08-22 14:52:17 -0400

    [Instances][ACID] Updates for some Naxxramas creature AI and data

---
+++ Updates/0795_naxx_updates.sql
@@ -0,0 +1,110 @@
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=1600, RangedBaseAttackTime=1600, Detection=40 WHERE Entry=15932; -- Gluth (with old detection value of 70, he will aggro before players even jump down from the pipe leading into the room...)
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=15931; -- Grobbulus
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16017; -- Patchwork Golem
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16018; -- Bile Retcher
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16020; -- Mad Scientist
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16021; -- Living Monstrosity
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16022; -- Surgical Assistant
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16024; -- Embalming Slime
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16025; -- Stitched Spewer
+UPDATE creature_template SET MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16027; -- Living Poison
+UPDATE creature_template SET UnitFlags=64 WHERE Entry=16028; -- Patchwerk
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=16029; -- Sludge Belcher
+UPDATE creature_template SET Faction=16, UnitFlags=64, MeleeBaseAttackTime=1800, RangedBaseAttackTime=1800 WHERE Entry=16375; -- Sewage Slime
+UPDATE creature_template SET Faction=114, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE Entry=16400; -- Toxic Tunnel
+UPDATE creature_template SET UnitFlags=33554944, SpeedWalk=1, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE Entry=17231; -- Garden Gas
+UPDATE creature_template SET MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE Entry=16381; -- Archmage Tarsis Kir-Moldir
+UPDATE creature_template SET UnitFlags=0 WHERE Entry=16154; -- Risen Deathknight
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=3500, RangedBaseAttackTime=3500, SpeedWalk=1 WHERE Entry=16145; -- Deathknight Captain
+UPDATE creature_template SET UnitFlags=64, RangedBaseAttackTime=1000, Faction=312 WHERE Entry=15977; -- Infectious Skitterer
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1500, RangedBaseAttackTime=1500, SpeedWalk=0.777776 WHERE Entry=16244; -- Infectious Ghoul
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1800, RangedBaseAttackTime=1800, Faction=14 WHERE Entry=16243; -- Plague Slime
+UPDATE creature_template SET UnitFlags=64, SpeedWalk=1 WHERE Entry=16146; -- Deathknight
+UPDATE creature_template SET UnitFlags=64, Faction=312 WHERE Entry=15974; -- Dread Creeper
+UPDATE creature_template SET UnitFlags=64, Faction=312 WHERE Entry=15975; -- Carrion Spinner
+UPDATE creature_template SET UnitFlags=64, Faction=312 WHERE Entry=15976; -- Venom Stalker
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=3000, RangedBaseAttackTime=3000 WHERE Entry=16165; -- Necro Knight
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1667, RangedBaseAttackTime=1667, SpeedWalk=1 WHERE Entry=16156; -- Dark Touched Warrior
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE Entry=16164; -- Shade of Naxxramas
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=3000, RangedBaseAttackTime=3000, Faction=233 WHERE Entry=16168; -- Stoneskin Gargoyle
+UPDATE creature_template SET UnitFlags=64, SpeedWalk=1 WHERE Entry=16193; -- Skeletal Smith
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1667, RangedBaseAttackTime=1667, SpeedWalk=1, SpeedRun=1.42857 WHERE Entry=16158; -- Death Touched Warrior
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1500, RangedBaseAttackTime=1500, Faction=16 WHERE Entry=16036; -- Frenzied Bat
+UPDATE creature_template SET UnitFlags=0 WHERE Entry=16068; -- Larva
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, Faction=16 WHERE Entry=16037; -- Plagued Bat
+UPDATE creature_template SET UnitFlags=64, Faction=233 WHERE Entry=16167; -- Bony Construct
+UPDATE creature_template SET UnitFlags=0, SpeedWalk=0.666668, Faction=16 WHERE Entry=16034; -- Plague Beast
+UPDATE creature_template SET UnitFlags=0, SpeedRun=1.14286, Faction=16 WHERE Entry=16297; -- Mutated Grub
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedRun=1.42857 WHERE Entry=16163; -- Deathknight Cavalier
+UPDATE creature_template SET UnitFlags=64, RangedBaseAttackTime=1200, Faction=312 WHERE Entry=15978; -- Crypt Reaver
+UPDATE creature_template SET UnitFlags=0, SpeedWalk=1, SpeedRun=1.42857 WHERE Entry=16157; -- Doom Touched Warrior
+UPDATE creature_template SET UnitFlags=33554432 WHERE Entry=16137; -- Naxxramas Military Sub-Boss Trigger
+UPDATE creature_template SET UnitFlags=64, Faction=312 WHERE Entry=16573; -- Crypt Guard
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE Entry=15956; -- Anub'Rekhan
+UPDATE creature_template SET UnitFlags=33555200 WHERE Entry=16082; -- Naxxramas Trigger
+UPDATE creature_template SET UnitFlags=64 WHERE Entry=16067; -- Skeletal Steed
+UPDATE creature_template SET Faction=21 WHERE Entry=16142; -- Bile Sludge
+UPDATE creature_template SET UnitFlags=0, SpeedWalk=0.777776 WHERE Entry=16447; -- Plagued Ghoul
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=3000, RangedBaseAttackTime=3000, SpeedWalk=1, SpeedRun=1.42857, Faction=233 WHERE Entry=16446; -- Plagued Gargoyle
+UPDATE creature_template SET UnitFlags=0, SpeedWalk=1, SpeedRun=1.42857 WHERE Entry=16158; -- Death Touched Warrior
+UPDATE creature_template SET UnitFlags=64, SpeedWalk=2, SpeedRun=2.14286, Faction=16 WHERE Entry=16448; -- Plagued Deathhound
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedWalk=1, SpeedRun=1.71429 WHERE Entry=16452; -- Necro Knight Guardian
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedWalk=1, SpeedRun=1.71429, Faction=21 WHERE Entry=16451; -- Deathknight Vindicator
+UPDATE creature_template SET UnitFlags=0, SpeedWalk=1, SpeedRun=1.42857 WHERE Entry=16449; -- Spirit of Naxxramas
+UPDATE creature_template SET UnitFlags=64, SpeedRun=1.42857 WHERE Entry=16861; -- Death Lord
+UPDATE creature_template SET UnitFlags=33554432, SpeedRun=1 WHERE Entry=16211; -- Naxxramas Combat Dummy
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=3500, RangedBaseAttackTime=3500, SpeedRun=1.42857 WHERE Entry=16803; -- Deathknight Understudy
+UPDATE creature_template SET UnitFlags=512, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedWalk=1 WHERE Entry=16365; -- Master Craftsman Omarion
+UPDATE creature_template SET UnitFlags=64, SpeedRun=1.71429 WHERE Entry=16061; -- Instructor Razuvious
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1200, RangedBaseAttackTime=1200, SpeedRun=1.71429 WHERE Entry=16216; -- Unholy Swords
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1800, RangedBaseAttackTime=1800, SpeedRun=1.71429 WHERE Entry=16194; -- Unholy Axe
+UPDATE creature_template SET UnitFlags=0, SpeedRun=1.71429 WHERE Entry=16215; -- Unholy Staff
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedWalk=1, SpeedRun=1.71429, Faction=233 WHERE Entry=16368; -- Necropolis Acolyte
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, Faction=312 WHERE Entry=16506; -- Naxxramas Worshipper
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedRun=1.42857, Faction=312 WHERE Entry=16505; -- Naxxramas Follower
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=15953; -- Grand Widow Faerlina
+UPDATE creature_template SET UnitFlags=33685504 WHERE Entry=16363; -- Grobbulus Cloud
+UPDATE creature_template SET UnitFlags=64, RangedBaseAttackTime=1800, SpeedWalk=1 WHERE Entry=16290; -- Fallout Slime
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, Faction=312 WHERE Entry=15981; -- Naxxramas Acolyte
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, Faction=312 WHERE Entry=15980; -- Naxxramas Cultist
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedWalk=0.8 WHERE Entry=15929; -- Stalagg
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedWalk=0.8 WHERE Entry=15930; -- Feugen
+UPDATE creature_template SET UnitFlags=64, RangedBaseAttackTime=3000, Faction=233 WHERE Entry=16360; -- Zombie Chow
+UPDATE creature_template SET MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedWalk=1 WHERE Entry=16218; -- Tesla Coil
+UPDATE creature_template SET UnitFlags=33816896, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedWalk=1, SpeedRun=1.42857 WHERE Entry=15928; -- Thaddius
+UPDATE creature_template SET UnitFlags=33554432 WHERE Entry=16006; -- InCombat Trigger
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedRun=1.71429 WHERE Entry=15936; -- Heigan the Unclean
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500 WHERE Entry=15954; -- Noth the Plaguebringer
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, Faction=14 WHERE Entry=16060; -- Gothik the Harvester
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=2700, RangedBaseAttackTime=2700 WHERE Entry=16984; -- Plagued Warrior
+UPDATE creature_template SET UnitFlags=0, SpeedWalk=0.888888 WHERE Entry=16983; -- Plagued Champion
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedRun=0.285714, Faction=16 WHERE Entry=16056; -- Diseased Maggot
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedRun=0.285714, Faction=16 WHERE Entry=16057; -- Rotting Maggot
+UPDATE creature_template SET UnitFlags=0, MeleeBaseAttackTime=1000, RangedBaseAttackTime=1000, SpeedRun=1, Faction=16 WHERE Entry=16236; -- Eye Stalk
+UPDATE creature_template SET UnitFlags=33554944, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedWalk=1, SpeedRun=0.992063 WHERE Entry=17293; -- Plague Wave
+UPDATE creature_template SET UnitFlags=64, SpeedRun=2 WHERE Entry=16011; -- Loatheb
+UPDATE creature_template SET UnitFlags=64, Faction=312 WHERE Entry=15979; -- Tomb Horror
+UPDATE creature_template SET SpeedRun=0.285714 WHERE Entry=16286; -- Spore
+UPDATE creature_template SET SpeedWalk=0.888888, SpeedRun=1.42857 WHERE Entry=16390; -- Deathchill Servant
+UPDATE creature_template SET UnitFlags=33555200, SpeedRun=1 WHERE Entry=16420; -- Portal of Shadows
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedWalk=1 WHERE Entry=16065; -- Lady Blaumeux
+UPDATE creature_template SET UnitFlags=64, RangedBaseAttackTime=2500, SpeedWalk=1 WHERE Entry=16064; -- Thane Korth'azz
+UPDATE creature_template SET UnitFlags=64, RangedBaseAttackTime=2500, SpeedWalk=1 WHERE Entry=16063; -- Sir Zeliek
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=1500, RangedBaseAttackTime=1500, SpeedWalk=1 WHERE Entry=16062; -- Highlord Mograine
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=1800, RangedBaseAttackTime=1800, SpeedRun=1.42857 WHERE Entry=16124; -- Unrelenting Trainee
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000, SpeedRun=1.42857 WHERE Entry=16127; -- Spectral Trainee
+UPDATE creature_template SET UnitFlags=64, SpeedRun=1.42857 WHERE Entry=16125; -- Unrelenting Deathknight
+UPDATE creature_template SET UnitFlags=64, SpeedRun=1.42857 WHERE Entry=16148; -- Spectral Deathknight
+UPDATE creature_template SET UnitFlags=64, SpeedRun=1.71429 WHERE Entry=16126; -- Unrelenting Rider
+UPDATE creature_template SET SpeedWalk=1.6, SpeedRun=1.38571 WHERE Entry=16149; -- Spectral Horse
+UPDATE creature_template SET UnitFlags=64, SpeedRun=1.71429 WHERE Entry=16150; -- Spectral Rider
+UPDATE creature_template SET UnitFlags=64, SpeedWalk=1, SpeedRun=1.42857, Faction=312 WHERE Entry=16453; -- Necro Stalker
+UPDATE creature_template SET MeleeBaseAttackTime=1000, RangedBaseAttackTime=1000, SpeedRun=2.14286 WHERE Entry=16698; -- Corpse Scarab
+UPDATE creature_template SET UnitFlags=64, MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedWalk=1, SpeedRun=1.71429 WHERE Entry=15952; -- Maexxna
+UPDATE creature_template SET UnitFlags=0, SpeedRun=1 WHERE Entry=16486; -- Web Wrap
+UPDATE creature_template SET MeleeBaseAttackTime=2500, RangedBaseAttackTime=2500, SpeedWalk=1 WHERE Entry=15990; -- Kel'Thuzad
+UPDATE creature_template SET UnitFlags=64 WHERE Entry=15989; -- Sapphiron
+UPDATE creature_template SET RangedBaseAttackTime=2000, SpeedWalk=1.2, SpeedRun=0.428571 WHERE Entry=16474; -- Blizzard
+UPDATE creature_template SET UnitFlags=33555200 WHERE Entry=17025; -- Sapphiron's Wing Buffet
+
+
