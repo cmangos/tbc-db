7a42e6786d1770552d55d2ccd8b7da7365f87167
MantisLord <sabinprosper@gmail.com>
2021-09-12 20:50:53 -0400

    Add some MC/Fear immunities for Blasted Lands NPCs

--- Updates/0836_mind_control_immunity.sql
+++ Updates/0836_mind_control_immunity.sql
@@ -6,5 +6,11 @@ UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=11138;
 UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=12578;
 -- Kroum, Jarrodenus
 UPDATE creature_template SET MechanicImmuneMask=1|16 WHERE Entry IN (8610,12577);
+-- Alexandra Constantine
+UPDATE creature_template SET MechanicImmuneMask=1|16 WHERE Entry=8609;
+-- Agent Proudwell
+UPDATE creature_template SET UnitFlags=UnitFlags&~256, MechanicImmuneMask=1|16 WHERE Entry=19942;
+-- Watcher Theronus
+UPDATE creature_template SET MechanicImmuneMask=1|16 WHERE Entry=19567;
 
 
