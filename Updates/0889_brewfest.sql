cb25105333425e8441ad6708a10accaaf9b0ee90
killerwife <killerwife@gmail.com>
2021-10-05 21:46:46 +0200

    Brewfest: Disable access to Welcome to Brewfest quests

--- Updates/0889_brewfest.sql
+++ Updates/0889_brewfest.sql
@@ -581,4 +581,8 @@ UPDATE creature_template SET Faction=35,UnitFlags=33024 WHERE entry IN(23795);
 INSERT INTO creature_spawn_data_template(entry,UnitFlags,Faction) VALUES
 (10000,54,32768);
 
+-- disable quest Welcome to brewfest
+DELETE FROM creature_involvedrelation WHERE quest IN(11442,11447);
+DELETE FROM creature_questrelation WHERE quest IN(11442,11447);
+
 
