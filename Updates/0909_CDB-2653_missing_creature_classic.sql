c76758df71121e9728b1e2f776bf6387a4b91283
killerwife <killerwife@gmail.com>
2021-10-10 18:06:50 +0200

    Fix build due to Rank keyword misuse

--- Updates/0909_CDB-2653_missing_creature_classic.sql
+++ Updates/0909_CDB-2653_missing_creature_classic.sql
@@ -1,5 +1,5 @@
 -- Add missing classic creatures.
-INSERT INTO creature_template (Entry, ModelId1, ModelId2, ModelId3, ModelId4, Name, SubName, UnitClass, Rank, Family, CreatureType, CreatureTypeFlags, PetSpellDataId, HealthMultiplier, PowerMultiplier, MinLevel, MaxLevel, Faction, ArmorMultiplier) VALUES
+INSERT INTO creature_template (Entry, ModelId1, ModelId2, ModelId3, ModelId4, Name, SubName, UnitClass, `Rank`, Family, CreatureType, CreatureTypeFlags, PetSpellDataId, HealthMultiplier, PowerMultiplier, MinLevel, MaxLevel, Faction, ArmorMultiplier) VALUES
 (16509, 8469, 0, 0, 0, 'Argent Warhorse', NULL, 1, 0, 0, 1, 0, 0, 1, 1, 60, 60, 35, -1),
 (16510, 14584, 0, 0, 0, 'Argent Charger', NULL, 1, 0, 0, 1, 0, 0, 1, 1, 63, 63, 814, -1);
 
