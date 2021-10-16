7d59ada3e559fa5a6538d594be56ce7bd4eff589
anonxs <anonxs@web.de>
2021-10-16 22:07:04 +0200

    Revert Correct spell_proc_event for Scarab of the Infinite Cycle 28190 - will be fixed by spellscript "spell_spell_haste_healer_trinket"

--- Updates/0924_misc_spell_fixes.sql
+++ Updates/0924_misc_spell_fixes.sql
@@ -10,15 +10,7 @@ UPDATE `spell_affect` SET `SpellFamilyMask` =
 1140850688 -- Ghostly Strike SpellFamilyFlags
 WHERE `entry` = 14143; -- 25870467590 befor (2,4,512,33554432,67108864,8589934592,17179869184) -- 26977763846
 
--- Correct spell_proc_event for Scarab of the Infinite Cycle 28190 - it procced on all postives casts, like buffing stamina etc
-
--- Triggers: Spell 33953 listed in `spell_proc_event` has exactly same proc flags as in spell.dbc, field value redundant
--- PROC_FLAG_DONE_SPELL_MAGIC_DMG_CLASS_POS 16384 currently does not include hot applications
-
--- Your direct healing and heal over time spells have a chance to increase your spell haste rating by 320 for 6 secs.
--- https://www.wowhead.com/item=28190/scarab-of-the-infinite-cycle&bonus=4095#comments:id=91417
--- Hot Application should also Procc it (Renew, Lifebloom and Rejuvenation maybe even Gift of the Naaru), not the periodic healing, currently not fixable but druid doesnt care about this trinket too much anyway (pve)
-UPDATE `spell_proc_event` SET `procFlags` = 16384, `procex` = 0 WHERE `entry` = 33953; -- 0 65536
+-- Correct spell_proc_event for Scarab of the Infinite Cycle 28190 - will be fixed by spellscript "spell_spell_haste_healer_trinket"
 
 -- Add more missing spell_script_target for Raise Dead 34019
 REPLACE INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
