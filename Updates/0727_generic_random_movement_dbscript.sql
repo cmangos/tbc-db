fcd5c053f0edfcd7ddee6d1ce4f4bb08a9e54cf6
anonxs <anonxs@web.de>
2021-11-26 22:09:49 +0100

    [Update] 0727_generic_random_movement_dbscript - Use more generic description as intended by the actual usage of the command seen by its usecases

--- Updates/0727_generic_random_movement_dbscript.sql
+++ Updates/0727_generic_random_movement_dbscript.sql
@@ -3,5 +3,5 @@
 -- 19162, 13 last point, aligns with this change (-1674.3, 5287.94, -50.6398)	-> (-1668.49, 5284.1, -49.4239)
 -- 19155, 24 last point, aligns with this change (-1937.06, 5173.81, -40.2092)	-> (-1939.99, 5179.95, -40.2092)
 -- 20195, 16 last point, aligns with this change (430.253, 2896.79, 52.6058)	-> (430.754, 2896.12, 52.6334)
-UPDATE `dbscripts_on_creature_movement` SET `data_flags` = `data_flags`|8, `comments` = 'GENERIC - Set Random Movement, wander_distance 5 at Current Waypoint' WHERE `id` = 2;
+UPDATE `dbscripts_on_creature_movement` SET `data_flags` = `data_flags`|8, `comments` = 'GENERIC - MovementType 1 and Spawndist 5' WHERE `id` = 2;
 
