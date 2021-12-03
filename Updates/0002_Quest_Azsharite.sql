8f602d0458acd99de240d7505bd689bb33729ee3
Miraco <daaniel743mail.com>
2021-12-03 15:12:35 +0100

    also update quest_template to have start script

--- Updates/0002_Quest_Azsharite.sql
+++ Updates/0002_Quest_Azsharite.sql
@@ -2,3 +2,5 @@ UPDATE `broadcast_text` SET `ChatTypeID`='4' WHERE (`Id`='4576');
 
 DELETE FROM `dbscripts_on_quest_start` WHERE id = '3602';
 INSERT INTO `dbscripts_on_quest_start` (`id`, `command`, `dataint`, `comments`) VALUES ('3602', '0', '4576', 'Azsharite: Loramus Thalipedes - Whisper Text');
+
+UPDATE `quest_template` SET `StartScript`='3602' WHERE (`entry`='3602');
