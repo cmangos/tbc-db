c78981963c54037846a677f92cd7c7c39d645dc5
_daanieL <daaniel743@gmail.com>
2021-12-04 18:47:25 +0100

    Update 0009_Quest_spirit_totem .sql
    Typo

--- Updates/0009_Quest_spirit_totem .sql
+++ Updates/0009_Quest_spirit_totem .sql
@@ -2,7 +2,7 @@ DELETE FROM dbscripts_on_quest_end WHERE id = '8412';
 INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `data_flags`, `dataint`, `x`, `y`, `z`, `o`, `comments`) VALUES 
 ('8412', '0', '21', '1', '0', '0', '0', '0', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Set ActiveObject'),
 ('8412', '0', '3', '0', '0', '0', '0', '0', '253.517', '-1459.8', '52.1418', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Move'),
-('8412', '0', '29', '2', '0', '0', '0', '0', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Toggle NpcFlags'),
+('8412', '0', '29', '2', '0', '0', '0', '0', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Remove NpcFlags'),
 ('8412', '4000', '3', '0', '0', '0', '0', '0', '254.261', '-1453.83', '51.9142', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Move'),
 ('8412', '8000', '1', '16', '0', '0', '0', '0', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Emote OneShotKneel'),
 ('8412', '8000', '0', '0', '0', '0', '0', '10864', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Say Tex'),
@@ -10,7 +10,7 @@ INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `command`, `datalong`, `dat
 ('8412', '17000', '3', '0', '0', '0', '0', '0', '250.84', '-1470.58', '55.4637', '1.39626', 'Spirit Totem: Bath\'rah the Windwatcher - Move'),
 ('8412', '17000', '0', '0', '0', '0', '0', '10865', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Say Tex'),
 ('8412', '20000', '21', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Remove ActiveObject'),
-('8412', '20000', '29', '2', '1', '0', '0', '0', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Toggle NpcFlags');
+('8412', '20000', '29', '2', '1', '0', '0', '0', '0', '0', '0', '0', 'Spirit Totem: Bath\'rah the Windwatcher - Add NpcFlags');
 
 UPDATE `quest_template` SET `CompleteScript`=8412 WHERE `entry`=8412;
 
