228a1ad7a83cab26395968b0b19c525ef5034e50
killerwife <killerwife@gmail.com>
2022-01-12 10:25:21 +0100

    [ACID] SWP: Make felfire fiends despawn on evade rather than portal death

--- ACID/acid_tbc.sql
+++ ACID/acid_tbc.sql
@@ -29077,12 +29077,13 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 ('2559801','25598','11','0','100','0','0','0','0','0','0','0','50','0','0','0','0','0','0','0','0','0','0','0','Volatile Felfire Fiend - React passive on spawn'),
 ('2559802','25598','29','0','100','0','2000','2000','0','0','0','0','50','2','0','0','38','0','0','0','0','0','0','0','Volatile Felfire Fiend - React Aggressive and Zone Combat Pulse on Timer'),
 ('2559803','25598','6','0','100','0','0','0','0','0','0','0','11','45779','0','0','0','0','0','0','0','0','0','0','Volatile Felfire Fiend - Cast Felfire Fission on Death'),
+('2559804','25598','7','0','100','0','0','0','0','0','0','0','41','1000','0','0','0','0','0','0','0','0','0','0','Volatile Felfire Fiend - Despawn on Evade'),
 -- Cataclysm Hound 25599
 ('2559901','25599','0','0','100','1024','10000','20000','10000','20000','0','0','11','47399','0','32','0','0','0','0','0','0','0','0','Cataclysm Hound - Cast Frenzy'),
 ('2559902','25599','0','0','100','1025','5000','15000','8000','16000','0','0','11','46292','4','512','0','0','0','0','0','0','0','0','Cataclysm Hound - Cast Cataclysm Breath'),
 -- Felfire Portal 25603
 ('2560301','25603','11','0','100','0','0','0','0','0','0','0','11','46464','0','0','50','0','0','0','0','0','0','0','Felfire Portal - Cast Summon Felfire Fiend and React Passive on Spawn'),
-('2560302','25603','11','0','100','0','0','0','0','0','0','0','62','7','0','0','0','0','0','0','0','0','0','0','Felfire Portal - Enable Despawn Aggregation on Evade and Death'),
+('2560302','25603','11','0','100','0','0','0','0','0','0','0','62','3','0','0','0','0','0','0','0','0','0','0','Felfire Portal - Enable Despawn Aggregation on Evade'),
 -- Kil'jaeden 25608 - npc_kiljaeden_controller
 -- Vindicator Moorba (25632) - NSR
 -- Captain Selana (25638) - NSR
