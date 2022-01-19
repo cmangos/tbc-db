b5944c19a45ac4342840c1b8b4747675655d7c4b
MantisLord <sabinprosper@gmail.com>
2022-01-19 09:37:56 -0500

    AQWarEffort: Fix gameobject type for The Scarab Gong 180717
    Based on data in vmangos

--- Updates/0069_scarab_gong.sql
+++ Updates/0069_scarab_gong.sql
@@ -7,4 +7,7 @@ DELETE FROM gameobject_involvedrelation WHERE id=180717 AND quest=8743;
 INSERT INTO gameobject_involvedrelation (id, quest) VALUES
 (180717, 8743);
 
+-- The Scarab Gong - change to GAMEOBJECT_TYPE_QUESTGIVER
+UPDATE gameobject_template SET `type`=2 WHERE entry=180717;
+
 
