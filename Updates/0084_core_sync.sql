ab59cad11f26434e21ba02a39cce330877c30213
killerwife <killerwife@gmail.com>
2022-01-23 23:52:52 +0100

    Fix defaults in new renamed for wps

--- Updates/0084_core_sync.sql
+++ Updates/0084_core_sync.sql
@@ -10,9 +10,9 @@ ALTER TABLE `waypoint_path` CHANGE `point` `Point` MEDIUMINT UNSIGNED NOT NULL;
 ALTER TABLE `waypoint_path` CHANGE `position_x` `PositionX` FLOAT NOT NULL;
 ALTER TABLE `waypoint_path` CHANGE `position_y` `PositionY` FLOAT NOT NULL;
 ALTER TABLE `waypoint_path` CHANGE `position_z` `PositionZ` FLOAT NOT NULL;
-ALTER TABLE `waypoint_path` CHANGE `orientation` `Orientation` FLOAT NOT NULL;
-ALTER TABLE `waypoint_path` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL;
-ALTER TABLE `waypoint_path` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL;
+ALTER TABLE `waypoint_path` CHANGE `orientation` `Orientation` FLOAT NOT NULL DEFAULT '100';
+ALTER TABLE `waypoint_path` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `waypoint_path` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `waypoint_path` CHANGE `comment` `Comment` TEXT NULL;
 ALTER TABLE `waypoint_path` DROP PRIMARY KEY;
 ALTER TABLE `waypoint_path` ADD PRIMARY KEY (`PathId`, `Point`);
@@ -23,31 +23,31 @@ ALTER TABLE `creature_movement` CHANGE `point` `Point` MEDIUMINT UNSIGNED NOT NU
 ALTER TABLE `creature_movement` CHANGE `position_x` `PositionX` FLOAT NOT NULL;
 ALTER TABLE `creature_movement` CHANGE `position_y` `PositionY` FLOAT NOT NULL;
 ALTER TABLE `creature_movement` CHANGE `position_z` `PositionZ` FLOAT NOT NULL;
-ALTER TABLE `creature_movement` CHANGE `orientation` `Orientation` FLOAT NOT NULL;
-ALTER TABLE `creature_movement` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL;
-ALTER TABLE `creature_movement` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL;
+ALTER TABLE `creature_movement` CHANGE `orientation` `Orientation` FLOAT NOT NULL DEFAULT '100';
+ALTER TABLE `creature_movement` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `creature_movement` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `creature_movement` CHANGE `comment` `Comment` TEXT NULL;
 
 ALTER TABLE `creature_movement_template` CHANGE `entry` `Entry` MEDIUMINT UNSIGNED NOT NULL;
-ALTER TABLE `creature_movement_template` CHANGE `pathId` `PathId` INT UNSIGNED NOT NULL;
+ALTER TABLE `creature_movement_template` CHANGE `pathId` `PathId` INT UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `creature_movement_template` CHANGE `point` `Point` MEDIUMINT UNSIGNED NOT NULL;
 ALTER TABLE `creature_movement_template` CHANGE `position_x` `PositionX` FLOAT NOT NULL;
 ALTER TABLE `creature_movement_template` CHANGE `position_y` `PositionY` FLOAT NOT NULL;
 ALTER TABLE `creature_movement_template` CHANGE `position_z` `PositionZ` FLOAT NOT NULL;
-ALTER TABLE `creature_movement_template` CHANGE `orientation` `Orientation` FLOAT NOT NULL;
-ALTER TABLE `creature_movement_template` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL;
-ALTER TABLE `creature_movement_template` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL;
+ALTER TABLE `creature_movement_template` CHANGE `orientation` `Orientation` FLOAT NOT NULL DEFAULT '100';
+ALTER TABLE `creature_movement_template` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `creature_movement_template` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `creature_movement_template` CHANGE `comment` `Comment` TEXT NULL;
 
 ALTER TABLE `script_waypoint` CHANGE `entry` `Entry` MEDIUMINT UNSIGNED NOT NULL;
-ALTER TABLE `script_waypoint` CHANGE `pathId` `PathId` INT UNSIGNED NOT NULL;
+ALTER TABLE `script_waypoint` CHANGE `pathId` `PathId` INT UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `script_waypoint` CHANGE `pointid` `Point` MEDIUMINT UNSIGNED NOT NULL;
 ALTER TABLE `script_waypoint` CHANGE `position_x` `PositionX` FLOAT NOT NULL;
 ALTER TABLE `script_waypoint` CHANGE `position_y` `PositionY` FLOAT NOT NULL;
 ALTER TABLE `script_waypoint` CHANGE `position_z` `PositionZ` FLOAT NOT NULL;
-ALTER TABLE `script_waypoint` CHANGE `orientation` `Orientation` FLOAT NOT NULL;
-ALTER TABLE `script_waypoint` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL;
-ALTER TABLE `script_waypoint` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL;
+ALTER TABLE `script_waypoint` CHANGE `orientation` `Orientation` FLOAT NOT NULL DEFAULT '100';
+ALTER TABLE `script_waypoint` CHANGE `waittime` `WaitTime` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `script_waypoint` CHANGE `script_id` `ScriptId` MEDIUMINT UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `script_waypoint` CHANGE `comment` `Comment` TEXT NULL;
 
 
