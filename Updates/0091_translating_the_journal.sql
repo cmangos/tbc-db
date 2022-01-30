ddc28436e44ae1d35be6efb9f3a49dfd8432c893
MantisLord <sabinprosper@gmail.com>
2022-01-30 18:26:54 -0500

    Fix typo in 0091_translating_the_journal
    Thanks to @NeatElves for spotting

--- Updates/0091_translating_the_journal.sql
+++ Updates/0091_translating_the_journal.sql
@@ -28,12 +28,12 @@ INSERT INTO `creature_movement_template` (`Entry`, `PathId`, `Point`, `PositionX
 (6868, 0, 2, -6656.993, -2147.749, 249.6267, 100, 0, 0, 'custom'),
 (6868, 0, 3, -6657.808, -2158.299, 245.3514, 100, 0, 0, 'custom'),
 (6868, 0, 4, -6673.226, -2180.4727, 243.9145, 100, 0, 0, ''),
-(6868, 0, 5, -6681.091, -2169.716, 244.14854, 1.244383, 4000, 233801, 'custom, blizzlike: -6679.262, -2170.4294, 244.22798'),
+(6868, 0, 5, -6681.091, -2169.716, 244.14854, 1.244383, 4000, 686801, 'custom, blizzlike: -6679.262, -2170.4294, 244.22798'),
 (6868, 0, 6, -6673.226, -2180.4727, 243.9145, 100, 0, 0, 'custom'),
 (6868, 0, 7, -6657.808, -2158.299, 245.3514, 100, 0, 0, 'custom'),
 (6868, 0, 8, -6656.993, -2147.749, 249.6267, 100, 0, 0, 'custom'),
 (6868, 0, 9, -6654.75, -2145.6396, 251.954498, 100, 0, 0, 'custom'),
-(6868, 0, 10, -6653.346, -2139.3271, 251.90773, 5.480333805084228515, 5000, 233802, '');
+(6868, 0, 10, -6653.346, -2139.3271, 251.90773, 5.480333805084228515, 5000, 686802, '');
 DELETE FROM dbscripts_on_creature_movement WHERE id IN (686801,686802);
 INSERT INTO dbscripts_on_creature_movement (id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, condition_id, comments) VALUES
 (686801, 1000, 0, 1, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Translating the Journal: Jarkal Mossmeld - Emote OneShotKneel'),
