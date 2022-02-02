4bfe1a250c6b533792726254fd247037a548ef5b
Miraco <daaniel743@gmail.com>
2022-02-02 23:08:57 +0100

    rename file, and assign quest_start script to quest

--- Updates/0099_quest_enchantet_azsharite_ fel_weaponry.sql
+++ Updates/0099_quest_enchantet_azsharite_ fel_weaponry.sql
@@ -5,3 +5,5 @@ INSERT INTO `dbscripts_on_quest_start` (`id`, `delay`, `command`, `datalong`, `d
 ('3625', '5000', '1', '0', '0', '0', '0', '0', 'Enchanted Azsharite Fel Weaponry: Galvan the Ancient - Set Emote State to None'),
 ('3625', '5000', '7', '3625', '0', '0', '0', '0', 'Enchanted Azsharite Fel Weaponry: Player - Complete Quest'),
 ('3625', '5000', '0', '0', '0', '0', '0', '4542', 'Enchanted Azsharite Fel Weaponry: Galvan the Ancient - Say Text');
+
+UPDATE `quest_template` SET `CompleteScript`=3625 WHERE `entry` = 3625; 
