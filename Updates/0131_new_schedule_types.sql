b5ba1ceb3a54cfae14efe4b51728ee5adb7adcac
killerwife <killerwife@gmail.com>
2022-02-28 18:07:02 +0100

    Also add noblegarden

--- Updates/0131_new_schedule_types.sql
+++ Updates/0131_new_schedule_types.sql
@@ -2,5 +2,6 @@ UPDATE game_event SET schedule_type=12 WHERE entry=7; -- Lunar - lunar
 UPDATE game_event SET schedule_type=11 WHERE entry=8; -- Love is in the Air - Yearly
 UPDATE game_event SET schedule_type=11 WHERE entry=2; -- Feast of Winter Veil - Yearly
 UPDATE game_event SET schedule_type=11 WHERE entry=10; -- Childrens Week - Yearly
+UPDATE game_event SET schedule_type=13 WHERE entry=28; -- Noblegarden - easter
 
 
