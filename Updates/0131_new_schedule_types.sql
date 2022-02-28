042c0a230888aa9f872bcd14283d975a586f76a3
killerwife <killerwife@gmail.com>
2022-02-28 18:04:21 +0100

    Add new game event schedule types

---
+++ Updates/0131_new_schedule_types.sql
@@ -0,0 +1,6 @@
+UPDATE game_event SET schedule_type=12 WHERE entry=7; -- Lunar - lunar
+UPDATE game_event SET schedule_type=11 WHERE entry=8; -- Love is in the Air - Yearly
+UPDATE game_event SET schedule_type=11 WHERE entry=2; -- Feast of Winter Veil - Yearly
+UPDATE game_event SET schedule_type=11 WHERE entry=10; -- Childrens Week - Yearly
+
+
