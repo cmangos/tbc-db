e71814149c5d4410f8c5c076da08edc641946777
Nytanath <nytanath@protonmail.com>
2022-03-06 15:16:52 +0100

    [CDB][3761] Add missing instance_template entries for battlegrounds, PvP instances in Orgrimmar/Stormwind and Deeprun Tram.
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/64994b04b20bc3b1757ae8b0568bcccc268879b5

---
+++ Updates/0151_CDB-3761_instance_template.sql
@@ -0,0 +1,10 @@
+-- Just data alignment
+DELETE FROM `instance_template` WHERE `map` IN (369, 449, 450);
+INSERT INTO `instance_template` (`map`, `parent`, `levelMin`, `levelMax`, `maxPlayers`, `reset_delay`, `ghostEntranceMap`, `ghostEntranceX`, `ghostEntranceY`, `ScriptName`, `mountAllowed`) VALUES
+-- (30, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- 30	0	10	0	50	0		0
+(369, 0, 0, 0, 0, 0, 0, 0, 0, '', 0),
+(449, 0, 0, 0, 0, 0, 0, 0, 0, '', 0),
+(450, 0, 0, 0, 0, 0, 0, 0, 0, '', 0),
+-- (489, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- 489	0	10	0	50	0		0
+-- (529, 0, 0, 0, 0, 0, 0, 0, 0, '', 0); -- 529	0	10	0	50	0		0
+
