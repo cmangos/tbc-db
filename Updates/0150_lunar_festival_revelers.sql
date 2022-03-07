6a5569b4e9ecbe5f7216a865158aefb8e93aeebd
Nytanath <nytanath@protonmail.com>
2022-03-07 15:08:00 +0100

    Adjust faction and levels of some Lunar Festival Revelers.

---
+++ Updates/0150_lunar_festival_revelers.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `Faction`=35 WHERE `Entry` IN (15694, 15719, 15905, 15906, 15907, 15908);
+UPDATE `creature_template` SET `MinLevel`=1 WHERE `Entry`=15906;
+UPDATE `creature_template` SET `MaxLevel`=60 WHERE `Entry`=15694;
+
