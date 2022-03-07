a8b68305781a2048ed8a3ddd049bd0400e003081
killerwife <killerwife@gmail.com>
2022-03-07 21:24:32 +0100

    Add core revision at correct place

--- Updates/0159_CDB-3769_interaction_pause_timer.sql
+++ Updates/0159_CDB-3769_interaction_pause_timer.sql
@@ -1,3 +1,9 @@
+ALTER TABLE db_version CHANGE COLUMN required_s2442_01_mangos_npc_vendor required_s2443_01_mangos_creature_addon bit;
+
+ALTER TABLE `creature_template` ADD COLUMN `InteractionPauseTimer` INT(10) DEFAULT -1 NOT NULL AFTER `GossipMenuId`;
+
+
+
 -- Remove gossip flag changes from Private Thorsen
 DELETE FROM `dbscripts_on_creature_movement` WHERE `command`=29 AND `id` IN (73801, 73807);
 
