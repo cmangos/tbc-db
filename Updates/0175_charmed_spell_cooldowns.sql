4487fe8fe4d4e5ac9851f8933d5bc23283ee7958
Nytanath <nytanath@protonmail.com>
2022-03-18 16:39:53 +0100

    Add some known serverside cooldowns to charmed spells.

---
+++ Updates/0175_charmed_spell_cooldowns.sql
@@ -0,0 +1,4 @@
+UPDATE `spell_template` SET `RecoveryTime`=30000 WHERE `Id`=13398;
+UPDATE `spell_template` SET `RecoveryTime`=60000 WHERE `Id`=34019;
+UPDATE `spell_template` SET `RecoveryTime`=10000 WHERE `Id`=34073;
+
