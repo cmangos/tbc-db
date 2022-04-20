9e5de05f1de78fe574fdb29cec440f6e21942583
AnonXS <AnonXS@users.noreply.github.com>
2022-04-21 00:08:01 +0200

    [DBError] Remove game_event_time for GAME_EVENT_SCHEDULE_LUNAR_NEW_YEAR & GAME_EVENT_SCHEDULE_EASTER
    Back to 0 Errors

--- Updates/0131_new_schedule_types.sql
+++ Updates/0131_new_schedule_types.sql
@@ -4,4 +4,6 @@ UPDATE game_event SET schedule_type=11 WHERE entry=2; -- Feast of Winter Veil - 
 UPDATE game_event SET schedule_type=11 WHERE entry=10; -- Childrens Week - Yearly
 UPDATE game_event SET schedule_type=13 WHERE entry=28; -- Noblegarden - easter
 
+DELETE FROM `game_event_time` WHERE `entry` IN (7,28); -- enum GameEventScheduleType
+-- https://github.com/cmangos/issues/wiki/Game_event
 
