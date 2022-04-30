5ac3912f6ba54b37f414db1340e44fb307143d69
insunaa <dev.insuna@gmail.com>
2022-04-30 17:44:40 +0200

    add death prevention to red vs blue fighters

---
+++ Updates/0214_theramore_sparring.sql
@@ -0,0 +1,7 @@
+REPLACE INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `comment`) VALUES
+(492304, 4923, 0, 100, 1025, 0, 100, 180000, 185000, 42, 1, 'Guard Jarad - Set Death Prevention on Aggro'),
+(509204, 5092, 0, 100, 1025, 0, 100, 180000, 185000, 42, 1, 'Guard Lana - Set Death Prevention on Aggro'),
+(509404, 5094, 0, 100, 1025, 0, 100, 180000, 185000, 42, 1, 'Guard Tark - Set Death Prevention on Aggro'),
+(492204, 4922, 0, 100, 1025, 0, 100, 180000, 185000, 42, 1, 'Guard Edward - Set Death Prevention on Aggro'),
+(509104, 5091, 0, 100, 1025, 0, 100, 180000, 185000, 42, 1, 'Guard Kahil - Set Death Prevention on Aggro'),
+(509304, 5093, 0, 100, 1025, 0, 100, 180000, 185000, 42, 1, 'Guard Narrisha - Set Death Prevention on Aggro');
