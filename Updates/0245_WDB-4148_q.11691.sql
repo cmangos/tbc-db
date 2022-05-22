00215c7fe96b6b0e862de3ea1e49740bec7752e2
AnonXS <AnonXS@users.noreply.github.com>
2022-05-22 18:54:20 +0200

    Update 0245_WDB-4148_q.11691 - Player Name should be visible in Chatbox when displaying these texts

--- Updates/0245_WDB-4148_q.11691.sql
+++ Updates/0245_WDB-4148_q.11691.sql
@@ -5,7 +5,7 @@
 DoDisplayText entry 24895 tried to process emote for invalid source Gameobject (Entry: 187882 Guid: 5470023)
  DB-SCRIPTS: Process table `dbscripts_on_quest_end` id 11691, could not display text 24895 properly
 */
-UPDATE dbscripts_on_quest_end SET data_flags = 2 WHERE id = 11691 AND dataint > 1;
+UPDATE dbscripts_on_quest_end SET data_flags = 6 WHERE id = 11691 AND dataint > 1;
 
 -- player displays these texts: 24894,24893,24895
 -- Ahune, your strength grows no more!
