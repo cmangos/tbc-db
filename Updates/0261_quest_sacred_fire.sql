ee958ef4b15a73453094cb8d8e1ff774360319e6
Miraco <daaniel743@gmail.com>
2022-06-10 17:35:46 +0200

    correction

--- Updates/0261_quest_sacred_fire.sql
+++ Updates/0261_quest_sacred_fire.sql
@@ -38,7 +38,7 @@ INSERT INTO `creature_movement_template` (`entry`, `pathId`, `point`, `PositionX
 -- Quest Sacred Fire
 DELETE FROM dbscripts_on_quest_end WHERE id = '5062';
 INSERT INTO dbscripts_on_quest_end(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
-(5062,0,0,15,17117,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Sacred Fire - Magatha Grimtotem - Cast Spell'),
+(5062,0,0,15,17117,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Sacred Fire - Magatha Grimtotem - Cast Spell');
 
 
 UPDATE `quest_template` SET `CompleteScript`='5062' WHERE `entry`='5062'; 
