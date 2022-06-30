9536d4cf8f8f99aa2f3246326097262d40f4e396
Grz3s <Grz3s@users.noreply.github.com>
2022-06-30 18:01:10 +0200

    [WDB][4242] Gnomeregan Conjuror & Silvermoon Magister - update
    removed from event 100
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/5cd5f75b6802a2ea1c1aed7a8e32932407373e2e

---
+++ Updates/0388_WDB-4243_creatures_removed_from_event.sql
@@ -0,0 +1,4 @@
+-- bring back Gnomeregan Conjuror & Silvermoon Magister to 'The Stair of Destiny'
+DELETE FROM game_event_creature WHERE guid IN (68315,68316,68317,68318); -- ids 19006,19007
+-- 100	0	5184000	2592000	0	0	0	Before the Storm - Expansion Launch event
+
