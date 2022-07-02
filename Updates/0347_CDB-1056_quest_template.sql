5c4e5ccce0ec2676b94c8ae01635d92240613ebf
AnonXS <AnonXS@users.noreply.github.com>
2022-07-02 12:51:02 +0200

    Revert [CDB][1056] Quest: A Terrible Purpose - was ported to dbscripts_on_quest_end

--- Updates/0347_CDB-1056_quest_template.sql
+++ Updates/0347_CDB-1056_quest_template.sql
@@ -1,2 +1,3 @@
+-- SELECT * FROM dbscripts_on_quest_end where id IN (8287);
+-- UPDATE `quest_template` SET `RewMailTemplateId`='110', `RewMailDelaySecs`='86400' WHERE `entry`='8287';
 
-UPDATE `quest_template` SET `RewMailTemplateId`='110', `RewMailDelaySecs`='86400' WHERE `entry`='8287';
