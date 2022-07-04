6724f52f456e274bfd10a93c795db5e186847c26
Miraco <daaniel743@gmail.com>
2022-07-04 23:03:09 +0200

    Fix typo in [0312] quest 'A humble task'

--- Updates/0312_quest_humble_task.sql
+++ Updates/0312_quest_humble_task.sql
@@ -2,7 +2,7 @@
 DELETE FROM `dbscripts_on_quest_start` WHERE id = '753';
 INSERT INTO dbscripts_on_quest_start(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
 (753,0,0,0,0,0,0,0,0,0,1528,0,0,0,0,0,0,0,0,0,0,'Greatmother Hawkwind - Say Text'),
-(1960,40000,0,31,2991,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'A Humble Task - Spam Prevention');
+(753,20000,0,31,2991,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'A Humble Task - Spam Prevention');
 
 UPDATE `quest_template` SET `StartScript`='753' WHERE `entry`='753';
 
