eaa962878bc77d1a836e6a5cf302ef6ec359d7fd
Grz3s <Grz3s@users.noreply.github.com>
2022-07-11 00:03:25 +0200

    [WDB][4260][ACID] Engineering: Target Dummy
    Fixed (Req core update for spell:27791)
    
    - Target Dummy 2673
    - Advanced Target Dummy 2674
    - Masterwork Target Dummy 12426
    
    Ref: https://github.com/cmangos/wotlk-db/commit/576cc496081e7359fa58f4a66889e0e3ff8d9b9c#commitcomment-78100588
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/688b64ff1272d873c17c215d2bc3aa518241f0e0

---
+++ Updates/0390_WDB-4260_Target Dummy.sql
@@ -0,0 +1,8 @@
+-- Engineering
+-- Target Dummy 2673
+UPDATE creature_template SET MinLevel = 20, MaxLevel = 20 WHERE entry = 2673;
+-- Advanced Target Dummy 2674
+UPDATE creature_template SET MinLevel = 40, MaxLevel = 40 WHERE entry = 2674;
+-- Masterwork Target Dummy 12426
+UPDATE creature_template SET MinLevel = 55, MaxLevel = 55 WHERE entry = 12426;
+
