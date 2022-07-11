dce5dc5c0c2311803ffd93bf764c8c1880a14256
AnonXS <AnonXS@users.noreply.github.com>
2022-07-11 20:13:59 +0200

    Reinsert Blackout Mask as more was wrong then just Mind Flay and VT missing
    based on https://github.com/cmangos/classic-db/commit/b1b7e676c4c46f387d5ef5da346f2a95598e60a3

--- Updates/0364_blackout_proc_flags.sql
+++ Updates/0364_blackout_proc_flags.sql
@@ -1,4 +1,25 @@
 -- first mask applicable to vanilla, second only to tbc, none to wotlk
-UPDATE spell_proc_event SET SpellFamilyMask0=SpellFamilyMask0|(0x0000000000800000 + 0x0000040000000000) WHERE entry=15268;
+UPDATE spell_proc_event SET SpellFamilyMask0=(8192+32768+524288+8388608+33554432+8589934592+4398046511104) WHERE entry=15268; -- |(0x0000000000800000 + 0x0000040000000000) Now 4406678953984
 
+-- 0x80000 (524288) = Touch of Weakness (Procced Damage Spell) - only mask missing in classicmangos is 524288, mana burn was wrong in classicmangos
+-- Mind Flay (this commit, 0x0000000000800000 (8388608)), Vampiric Touch (0x0000040000000000 (4398046511104) nonexistant in classic)
+
+-- 8129	Mana Burn	6	16 -- https://wowpedia.fandom.com/wiki/Blackout?direction=prev&oldid=1669035 - NOT
+-- 8092	Mind Blast	6	8192
+-- 589	Shadow Word: Pain	6	32768
+-- 2943	Touch of Weakness	6	524288
+-- 15407	Mind Flay	6	8388608
+-- 2944	Devouring Plague	6	17592219598848 (33554432)
+-- 28377	Shadowguard	6	33554432
+-- 32379	Shadow Word: Death	6	8589934592 (not classic)
+-- 34914	Vampiric Touch	6	4398046511104 (not classic)
+
+-- Feedback? s.6347 - https://wowpedia.fandom.com/wiki/Blackout?direction=prev&oldid=1669035 - NOT
+
+-- classic: https://wowpedia.fandom.com/wiki/Blackout?direction=next&oldid=282365 (still no Feedback or Mana Burn)
+
+-- Current tbcmask is 4406746062848 (4398046511104+8589934592+67108864,33554432,8388608,524288,32768,8192)
+-- -4398046511104 = 8699551744 (Vampiric Touch)
+-- -8589934592 = 109617152 (Shadow Word: Death)
+-- 67108864,33554432,8388608,524288,32768,8192 (67108864 wrong as ToW DMG Component is 524288)
 
