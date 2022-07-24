51524eeb7cd6fb11e2bcf69b9e868e48106677d9
AnonXS <AnonXS@users.noreply.github.com>
2022-07-24 23:03:29 +0200

    Improve PPM for Mongoose and Executioner s.28093,42976

---
+++ Updates/0403_mongoose_executioner_ppm.sql
@@ -0,0 +1,3 @@
+UPDATE `spell_proc_item_enchant` SET `ppmRate` = 1.2 WHERE `entry` = 28093; -- Lightning Speed
+UPDATE `spell_proc_item_enchant` SET `ppmRate` = 1.2 WHERE `entry` = 42976; -- Executioner
+
