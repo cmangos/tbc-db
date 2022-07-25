eaa8ca375608daac8bf26bc2f2921a4f9e8efee1
_daanieL <daaniel743@gmail.com>
2022-07-25 12:18:21 +0200

    Add The Mortar: Reloaded to Goblin Engineering Trainer (#1105)
    * Add The Mortar: Reloaded to Goblin Engineering Trainer
    
    Based on https://tbc.wowhead.com/spell=13240/the-mortar-reloaded
    
    Co-authored-by: AnonXS <anonxsdev@gmail.com>

---
+++ Updates/0410_goblin_engineering.sql
@@ -0,0 +1,9 @@
+-- Add The Mortar: Reloaded to Goblin Engineering Trainer
+-- Based on https://tbc.wowhead.com/spell=13240/the-mortar-reloaded
+DELETE FROM npc_trainer_template WHERE entry = '1003' AND spell = '13240';
+INSERT INTO `npc_trainer_template` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `condition_id`) VALUES ('1003', '13240', '1500', '202', '205', '0', '0'); -- 15s, 205skillvalue
+
+-- used by
+-- 8126	Nixx Sprocketspring	Goblin Engineering Trainer
+-- 8738	Vazario Linkgrease	Goblin Engineering Trainer
+
