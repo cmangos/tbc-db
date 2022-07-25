2bebbf228eae24df6ca466c053bab0a7f9356dc6
AnonXS <anonxsdev@gmail.com>
2022-07-25 12:17:59 +0200

    Update and rename 0398_goblin_engineering.sql to 0410_goblin_engineering.sql
    fix typo, add info

--- Updates/0398_goblin_engineering.sql
+++
@@ -1,4 +0,0 @@
--- Add The Mortar: Reloaded to Goblin Engineering Trainer
--- Based on https://tbc.wowhead.com/spell=13240/the-mortar-reloaded
-DELETE FROM npc_trainer_template WHERE entry = '1003' AND spell '13240';
-INSERT INTO `npc_trainer_template` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `condition_id`) VALUES ('1003', '13240', '1500', '202', '205', '0', '0');
