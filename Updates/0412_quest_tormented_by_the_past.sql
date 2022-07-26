f8ef8b9a9a6f26d71814568a37395945c05ccf4a
_daanieL <daaniel743@gmail.com>
2022-07-26 18:14:42 +0200

    Update 0412_quest_tormented_by_the_past.sql

--- Updates/0412_quest_tormented_by_the_past.sql
+++ Updates/0412_quest_tormented_by_the_past.sql
@@ -16,8 +16,8 @@ INSERT INTO dbscripts_on_relay(id, delay, priority, command, datalong, datalong2
 -- Start script for Quest 'Remorseful Highborne'
 DELETE FROM `dbscripts_on_quest_start` WHERE id = '5252';
 INSERT INTO dbscripts_on_quest_start(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
-(5252,0,0,0,50,0,0,0,0,0,6505,6506,6507,0,0,0,0,0,0,0,0,'Remorseful Highborne - Say RND Text'),
-(947,20000,0,31,10684,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Remorseful Highborne - Spam Prevention');
+(5252,0,0,0,0,0,0,0,0,0,6505,6506,6507,0,0,0,0,0,0,0,0,'Remorseful Highborne - Say RND Text'),
+(5252,20000,0,31,10684,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Remorseful Highborne - Spam Prevention');
 
 UPDATE `quest_template` SET `StartScript`='5252' WHERE `entry`='5252';
 
