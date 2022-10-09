2d74377b87485c91bc3e11ab15dc5ce583b05adb
AnonXS <AnonXS@users.noreply.github.com>
2022-10-09 18:34:58 +0200

    Add Vendor Slots for Indormi 23437 & Tydormu 23381

---
+++ Updates/0540_hyjal_vendors.sql
@@ -0,0 +1,120 @@
+-- ["Indormi"] = {
+UPDATE `npc_vendor` SET `slot` = 1 WHERE `entry` = 23437 AND `item` = 32274; -- Design: Bold Crimson Spinel
+UPDATE `npc_vendor` SET `slot` = 2 WHERE `entry` = 23437 AND `item` = 32283; -- Design: Bright Crimson Spinel
+UPDATE `npc_vendor` SET `slot` = 3 WHERE `entry` = 23437 AND `item` = 32277; -- Design: Delicate Crimson Spinel
+UPDATE `npc_vendor` SET `slot` = 4 WHERE `entry` = 23437 AND `item` = 32282; -- Design: Runed Crimson Spinel
+UPDATE `npc_vendor` SET `slot` = 5 WHERE `entry` = 23437 AND `item` = 32284; -- Design: Subtle Crimson Spinel
+UPDATE `npc_vendor` SET `slot` = 6 WHERE `entry` = 23437 AND `item` = 32281; -- Design: Teardrop Crimson Spinel
+UPDATE `npc_vendor` SET `slot` = 7 WHERE `entry` = 23437 AND `item` = 32286; -- Design: Solid Empyrean Sapphire
+UPDATE `npc_vendor` SET `slot` = 8 WHERE `entry` = 23437 AND `item` = 32288; -- Design: Lustrous Empyrean Sapphire
+UPDATE `npc_vendor` SET `slot` = 9 WHERE `entry` = 23437 AND `item` = 32287; -- Design: Sparkling Empyrean Sapphire
+UPDATE `npc_vendor` SET `slot` = 10 WHERE `entry` = 23437 AND `item` = 32290; -- Design: Brilliant Lionseye
+UPDATE `npc_vendor` SET `slot` = 11 WHERE `entry` = 23437 AND `item` = 32291; -- Design: Smooth Lionseye
+UPDATE `npc_vendor` SET `slot` = 12 WHERE `entry` = 23437 AND `item` = 32293; -- Design: Gleaming Lionseye
+UPDATE `npc_vendor` SET `slot` = 13 WHERE `entry` = 23437 AND `item` = 32294; -- Design: Thick Lionseye
+UPDATE `npc_vendor` SET `slot` = 14 WHERE `entry` = 23437 AND `item` = 32299; -- Design: Balanced Shadowsong Amethyst
+UPDATE `npc_vendor` SET `slot` = 15 WHERE `entry` = 23437 AND `item` = 32300; -- Design: Infused Shadowsong Amethyst
+UPDATE `npc_vendor` SET `slot` = 16 WHERE `entry` = 23437 AND `item` = 32301; -- Design: Glowing Shadowsong Amethyst
+UPDATE `npc_vendor` SET `slot` = 17 WHERE `entry` = 23437 AND `item` = 32306; -- Design: Glinting Pyrestone
+UPDATE `npc_vendor` SET `slot` = 18 WHERE `entry` = 23437 AND `item` = 32305; -- Design: Luminous Pyrestone
+UPDATE `npc_vendor` SET `slot` = 19 WHERE `entry` = 23437 AND `item` = 32304; -- Design: Potent Pyrestone
+UPDATE `npc_vendor` SET `slot` = 20 WHERE `entry` = 23437 AND `item` = 32311; -- Design: Dazzling Seaspray Emerald
+UPDATE `npc_vendor` SET `slot` = 21 WHERE `entry` = 23437 AND `item` = 32312; -- Design: Jagged Seaspray Emerald
+UPDATE `npc_vendor` SET `slot` = 22 WHERE `entry` = 23437 AND `item` = 32310; -- Design: Radiant Seaspray Emerald
+UPDATE `npc_vendor` SET `slot` = 23 WHERE `entry` = 23437 AND `item` = 32309; -- Design: Enduring Seaspray Emerald
+UPDATE `npc_vendor` SET `slot` = 24 WHERE `entry` = 23437 AND `item` = 32308; -- Design: Wicked Pyrestone
+UPDATE `npc_vendor` SET `slot` = 25 WHERE `entry` = 23437 AND `item` = 32302; -- Design: Royal Shadowsong Amethyst
+UPDATE `npc_vendor` SET `slot` = 26 WHERE `entry` = 23437 AND `item` = 32292; -- Design: Rigid Lionseye
+-- guessed as added in later patch
+UPDATE `npc_vendor` SET `slot` = 27 WHERE `entry` = 23437 AND `item` = 35762; -- Design: Reckless Pyrestone
+UPDATE `npc_vendor` SET `slot` = 28 WHERE `entry` = 23437 AND `item` = 35763; -- Design: Quick Lionseye
+UPDATE `npc_vendor` SET `slot` = 29 WHERE `entry` = 23437 AND `item` = 35764; -- Design: Steady Seaspray Emerald
+UPDATE `npc_vendor` SET `slot` = 30 WHERE `entry` = 23437 AND `item` = 35765; -- Design: Forceful Seaspray Emerald
+
+-- ["Tydormu"] = {
+UPDATE `npc_vendor` SET `slot` = 1 WHERE `entry` = 23381 AND `item` = 31060; -- Gloves of Absolution
+UPDATE `npc_vendor` SET `slot` = 2 WHERE `entry` = 23381 AND `item` = 31063; -- Cowl of Absolution
+UPDATE `npc_vendor` SET `slot` = 3 WHERE `entry` = 23381 AND `item` = 31068; -- Breeches of Absolution
+UPDATE `npc_vendor` SET `slot` = 4 WHERE `entry` = 23381 AND `item` = 31069; -- Mantle of Absolution
+UPDATE `npc_vendor` SET `slot` = 5 WHERE `entry` = 23381 AND `item` = 31066; -- Vestments of Absolution
+UPDATE `npc_vendor` SET `slot` = 6 WHERE `entry` = 23381 AND `item` = 31061; -- Handguards of Absolution
+UPDATE `npc_vendor` SET `slot` = 7 WHERE `entry` = 23381 AND `item` = 31064; -- Hood of Absolution
+UPDATE `npc_vendor` SET `slot` = 8 WHERE `entry` = 23381 AND `item` = 31067; -- Leggings of Absolution
+UPDATE `npc_vendor` SET `slot` = 9 WHERE `entry` = 23381 AND `item` = 31070; -- Shoulderpads of Absolution
+UPDATE `npc_vendor` SET `slot` = 10 WHERE `entry` = 23381 AND `item` = 31065; -- Shroud of Absolution
+UPDATE `npc_vendor` SET `slot` = 11 WHERE `entry` = 23381 AND `item` = 31055; -- Gloves of the Tempest
+UPDATE `npc_vendor` SET `slot` = 12 WHERE `entry` = 23381 AND `item` = 31056; -- Cowl of the Tempest
+UPDATE `npc_vendor` SET `slot` = 13 WHERE `entry` = 23381 AND `item` = 31058; -- Leggings of the Tempest
+UPDATE `npc_vendor` SET `slot` = 14 WHERE `entry` = 23381 AND `item` = 31059; -- Mantle of the Tempest
+UPDATE `npc_vendor` SET `slot` = 15 WHERE `entry` = 23381 AND `item` = 31057; -- Robes of the Tempest
+UPDATE `npc_vendor` SET `slot` = 16 WHERE `entry` = 23381 AND `item` = 31050; -- Gloves of the Malefic
+UPDATE `npc_vendor` SET `slot` = 17 WHERE `entry` = 23381 AND `item` = 31051; -- Hood of the Malefic
+UPDATE `npc_vendor` SET `slot` = 18 WHERE `entry` = 23381 AND `item` = 31053; -- Leggings of the Malefic
+UPDATE `npc_vendor` SET `slot` = 19 WHERE `entry` = 23381 AND `item` = 31054; -- Mantle of the Malefic
+UPDATE `npc_vendor` SET `slot` = 20 WHERE `entry` = 23381 AND `item` = 31052; -- Robe of the Malefic
+UPDATE `npc_vendor` SET `slot` = 21 WHERE `entry` = 23381 AND `item` = 31026; -- Slayer's Handguards
+UPDATE `npc_vendor` SET `slot` = 22 WHERE `entry` = 23381 AND `item` = 31027; -- Slayer's Helm
+UPDATE `npc_vendor` SET `slot` = 23 WHERE `entry` = 23381 AND `item` = 31029; -- Slayer's Legguards
+UPDATE `npc_vendor` SET `slot` = 24 WHERE `entry` = 23381 AND `item` = 31030; -- Slayer's Shoulderpads
+UPDATE `npc_vendor` SET `slot` = 25 WHERE `entry` = 23381 AND `item` = 31028; -- Slayer's Chestguard
+UPDATE `npc_vendor` SET `slot` = 26 WHERE `entry` = 23381 AND `item` = 31034; -- Thunderheart Gauntlets
+UPDATE `npc_vendor` SET `slot` = 27 WHERE `entry` = 23381 AND `item` = 31039; -- Thunderheart Cover
+UPDATE `npc_vendor` SET `slot` = 28 WHERE `entry` = 23381 AND `item` = 31044; -- Thunderheart Leggings
+UPDATE `npc_vendor` SET `slot` = 29 WHERE `entry` = 23381 AND `item` = 31048; -- Thunderheart Pauldrons
+UPDATE `npc_vendor` SET `slot` = 30 WHERE `entry` = 23381 AND `item` = 31042; -- Thunderheart Chestguard
+UPDATE `npc_vendor` SET `slot` = 31 WHERE `entry` = 23381 AND `item` = 31032; -- Thunderheart Gloves
+UPDATE `npc_vendor` SET `slot` = 32 WHERE `entry` = 23381 AND `item` = 31037; -- Thunderheart Helmet
+UPDATE `npc_vendor` SET `slot` = 33 WHERE `entry` = 23381 AND `item` = 31045; -- Thunderheart Legguards
+UPDATE `npc_vendor` SET `slot` = 34 WHERE `entry` = 23381 AND `item` = 31047; -- Thunderheart Spaulders
+UPDATE `npc_vendor` SET `slot` = 35 WHERE `entry` = 23381 AND `item` = 31041; -- Thunderheart Tunic
+UPDATE `npc_vendor` SET `slot` = 36 WHERE `entry` = 23381 AND `item` = 31035; -- Thunderheart Handguards
+UPDATE `npc_vendor` SET `slot` = 37 WHERE `entry` = 23381 AND `item` = 31040; -- Thunderheart Headguard
+UPDATE `npc_vendor` SET `slot` = 38 WHERE `entry` = 23381 AND `item` = 31046; -- Thunderheart Pants
+UPDATE `npc_vendor` SET `slot` = 39 WHERE `entry` = 23381 AND `item` = 31049; -- Thunderheart Shoulderpads
+UPDATE `npc_vendor` SET `slot` = 40 WHERE `entry` = 23381 AND `item` = 31043; -- Thunderheart Vest
+UPDATE `npc_vendor` SET `slot` = 41 WHERE `entry` = 23381 AND `item` = 31001; -- Gronnstalker's Gloves
+UPDATE `npc_vendor` SET `slot` = 42 WHERE `entry` = 23381 AND `item` = 31003; -- Gronnstalker's Helmet
+UPDATE `npc_vendor` SET `slot` = 43 WHERE `entry` = 23381 AND `item` = 31005; -- Gronnstalker's Leggings
+UPDATE `npc_vendor` SET `slot` = 44 WHERE `entry` = 23381 AND `item` = 31006; -- Gronnstalker's Spaulders
+UPDATE `npc_vendor` SET `slot` = 45 WHERE `entry` = 23381 AND `item` = 31004; -- Gronnstalker's Chestguard
+UPDATE `npc_vendor` SET `slot` = 46 WHERE `entry` = 23381 AND `item` = 31011; -- Skyshatter Grips
+UPDATE `npc_vendor` SET `slot` = 47 WHERE `entry` = 23381 AND `item` = 31015; -- Skyshatter Cover
+UPDATE `npc_vendor` SET `slot` = 48 WHERE `entry` = 23381 AND `item` = 31021; -- Skyshatter Pants
+UPDATE `npc_vendor` SET `slot` = 49 WHERE `entry` = 23381 AND `item` = 31024; -- Skyshatter Pauldrons
+UPDATE `npc_vendor` SET `slot` = 50 WHERE `entry` = 23381 AND `item` = 31018; -- Skyshatter Tunic
+UPDATE `npc_vendor` SET `slot` = 51 WHERE `entry` = 23381 AND `item` = 31007; -- Skyshatter Gloves
+UPDATE `npc_vendor` SET `slot` = 52 WHERE `entry` = 23381 AND `item` = 31012; -- Skyshatter Helmet
+UPDATE `npc_vendor` SET `slot` = 53 WHERE `entry` = 23381 AND `item` = 31019; -- Skyshatter Leggings
+UPDATE `npc_vendor` SET `slot` = 54 WHERE `entry` = 23381 AND `item` = 31022; -- Skyshatter Shoulderpads
+UPDATE `npc_vendor` SET `slot` = 55 WHERE `entry` = 23381 AND `item` = 31016; -- Skyshatter Chestguard
+UPDATE `npc_vendor` SET `slot` = 56 WHERE `entry` = 23381 AND `item` = 31008; -- Skyshatter Gauntlets
+UPDATE `npc_vendor` SET `slot` = 57 WHERE `entry` = 23381 AND `item` = 31014; -- Skyshatter Headguard
+UPDATE `npc_vendor` SET `slot` = 58 WHERE `entry` = 23381 AND `item` = 31020; -- Skyshatter Legguards
+UPDATE `npc_vendor` SET `slot` = 59 WHERE `entry` = 23381 AND `item` = 31023; -- Skyshatter Mantle
+UPDATE `npc_vendor` SET `slot` = 60 WHERE `entry` = 23381 AND `item` = 31017; -- Skyshatter Breastplate
+UPDATE `npc_vendor` SET `slot` = 61 WHERE `entry` = 23381 AND `item` = 30982; -- Lightbringer Gauntlets
+UPDATE `npc_vendor` SET `slot` = 62 WHERE `entry` = 23381 AND `item` = 30989; -- Lightbringer War-Helm
+UPDATE `npc_vendor` SET `slot` = 63 WHERE `entry` = 23381 AND `item` = 30993; -- Lightbringer Greaves
+UPDATE `npc_vendor` SET `slot` = 64 WHERE `entry` = 23381 AND `item` = 30997; -- Lightbringer Shoulderbraces
+UPDATE `npc_vendor` SET `slot` = 65 WHERE `entry` = 23381 AND `item` = 30990; -- Lightbringer Breastplate
+UPDATE `npc_vendor` SET `slot` = 66 WHERE `entry` = 23381 AND `item` = 30983; -- Lightbringer Gloves
+UPDATE `npc_vendor` SET `slot` = 67 WHERE `entry` = 23381 AND `item` = 30988; -- Lightbringer Greathelm
+UPDATE `npc_vendor` SET `slot` = 68 WHERE `entry` = 23381 AND `item` = 30994; -- Lightbringer Leggings
+UPDATE `npc_vendor` SET `slot` = 69 WHERE `entry` = 23381 AND `item` = 30996; -- Lightbringer Pauldrons
+UPDATE `npc_vendor` SET `slot` = 70 WHERE `entry` = 23381 AND `item` = 30992; -- Lightbringer Chestpiece
+UPDATE `npc_vendor` SET `slot` = 71 WHERE `entry` = 23381 AND `item` = 30985; -- Lightbringer Handguards
+UPDATE `npc_vendor` SET `slot` = 72 WHERE `entry` = 23381 AND `item` = 30987; -- Lightbringer Faceguard
+UPDATE `npc_vendor` SET `slot` = 73 WHERE `entry` = 23381 AND `item` = 30995; -- Lightbringer Legguards
+UPDATE `npc_vendor` SET `slot` = 74 WHERE `entry` = 23381 AND `item` = 30998; -- Lightbringer Shoulderguards
+UPDATE `npc_vendor` SET `slot` = 75 WHERE `entry` = 23381 AND `item` = 30991; -- Lightbringer Chestguard
+UPDATE `npc_vendor` SET `slot` = 76 WHERE `entry` = 23381 AND `item` = 30969; -- Onslaught Gauntlets
+UPDATE `npc_vendor` SET `slot` = 77 WHERE `entry` = 23381 AND `item` = 30972; -- Onslaught Battle-Helm
+UPDATE `npc_vendor` SET `slot` = 78 WHERE `entry` = 23381 AND `item` = 30977; -- Onslaught Greaves
+UPDATE `npc_vendor` SET `slot` = 79 WHERE `entry` = 23381 AND `item` = 30979; -- Onslaught Shoulderblades
+UPDATE `npc_vendor` SET `slot` = 80 WHERE `entry` = 23381 AND `item` = 30975; -- Onslaught Breastplate
+UPDATE `npc_vendor` SET `slot` = 81 WHERE `entry` = 23381 AND `item` = 30970; -- Onslaught Handguards
+UPDATE `npc_vendor` SET `slot` = 82 WHERE `entry` = 23381 AND `item` = 30974; -- Onslaught Greathelm
+UPDATE `npc_vendor` SET `slot` = 83 WHERE `entry` = 23381 AND `item` = 30978; -- Onslaught Legguards
+UPDATE `npc_vendor` SET `slot` = 84 WHERE `entry` = 23381 AND `item` = 30980; -- Onslaught Shoulderguards
+UPDATE `npc_vendor` SET `slot` = 85 WHERE `entry` = 23381 AND `item` = 30976; -- Onslaught Chestguard
+
