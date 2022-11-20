910b3a5c5f292f7f70fe32931801584b684a5e14
AnonXS <anonxsdev@gmail.com>
2022-11-20 14:40:57 +0100

    [CDB][DBError] Correct Goblin Mortar Spell (thx @NeatElves)
    Table `npc_trainer` for trainer (Entry: 8126) has non-learning spell 13240, ignore
    Table `npc_trainer` for trainer (Entry: 8738) has non-learning spell 13240, ignore
    
    Linked to https://github.com/cmangos/tbc-db/commit/eaa8ca375608daac8bf26bc2f2921a4f9e8efee1#commitcomment-79472283
    
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/3d6144927ed26b8dab0c7400690aed6042679b71

--- Updates/0410_goblin_engineering.sql
+++ Updates/0410_goblin_engineering.sql
@@ -1,7 +1,7 @@
 -- Add The Mortar: Reloaded to Goblin Engineering Trainer
 -- Based on https://tbc.wowhead.com/spell=13240/the-mortar-reloaded
-DELETE FROM npc_trainer_template WHERE entry = '1003' AND spell = '13240';
-INSERT INTO `npc_trainer_template` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `condition_id`) VALUES ('1003', '13240', '1500', '202', '205', '0', '0'); -- 15s, 205skillvalue
+DELETE FROM npc_trainer_template WHERE entry = '1003' AND spell IN ('13240','12768');
+INSERT INTO `npc_trainer_template` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `condition_id`) VALUES ('1003', '12768', '1500', '202', '205', '0', '0'); -- 15s, 205skillvalue
 
 -- used by
 -- 8126	Nixx Sprocketspring	Goblin Engineering Trainer
