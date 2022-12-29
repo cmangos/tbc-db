4cbf1107c26812d4ae439fc2daed5bcf4bfcc856
Grz3s <Grz3s@users.noreply.github.com>
2022-12-29 01:39:58 +0100

    [WDB][4571] q.8341 'Lords of the Council'
    corrected
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/519
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/918172bf9310094339be18bb679558fd54f3bc31

---
+++ Updates/0622_WDB-4571_q.8341.sql
@@ -0,0 +1,3 @@
+-- q.8341 'Lords of the Council'
+-- To see this quest you must be at least revered with Cenarion Circle.
+UPDATE quest_template SET /*RequiredMinRepValue=12000,*/ PrevQuestId = 0 WHERE entry=8341; -- 8343
