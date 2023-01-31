b5bbdfe9c2ee2aad7ca8b8e583466732340525f1
Grz3s <Grz3s@users.noreply.github.com>
2023-01-31 15:46:27 +0100

    [WDB][4667] Scarlet High Clerist 1839
    moved to confirmed possition
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/62f71669fe83f65cc7344477cd0731f7d19ae30a

---
+++ Updates/0652_WDB-4667_c.1839.sql
@@ -0,0 +1,3 @@
+-- Scarlet High Clerist 1839 - fix position (may have waypoints or diff spawn points)
+UPDATE creature SET position_x = 2686.4521, position_y = -1949.7223, position_z = 72.23332, orientation=0.331612557172775268 WHERE id = 1839 AND `guid` = 49764;
+
