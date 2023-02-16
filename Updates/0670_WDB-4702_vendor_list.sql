b84ed846cd792d37c64ff2d891713d9e29ec8ef9
Grz3s <Grz3s@users.noreply.github.com>
2023-02-17 00:07:27 +0100

    [WDB][4702] vendor list corrections for several npcs
    data taken from 3.x.x
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/29738e1d8629894ce8ed50d59886856121e6fbb1

---
+++ Updates/0670_WDB-4702_vendor_list.sql
@@ -0,0 +1,72 @@
+-- Godric Rothgar 1213
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 2129 AND `entry` = 234; -- Large Round Shield
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 2379 AND `entry` = 234; -- Tarnished Chain Vest
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 2380 AND `entry` = 234; -- Tarnished Chain Belt
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 2381 AND `entry` = 234; -- Tarnished Chain Leggings
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 2383 AND `entry` = 234; -- Tarnished Chain Boots
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 2384 AND `entry` = 234; -- Tarnished Chain Bracers
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 2385 AND `entry` = 234; -- Tarnished Chain Gloves
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 17184 AND `entry` = 234; -- Small Shield
+
+-- Dermot Johns 190
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 2117 AND `entry` = 214; -- Thin Cloth Shoes
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 2119 AND `entry` = 214; -- Thin Cloth Gloves
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 2120 AND `entry` = 214; -- Thin Cloth Pants
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 2121 AND `entry` = 214; -- Thin Cloth Armor
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 2122 AND `entry` = 214; -- Cracked Leather Belt
+UPDATE `npc_vendor_template` SET `slot` = 10 WHERE `item` = 2123 AND `entry` = 214; -- Cracked Leather Boots
+UPDATE `npc_vendor_template` SET `slot` = 11 WHERE `item` = 2124 AND `entry` = 214; -- Cracked Leather Bracers
+UPDATE `npc_vendor_template` SET `slot` = 12 WHERE `item` = 2125 AND `entry` = 214; -- Cracked Leather Gloves
+UPDATE `npc_vendor_template` SET `slot` = 9 WHERE `item` = 2126 AND `entry` = 214; -- Cracked Leather Pants
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 2127 AND `entry` = 214; -- Cracked Leather Vest
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 3599 AND `entry` = 214; -- Thin Cloth Belt
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 3600 AND `entry` = 214; -- Thin Cloth Bracers
+
+-- Brother Danil 152
+-- vendor list corrected
+-- Duokna 3158
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 159 AND `entry` = 426; -- Refreshing Spring Water
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 2512 AND `entry` = 426; -- Rough Arrow
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 2516 AND `entry` = 426; -- Light Shot
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 4540 AND `entry` = 426; -- Tough Hunk of Bread
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 25861 AND `entry` = 426; -- Crude Throwing Axe
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 28979 AND `entry` = 426; -- Light Throwing Knife
+
+-- Janos Hammerknuckle 78
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 1194 AND `entry` = 320; -- Bastard Sword
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 2130 AND `entry` = 320; -- Club
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 2131 AND `entry` = 320; -- Shortsword
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 2132 AND `entry` = 320; -- Short Staff
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 2134 AND `entry` = 320; -- Hand Axe
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 2139 AND `entry` = 320; -- Dirk
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 2479 AND `entry` = 320; -- Broad Axe
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 2480 AND `entry` = 320; -- Large Club
+
+-- Kaita Deepforge 5512 - 116 template
+-- vendor list corrected
+UPDATE `npc_vendor` SET `slot` = 1 WHERE `item` = 12162 AND `entry` = 5512; -- Plans: Hardened Iron Shortsword
+UPDATE `npc_vendor_template` SET `slot` = 101 WHERE `item` = 2901; -- Mining Pick (1)
+UPDATE `npc_vendor_template` SET `slot` = 102 WHERE `item` = 5956; -- Blacksmith Hammer (2)
+UPDATE `npc_vendor_template` SET `slot` = 103 WHERE `item` = 2880; -- Weak Flux (3)
+UPDATE `npc_vendor_template` SET `slot` = 104 WHERE `item` = 3466; -- Strong Flux (4)
+UPDATE `npc_vendor_template` SET `slot` = 105 WHERE `item` = 3857; -- Coal (5)
+UPDATE `npc_vendor_template` SET `slot` = 106 WHERE `item` = 18567; -- Elemental Flux (6)
+
+-- Roberto Pupellyverbos 277
+-- vendor list corrected
+-- Julia Gallina 1301
+-- vendor list corrected
+UPDATE `npc_vendor` SET `slot` = 1 WHERE `item` = 1941 AND `entry` = 277; -- Cask of Merlot
+UPDATE `npc_vendor_template` SET `slot` = 102 WHERE `item` = 2593 AND `entry` = 402; -- Flask of Port (0)
+UPDATE `npc_vendor_template` SET `slot` = 104 WHERE `item` = 2594 AND `entry` = 402; -- Flagon of Mead (0)
+UPDATE `npc_vendor_template` SET `slot` = 105 WHERE `item` = 2595 AND `entry` = 402; -- Jug of Bourbon (0)
+UPDATE `npc_vendor_template` SET `slot` = 103 WHERE `item` = 2596 AND `entry` = 402; -- Skin of Dwarven Stout (0)
+UPDATE `npc_vendor_template` SET `slot` = 101 WHERE `item` = 2723 AND `entry` = 402; -- Bottle of Pinot Noir (0)
+
+-- Olvia 3312 - 425 template
+-- vendor list corrected
+
