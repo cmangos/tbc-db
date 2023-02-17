6c738596a8f1f7bb2a3f62354196080952fef224
Grz3s <Grz3s@users.noreply.github.com>
2023-02-17 15:46:00 +0100

    [WDB][4704] area around Westbrook Garrison
    also more updates to [4702]
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/0210981283a22060c32eac4fa92ca3a13626c324
    
    Co-authored-by: AnonXS <AnonXS@users.noreply.github.com>

--- Updates/0670_WDB-4702_vendor_list.sql
+++ Updates/0670_WDB-4702_vendor_list.sql
@@ -70,3 +70,138 @@ UPDATE `npc_vendor_template` SET `slot` = 101 WHERE `item` = 2723 AND `entry` = 
 -- Olvia 3312 - 425 template
 -- vendor list corrected
 
+-- Veldan Lightfoot 896 - 218 template
+-- vendor list corrected
+UPDATE `creature_template` SET `VendorTemplateId` = 0 WHERE `entry` = 896;
+DELETE FROM `npc_vendor` WHERE entry = 896;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(896,796,0,0,4,0,0,'Rough Leather Boots'),
+(896,797,0,0,6,0,0,'Rough Leather Gloves'),
+(896,798,0,0,3,0,0,'Rough Leather Pants'),
+(896,799,0,0,1,0,0,'Rough Leather Vest'),
+(896,843,0,0,10,0,0,'Tanned Leather Boots'),
+(896,844,0,0,12,0,0,'Tanned Leather Gloves'),
+(896,845,0,0,9,0,0,'Tanned Leather Pants'),
+(896,846,0,0,7,0,0,'Tanned Leather Jerkin'),
+(896,1839,0,0,2,0,0,'Rough Leather Belt'),
+(896,1840,0,0,5,0,0,'Rough Leather Bracers'),
+(896,1843,0,0,8,0,0,'Tanned Leather Belt'),
+(896,1844,0,0,11,0,0,'Tanned Leather Bracers');
+
+UPDATE `npc_vendor_template` SET `slot` = 104 WHERE `item` = 796 AND `entry` = 218;
+UPDATE `npc_vendor_template` SET `slot` = 106 WHERE `item` = 797 AND `entry` = 218;
+UPDATE `npc_vendor_template` SET `slot` = 103 WHERE `item` = 798 AND `entry` = 218;
+UPDATE `npc_vendor_template` SET `slot` = 101 WHERE `item` = 799 AND `entry` = 218;
+UPDATE `npc_vendor_template` SET `slot` = 102 WHERE `item` = 1839 AND `entry` = 218;
+UPDATE `npc_vendor_template` SET `slot` = 105 WHERE `item` = 1840 AND `entry` = 218;
+
+-- Quartermaster Hicks 1645 - 324 template
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 2520 AND `entry` = 324;
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 2521 AND `entry` = 324;
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 2522 AND `entry` = 324;
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 2523 AND `entry` = 324;
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 2524 AND `entry` = 324;
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 2525 AND `entry` = 324;
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 2526 AND `entry` = 324;
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 2527 AND `entry` = 324;
+
+-- Sergeant De Vries 955 - 404 template
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 2723 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 2593 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 2596 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 2594 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 2595 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 159 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 1179 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 1205 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 9 WHERE `item` = 1708 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 10 WHERE `item` = 1645 AND `entry` = 404;
+UPDATE `npc_vendor_template` SET `slot` = 11 WHERE `item` = 8766 AND `entry` = 404;
+
+-- Kira Songshine 3937 - 415 template
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 4540 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 4541 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 4542 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 4544 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 4601 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 8950 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 159 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 1179 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 9 WHERE `item` = 1205 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 10 WHERE `item` = 1708 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 11 WHERE `item` = 1645 AND `entry` = 415;
+UPDATE `npc_vendor_template` SET `slot` = 11 WHERE `item` = 8766 AND `entry` = 415;
+
+-- Corina Steele 54 - 4 template
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 2488 AND `entry` = 4;
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 2489 AND `entry` = 4;
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 2490 AND `entry` = 4;
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 2491 AND `entry` = 4;
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 2492 AND `entry` = 4;
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 2493 AND `entry` = 4;
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 2494 AND `entry` = 4;
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 2495 AND `entry` = 4;
+
+-- Kurran Steele 74 - 213 template
+-- vendor list corrected
+UPDATE `npc_vendor_template` SET `slot` = 1 WHERE `item` = 795 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 2 WHERE `item` = 3602 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 3 WHERE `item` = 794 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 4 WHERE `item` = 792 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 5 WHERE `item` = 3603 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 6 WHERE `item` = 793 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 7 WHERE `item` = 799 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 8 WHERE `item` = 1839 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 9 WHERE `item` = 798 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 10 WHERE `item` = 796 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 11 WHERE `item` = 1840 AND `entry` = 213;
+UPDATE `npc_vendor_template` SET `slot` = 12 WHERE `item` = 797 AND `entry` = 213;
+
+-- ------------------------
+-- Templates
+-- group moved to templates
+DELETE FROM `npc_vendor` WHERE entry IN (1249,2046,3613,5106,17930) AND `item` IN (1201,2392,2393,2394,2395,2396,2397,17186);
+DELETE FROM `npc_vendor_template` WHERE entry IN (233,1000); -- 233 had 2392,2393,2394,2395,2396,2397 was assigned to 1240,1249,3613,5106,17930
+INSERT INTO `npc_vendor_template` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES 
+(1000,1201,0,0,8,0,0,'Dull Heater Shield'),
+(1000,2392,0,0,1,0,0,'Light Mail Armor'),
+(1000,2393,0,0,2,0,0,'Light Mail Belt'),
+(1000,2394,0,0,3,0,0,'Light Mail Leggings'),
+(1000,2395,0,0,4,0,0,'Light Mail Boots'),
+(1000,2396,0,0,5,0,0,'Light Mail Bracers'),
+(1000,2397,0,0,6,0,0,'Light Mail Gloves'),
+(1000,17186,0,0,7,0,0,'Small Targe');
+UPDATE `creature_template` SET `VendorTemplateId` = 1000 WHERE `entry` IN (1249,2046,3613,5106);
+UPDATE `creature_template` SET `VendorTemplateId` = 0 WHERE `entry` IN (1240,17930);
+DELETE FROM `npc_vendor` WHERE entry = 17930;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES 
+(17930,1201,0,0,8,0,0,'Dull Heater Shield'),
+(17930,2392,0,0,1,0,0,'Light Mail Armor'),
+(17930,2393,0,0,2,0,0,'Light Mail Belt'),
+(17930,2394,0,0,3,0,0,'Light Mail Leggings'),
+(17930,2395,0,0,4,0,0,'Light Mail Boots'),
+(17930,2396,0,0,5,0,0,'Light Mail Bracers'),
+(17930,2397,0,0,6,0,0,'Light Mail Gloves'),
+(17930,17186,0,0,7,0,0,'Small Targe'),
+(17930,2488,0,0,11,0,0,'Gladius'),
+(17930,2489,0,0,12,0,0,'Two-handed Sword'),
+(17930,2490,0,0,13,0,0,'Tomahawk'),
+(17930,2491,0,0,14,0,0,'Large Axe'),
+(17930,2492,0,0,15,0,0,'Cudgel'),
+(17930,2493,0,0,16,0,0,'Wooden Mallet'),
+(17930,2494,0,0,17,0,0,'Stiletto'),
+(17930,2495,0,0,18,0,0,'Walking Stick');
+
+DELETE FROM `npc_vendor` WHERE entry = 1240;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES 
+(1240,2392,0,0,1,0,0,'Light Mail Armor'),
+(1240,2393,0,0,2,0,0,'Light Mail Belt'),
+(1240,2394,0,0,3,0,0,'Light Mail Leggings'),
+(1240,2395,0,0,4,0,0,'Light Mail Boots'),
+(1240,2396,0,0,5,0,0,'Light Mail Bracers'),
+(1240,2397,0,0,6,0,0,'Light Mail Gloves');
+
