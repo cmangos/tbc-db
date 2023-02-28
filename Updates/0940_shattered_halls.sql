f19d6fe48fa5067b147d5eb206c4f4b6f37ed756
Miraco <daaniel743@gmail.com>
2023-02-28 14:24:49 +0100

    add shattered hand brawler to spell_list
    he uses 3 spells:
    - thrash on aggro (passive spell) via creatureai
    - curse of shattered hand on current target (as long the target dont have the debuff) creature ai
    - kick on rnd casting player in range (spell_list)
    
    maybe update timers for kick (have it sniffed 10minutes)

--- Updates/0940_shattered_halls.sql
+++ Updates/0940_shattered_halls.sql
@@ -1,6 +1,9 @@
 -- Shattered Halls rework
 -- Convert creature_spell_list
 REPLACE INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
+-- Shattered Hand Brawler
+(1659301, 1, 36033, 0, 102, 0, 100, 0, 4000, 8000, 12000, 15000, "Shattered Hand Brawler - Kick on rnd casting"),
+
 -- Shattered Hand Sentry
 -- can also charge mid fight not only on aggro
 -- https://youtu.be/co4q9FEmLaE?t=189
@@ -12,6 +15,10 @@ REPLACE INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scri
 (1742001, 1, 30474, 0, 1, 0, 100, 0, 6000, 13000, 12000, 16000, "Shattered Hand Heathen (Normal) - Bloodthirst"),
 (2058701, 1, 35949, 0, 1, 0, 100, 0, 6000, 13000, 12000, 16000, "Shattered Hand Heathen (Heroic) - Bloodthirst");
 
+-- Shattered Hand Brawler
+UPDATE `creature_template` SET SpellList=1659301 WHERE entry = 16593;
+UPDATE `creature_template` SET SpellList=1659301 WHERE entry = 20582;
+
 -- Shattered Hand Sentry
 UPDATE `creature_template` SET SpellList=1650701 WHERE entry = 16507;
 UPDATE `creature_template` SET SpellList=2059301 WHERE entry = 20593;
@@ -21,6 +28,7 @@ UPDATE `creature_template` SET SpellList=1742001 WHERE entry = 17420;
 UPDATE `creature_template` SET SpellList=2058701 WHERE entry = 20587;
 
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
+(1659301, "Shattered Halls - Shattered Hand Brawler", 0, 0),
 (1650701, "Shattered Halls - Shattered Hand Sentry (Normal)", 0, 0),
 (1742001, "Shattered Halls - Shattered Hand Heathen (Normal)", 0, 0),
 (2058701, "Shattered Halls - Shattered Hand Heathen (Heroic)", 0, 0),
