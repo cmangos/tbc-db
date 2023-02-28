27ff6e33d8019cccaabecfd7d2a460f6ceb3b0bb
Miraco <daaniel743@gmail.com>
2023-02-28 18:43:21 +0100

    add Shadowmoon Darkcaster to creature_spell_list
    timers need update (checking sniff)

--- Updates/0940_shattered_halls.sql
+++ Updates/0940_shattered_halls.sql
@@ -1,7 +1,7 @@
 -- Shattered Halls rework
 -- Convert creature_spell_list
 
-DELETE FROM creature_spell_list WHERE Id IN (1650701, 2059301, 1659301, 1659401, 2057601, 1742001, 2058701);
+DELETE FROM creature_spell_list WHERE Id IN (1650701, 2059301, 1659301, 1659401, 2057601, 1742001, 2058701, 1769401, 2057701);
 INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
 -- Shattered Hand Sentry
 -- can also charge mid fight not only on aggro
@@ -21,6 +21,13 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 (2057601, 0, 36052, 0, 202, 0, 100, 0, 6400, 14000, 30000, 35000, "Shadowmoon Acolyte (Heroic) - Power Word: Shield Friendly Missing 90% including self"),
 (2057601, 1, 35943, 0, 201, 0, 100, 0, 6000, 10000, 12000, 15000, "Shadowmoon Acolyte (Heroic) - Prayer of Healing Friendly Missing 50% including self"),
 (2057601, 2, 15587, 2, 1, 0, 100, 0, 0, 0, 0, 0, "Shadowmoon Acolyte (Heroic) - Mind Blast on Current"),
+-- Shadowmoon Darkcaster
+(1769401, 0, 12542, 0, 101, 0, 100, 0, 4000, 8000, 13000, 18000, "Shadowmoon Darkcaster (Normal) - Fear on RND not Tank"),
+(1769401, 1, 11990, 0, 100, 0, 100, 0, 2000, 6000, 14000, 16000, "Shadowmoon Darkcaster (Normal) - Rain of Fire on RND"),
+(1769401, 2, 12471, 2, 1, 0, 100, 0, 0, 0, 0, 0, "Shadowmoon Darkcaster (Normal) - Shadow Bolt on Current"),
+(2057701, 0, 12542, 0, 101, 0, 100, 0, 4000, 8000, 13000, 18000, "Shadowmoon Darkcaster (Normal) - Fear on RND not Tank"),
+(2057701, 1, 33508, 0, 100, 0, 100, 0, 2000, 6000, 14000, 16000, "Shadowmoon Darkcaster (Normal) - Rain of Fire on RND"),
+(2057701, 2, 15232, 2, 1, 0, 100, 0, 0, 0, 0, 0, "Shadowmoon Darkcaster (Heroic) - Shadow Bolt on Current"),
 -- Shattered Hand Heathen
 (1742001, 1, 30474, 0, 1, 0, 100, 0, 6000, 13000, 12000, 16000, "Shattered Hand Heathen (Normal) - Bloodthirst"),
 (2058701, 1, 35949, 0, 1, 0, 100, 0, 6000, 13000, 12000, 16000, "Shattered Hand Heathen (Heroic) - Bloodthirst");
@@ -37,16 +44,22 @@ UPDATE `creature_template` SET SpellList=1659301 WHERE entry = 20582;
 UPDATE `creature_template` SET SpellList=1659401 WHERE entry = 16594;
 UPDATE `creature_template` SET SpellList=2057601 WHERE entry = 20576;
 
+-- Shadowmoon Darkcaster
+UPDATE `creature_template` SET SpellList=1769401 WHERE entry = 17694;
+UPDATE `creature_template` SET SpellList=2057701 WHERE entry = 20577;
+
 -- Shattered Hand heathen
 UPDATE `creature_template` SET SpellList=1742001 WHERE entry = 17420;
 UPDATE `creature_template` SET SpellList=2058701 WHERE entry = 20587;
 
+DELETE FROM creature_spell_list_entry WHERE Id IN (1650701, 2059301, 1659301, 1659401, 2057601, 1742001, 2058701, 1769401, 2057701);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
 (1650701, "Shattered Halls - Shattered Hand Sentry (Normal)", 0, 0),
+(2059301, "Shattered Halls - Shattered Hand Sentry (Heroic)", 0, 0),
 (1659301, "Shattered Halls - Shattered Hand Brawler", 0, 0),
 (1659401, "Shattered Halls - Shadowmoon Acolyte (Normal)", 0, 0),
-(1742001, "Shattered Halls - Shattered Hand Heathen (Normal)", 0, 0),
-(1659401, "Shattered Halls - Shadowmoon Acolyte (Normal)", 0, 0),
 (2057601, "Shattered Halls - Shadowmoon Acolyte (Heroic)", 0, 0),
-(2058701, "Shattered Halls - Shattered Hand Heathen (Heroic)", 0, 0),
-(2059301, "Shattered Halls - Shattered Hand Sentry (Heroic)", 0, 0);
+(1769401, "Shattered Halls - Shadowmoon Darkcaster (Normal)", 0, 0),
+(2057701, "Shattered Halls - Shadowmoon Darkcaster (Heroic)", 0, 0),
+(1742001, "Shattered Halls - Shattered Hand Heathen (Normal)", 0, 0),
+(2058701, "Shattered Halls - Shattered Hand Heathen (Heroic)", 0, 0);
