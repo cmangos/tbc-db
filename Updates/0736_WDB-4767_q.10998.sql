46eb53f96baa9e34fdd986c1741bd28eb16cbc45
Grz3s <Grz3s@users.noreply.github.com>
2023-04-26 22:32:11 +0200

    [WDB] Update [4767] ...
    one more text  = Yell
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/3490bb43a2ff57ce5fb67274c8d3c6c851327c6f

--- Updates/0736_WDB-4767_q.10998.sql
+++ Updates/0736_WDB-4767_q.10998.sql
@@ -15,5 +15,5 @@
 DELETE FROM dbscripts_on_creature_death WHERE id IN (22911);
 INSERT INTO dbscripts_on_creature_death (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 (22911,1,18,5000,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'despawn Self');
-UPDATE `broadcast_text` SET `ChatTypeID`= 1 WHERE `Id` IN (20773);
+UPDATE `broadcast_text` SET `ChatTypeID`= 1 WHERE `Id` IN (20773,21264);
 
