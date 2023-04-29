85b5777646934945f93d96f3efba308594c4b775
AnonXS <AnonXS@users.noreply.github.com>
2023-04-30 00:49:40 +0200

    Add second Tirion Fordring to q.5944 "In Dreams" Turn-In
    Based on classicmangos & pov

---
+++ Updates/0744_q.5944_turn_in.sql
@@ -0,0 +1,5 @@
+-- https://www.wowhead.com/classic/quest=5944/in-dreams#videos:id=169354 - 12:30
+-- Followup to https://github.com/cmangos/tbc-db/commit/27c25d1dc1a4d4b156054fd2da705f368c410ba3
+-- Already correct in classicmangos
+INSERT INTO `creature_involvedrelation` (`id`, `quest`) VALUES (1855, 5944);
+
