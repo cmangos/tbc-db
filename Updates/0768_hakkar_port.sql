a5f845e0224435d78d9806580cec90eae8769abe
killerwife <killerwife@gmail.com>
2023-05-14 18:19:25 +0200

    One more core rev

--- Updates/0768_hakkar_port.sql
+++ Updates/0768_hakkar_port.sql
@@ -1,3 +1,9 @@
+ALTER TABLE db_version CHANGE COLUMN required_s2464_01_mangos_string_id required_s2465_01_mangos_creature_equip_drop bit;
+
+ALTER TABLE creature DROP COLUMN equipment_id;
+
+
+
 ALTER TABLE db_version CHANGE COLUMN required_s2465_01_mangos_creature_equip_drop required_s2466_01_mangos_spell_list_conditions bit;
 
 -- change of operation enum for CONDITION_WORLDSTATE to official enum
