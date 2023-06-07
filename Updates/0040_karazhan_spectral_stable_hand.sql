282042258d2652f0862d654d23977fd606fc307f
Miraco <daaniel743@gmail.com>
2023-06-07 18:17:02 +0200

    update spell_timers

--- Updates/0040_karazhan_spectral_stable_hand.sql
+++ Updates/0040_karazhan_spectral_stable_hand.sql
@@ -21,14 +21,14 @@ DELETE FROM `creature_spell_list` WHERE `Id` IN (1554701, 1554801, 1555101);
 INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
 (1554701, 1, 29320, 0, 110, 0, 100, 0, 5000, 20000, 30000, 40000, "Spectral Charger - Charge - ATTACKING_TARGET_FARTHEST_AWAY - SELECT_FLAG_IN_LOS & SELECT_FLAG_PLAYER"), 
 -- Spell List for Spectral Stallion
-(1554801, 1, 29323, 0, 2, 0, 100, 0, 6000, 18000, 6000, 18000 , "Spectral Stallion - Cast Absorb Vitality - on self"),
-(1554801, 2, 29577, 0, 1, 0, 100, 0, 6000, 18000, 10000, 20000, "Spectral Stallion - Cast Hoof Strike - on current");
+(1554801, 1, 29323, 0, 2, 0, 100, 0, 6000, 13000, 7000, 14000 , "Spectral Stallion - Cast Absorb Vitality - on self"),
+(1554801, 2, 29577, 0, 1, 0, 100, 0, 6500, 16000, 11000, 22000, "Spectral Stallion - Cast Hoof Strike - on current");
 -- Spell List for Spectral Stable Hand
 INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-(1555101, 1, 29339, 1, -5320001, 0, 0, 100, 0, 10000, 10000, 20000, 30000, "Spectral Stable Hand - Cast Healing Touch - condition friendly missing 50%"),
-(1555101, 2, 29340, 1, -5320002, 0, 0, 100, 0, 6000, 18000, 10000, 20000, "Spectral Stable Hand - Cast Whip Rage - condition friendly missing buff"),
-(1555101, 3, 18812, 0, -1, 1, 0, 100, 0, 6000, 15000, 10000, 2000, "Spectral Stable Hand - Cast Knockback - on current"),
-(1555101, 4, 6016, 0, -1, 1, 0, 100, 0, 6000, 18000, 45000, 45000, "Spectral Stable Hand - Cast Pierce Armor - on current");
+(1555101, 1, 29339, 1, -5320001, 0, 0, 100, 0, 10000, 15000, 20000, 30000, "Spectral Stable Hand - Cast Healing Touch - condition friendly missing 50%"),
+(1555101, 2, 29340, 1, -5320002, 0, 0, 100, 0, 6000, 14000, 10000, 25000, "Spectral Stable Hand - Cast Whip Rage - condition friendly missing buff"),
+(1555101, 3, 18812, 0, -1, 1, 0, 100, 0, 6000, 13000, 12000, 17000, "Spectral Stable Hand - Cast Knockback - on current"),
+(1555101, 4, 6016, 0, -1, 1, 0, 100, 0, 6000, 15000, 45000, 45000, "Spectral Stable Hand - Cast Pierce Armor - on current");
 
 DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (1554701, 1554801, 1555101);
 INSERT INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
