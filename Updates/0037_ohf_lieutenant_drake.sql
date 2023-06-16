b52e7b422c4237a2307a5323e3710a43cf1dfcd1
killerwife <killerwife@gmail.com>
2023-06-16 22:15:00 +0200

    Dungeon: Fill out probability for all recently reworked bosses
    Will provide extra randomness

--- Updates/0037_ohf_lieutenant_drake.sql
+++ Updates/0037_ohf_lieutenant_drake.sql
@@ -6,16 +6,16 @@ INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRange
 (2053501, 'OHF - Lieutenant Drake - Heroic', 0, 0);
 DELETE FROM creature_spell_list WHERE Id IN(1784801,2053501);
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-('1784801', '0', '33789', '0', '-1', '1', '0', '100', '0','18500','22500','25000','35000', 'Lieutenant Drake - Frightening Shout on Current'),
-('1784801', '1', '31909', '0', '-1', '0', '0', '100', '0', '12300','14500','18100','22900', 'Lieutenant Drake - Whirlwind'),
-('1784801', '2', '31911', '0', '-1', '1', '0', '100', '0', '7400','11900','14900','24800', 'Lieutenant Drake - Mortal Strike on Current'),
-('1784801', '3', '9080', '0', '-1', '100', '0', '100', '0', '6100','7200','6100','22900', 'Lieutenant Drake - Hamstring on Current'),
-('1784801', '4', '33792', '0', '-1', '101', '0', '100', '0', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Random Player non tank');
+('1784801', '0', '33789', '0', '-1', '1', '0', '100', '4','18500','22500','25000','35000', 'Lieutenant Drake - Frightening Shout on Current'),
+('1784801', '1', '31909', '0', '-1', '0', '0', '100', '2', '12300','14500','18100','22900', 'Lieutenant Drake - Whirlwind'),
+('1784801', '2', '31911', '0', '-1', '1', '0', '100', '1', '7400','11900','14900','24800', 'Lieutenant Drake - Mortal Strike on Current'),
+('1784801', '3', '9080', '0', '-1', '100', '0', '100', '1', '6100','7200','6100','22900', 'Lieutenant Drake - Hamstring on Current'),
+('1784801', '4', '33792', '0', '-1', '101', '0', '100', '4', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Random Player non tank');
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-('2053501', '0', '33789', '0', '-1', '1', '0', '100', '0','18500','22500','25000','35000', 'Lieutenant Drake - Frightening Shout on Current'),
-('2053501', '1', '31909', '0', '-1', '0', '0', '100', '0', '12300','14500','18100','22900', 'Lieutenant Drake - Whirlwind'),
-('2053501', '2', '31911', '0', '-1', '1', '0', '100', '0', '7400','11900','14900','24800', 'Lieutenant Drake - Mortal Strike on Current'),
-('2053501', '3', '9080', '0', '-1', '100', '0', '100', '0', '6100','7200','6100','22900', 'Lieutenant Drake - Hamstring on Current'),
-('2053501', '4', '33792', '0', '-1', '101', '0', '100', '0', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Random Player non tank');
+('2053501', '0', '33789', '0', '-1', '1', '0', '100', '4','18500','22500','25000','35000', 'Lieutenant Drake - Frightening Shout on Current'),
+('2053501', '1', '31909', '0', '-1', '0', '0', '100', '2', '12300','14500','18100','22900', 'Lieutenant Drake - Whirlwind'),
+('2053501', '2', '31911', '0', '-1', '1', '0', '100', '1', '7400','11900','14900','24800', 'Lieutenant Drake - Mortal Strike on Current'),
+('2053501', '3', '9080', '0', '-1', '100', '0', '100', '1', '6100','7200','6100','22900', 'Lieutenant Drake - Hamstring on Current'),
+('2053501', '4', '33792', '0', '-1', '101', '0', '100', '4', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Random Player non tank');
 
 
--- Updates/0039_ohf_captain_skarloc.sql
+++ Updates/0039_ohf_captain_skarloc.sql
@@ -6,17 +6,17 @@ INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRange
 (2052101, 'OHF - Captain Skarloc - Heroic', 0, 0);
 DELETE FROM creature_spell_list WHERE Id IN(1786201,2052101);
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-('1786201', '0', '8258', '0', '-1', '0', '0', '100', '0','8000','8000','240000','240000', 'Captain Skarloc - Devotion Aura'),
-('1786201', '1', '31904', '0', '-1', '0', '0', '100', '0','2000','10000','14000','21000', 'Captain Skarloc - Holy Shield'),
-('1786201', '2', '13005', '0', '-1', '1', '0', '100', '0','7100','9200','10000','16000', 'Captain Skarloc - Hammer of Justice on Current'),
-('1786201', '3', '29380', '0', '-1', '3', '0', '100', '0','5000','10000','10000','14000', 'Captain Skarloc - Cleanse on Friendly Dispel'),
-('1786201', '4', '29427', '0', '-1', '204', '0', '100', '0','5000','10000','13300','15700', 'Captain Skarloc - Holy Light on Friendly missing 50% HP'),
-('2052101', '0', '8258', '0', '-1', '0', '0', '100', '0','8000','8000','240000','240000', 'Captain Skarloc - Devotion Aura'),
-('2052101', '1', '31904', '0', '-1', '0', '0', '100', '0','2000','10000','14000','21000', 'Captain Skarloc - Holy Shield'),
-('2052101', '2', '13005', '0', '-1', '1', '0', '100', '0','5100','7200','10000','13000', 'Captain Skarloc - Hammer of Justice on Current'),
-('2052101', '3', '29380', '0', '-1', '3', '0', '100', '0','5000','10000','10000','14000', 'Captain Skarloc - Cleanse on Friendly Dispel'),
-('2052101', '4', '29427', '0', '-1', '204', '0', '100', '0','5000','10000','13300','15700', 'Captain Skarloc - Holy Light on Friendly missing 50% HP'),
-('2052101', '5', '38385', '0', '-1', '0', '0', '100', '0','3000','9000','11000','13000', 'Captain Skarloc - Consecration');
+('1786201', '0', '8258', '0', '-1', '0', '0', '100', '50','8000','8000','240000','240000', 'Captain Skarloc - Devotion Aura'),
+('1786201', '1', '31904', '0', '-1', '0', '0', '100', '1','2000','10000','14000','21000', 'Captain Skarloc - Holy Shield'),
+('1786201', '2', '13005', '0', '-1', '1', '0', '100', '1','7100','9200','10000','16000', 'Captain Skarloc - Hammer of Justice on Current'),
+('1786201', '3', '29380', '0', '-1', '3', '0', '100', '10','5000','10000','10000','14000', 'Captain Skarloc - Cleanse on Friendly Dispel'),
+('1786201', '4', '29427', '0', '-1', '204', '0', '100', '10','5000','10000','13300','15700', 'Captain Skarloc - Holy Light on Friendly missing 50% HP'),
+('2052101', '0', '8258', '0', '-1', '0', '0', '100', '50','8000','8000','240000','240000', 'Captain Skarloc - Devotion Aura'),
+('2052101', '1', '31904', '0', '-1', '0', '0', '100', '1','2000','10000','14000','21000', 'Captain Skarloc - Holy Shield'),
+('2052101', '2', '13005', '0', '-1', '1', '0', '100', '1','5100','7200','10000','13000', 'Captain Skarloc - Hammer of Justice on Current'),
+('2052101', '3', '29380', '0', '-1', '3', '0', '100', '10','5000','10000','10000','14000', 'Captain Skarloc - Cleanse on Friendly Dispel'),
+('2052101', '4', '29427', '0', '-1', '204', '0', '100', '10','5000','10000','13300','15700', 'Captain Skarloc - Holy Light on Friendly missing 50% HP'),
+('2052101', '5', '38385', '0', '-1', '0', '0', '100', '10','3000','9000','11000','13000', 'Captain Skarloc - Consecration');
 
 UPDATE creature_template SET SpellList = 1783301 WHERE entry=17833;
 UPDATE creature_template SET SpellList = 2053001 WHERE entry=20530;
@@ -26,14 +26,14 @@ INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRange
 (2053001, 'OHF - Durnholde Warden - Heroic', 0, 0);
 DELETE FROM creature_spell_list WHERE Id IN(1783301,2053001);
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-('1783301', '0', '15654', '0', '-1', '1', '0', '100', '0','3800','8400','10800','24100', 'Durnholde Warden - Shadow Word: Pain on Current'),
-('1783301', '1', '22884', '0', '-1', '0', '0', '100', '0','18800','33800','30100','48300', 'Durnholde Warden - Psychic Scream'),
-('1783301', '2', '17201', '0', '-1', '3', '0', '100', '0','7000','12000','10000','16000', 'Durnholde Warden - Dispel Magic on Friendly Dispel'),
-('1783301', '3', '15586', '0', '-1', '204', '0', '100', '0','5000','10000','12100','18200', 'Durnholde Warden - Heal on Friendly missing 50%'),
-('2053001', '0', '34941', '0', '-1', '1', '0', '100', '0','3800','8400','10800','24100', 'Durnholde Warden - Shadow Word: Pain on Current'),
-('2053001', '1', '22884', '0', '-1', '0', '0', '100', '0','18800','33800','30100','48300', 'Durnholde Warden - Psychic Scream'),
-('2053001', '2', '17201', '0', '-1', '3', '0', '100', '0','7000','12000','10000','16000', 'Durnholde Warden - Dispel Magic on Friendly Dispel'),
-('2053001', '3', '22883', '0', '-1', '204', '0', '100', '0','5000','10000','12100','18200', 'Durnholde Warden - Heal on Friendly missing 50%');
+('1783301', '0', '15654', '0', '-1', '1', '0', '100', '1','3800','8400','10800','24100', 'Durnholde Warden - Shadow Word: Pain on Current'),
+('1783301', '1', '22884', '0', '-1', '0', '0', '100', '5','18800','33800','30100','48300', 'Durnholde Warden - Psychic Scream'),
+('1783301', '2', '17201', '0', '-1', '3', '0', '100', '5','7000','12000','10000','16000', 'Durnholde Warden - Dispel Magic on Friendly Dispel'),
+('1783301', '3', '15586', '0', '-1', '204', '0', '100', '5','5000','10000','12100','18200', 'Durnholde Warden - Heal on Friendly missing 50%'),
+('2053001', '0', '34941', '0', '-1', '1', '0', '100', '1','3800','8400','10800','24100', 'Durnholde Warden - Shadow Word: Pain on Current'),
+('2053001', '1', '22884', '0', '-1', '0', '0', '100', '5','18800','33800','30100','48300', 'Durnholde Warden - Psychic Scream'),
+('2053001', '2', '17201', '0', '-1', '3', '0', '100', '5','7000','12000','10000','16000', 'Durnholde Warden - Dispel Magic on Friendly Dispel'),
+('2053001', '3', '22883', '0', '-1', '204', '0', '100', '5','5000','10000','12100','18200', 'Durnholde Warden - Heal on Friendly missing 50%');
 
 UPDATE creature_template SET SpellList = 1786001 WHERE entry=17860;
 UPDATE creature_template SET SpellList = 2052901 WHERE entry=20529;
@@ -43,9 +43,9 @@ INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRange
 (2052901, 'OHF - Durnholde Veteran - Heroic', 0, 0);
 DELETE FROM creature_spell_list WHERE Id IN(1786001,2052901);
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-('1786001', '0', '15581', '0', '-1', '1', '0', '100', '0','4800','10900','3600','10900', 'Durnholde Veteran - Sinister Strike on Current'),
-('1786001', '1', '15582', '0', '-1', '1', '0', '100', '0','5000','10000','10000','12200', 'Durnholde Veteran - Backstab on Current'),
-('2052901', '0', '15581', '0', '-1', '1', '0', '100', '0','4800','10900','3600','10900', 'Durnholde Veteran - Sinister Strike on Current'),
-('2052901', '1', '15582', '0', '-1', '1', '0', '100', '0','5000','10000','10000','12200', 'Durnholde Veteran - Backstab on Current');
+('1786001', '0', '15581', '0', '-1', '1', '0', '100', '1','4800','10900','3600','10900', 'Durnholde Veteran - Sinister Strike on Current'),
+('1786001', '1', '15582', '0', '-1', '1', '0', '100', '1','5000','10000','10000','12200', 'Durnholde Veteran - Backstab on Current'),
+('2052901', '0', '15581', '0', '-1', '1', '0', '100', '1','4800','10900','3600','10900', 'Durnholde Veteran - Sinister Strike on Current'),
+('2052901', '1', '15582', '0', '-1', '1', '0', '100', '1','5000','10000','10000','12200', 'Durnholde Veteran - Backstab on Current');
 
 
