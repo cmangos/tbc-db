72dce66535a0b86709d99daf5afc3e9dd9b48caa
killerwife <killerwife@gmail.com>
2023-06-20 18:18:27 +0200

    MT: Fix typos in pandemonius

--- Updates/0061_mt_pandemonius.sql
+++ Updates/0061_mt_pandemonius.sql
@@ -6,10 +6,10 @@ INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRange
 (2026701, 'MT - Pandemonius - Heroic', 0, 0);
 DELETE FROM creature_spell_list WHERE Id IN(1834101,2026701);
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-('1834101', '0', '32325', '0', '-1', '101', '100', '100', '1','15000','20000','27000','32000', 'Pandemonius - Void Blast on Random'),
-('1834101', '1', '32358', '0', '-1', '101', '0', '100', '1','13000','15000','18000','30000', 'Pandemonius - Dark Shell'),
-('2026701', '0', '38760', '0', '-1', '101', '100', '100', '1','15000','20000','27000','32000', 'Pandemonius - Void Blast on Random'),
-('2026701', '1', '38759', '0', '-1', '101', '0', '100', '1','13000','15000','18000','30000', 'Pandemonius - Dark Shell');
+('1834101', '0', '32325', '0', '-1', '101', '0', '100', '1','15000','20000','27000','32000', 'Pandemonius - Void Blast on Random'),
+('1834101', '1', '32358', '0', '-1', '0', '0', '100', '1','13000','15000','18000','30000', 'Pandemonius - Dark Shell'),
+('2026701', '0', '38760', '0', '-1', '101', '0', '100', '1','15000','20000','27000','32000', 'Pandemonius - Void Blast on Random'),
+('2026701', '1', '38759', '0', '-1', '0', '0', '100', '1','13000','15000','18000','30000', 'Pandemonius - Dark Shell');
 
 UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10561 WHERE Id IN(17771);
 UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10562 WHERE Id IN(17772);
