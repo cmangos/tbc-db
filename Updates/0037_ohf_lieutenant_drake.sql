21c2c7e8d19bf6eaa9ec56cdbf15a2ac9ff4b3dc
AnonXS <AnonXS@users.noreply.github.com>
2023-06-24 20:44:34 +0200

    [0037] Lieutenant Drake should cast Exploding Shot on Current
    https://github.com/cmangos/issues/issues/3444#issuecomment-1575100587

--- Updates/0037_ohf_lieutenant_drake.sql
+++ Updates/0037_ohf_lieutenant_drake.sql
@@ -10,12 +10,12 @@ INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, T
 ('1784801', '1', '31909', '0', '-1', '0', '0', '100', '2', '12300','14500','18100','22900', 'Lieutenant Drake - Whirlwind'),
 ('1784801', '2', '31911', '0', '-1', '1', '0', '100', '1', '7400','11900','14900','24800', 'Lieutenant Drake - Mortal Strike on Current'),
 ('1784801', '3', '9080', '0', '-1', '100', '0', '100', '1', '6100','7200','6100','22900', 'Lieutenant Drake - Hamstring on Current'),
-('1784801', '4', '33792', '0', '-1', '101', '0', '100', '4', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Random Player non tank');
+('1784801', '4', '33792', '0', '-1', '1', '0', '100', '4', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Current');
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
 ('2053501', '0', '33789', '0', '-1', '1', '0', '100', '4','18500','22500','25000','35000', 'Lieutenant Drake - Frightening Shout on Current'),
 ('2053501', '1', '31909', '0', '-1', '0', '0', '100', '2', '12300','14500','18100','22900', 'Lieutenant Drake - Whirlwind'),
 ('2053501', '2', '31911', '0', '-1', '1', '0', '100', '1', '7400','11900','14900','24800', 'Lieutenant Drake - Mortal Strike on Current'),
 ('2053501', '3', '9080', '0', '-1', '100', '0', '100', '1', '6100','7200','6100','22900', 'Lieutenant Drake - Hamstring on Current'),
-('2053501', '4', '33792', '0', '-1', '101', '0', '100', '4', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Random Player non tank');
+('2053501', '4', '33792', '0', '-1', '1', '0', '100', '4', '20500','24100','12100','26600', 'Lieutenant Drake - Exploding Shot on Current');
 
 
