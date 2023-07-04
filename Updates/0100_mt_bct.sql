7ac922a437098a636858c2353482611c280d25e7
killerwife <killerwife@gmail.com>
2023-07-04 21:35:32 +0200

    MT: Move boss MT texts to BCT

---
+++ Updates/0100_mt_bct.sql
@@ -0,0 +1,13 @@
+UPDATE broadcast_text SET ChatTypeID=1,EmoteId1=1 WHERE Id IN(20633);
+UPDATE broadcast_text SET ChatTypeID=1,EmoteId1=1 WHERE Id IN(20634);
+
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10539 WHERE Id IN(17784);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10541 WHERE Id IN(17780);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10542 WHERE Id IN(17781);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10543 WHERE Id IN(17782);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10544 WHERE Id IN(17779);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10545 WHERE Id IN(17778);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10540 WHERE Id IN(17783);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10546 WHERE Id IN(17777);
+
+
