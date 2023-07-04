d7631f4fb99bc9150462c69591885426246e5243
killerwife <killerwife@gmail.com>
2023-07-04 21:01:30 +0200

    UB: Add all bct

---
+++ Updates/0099_underbog_bct.sql
@@ -0,0 +1,9 @@
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10385 WHERE Id IN(17506);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10384 WHERE Id IN(17505);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10386 WHERE Id IN(17507);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10383 WHERE Id IN(17511);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10387 WHERE Id IN(17508);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10388 WHERE Id IN(17509);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10389 WHERE Id IN(17510);
+
+
