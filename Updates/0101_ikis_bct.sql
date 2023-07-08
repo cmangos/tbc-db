68a2416525f759978366761e58876cf0f6f3deb5
killerwife <killerwife@gmail.com>
2023-07-08 09:04:47 +0200

    SH: Port ikiss to bct

---
+++ Updates/0101_ikis_bct.sql
@@ -0,0 +1,9 @@
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10557 WHERE Id IN(17769);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10554 WHERE Id IN(17765);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10555 WHERE Id IN(17767);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10556 WHERE Id IN(17768);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10558 WHERE Id IN(17763);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10559 WHERE Id IN(17764);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10560 WHERE Id IN(17762);
+UPDATE broadcast_text SET ChatTypeID=3 WHERE Id IN(19738);
+
