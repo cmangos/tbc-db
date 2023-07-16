ca18de2a3880b956aa3abadfebb30c5cfde35a24
killerwife <killerwife@gmail.com>
2023-07-16 14:52:34 +0200

    SV: Add BCT to kalithresh

---
+++ Updates/0120_sv_kalithresh.sql
@@ -0,0 +1,8 @@
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10390 WHERE Id IN(17724);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10391 WHERE Id IN(17725);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10392 WHERE Id IN(17726);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10393 WHERE Id IN(17727);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10394 WHERE Id IN(17728);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10395 WHERE Id IN(17729);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10397 WHERE Id IN(17730);
+
