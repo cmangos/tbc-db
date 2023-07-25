12dd2eac9e7a0edf16c33a43e8a155f5765ff10f
killerwife <killerwife@gmail.com>
2023-07-25 17:03:52 +0200

    SHH: Add Nethekurse spell_list

--- Updates/0129_shh_nethekurse.sql
+++ Updates/0129_shh_nethekurse.sql
@@ -23,4 +23,20 @@ UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10274,EmoteID1=0 WHERE I
 UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10275,EmoteID1=0 WHERE Id IN(16866);
 UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10276,EmoteID1=0 WHERE Id IN(16862);
 
+UPDATE creature_template SET SpellList = 1680701 WHERE entry=16807;
+UPDATE creature_template SET SpellList = 2056801 WHERE entry=20568;
+DELETE FROM creature_spell_list_entry WHERE Id IN(1680701,2056801);
+INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
+(1680701, 'SHH - Grand Warlock Nethekurse - Normal', 0, 0),
+(2056801, 'SHH - Grand Warlock Nethekurse - Heroic', 0, 0);
+DELETE FROM creature_spell_list WHERE Id IN(1680701,2056801);
+INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
+('1680701', '0', '30495', '0', '-1', '1', '0', '100', '1','5000','5000','6000','8500', 'Grand Warlock Nethekurse - Shadow Cleave on Current'),
+('1680701', '1', '30496', '0', '-1', '100', '0', '100', '1','8000','8000','7500','15000', 'Grand Warlock Nethekurse - Lesser Shadow Fissure on Random Player'),
+('1680701', '2', '30500', '0', '-1', '100', '0', '100', '1','20000','20000','15000','20000', 'Grand Warlock Nethekurse - Death Coil on Random Player'),
+('1680701', '3', '30502', '0', '872', '0', '0', '100', '1','0','0','0','0', 'Grand Warlock Nethekurse - Dark Spin'),
+('2056801', '0', '35953', '0', '-1', '1', '0', '100', '1','5000','5000','6000','8500', 'Grand Warlock Nethekurse - Shadow Slam on Current'),
+('2056801', '1', '30496', '0', '-1', '100', '0', '100', '1','8000','8000','7500','15000', 'Grand Warlock Nethekurse - Lesser Shadow Fissure on Random Player'),
+('2056801', '2', '30500', '0', '-1', '100', '0', '100', '1','20000','20000','15000','20000', 'Grand Warlock Nethekurse - Death Coil on Random Player'),
+('2056801', '3', '30502', '0', '872', '0', '0', '100', '1','0','0','0','0', 'Grand Warlock Nethekurse - Dark Spin');
 
