2a0fbe60df9ef8b16dd3646a899563f614ba0564
Miraco <daaniel743@gmail.com>
2023-08-07 21:16:37 +0200

    update some timers and fix db errors

--- Updates/0148_ramparts_ai.sql
+++ Updates/0148_ramparts_ai.sql
@@ -5,12 +5,12 @@
 -- Bonechewer Hungerer - 17259
 DELETE FROM creature_spell_list WHERE Id IN (1725901, 1805301);
 INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-(1725901, 1, 14516, 0, 1, 0, 100, 0, 4500, 20000, 1000, 20000, "Bonechewer Hungerer - Strik - on current"),
-(1725901, 2, 6713, 0, 1, 0, 100, 0, 4000, 25000, 6000, 28000, "Bonechewer Hungerer - Disarm - on current"),
+(1725901, 1, 14516, 0, 1, 0, 100, 0, 4500, 25000, 1000, 25000, "Bonechewer Hungerer - Strik - on current"),
+(1725901, 2, 6713, 0, 1, 0, 100, 0, 3000, 25000, 6000, 28000, "Bonechewer Hungerer - Disarm - on current"),
 (1725901, 3, 16244, 0, 2, 0, 100, 0, 1000, 19000, 10000, 20000, "Bonechewer Hungerer - Demoralizing Shout - on self"),
 -- heroic
-(1805301, 1, 14516, 0, 1, 0, 100, 0, 4500, 20000, 1000, 20000, "Bonechewer Hungerer - Strik - on current"),
-(1805301, 2, 6713, 0, 1, 0, 100, 0, 4000, 25000, 6000, 28000, "Bonechewer Hungerer - Disarm - on current"),
+(1805301, 1, 14516, 0, 1, 0, 100, 0, 4500, 25000, 1000, 25000, "Bonechewer Hungerer - Strik - on current"),
+(1805301, 2, 6713, 0, 1, 0, 100, 0, 3000, 25000, 6000, 28000, "Bonechewer Hungerer - Disarm - on current"),
 (1805301, 3, 16244, 0, 2, 0, 100, 0, 1000, 19000, 10000, 20000, "Bonechewer Hungerer - Demoralizing Shout - on self");
 
 
@@ -29,8 +29,7 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 -- heroic
 (1805401, 1, 30621, 0, 1, 0, 100, 0, 5000, 19000, 21000, 30000, "Bonechewer Ravener - Kidney Shot - on current");
 
-
-DELETE FROM creature_spell_list_entry WHERE Id IN (1726401, 1805301);
+DELETE FROM creature_spell_list_entry WHERE Id IN (1726401, 1805401);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
 (1726401, "Hellfire Ramparts - Bonechewer Ravener - Normal", 0, 0),
 (1805401, "Hellfire Ramparts - Bonechewer Ravener - Heroic", 0, 0);
@@ -47,11 +46,10 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 (1804901, 1, 36808, 0, 1, 0, 100, 0, 16000, 28000, 20000, 45000, "Bleeding Hollow Darkcaster - Rain of Fire - on current"),
 (1804901, 2, 36807, 2, 1, 0, 100, 0, 1400, 3600, 1500, 3600, "Bleeding Hollow Darkcaster - Scorch - on current");
 
-
-DELETE FROM creature_spell_list_entry WHERE Id IN (1726901, 1805301);
+DELETE FROM creature_spell_list_entry WHERE Id IN (1726901, 1804901);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
-(1726901, "Hellfire Ramparts - Bleeding Hollow Darkcaster - Normal", 0, 0),
-(1804901, "Hellfire Ramparts - Bleeding Hollow Darkcaster - Heroic", 0, 0);
+(1726901, "Hellfire Ramparts - Bleeding Hollow Darkcaster - Normal", 0, 80),
+(1804901, "Hellfire Ramparts - Bleeding Hollow Darkcaster - Heroic", 0, 80);
 
 UPDATE creature_template SET SpellList = '1726901' WHERE entry = '17269';
 UPDATE creature_template SET SpellList = '1804901' WHERE entry = '18049';
@@ -65,9 +63,7 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 (1804801, 1, 30614, 0, 1, 0, 100, 0, 12100, 15300, 13300, 18200, "Bleeding Hollow Archer - Aimed Shot - on current"),
 (1804801, 2, 31942, 0, 1, 0, 100, 0, 13000, 16000, 14000, 19000, "Bleeding Hollow Archer - Multi Shot - on random");
 
-
-
-DELETE FROM creature_spell_list_entry WHERE Id IN (1727001, 1805301);
+DELETE FROM creature_spell_list_entry WHERE Id IN (1727001, 1804801);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
 (1727001, "Hellfire Ramparts - Bleeding Hollow Archer - Normal", 0, 0),
 (1804801, "Hellfire Ramparts - Bleeding Hollow Archer - Heroic", 0, 0);
@@ -129,8 +125,6 @@ UPDATE creature_template SET SpellList = '1805501' WHERE entry = '18055';
 
 -- Hellfire Watcher 17309
 -- Heal and Renew when someone is missing 50% health including self
--- ToDo:
--- recheck heroic diff, Heal (30643) 
 DELETE FROM creature_spell_list WHERE Id IN (1730901, 1805801);
 INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
 (1730901, 1, 8362, 0, 1, 0, 100, 0, 2000, 10000, 12000, 19000, "Hellfire Watcher - Renew - on friendly missing 50% including self"),
@@ -158,7 +152,7 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 (1805101, 1, 9128, 0, 2, 0, 100, 0, 4200, 12400, 15200, 20500, "Bonechewer Beastmaster - Battle Shout - on self"),
 (1805101, 2, 10966, 0, 1, 0, 100, 0, 1200, 10400, 10800, 20500, "Bonechewer Beastmaster - Uppercut - on current");
 
-DELETE FROM creature_spell_list_entry WHERE Id IN (1730901, 1805801);
+DELETE FROM creature_spell_list_entry WHERE Id IN (1745501, 1805101);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
 (1745501, "Hellfire Ramparts - Bonechewer Beastmaster - Normal", 0, 0),
 (1805101, "Hellfire Ramparts - Bonechewer Beastmaster - Heroic", 0, 0);
@@ -175,10 +169,10 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 (1805001, 1, 30615, 0, 101, 0, 100, 0, 6600, 23500, 17100, 23800, "Bleeding Hollow Scryer - Fear - random not tank"),
 (1805001, 2, 15232, 2, 1, 0, 100, 0, 3600, 4800, 3600, 4800, "Bleeding Hollow Scryer - Shadowbolt - on current");
 
-DELETE FROM creature_spell_list_entry WHERE Id IN (1730901, 1805001);
+DELETE FROM creature_spell_list_entry WHERE Id IN (1747801, 1805001);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
-(1747801, "Hellfire Ramparts - Bleeding Hollow Scryer - Normal", 0, 0),
-(1805001, "Hellfire Ramparts - Bleeding Hollow Scryer - Heroic", 0, 0);
+(1747801, "Hellfire Ramparts - Bleeding Hollow Scryer - Normal", 0, 80),
+(1805001, "Hellfire Ramparts - Bleeding Hollow Scryer - Heroic", 0, 80);
 
 UPDATE creature_template SET SpellList = '1747801' WHERE entry = '17478';
 UPDATE creature_template SET SpellList = '1805001' WHERE entry = '18050';
@@ -190,7 +184,7 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 -- heroic
 (1805701, 1, 30621, 0, 1, 0, 100, 0, 1200, 7900, 20800, 24100, "Hellfire Sentry - Kidney Shot - on Current");
 
-DELETE FROM creature_spell_list_entry WHERE Id IN (1730901, 1805001);
+DELETE FROM creature_spell_list_entry WHERE Id IN (1751701, 1805701);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
 (1751701, "Hellfire Ramparts - Hellfire Sentry - Normal", 0, 0),
 (1805701, "Hellfire Ramparts - Hellfire Sentry - Heroic", 0, 0);
@@ -205,7 +199,7 @@ INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, Scrip
 -- heroic
 (1805601, 1, 15785, 0, 105, 0, 100, 0, 2000, 8000, 11000, 13000, "Fiendish Hound - Mana Burn - on mana user");
 
-DELETE FROM creature_spell_list_entry WHERE Id IN (1730901, 1805001);
+DELETE FROM creature_spell_list_entry WHERE Id IN (1754001, 1805601);
 REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
 (1754001, "Hellfire Ramparts - Fiendish Hound - Normal", 0, 0),
 (1805601, "Hellfire Ramparts - Fiendish Hound - Heroic", 0, 0);
