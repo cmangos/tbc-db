2633ba0b5910e01e70cb2ee9a9fd9486005b7c6d
Miraco <daaniel743@gmail.com>
2023-08-08 20:31:00 +0200

    Shattered Hand Reaver

--- Updates/0158_shattered_halls_ai.sql
+++ Updates/0158_shattered_halls_ai.sql
@@ -72,3 +72,20 @@ REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceR
 UPDATE `creature_template` SET SpellList=1659401 WHERE entry = 16594;
 UPDATE `creature_template` SET SpellList=2057601 WHERE entry = 20576;
 
+
+-- Shattered Hand Reaver  16699
+DELETE FROM creature_spell_list WHERE Id IN (1669901, 2059001);
+INSERT INTO `creature_spell_list` (Id, Position, SpellId, Flags, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
+(1669901, 1, 30471, 0, 1, 0, 100, 0, 10000, 22000, 15000, 28000, "Shattered Hand Reaver  (Normal) - Uppercut - on Current"),
+(1669901, 2, 15496, 0, 1, 0, 100, 0, 6000, 21000, 6000, 21000, "Shattered Hand Reaver  (Normal) - Cleave - on Current"),
+-- heroic
+(2059001, 1, 30471, 0, 1, 0, 100, 0, 10000, 22000, 15000, 28000, "Shattered Hand Reaver  (Normal) - Uppercut - on Current"),
+(2059001, 2, 15496, 0, 1, 0, 100, 0, 6000, 21000, 6000, 21000, "Shattered Hand Reaver  (Normal) - Cleave - on Current");
+
+DELETE FROM creature_spell_list_entry WHERE Id IN (1669901, 2059001);
+REPLACE INTO `creature_spell_list_entry` (Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
+(1669901, "Shattered Halls - Shattered Hand Reaver  - Normal", 0, 80),
+(2059001, "Shattered Halls - Shattered Hand Reaver  - Heroic", 0, 80);
+
+UPDATE `creature_template` SET SpellList=1669901 WHERE entry = 16699;
+UPDATE `creature_template` SET SpellList=2059001 WHERE entry = 20590;
