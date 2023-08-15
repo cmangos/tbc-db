5358af88f9814dc896853f54526c60cd578a0af1
Tobschinski <tobschinski@gmail.com>
2023-08-15 23:21:30 +0200

    [CDB] Zul'Gurub - Troll (2) Patrol 000 & 001
    (cherry picked from commit https://github.com/cmangos/classic-db/commit/f51c75eaec8837fe11d786e8328646407700336e

---
+++ Updates/0168_CDB-4464_zulgurub_speed.sql
@@ -0,0 +1,8 @@
+
+UPDATE `creature_template` SET `SpeedWalk` = '1' WHERE (`Entry` = '11339'); -- 1.32
+UPDATE `creature_template` SET `SpeedWalk` = '1' WHERE (`Entry` = '11350'); -- 1.32
+UPDATE `creature_template` SET `SpeedWalk` = '1' WHERE (`Entry` = '11351'); -- 1.32
+UPDATE `creature_template` SET `SpeedWalk` = '1' WHERE (`Entry` = '11353'); -- 1.32
+UPDATE `creature_template` SET `SpeedWalk` = '1' WHERE (`Entry` = '11830'); -- 1.32
+UPDATE `creature_template` SET `SpeedWalk` = '1' WHERE (`Entry` = '11831'); -- 1.32
+
