261dc79d8c399601ceb38581408d6e84c37cbfa8
Grz3s <Grz3s@users.noreply.github.com>
2023-08-25 00:00:19 +0200

    [WDB][4883] Lagoon Eel 20290
    small update
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/715
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/a2c943579278a8a2a4354a48269725145e7f6ccc

---
+++ Updates/0174_WDB-4883_c.20290.sql
@@ -0,0 +1,13 @@
+-- Lagoon Eel 20290
+UPDATE creature SET position_x = -344.510437, position_y = 6637.530273, position_z = 7.514392 WHERE guid = 72107;
+UPDATE creature SET position_x = -266.563202, position_y = 6724.249023, position_z = 5.002598 WHERE guid = 72110;
+UPDATE creature SET position_x = -273.004425, position_y = 6562.750488, position_z = 6.514277 WHERE guid = 72106;
+UPDATE creature SET position_x = -347.623871, position_y = 6597.110840, position_z = 10.135850 WHERE guid = 72104;
+UPDATE creature SET position_x = -113.820671, position_y = 6411.087402, position_z = 10.074532 WHERE guid = 72084;
+UPDATE creature SET position_x = -57.937263, position_y = 6412.229492, position_z = 5.406971 WHERE guid = 72085;
+UPDATE creature SET position_x = -31.094452, position_y = 6475.952148, position_z = 9.218726 WHERE guid = 72088;
+UPDATE creature SET position_x = -159.188782, position_y = 6507.104004, position_z = 7.030706 WHERE guid = 72093;
+UPDATE creature SET position_x = -102.427605, position_y = 6408.821777, position_z = 5.923011 WHERE guid = 72084;
+UPDATE creature SET position_x = -14.772385, position_y = 6480.810059, position_z = 4.904004 WHERE guid = 72088;
+UPDATE creature SET position_x = -344.064697, position_y = 6590.368164, position_z = 5.176247 WHERE guid = 72104;
+
