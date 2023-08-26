d3c194295d60eb73b380853754810482f8325271
killerwife <killerwife@gmail.com>
2023-08-26 12:55:07 +0200

    Botanica: Add bct
    https://github.com/cmangos/issues/issues/3444

---
+++ Updates/0172_botanica_bct.sql
@@ -0,0 +1,27 @@
+-- botanist
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11144,EmoteId1=0 WHERE Id IN(19944);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11145,EmoteId1=0 WHERE Id IN(19945);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11146,EmoteId1=0 WHERE Id IN(19946);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11147,EmoteId1=0 WHERE Id IN(19947);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11148,EmoteId1=0 WHERE Id IN(19948);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11149,EmoteId1=0 WHERE Id IN(19949);
+-- laj
+UPDATE broadcast_text SET ChatTypeID=2,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(10755);
+-- thorngrin
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11212,EmoteId1=0 WHERE Id IN(20019);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11207,EmoteId1=0 WHERE Id IN(20014);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11208,EmoteId1=0 WHERE Id IN(20015);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11206,EmoteId1=0 WHERE Id IN(20013);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11209,EmoteId1=0 WHERE Id IN(20016);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11210,EmoteId1=0 WHERE Id IN(20017);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11211,EmoteId1=0 WHERE Id IN(20018);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11205,EmoteId1=0 WHERE Id IN(20012);
+-- warp splinter
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11230,EmoteId1=0 WHERE Id IN(20024);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11231,EmoteId1=0 WHERE Id IN(20025);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11232,EmoteId1=0 WHERE Id IN(20026);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11233,EmoteId1=0 WHERE Id IN(18376);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11234,EmoteId1=0 WHERE Id IN(20022);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11235,EmoteId1=0 WHERE Id IN(20028);
+
+
