2a02a2e7b13c359ddc0be003f62049e270c220e6
killerwife <killerwife@gmail.com>
2023-09-15 08:34:33 +0200

    Mechanar: Add mechanar bcts

---
+++ Updates/0202_mechanar_bct.sql
@@ -0,0 +1,27 @@
+-- capacitus
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11165,EmoteId1=0 WHERE Id IN(19955);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11166,EmoteId1=0 WHERE Id IN(19956);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11167,EmoteId1=0 WHERE Id IN(19957);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11163,EmoteId1=0 WHERE Id IN(19953);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11164,EmoteId1=0 WHERE Id IN(19954);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11162,EmoteId1=0 WHERE Id IN(19952);
+-- sepethrea
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11186,EmoteId1=0 WHERE Id IN(19959);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11191,EmoteId1=0 WHERE Id IN(19965);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11189,EmoteId1=0 WHERE Id IN(19963);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11190,EmoteId1=0 WHERE Id IN(19964);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11187,EmoteId1=0 WHERE Id IN(19961);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11188,EmoteId1=0 WHERE Id IN(19962);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11192,EmoteId1=0 WHERE Id IN(19966);
+-- pathaleon
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11193,EmoteId1=0 WHERE Id IN(20041);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11197,EmoteId1=0 WHERE Id IN(18716);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11198,EmoteId1=0 WHERE Id IN(20040);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11196,EmoteId1=0 WHERE Id IN(18717);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11199,EmoteId1=0 WHERE Id IN(18725);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11194,EmoteId1=0 WHERE Id IN(20042);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11195,EmoteId1=0 WHERE Id IN(20043);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=11200,EmoteId1=0 WHERE Id IN(20044);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=0,EmoteId1=53 WHERE Id IN(19059);
+
+
