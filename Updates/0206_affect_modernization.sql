0f0bc3109e8d537d2d405fbd14886760d11cf16c
killerwife <killerwife@gmail.com>
2023-09-17 12:52:28 +0200

    Add Increased Judgement of Light affect mask

--- Updates/0206_affect_modernization.sql
+++ Updates/0206_affect_modernization.sql
@@ -3,4 +3,9 @@ DELETE FROM spell_affect WHERE entry=29203;
 INSERT INTO spell_affect(entry, effectId, SpellFamilyMask) VALUES
 (29203,0,0x000000000040);
 
+-- Increased Judgement of Light
+DELETE FROM spell_affect WHERE entry IN(37182);
+INSERT INTO spell_affect(entry, effectId, SpellFamilyMask) VALUES
+(37182, 0, 0x0000000100000000);
+
 
