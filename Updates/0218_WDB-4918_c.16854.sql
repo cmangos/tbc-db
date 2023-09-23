f31716a2c73ff4b5e193e3a3850269ea790f74e2
Grz3s <Grz3s@users.noreply.github.com>
2023-09-23 20:38:35 +0200

    [WDB][4918] Eldinarcus 16854
    corrected - tbc has 2 more spawn locations
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/0f61f243ea261d166fdc2f2ec761806c71af979b

---
+++ Updates/0218_WDB-4918_c.16854.sql
@@ -0,0 +1,4 @@
+-- Eldinarcus 16854
+-- has randommovement
+UPDATE creature SET /*position_x = 9555.51, position_y = -7851.73, position_z = 18.6468, movementtype = 1,*/ spawndist = 25 WHERE id = 16854; -- up from 10
+
