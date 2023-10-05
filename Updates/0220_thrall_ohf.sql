39aa1e218b4bac8c2059ea905463960322a21d09
killerwife <killerwife@gmail.com>
2023-10-05 10:42:27 +0200

    OHF: Add thrall event bcts

---
+++ Updates/0220_thrall_ohf.sql
@@ -0,0 +1,65 @@
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10406,EmoteId1=1 WHERE Id IN(15909);
+
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16729);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16731);
+
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15891);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(19537);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15894);
+UPDATE broadcast_text SET ChatTypeID=2,SoundEntriesID1=0,EmoteId1=5 WHERE Id IN(16499);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15958);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15968);
+UPDATE broadcast_text SET ChatTypeID=2,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15970);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15969);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15974);
+
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10418,EmoteId1=0 WHERE Id IN(16016);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10419,EmoteId1=0 WHERE Id IN(16019);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10420,EmoteId1=0 WHERE Id IN(16021);
+
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15976);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15986);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(19536);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(19538);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(19534);
+
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10465,EmoteId1=0 WHERE Id IN(17622);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15893);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10466,EmoteId1=0 WHERE Id IN(15908);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10467,EmoteId1=0 WHERE Id IN(17623);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10468,EmoteId1=0 WHERE Id IN(15916);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10469,EmoteId1=1 WHERE Id IN(15948);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15977);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15978);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10470,EmoteId1=1 WHERE Id IN(16017);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10471,EmoteId1=5 WHERE Id IN(16020);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10472,EmoteId1=0 WHERE Id IN(16089);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10458,EmoteId1=0 WHERE Id IN(17610);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10459,EmoteId1=0 WHERE Id IN(17611);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10460,EmoteId1=0 WHERE Id IN(17612);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10461,EmoteId1=0 WHERE Id IN(17613);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10448,EmoteId1=0 WHERE Id IN(17600);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10449,EmoteId1=0 WHERE Id IN(17601);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10450,EmoteId1=0 WHERE Id IN(17602);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10451,EmoteId1=0 WHERE Id IN(17603);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10452,EmoteId1=0 WHERE Id IN(17604);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10453,EmoteId1=0 WHERE Id IN(17605);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10454,EmoteId1=0 WHERE Id IN(17606);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10455,EmoteId1=0 WHERE Id IN(17607);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10456,EmoteId1=0 WHERE Id IN(17608);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=10457,EmoteId1=0 WHERE Id IN(17609);
+
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16082);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15979);
+
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(15892);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16087);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16088);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16090);
+
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16083);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16085);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16084);
+UPDATE broadcast_text SET ChatTypeID=0,SoundEntriesID1=0,EmoteId1=0 WHERE Id IN(16086);
+
+
