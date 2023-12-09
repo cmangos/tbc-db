aafc7ec079ed443bf5f284bae6fdd7ab3e1249fe
AnonXS <AnonXS@users.noreply.github.com>
2023-12-09 16:40:57 +0100

    Update Orgrimmar Grunt 3296 Spawn too
    path was changed
    
    Based on https://github.com/cmangos/classic-db/commit/e42eeba64248245b5b12f982f128521167939a1a

---
+++ Updates/0303_Orgrimmar_waypoints_spawn.sql
@@ -0,0 +1,7 @@
+-- Orgrimmar
+-- some of waypoints around Orgrimmar were partly missing or wrong (custom made) - corrected
+
+-- Orgrimmar Grunt 3296
+-- Waypoints
+UPDATE creature SET position_x = 1613.801, position_y = -4267.5454, position_z = 48.003452, spawndist = 0, MovementType = 4 WHERE guid IN (10347); -- linear
+
