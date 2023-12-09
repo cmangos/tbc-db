a440ac6dbe97a547af6d417e35685c2229bcf574
Grz3s <Grz3s@users.noreply.github.com>
2023-12-10 00:25:45 +0100

    [WDB][5215] Megelon 16475
    poss corrected
    
    9551_V3_0_9_9551\2009-02-14-16-43_9551_MIHASYA
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/b1962a971e4287af5449e47c651c3fd188322af8

---
+++ Updates/0304_WDB-5215_c.16475.sql
@@ -0,0 +1,4 @@
+-- Megelon 16475
+-- poss corrected
+UPDATE creature SET position_x = -3985.072, position_y = -13877.058, position_z = 92.04234, orientation = 5.131268024444580078, movementtype = 0, spawndist = 0 WHERE id = 16475;
+
