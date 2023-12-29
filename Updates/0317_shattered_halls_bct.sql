5ebed58381c36f41b13364eec3a1882e54f32295
Miraco <daaniel743@gmail.com>
2023-12-29 13:51:44 +0100

    Add SoundEntries to bcts used from shattered hand legionnaire's

---
+++ Updates/0317_shattered_halls_bct.sql
@@ -0,0 +1,27 @@
+-- Add Sound Entries and correct some broadcast_texts
+-- Figther Down!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10172' WHERE Id = '16356';
+-- Replacement, quickly!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10173' WHERE Id = '16357';
+-- Next warrior, now!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10174' WHERE Id = '16358';
+-- Fall in! Mok-thora ka!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10175' WHERE Id = '16359';
+-- Where is my Support? 
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10176' WHERE Id = '16360';
+-- Next warrior, step up!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10180' WHERE Id = '16361';
+-- Join the fight! Agrama-ka!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10181' WHERE Id = '16362';
+
+
+-- Wake up, we're under attack! 16363
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10182' WHERE Id = '16363';
+-- Sleep on your own time! 16364
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10183' WHERE Id = '16364';
+-- Get up! 16365
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10184' WHERE Id = '16365';
+-- On your feet! 16366
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10185' WHERE Id = '16366';
+-- No time for slumber! Join the fight! 16367
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10186' WHERE Id = '16367';
