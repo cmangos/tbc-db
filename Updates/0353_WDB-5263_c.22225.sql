71a1d8fb444e09c8139280471ede6971d2fdde61
Grz3s <Grz3s@users.noreply.github.com>
2024-01-07 02:02:18 +0100

    [WDB][5263] Reagan Mancuso 22225
    Vendor list corrected
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/70b71afe999bb403c4f38ffc3ad2dba7483b9360

---
+++ Updates/0353_WDB-5263_c.22225.sql
@@ -0,0 +1,15 @@
+-- Reagan Mancuso 22225
+-- Vendor list corrected
+UPDATE npc_vendor_template SET Slot = 1 WHERE entry = 101 AND item = 28458; -- Bold Tourmaline
+UPDATE npc_vendor_template SET Slot = 2 WHERE entry = 101 AND item = 28462; -- Bright Tourmaline
+UPDATE npc_vendor_template SET Slot = 3 WHERE entry = 101 AND item = 28459; -- Delicate Tourmaline
+UPDATE npc_vendor_template SET Slot = 4 WHERE entry = 101 AND item = 28461; -- Runed Tourmaline
+UPDATE npc_vendor_template SET Slot = 5 WHERE entry = 101 AND item = 28460; -- Teardrop Tourmaline
+UPDATE npc_vendor_template SET Slot = 6 WHERE entry = 101 AND item = 28465; -- Lustrous Zircon
+UPDATE npc_vendor_template SET Slot = 7 WHERE entry = 101 AND item = 28463; -- Solid Zircon
+UPDATE npc_vendor_template SET Slot = 8 WHERE entry = 101 AND item = 28464; -- Sparkling Zircon
+UPDATE npc_vendor_template SET Slot = 9 WHERE entry = 101 AND item = 28466; -- Brilliant Amber
+UPDATE npc_vendor_template SET Slot = 10 WHERE entry = 101 AND item = 28469; -- Gleaming Amber
+UPDATE npc_vendor_template SET Slot = 11 WHERE entry = 101 AND item = 28468; -- Rigid Amber
+UPDATE npc_vendor_template SET Slot = 12 WHERE entry = 101 AND item = 28467; -- Smooth Amber
+UPDATE npc_vendor_template SET Slot = 13 WHERE entry = 101 AND item = 28470; -- Thick Amber
