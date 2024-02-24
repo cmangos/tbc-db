cdd724f98c57661215cd14b54ee56887e1016256
killerwife <killerwife@gmail.com>
2024-02-24 22:02:18 +0100

    Add CreatureStaticFlags2::NO_INTERRUPT_SCHOOL_COOLDOWN entries

---
+++ Updates/0378_no_category_static_flag.sql
@@ -0,0 +1,4 @@
+-- sourced from dbc_creaturedifficulty and likely not applicable to vanilla
+UPDATE creature_template SET StaticFlags2=StaticFlags2|0x00001000 WHERE entry IN('11949','15691','16457','16524','17534','17862','18780','19191','21964','22951','23399','23577','23596','24143','24144','24239','24664','24892','24987','25371','25840','26650');
+
+
