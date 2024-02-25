835ec4fd2eb03a0cf44634670d43e6c19c476240
killerwife <killerwife@gmail.com>
2024-02-25 13:24:05 +0100

    SL: Fix ambassador hellmaw not attacking and resetting properly

--- Updates/0085_sl_murmur.sql
+++ Updates/0085_sl_murmur.sql
@@ -8,7 +8,7 @@ DELETE FROM creature_spell_list_entry WHERE Id IN(1870801,2065701);
 INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRangedAttack) VALUES
 (1870801, 'SL - Murmur - Normal', 0, 0),
 (2065701, 'SL - Murmur - Heroic', 0, 0);
-DELETE FROM creature_spell_list WHERE Id IN(1873101,2065701);
+DELETE FROM creature_spell_list WHERE Id IN(1870801,2065701);
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
 ('1870801', '0', '33923', '0', '-1', '0', '0', '100', '1','31000','38000','31000','38000', 'Murmur - Sonic Boom'),
 ('1870801', '1', '33711', '0', '-1', '0', '0', '100', '1','21000','21000','21000','21000', 'Murmur - Murmur''s Touch'),
---
+++ Updates/0379_sl_fixes.sql
@@ -0,0 +1,3 @@
+-- banish handled in core
+UPDATE creature_template_addon SET auras='18950' WHERE entry IN(18731,20636);
+
