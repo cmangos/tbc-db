888c5b83e563c8c970fb36cc5285e5a7648d1114
Grz3s <Grz3s@users.noreply.github.com>
2024-02-25 17:30:43 +0100

    [WDB][5294][ACID] Kor'kron Defender 19362
    EAI Reconstructed
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/7a13aff92aef30b424c00b65f6350f054a1773dd

---
+++ Updates/0411_WDB-5294_c.19362.sql
@@ -0,0 +1,11 @@
+-- Kor'kron Defender 19362
+-- Relays ID's moved to another location (17950 - 17953 belong to quest script and was overwriten by mistake);
+DELETE FROM `dbscript_random_templates` WHERE `id` = 19975;
+INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
+(19975,0,16554,0,'Kor\'kron Defender 19362 - Random OOC Say 1'),
+(19975,0,16555,0,'Kor\'kron Defender 19362 - Random OOC Say 2'),
+(19975,0,16556,0,'Kor\'kron Defender 19362 - Random OOC Say 3'),
+(19975,0,16557,0,'Kor\'kron Defender 19362 - Random OOC Say 4'),
+(19975,0,16558,0,'Kor\'kron Defender 19362 - Random OOC Say 5'),
+(19975,0,16559,0,'Kor\'kron Defender 19362 - Random OOC Say 6');
+
