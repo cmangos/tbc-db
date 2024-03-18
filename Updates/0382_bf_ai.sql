9301137c24e768529e8e9f0bcdd4d6e86246e695
Miraco <daaniel743@gmail.com>
2024-03-18 10:59:01 +0100

    Add Shadowmoon Summoner

--- Updates/0382_bf_ai.sql
+++ Updates/0382_bf_ai.sql
@@ -37,3 +37,32 @@ INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `Combat
 
 UPDATE `creature_template` SET `SpellList` = 1737101 WHERE `entry` = 17371;
 UPDATE `creature_template` SET `SpellList` = 1861901 WHERE `entry` = 18619;
+
+
+-- Shadowmoon Summoner
+-- Important Notes:
+-- This npc has no caster mode, he will melee and cast spells
+-- Can have multiple Seductress summoned 
+-- Has a low chance to also summon multiple Felhound Manastalkers
+DELETE FROM `creature_template_spells` WHERE `entry` IN (17395, 18617);
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (1739501, 1861701);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(1739501, 'Blood Furnace - Shadowmoon Summoner - Normal', 0, 90),
+(1861701, 'Blood Furnace - Shadowmoon Summoner - Heroic', 0, 90);
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (1739501, 1861701);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(1739501, 1, 30851, 0, -1, 0, 0, 20, 0, 5000, 15000, 30000, 40000, 'Shadowmoon Summoner - Summon Felhound Manastalker'),
+(1739501, 2, 30853, 0, -1, 0, 0, 100, 0, 5000, 13000, 20000, 30000, 'Shadowmoon Summoner - Summon Seductress'),
+(1739501, 3, 18399, 0, -1, 1, 0, 100, 0, 7000, 20000, 14000, 22000, 'Shadowmoon Summoner - Flamestrike - current'),
+(1739501, 4, 15242, 0, -1, 1, 0, 100, 0, 2500, 8000, 4000, 15000, 'Shadowmoon Summoner - Fireball - current'),
+(1861701, 1, 30851, 0, -1, 0, 0, 20, 0, 5000, 15000, 30000, 40000, 'Shadowmoon Summoner - Summon Felhound Manastalker'),
+(1861701, 2, 30853, 0, -1, 0, 0, 100, 0, 5000, 13000, 20000, 30000, 'Shadowmoon Summoner - Summon Seductress'),
+(1861701, 3, 16102, 0, -1, 1, 0, 100, 0, 7000, 20000, 14000, 22000, 'Shadowmoon Summoner - Flamestrike - current'),
+(1861701, 4, 17290, 0, -1, 1, 0, 100, 0, 2500, 8000, 4000, 15000, 'Shadowmoon Summoner - Fireball - current');
+
+UPDATE `creature_template` SET `SpellList` = 1739501 WHERE `entry` = 17395;
+UPDATE `creature_template` SET `SpellList` = 1861701 WHERE `entry` = 18617;
+
+
