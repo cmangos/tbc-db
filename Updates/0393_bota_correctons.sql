4b1822e3c26169fbf0c936d59d5bebdd581d14fa
Miraco <daaniel743@gmail.com>
2024-04-07 08:47:46 +0200

    Fix DB error and add correct speeds for first npcs

---
+++ Updates/0393_bota_correctons.sql
@@ -0,0 +1,20 @@
+-- Correct some movement speeds to wotlk classic sniff values
+-- Tempest-Forge Peacekeeper
+-- [2] WalkSpeed: 2.5 
+-- [2] RunSpeed: 8
+UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1.14286' WHERE entry IN (18405, 21578);
+
+-- Bloodwarder Mender
+-- [2] WalkSpeed: 2.5 
+-- [2] RunSpeed: 8
+UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1.14286' WHERE entry IN (19633, 21547);
+
+-- Bloodwarder Greenkeeper
+-- [2] WalkSpeed: 2.5 
+-- [2] RunSpeed: 8
+UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1.14286' WHERE entry IN (18419, 21546);
+
+-- Bloodwarder Protector
+-- [2] WalkSpeed: 2.5 
+-- [2] RunSpeed: 8
+UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1.14286' WHERE entry IN (17993, 21548);
