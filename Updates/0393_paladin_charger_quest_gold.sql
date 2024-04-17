c17830319fc433bce0eb4f4514de4ace4505f63d
evil-at-wow <evil.at.wow@gmail.com>
2024-04-17 18:48:24 +0200

    Fix the gold requirements of the alliance Paladin charger quest chain.

---
+++ Updates/0393_paladin_charger_quest_gold.sql
@@ -0,0 +1,6 @@
+
+-- Quest 'Emphasis on Sacrifice' requires 150g
+UPDATE `quest_template` SET RewOrReqMoney = -1500000 WHERE entry = 7637;
+
+-- Quest 'Manna-Enriched Horse Feed' requires 50g
+UPDATE `quest_template` SET RewOrReqMoney = -500000 WHERE entry = 7645;
