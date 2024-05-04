e538b3c08ce49fb3df35db02296428879342f3db
killerwife <killerwife@gmail.com>
2024-05-04 17:19:30 +0200

    Fill gaps in missing data for cls attributes by hand

---
+++ Updates/0402_tbc_cls_filler.sql
@@ -0,0 +1,26 @@
+UPDATE creature_template_classlevelstats SET Strength=143,Agility=91,Stamina=271,Intellect=30,Spirit=82 WHERE Level=63 AND Class=1;
+UPDATE creature_template_classlevelstats SET Strength=146,Agility=97,Stamina=276,Intellect=31,Spirit=85 WHERE Level=64 AND Class=1;
+UPDATE creature_template_classlevelstats SET Strength=151,Agility=107,Stamina=286,Intellect=31,Spirit=90 WHERE Level=66 AND Class=1;
+UPDATE creature_template_classlevelstats SET Strength=170,Agility=135,Stamina=322,Intellect=34,Spirit=108 WHERE Level=73 AND Class=1;
+
+UPDATE creature_template_classlevelstats SET Strength=22,Agility=20,Stamina=21,Intellect=20,Spirit=21 WHERE Level=1 AND Class=2;
+UPDATE creature_template_classlevelstats SET Strength=23,Agility=21,Stamina=22,Intellect=21,Spirit=22 WHERE Level=2 AND Class=2;
+
+UPDATE creature_template_classlevelstats SET Strength=26,Agility=23,Stamina=26,Intellect=23,Spirit=25 WHERE Level=7 AND Class=2;
+
+UPDATE creature_template_classlevelstats SET Strength=138,Agility=94,Stamina=252,Intellect=115,Spirit=108 WHERE Level=64 AND Class=2;
+UPDATE creature_template_classlevelstats SET Strength=140,Agility=96,Stamina=256,Intellect=118,Spirit=110 WHERE Level=65 AND Class=2;
+
+UPDATE creature_template_classlevelstats SET Strength=161,Agility=114,Stamina=193,Intellect=142,Spirit=130 WHERE Level=73 AND Class=2;
+
+UPDATE creature_template_classlevelstats SET Strength=20,Agility=20,Stamina=20,Intellect=24,Spirit=23 WHERE Level=1 AND Class=8;
+UPDATE creature_template_classlevelstats SET Strength=21,Agility=20,Stamina=20,Intellect=25,Spirit=24 WHERE Level=2 AND Class=8;
+
+UPDATE creature_template_classlevelstats SET Strength=23,Agility=21,Stamina=22,Intellect=28,Spirit=27 WHERE Level=5 AND Class=8;
+UPDATE creature_template_classlevelstats SET Strength=24,Agility=21,Stamina=22,Intellect=29,Spirit=28 WHERE Level=6 AND Class=8;
+
+UPDATE creature_template_classlevelstats SET Strength=148,Agility=38,Stamina=103,Intellect=334,Spirit=269 WHERE Level=71 AND Class=8;
+UPDATE creature_template_classlevelstats SET Strength=151,Agility=39,Stamina=105,Intellect=341,Spirit=275 WHERE Level=72 AND Class=8;
+UPDATE creature_template_classlevelstats SET Strength=154,Agility=39,Stamina=107,Intellect=348,Spirit=281 WHERE Level=73 AND Class=8;
+
+
