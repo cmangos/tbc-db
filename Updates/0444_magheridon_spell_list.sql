edd00e74a749992ab008b4c0d0a39d37159407f5
Miraco <daaniel743@gmail.com>
2024-06-26 09:03:15 +0200

    add spell_list to hellfire channeler

--- Updates/0444_magheridon_spell_list.sql
+++ Updates/0444_magheridon_spell_list.sql
@@ -15,3 +15,21 @@ INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `Combat
 (1882901, 6, 39175, 0, -1, 2, 0, 100, 0, 7000, 18000, 14000, 25000, 'Hellfire Warder - Shadow Bolt Volley - self');
 
 UPDATE `creature_template` SET `SpellList` = 1882901 WHERE `entry` = 18829;
+
+
+
+-- Hellfire Channeler
+DELETE FROM `creature_template_spells` WHERE `entry` = 17256;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` = 1725601;
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(1725601, 'Magtheridon\'s Lair - Hellfire Channeler', 0, 0);
+
+DELETE FROM `creature_spell_list` WHERE `Id` = 1725601;
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(1725601, 1, 30528, 0, -1, 201, 0, 100, 0, 5000, 19000, 11000, 21000, 'Hellfire Channeler - Dark Mending - under 50% including self'),
+(1725601, 2, 30530, 0, -1, 100, 0, 100, 0, 15000, 40000, 20000, 45000, 'Hellfire Channeler - Fear - random'),
+(1725601, 3, 30511, 0, -1, 100, 0, 100, 0, 11000, 50000, 60000, 80000, 'Hellfire Channeler - Burning Abyssal - random'),
+(1725601, 4, 30510, 0, -1, 2, 0, 100, 0, 8000, 19000, 11000, 22000, 'Hellfire Channeler - Shadow Bolt Volley - self');
+
+UPDATE `creature_template` SET `SpellList` = 1725601 WHERE `entry` = 17256;
