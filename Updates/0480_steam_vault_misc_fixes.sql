854ed5c90cec47178c69c7bc01c5a326ffcff700
Miraco <daaniel743@gmail.com>
2024-09-03 12:14:35 +0200

    coilfang oracle spell_list + ai corrections

--- Updates/0480_steam_vault_misc_fixes.sql
+++ Updates/0480_steam_vault_misc_fixes.sql
@@ -36,6 +36,14 @@ UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1.428' WHERE entry = 2
 UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1.428' WHERE entry = 21694;
 UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1.428' WHERE entry = 21914;
 
+-- Coilfang Oracle
+-- Before: SpeedWalk = 1,48 SpeedRun = 1,14286
+-- Sniff:
+-- WalkSpeed: 2.5
+-- RunSpeed: 7
+UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1' WHERE entry = 17803;
+UPDATE creature_template SET SpeedWalk = '1', SpeedRun = '1' WHERE entry = 20622;
+
 -- -----------
 -- SpellLists
 -- -----------
@@ -75,12 +83,12 @@ INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `Combat
 (1780101, 2, 35106, 0, -1, 1, 0, 100, 0, 6000, 23000, 6000, 23000, 'Coilfang Siren - Arcane Flare - current'),
 (1780101, 3, 15234, 0, -1, 1, 0, 100, 0, 3000, 10000, 3000, 10000, 'Coilfang Siren - Lightning Bolt - current'),
 
-(1780101, 1, 38660, 0, -1, 100, 0, 100, 0, 8000, 24000, 18000, 34000, 'Coilfang Siren - Fear - random'),
-(1780101, 2, 37856, 0, -1, 1, 0, 100, 0, 6000, 23000, 6000, 23000, 'Coilfang Siren - Arcane Flare - current'),
-(1780101, 3, 37664, 0, -1, 1, 0, 100, 0, 3000, 10000, 3000, 10000, 'Coilfang Siren - Lightning Bolt - current');
+(2062301, 1, 38660, 0, -1, 100, 0, 100, 0, 8000, 24000, 18000, 34000, 'Coilfang Siren - Fear - random'),
+(2062301, 2, 37856, 0, -1, 1, 0, 100, 0, 6000, 23000, 6000, 23000, 'Coilfang Siren - Arcane Flare - current'),
+(2062301, 3, 37664, 0, -1, 1, 0, 100, 0, 3000, 10000, 3000, 10000, 'Coilfang Siren - Lightning Bolt - current');
 
 UPDATE `creature_template` SET `SpellList` = 1780101 WHERE `entry` = 17801;
-UPDATE `creature_template` SET `SpellList` = 2062601 WHERE `entry` = 20623;
+UPDATE `creature_template` SET `SpellList` = 2062301 WHERE `entry` = 20623;
 
 
 -- Coilfang Engineer
@@ -117,12 +125,37 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 DELETE FROM `creature_spell_list` WHERE `Id` IN (2169401, 2191401);
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
 (2169401, 1, 40340, 0, -1, 2, 0, 100, 0, 7000, 20000, 15000, 28000, 'Bog Overlord - Trample - self'),
-(2169401, 1, 32065, 0, -1, 1, 0, 100, 0, 6000, 22000, 12000, 19000, 'Bog Overlord - Fungal Decay - current'),
-(2169401, 1, 37272, 0, -1, 1, 0, 100, 0, 5000, 18000, 5000, 18000, 'Bog Overlord - Poison Bolt- current'),
+(2169401, 2, 32065, 0, -1, 1, 0, 100, 0, 6000, 22000, 12000, 19000, 'Bog Overlord - Fungal Decay - current'),
+(2169401, 3, 37272, 0, -1, 1, 0, 100, 0, 5000, 18000, 5000, 18000, 'Bog Overlord - Poison Bolt- current'),
 
 (2191401, 1, 40340, 0, -1, 2, 0, 100, 0, 7000, 20000, 15000, 28000, 'Bog Overlord - Trample - self'),
-(2191401, 1, 32065, 0, -1, 1, 0, 100, 0, 6000, 22000, 12000, 19000, 'Bog Overlord - Fungal Decay - current'),
-(2191401, 1, 37862, 0, -1, 1, 0, 100, 0, 5000, 18000, 5000, 18000, 'Bog Overlord - Poison Bolt- current');
+(2191401, 2, 32065, 0, -1, 1, 0, 100, 0, 6000, 22000, 12000, 19000, 'Bog Overlord - Fungal Decay - current'),
+(2191401, 3, 37862, 0, -1, 1, 0, 100, 0, 5000, 18000, 5000, 18000, 'Bog Overlord - Poison Bolt- current');
 
 UPDATE `creature_template` SET `SpellList` = 2169401 WHERE `entry` = 21694;
 UPDATE `creature_template` SET `SpellList` = 2191401 WHERE `entry` = 21914;
+
+
+
+-- Coilfang Oracle
+DELETE FROM `creature_template_spells` WHERE `entry` = 17803;
+DELETE FROM `creature_template_spells` WHERE `entry` = 20622;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (1780301, 2062201);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(1780301, 'Steam Vault - Coilfang Oracle - Normal', 0, 0),
+(2062201, 'Steam Vault - Coilfang Oracle - Heroic', 0, 0);
+
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (1780301, 2062201);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(1780301, 1, 22883, 0, -1, 201, 0, 100, 0, 2000, 10000, 10000, 20000, 'Coilfang Oracle - Heal - Missing 50% including self'),
+(1780301, 2, 8281, 0, -1, 201, 0, 100, 0, 11000, 19000, 15000, 23000, 'Coilfang Oracle -  Sonic Burst - self'),
+(1780301, 3, 22582, 0, -1, 1, 0, 100, 0, 6000, 15000, 6000, 15000, 'Coilfang Oracle - Frost Shock - current'),
+
+(2062201, 1, 31730, 0, -1, 201, 0, 100, 0, 2000, 10000, 10000, 20000, 'Coilfang Oracle - Heal - Missing 50% including self'),
+(2062201, 2, 8281, 0, -1, 201, 0, 100, 0, 11000, 19000, 15000, 23000, 'Coilfang Oracle -  Sonic Burst - self'),
+(2062201, 3, 37865, 0, -1, 1, 0, 100, 0, 6000, 15000, 6000, 15000, 'Coilfang Oracle - Frost Shock - current');
+
+UPDATE `creature_template` SET `SpellList` = 1780301 WHERE `entry` = 17803;
+UPDATE `creature_template` SET `SpellList` = 2062201 WHERE `entry` = 20622;
