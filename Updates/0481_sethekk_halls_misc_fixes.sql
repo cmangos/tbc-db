58f0141f6a244900d83e7ac3d83cd4a171ac5b1f
Miraco <daaniel743@gmail.com>
2024-09-26 14:28:44 +0200

    more spell_lists

--- Updates/0481_sethekk_halls_misc_fixes.sql
+++ Updates/0481_sethekk_halls_misc_fixes.sql
@@ -106,3 +106,68 @@ INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `Combat
 
 UPDATE `creature_template` SET `SpellList` = 1832801 WHERE `entry` = 18328;
 UPDATE `creature_template` SET `SpellList` = 2069401 WHERE `entry` = 20694;
+
+
+-- Cobalt Serpent
+DELETE FROM `creature_template_spells` WHERE `entry` = 19428;
+DELETE FROM `creature_template_spells` WHERE `entry` = 20688;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (1942801, 2068801);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(1942801, 'Sethekk Halls - Cobalt Serpent - Normal', 0, 0),
+(2068801, 'Sethekk Halls - Cobalt Serpent - Heroic', 0, 0);
+
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (1942801, 2068801);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(1942801, 1, 38110, 0, -1, 1, 0, 100, 0, 6000, 19000, 12000, 25000, 'Cobalt Serpent - Wing Buffet - current'),
+(1942801, 2, 17503, 0, -1, 1, 0, 100, 0, 8000, 25000, 12000, 29000, 'Cobalt Serpent - Frostbolt - current'),
+(1942801, 3, 38193, 0, -1, 1, 0, 100, 0, 3000, 16000, 7000, 20000, 'Cobalt Serpent - Lightning Breath - current'),
+
+(2068801, 1, 38110, 0, -1, 1, 0, 100, 0, 6000, 19000, 12000, 25000, 'Cobalt Serpent - Wing Buffet - current'),
+(2068801, 2, 38238, 0, -1, 1, 0, 100, 0, 8000, 25000, 12000, 29000, 'Cobalt Serpent - Frostbolt - current'),
+(2068801, 3, 38133, 0, -1, 1, 0, 100, 0, 3000, 16000, 7000, 20000, 'Cobalt Serpent - Lightning Breath - current');
+
+
+UPDATE `creature_template` SET `SpellList` = 1942801 WHERE `entry` = 19428;
+UPDATE `creature_template` SET `SpellList` = 2068801 WHERE `entry` = 20688;
+
+-- Avian Ripper
+DELETE FROM `creature_template_spells` WHERE `entry` = 21891;
+DELETE FROM `creature_template_spells` WHERE `entry` = 21989;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (2189101, 2198901);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(2189101, 'Sethekk Halls - Avian Ripper - Normal', 0, 0),
+(2198901, 'Sethekk Halls - Avian Ripper - Heroic', 0, 0);
+
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (2189101, 2198901);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(2189101, 1, 38056, 0, -1, 1, 0, 100, 0, 5000, 18000, 8000, 21000, 'Avian Ripper - Flesh Rip - current'),
+(2198901, 1, 38056, 0, -1, 1, 0, 100, 0, 5000, 18000, 8000, 21000, 'Avian Ripper - LFlesh Rip - current');
+
+
+UPDATE `creature_template` SET `SpellList` = 2189101 WHERE `entry` = 21891;
+UPDATE `creature_template` SET `SpellList` = 2198901 WHERE `entry` = 21989;
+
+-- Sethekk Ravenguard 
+DELETE FROM `creature_template_spells` WHERE `entry` = 18322;
+DELETE FROM `creature_template_spells` WHERE `entry` = 20696;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (1832201, 2069601);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(1832201, 'Sethekk Halls - Sethekk Ravenguard  - Normal', 0, 0),
+(2069601, 'Sethekk Halls - Sethekk Ravenguard  - Heroic', 0, 0);
+
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (1832201, 2069601);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(1832201, 1, 32651, 0, -1, 2, 0, 100, 0, 6000, 25000, 17000, 28000, 'Sethekk Ravenguard  - Howling Screech - self'),
+(1832201, 2, 33964, 0, -1, 1, 0, 100, 0, 5000, 17000, 11000, 25000, 'Sethekk Ravenguard  - Bloodthirst - current'),
+
+(2069601, 1, 32651, 0, -1, 2, 0, 100, 0, 6000, 25000, 17000, 28000, 'Sethekk Ravenguard  - Howling Screech - self'),
+(2069601, 2, 40423, 0, -1, 1, 0, 100, 0, 5000, 17000, 11000, 25000, 'Sethekk Ravenguard  - Bloodthirst - current');
+
+UPDATE `creature_template` SET `SpellList` = 1832201 WHERE `entry` = 18322;
+UPDATE `creature_template` SET `SpellList` = 2069601 WHERE `entry` = 20696;
