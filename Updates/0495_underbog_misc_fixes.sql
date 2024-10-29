8d3798daee36548272e70b3f698e5734f3bfd139
Miraco <daaniel743@gmail.com>
2024-10-29 09:14:18 +0100

    Wrathfin Myrmidon and Wrathfin Sentry spell_list

--- Updates/0495_underbog_misc_fixes.sql
+++ Updates/0495_underbog_misc_fixes.sql
@@ -13,6 +13,12 @@ UPDATE creature_template SET SpeedWalk = 2/2.5, SpeedRun = 8/7 WHERE entry IN (1
 -- Bog Giant
 UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 8/7 WHERE entry IN (17723, 20164);
 
+-- Wrathfin Myrmidon
+UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 8/7 WHERE entry IN (17726, 20191);
+
+-- Wrathfin Sentry
+UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 8/7 WHERE entry IN (17727, 20192);
+
 -- -----------
 -- SpellLists
 -- -----------
@@ -81,3 +87,43 @@ INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `Combat
 
 UPDATE `creature_template` SET `SpellList` = 1772401 WHERE `entry` = 17724;
 UPDATE `creature_template` SET `SpellList` = 2018501 WHERE `entry` = 20185;
+
+
+-- Wrathfin Myrmidon
+DELETE FROM `creature_template_spells` WHERE `entry` = 17726;
+DELETE FROM `creature_template_spells` WHERE `entry` = 20191;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (1772601, 2019101);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(1772601, 'The Underbog - Wrathfin Myrmidon - Normal', 0, 0),
+(2019101, 'The Underbog - Wrathfin Myrmidon - Heroic', 0, 0);
+
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (1772601, 2019101);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(1772601, 1, 31410, 0, -1, 130, 0, 100, 0, 1000, 12000, 4000, 15000, 'Wrathfin Myrmidon - Coral Cut - top aggro aura not present'),
+(2019101, 1, 37973, 0, -1, 130, 0, 100, 0, 1000, 12000, 4000, 15000, 'Wrathfin Myrmidon - Coral Cut - top aggro aura not present');
+
+UPDATE `creature_template` SET `SpellList` = 1772601 WHERE `entry` = 17726;
+UPDATE `creature_template` SET `SpellList` = 2019101 WHERE `entry` = 20191;
+
+-- Wrathfin Sentry
+DELETE FROM `creature_template_spells` WHERE `entry` = 17727;
+DELETE FROM `creature_template_spells` WHERE `entry` = 20192;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (1772701, 2019201);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(1772701, 'The Underbog - Wrathfin Sentry - Normal', 0, 0),
+(2019201, 'The Underbog - Wrathfin Sentry - Heroic', 0, 0);
+
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (1772701, 2019201);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+(1772701, 1, 11972, 0, -1, 122, 0, 100, 0, 6000, 12000, 6000, 15000, 'Wrathfin Sentry - Shield Bash - random target casting'),
+(1772701, 2, 11976, 0, -1, 1, 0, 100, 0, 4000, 12000, 10000, 18000, 'Wrathfin Sentry - Strike - current'),
+
+(2019201, 1, 11972, 0, -1, 122, 0, 100, 0, 6000, 12000, 6000, 15000, 'Wrathfin Sentry - Shield Bash - random target casting'),
+(2019201, 2, 11976, 0, -1, 1, 0, 100, 0, 4000, 12000, 10000, 18000, 'Wrathfin Sentry - Strike - current');
+
+UPDATE `creature_template` SET `SpellList` = 1772701 WHERE `entry` = 17727;
+UPDATE `creature_template` SET `SpellList` = 2019201 WHERE `entry` = 20192;
