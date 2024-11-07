cdd47e7fd3de23ebc7b1a19d0142f95df9cab6f3
Miraco <daaniel743@gmail.com>
2024-11-07 08:09:17 +0100

    Add right side Fel Overseer that only use 1 path

--- Updates/0498_shadowlabs_misc_fixes.sql
+++ Updates/0498_shadowlabs_misc_fixes.sql
@@ -17,6 +17,9 @@ UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 8/7 WHERE entry IN 
 -- Fel Guardhound
 UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 8/7 WHERE entry IN (18642, 20651);
 
+-- Fel Overseer
+UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 8/7 WHERE entry IN (18796, 20652);
+
 -- -----------
 -- SpellLists
 -- -----------
