e370b30821185e7db2388d9a2065c9dc5a8c9544
killerwife <killerwife@gmail.com>
2024-11-30 17:40:36 +0100

    Add note about nethercite to file and comment out wipe

--- Updates/0503_outland_veins.sql
+++ Updates/0503_outland_veins.sql
@@ -5,6 +5,8 @@ https://www.wowhead.com/tbc/object=181556/adamantite-deposit
 https://www.wowhead.com/tbc/object=181569/rich-adamantite-deposit - rest
 https://www.wowhead.com/tbc/object=181570/rich-adamantite-deposit - nagrand only
 https://www.wowhead.com/tbc/object=181557/khorium-vein
+
+https://www.wowhead.com/tbc/object=185877/nethercite-deposit
 */
 
-DELETE FROM gameobject WHERE Id IN(181555, 181556, 181569, 181570, 181557) AND map=530 AND position_x NOT BETWEEN 11400 AND 13100;
+-- DELETE FROM gameobject WHERE Id IN(181555, 181556, 181569, 181570, 181557) AND map=530 AND position_x NOT BETWEEN 11400 AND 13100;
