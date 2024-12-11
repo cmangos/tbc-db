0410f47817d73ec277188b95f27298de67b7155a
Miraco <daaniel743@gmail.com>
2024-12-11 13:59:15 +0100

    add sql for netherstorm rework

---
+++ Updates/0520_netherstorm_south_east_island.sql
@@ -0,0 +1,17 @@
+-- ----------------
+-- Netherstorm 
+-- Sout East Island with Area 52 
+-- ---------------
+
+
+-- Bridge from Blade's Edge to netherstorm
+
+
+-- Spawns
+-- Also reguid them into tbc range
+SET @CGUID := 5307000; -- creatures
+
+
+-- Waypoints
+
+-- Scripts
