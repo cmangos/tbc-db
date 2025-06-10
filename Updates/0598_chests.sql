8d3fb276f98cbe47611500263fad9305ee99d3f1
killerwife <killerwife@gmail.com>
2025-06-10 12:22:53 +0200

    Add wip file for chests parse

---
+++ Updates/0598_chests.sql
@@ -0,0 +1,32 @@
+/*
+2843 - Battered Chest
+2844 - Tattered Chest
+2845 - Tattered Chest
+2846 - Tattered Chest
+2847 - Tattered Chest
+2849 - Battered Chest
+2850 - Solid Chest
+2852 - Solid Chest
+2855 - Solid Chest
+2857 - Solid Chest
+3714 - Alliance Strongbox
+3715 - Tattered Chest
+4096 - Tattered Chest
+4149 - Solid Chest
+105570 - Alliance Strongbox
+105578 - Tattered Chest
+105579 - Alliance Chest
+105581 - Tattered Chest
+106318 - Battered Chest
+106319 - Battered Chest
+111095 - Tattered Chest
+153451 - Solid Chest
+153453 - Solid Chest
+153454 - Solid Chest
+153463 - Large Solid Chest
+153468 - Large Mithril Bound Chest
+181798 - Fel Iron Chest
+181800 - Heavy Fel Iron Chest
+181802 - Adamantite Bound Chest
+181804 - Felsteel Chest
+*/
