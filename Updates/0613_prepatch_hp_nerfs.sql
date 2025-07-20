e5b886c86e966ebf2692f971af54706981437a70
AnonXS <anonxs@web.de>
2025-07-20 04:24:29 +0200

    Correct more nerfed hp for npcs
    probably some prepatch hp nerfes that bleed into tbc-db
    
    this is not "pre nerf"

---
+++ Updates/0613_prepatch_hp_nerfs.sql
@@ -0,0 +1,93 @@
+-- Fix Hp for prepatch hp nerfed mobs
+UPDATE `creature_template` SET `HealthMultiplier`='9' WHERE `entry`='15548'; -- Spectral Stallion (64629) - https://www.wowhead.com/tbc/npc=15548/spectral-stallion
+UPDATE `creature_template` SET `HealthMultiplier`='50' WHERE `entry`='15550'; -- Attumen the Huntsman (379400) - https://www.wowhead.com/tbc/npc=15550/attumen-the-huntsman
+UPDATE `creature_template` SET `HealthMultiplier`='4' WHERE `entry`='16178'; -- Phase Hound (27944) - https://www.wowhead.com/tbc/npc=16178/phase-hound
+UPDATE `creature_template` SET `HealthMultiplier`='6' WHERE `entry`='16406'; -- Phantom Attendant (34464) - https://www.wowhead.com/tbc/npc=16406/phantom-attendant
+UPDATE `creature_template` SET `HealthMultiplier`='2' WHERE `entry`='16409'; -- Phantom Guest (11178) - https://www.wowhead.com/tbc/npc=16409/phantom-guest
+UPDATE `creature_template` SET `HealthMultiplier`='6' WHERE `entry`='16412'; -- Ghostly Baker (43086) - https://www.wowhead.com/tbc/npc=16412/ghostly-baker
+UPDATE `creature_template` SET `HealthMultiplier`='8' WHERE `entry`='16414'; -- Ghostly Steward (57448) - https://www.wowhead.com/tbc/npc=16414/ghostly-steward
+UPDATE `creature_template` SET `HealthMultiplier`='6' WHERE `entry`='16425'; -- Phantom Guardsman (43086) - https://www.wowhead.com/tbc/npc=16425/phantom-guardsman
+UPDATE `creature_template` SET `HealthMultiplier`='8' WHERE `entry`='16470'; -- Ghostly Philanthropist (47224) - https://www.wowhead.com/tbc/npc=16470/ghostly-philanthropist
+UPDATE `creature_template` SET `HealthMultiplier`='9' WHERE `entry`='16472'; -- Phantom Stagehand (64629) - https://www.wowhead.com/tbc/npc=16472/phantom-stagehand
+UPDATE `creature_template` SET `HealthMultiplier`='10' WHERE `entry`='16482'; -- Trapped Soul (59030) - https://www.wowhead.com/tbc/npc=16482/trapped-soul
+UPDATE `creature_template` SET `HealthMultiplier`='10' WHERE `entry`='16595'; -- Fleshbeast (73800) - https://www.wowhead.com/tbc/npc=16595/fleshbeast
+UPDATE `creature_template` SET `HealthMultiplier`='2' WHERE `entry`='17067'; -- Phantom Hound (13972) - https://www.wowhead.com/tbc/npc=17067/phantom-hound
+UPDATE `creature_template` SET `HealthMultiplier`='1.9' WHERE `entry`='17096'; -- Astral Flare (13273) - https://www.wowhead.com/tbc/npc=17096/astral-flare
+UPDATE `creature_template` SET `HealthMultiplier`='12' WHERE `entry`='17229'; -- Kil'rek (58680) - https://www.wowhead.com/tbc/npc=17229/kilrek
+UPDATE `creature_template` SET `HealthMultiplier`='15' WHERE `entry`='17547'; -- Tinhead (110700) - https://www.wowhead.com/tbc/npc=17547/tinhead
+UPDATE `creature_template` SET `HealthMultiplier`='700' WHERE `entry`='17888'; -- Kaz'rogal (4249000) - https://www.wowhead.com/tbc/npc=17888/kazrogal
+UPDATE `creature_template` SET `HealthMultiplier`='22' WHERE `entry`='17899'; -- Shadowy Necromancer (122958) - https://www.wowhead.com/tbc/npc=17899/shadowy-necromancer
+UPDATE `creature_template` SET `HealthMultiplier`='3' WHERE `entry`='18404'; -- Bloodwarder Steward (20283) - https://www.wowhead.com/tbc/npc=18404/bloodwarder-steward
+UPDATE `creature_template` SET `HealthMultiplier`='5' WHERE `entry`='21549'; -- Bloodwarder Steward (28293) - https://www.wowhead.com/tbc/npc=18404/bloodwarder-steward
+UPDATE `creature_template` SET `HealthMultiplier`='15' WHERE `entry`='18806'; -- Solarium Priest (77490) - https://www.wowhead.com/tbc/npc=18806/solarium-priest
+UPDATE `creature_template` SET `HealthMultiplier`='4' WHERE `entry`='18925'; -- Solarium Agent (27944) - https://www.wowhead.com/tbc/npc=18925/solarium-agent
+UPDATE `creature_template` SET `HealthMultiplier`='2.25' WHERE `entry`='19513'; -- Mutate Fear-Shrieker (15719) - https://www.wowhead.com/tbc/npc=19513/mutate-fear-shrieker
+UPDATE `creature_template` SET `HealthMultiplier`='2.6' WHERE `entry`='21560'; -- Mutate Fear-Shrieker (20958) - https://www.wowhead.com/tbc/npc=19513/mutate-fear-shrieker
+UPDATE `creature_template` SET `HealthMultiplier`='400' WHERE `entry`='19514'; -- Al'ar (3035200) - https://www.wowhead.com/tbc/npc=19514/alar
+UPDATE `creature_template` SET `HealthMultiplier`='10' WHERE `entry`='19551'; -- Ember of Al'ar (69860) - https://www.wowhead.com/tbc/npc=19551/ember-of-alar
+UPDATE `creature_template` SET `HealthMultiplier`='600' WHERE `entry`='19622'; -- Kael'thas Sunstrider (3642000) - https://www.wowhead.com/tbc/npc=19622/kaelthas-sunstrider
+UPDATE `creature_template` SET `HealthMultiplier`='1.9' WHERE `entry`='19781'; -- Astral Flare (13273) - https://www.wowhead.com/tbc/npc=19781/astral-flare
+UPDATE `creature_template` SET `HealthMultiplier`='1.9' WHERE `entry`='19782'; -- Astral Flare (13273) - https://www.wowhead.com/tbc/npc=19782/astral-flare
+UPDATE `creature_template` SET `HealthMultiplier`='1.9' WHERE `entry`='19783'; -- Astral Flare (13273) - https://www.wowhead.com/tbc/npc=19783/astral-flare
+UPDATE `creature_template` SET `HealthMultiplier`='2.25' WHERE `entry`='19865'; -- Mutate Horror (15719) - https://www.wowhead.com/tbc/npc=19865/mutate-horror
+UPDATE `creature_template` SET `HealthMultiplier`='2.6' WHERE `entry`='21562'; -- Mutate Horror (20958) - https://www.wowhead.com/tbc/npc=19865/mutate-horror
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='20037'; -- Tempest Falconer (143600) - https://www.wowhead.com/tbc/npc=20037/tempest-falconer
+UPDATE `creature_template` SET `HealthMultiplier`='10' WHERE `entry`='20038'; -- Phoenix-Hawk Hatchling (69860) - https://www.wowhead.com/tbc/npc=20038/phoenix-hawk-hatchling
+UPDATE `creature_template` SET `MinLevel` = 72, `MaxLevel` = 72, `HealthMultiplier`='75', `DamageMultiplier` = 32 WHERE `entry`='20039'; -- Phoenix-Hawk (553500) - https://www.wowhead.com/tbc/npc=20039/phoenix-hawk
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='20042'; -- Tempest-Smith (179525) - https://www.wowhead.com/tbc/npc=20042/tempest-smith
+UPDATE `creature_template` SET `HealthMultiplier`='4' WHERE `entry`='20043'; -- Apprentice Star Scryer (22356) - https://www.wowhead.com/tbc/npc=20043/apprentice-star-scryer
+UPDATE `creature_template` SET `HealthMultiplier`='4' WHERE `entry`='20044'; -- Novice Astromancer (22356) - https://www.wowhead.com/tbc/npc=20044/novice-astromancer
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='20047'; -- Crimson Hand Battle Mage (143600) - https://www.wowhead.com/tbc/npc=20047/crimson-hand-battle-mage
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='20048'; -- Crimson Hand Centurion (179525) - https://www.wowhead.com/tbc/npc=20048/crimson-hand-centurion
+UPDATE `creature_template` SET `HealthMultiplier`='40' WHERE `entry`='20049'; -- Crimson Hand Blood Knight (229760) - https://www.wowhead.com/tbc/npc=20049/crimson-hand-blood-knight
+UPDATE `creature_template` SET `HealthMultiplier`='40' WHERE `entry`='20050'; -- Crimson Hand Inquisitor (236120) - https://www.wowhead.com/tbc/npc=20050/crimson-hand-inquisitor
+UPDATE `creature_template` SET `HealthMultiplier`='36' WHERE `entry`='20063'; -- Master Engineer Telonicus (273168) - https://www.wowhead.com/tbc/npc=20063/master-engineer-telonicus
+UPDATE `creature_template` SET `HealthMultiplier`='36' WHERE `entry`='20064'; -- Thaladred the Darkener (273168) - https://www.wowhead.com/tbc/npc=20064/thaladred-the-darkener
+UPDATE `creature_template` SET `HealthMultiplier`='35' WHERE `entry`='21218'; -- Vashj'ir Honor Guard (251335) - https://www.wowhead.com/tbc/npc=21218/vashjir-honor-guard
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='21224'; -- Tidewalker Depth-Seer (143600) - https://www.wowhead.com/tbc/npc=21224/tidewalker-depth-seer
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='21225'; -- Tidewalker Warrior (179525) - https://www.wowhead.com/tbc/npc=21225/tidewalker-warrior
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='21226'; -- Tidewalker Shaman (143600) - https://www.wowhead.com/tbc/npc=21226/tidewalker-shaman
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='21227'; -- Tidewalker Harpooner (179525) - https://www.wowhead.com/tbc/npc=21227/tidewalker-harpooner
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='21228'; -- Tidewalker Hydromancer (143600) - https://www.wowhead.com/tbc/npc=21228/tidewalker-hydromancer
+UPDATE `creature_template` SET `HealthMultiplier`='15' WHERE `entry`='21246'; -- Serpentshrine Sporebat (107715) - https://www.wowhead.com/tbc/npc=21246/serpentshrine-sporebat
+UPDATE `creature_template` SET `HealthMultiplier`='1.5' WHERE `entry`='21253'; -- Tainted Water Elemental (10772) - https://www.wowhead.com/tbc/npc=21253/tainted-water-elemental
+UPDATE `creature_template` SET `HealthMultiplier`='27' WHERE `entry`='21268'; -- Netherstrand Longbow (188622) - https://www.wowhead.com/tbc/npc=21268/netherstrand-longbow
+UPDATE `creature_template` SET `HealthMultiplier`='27' WHERE `entry`='21271'; -- Infinity Blades (188622) - https://www.wowhead.com/tbc/npc=21271/infinity-blades
+UPDATE `creature_template` SET `HealthMultiplier`='40.5' WHERE `entry`='21273'; -- Phaseshift Bulwark (282933) - https://www.wowhead.com/tbc/npc=21273/phaseshift-bulwark
+UPDATE `creature_template` SET `HealthMultiplier`='22.5' WHERE `entry`='21274'; -- Staff of Disintegration (157185) - https://www.wowhead.com/tbc/npc=21274/staff-of-disintegration
+UPDATE `creature_template` SET `HealthMultiplier`='10' WHERE `entry`='21364'; -- Phoenix Egg (69860) - https://www.wowhead.com/tbc/npc=21364/phoenix-egg
+UPDATE `creature_template` SET `HealthMultiplier`='1.4875' WHERE `entry`='21857'; -- Inner Demon (10392) - https://www.wowhead.com/tbc/npc=21857/inner-demon
+UPDATE `creature_template` SET `HealthMultiplier`='15' WHERE `entry`='21863'; -- Serpentshrine Lurker (104790) - https://www.wowhead.com/tbc/npc=21863/serpentshrine-lurker
+UPDATE `creature_template` SET `HealthMultiplier`='2.5' WHERE `entry`='21920'; -- Tidewalker Lurker (17953) - https://www.wowhead.com/tbc/npc=21920/tidewalker-lurker
+UPDATE `creature_template` SET `HealthMultiplier`='1' WHERE `entry`='22009'; -- Tainted Elemental (6986) - https://www.wowhead.com/tbc/npc=22009/tainted-elemental
+UPDATE `creature_template` SET `HealthMultiplier`='6' WHERE `entry`='22035'; -- Pure Spawn of Hydross (43086) - https://www.wowhead.com/tbc/npc=22035/pure-spawn-of-hydross
+UPDATE `creature_template` SET `HealthMultiplier`='6' WHERE `entry`='22036'; -- Tainted Spawn of Hydross (43086) - https://www.wowhead.com/tbc/npc=22036/tainted-spawn-of-hydross
+UPDATE `creature_template` SET `HealthMultiplier`='30' WHERE `entry`='22847'; -- Ashtongue Primalist (177090) - https://www.wowhead.com/tbc/npc=22847/ashtongue-primalist
+UPDATE `creature_template` SET `HealthMultiplier`='15' WHERE `entry`='22848'; -- Storm Fury (104790) - https://www.wowhead.com/tbc/npc=22848/storm-fury
+UPDATE `creature_template` SET `HealthMultiplier`='825' WHERE `entry`='22871'; -- Teron Gorefiend (5007750) - https://www.wowhead.com/tbc/npc=22871/teron-gorefiend
+UPDATE `creature_template` SET `HealthMultiplier`='30' WHERE `entry`='22882'; -- Shadowmoon Deathshaper (177090) - https://www.wowhead.com/tbc/npc=22882/shadowmoon-deathshaper
+UPDATE `creature_template` SET `HealthMultiplier`='0.5' WHERE `entry`='22929'; -- Greater Shadowfiend (2952) - https://www.wowhead.com/tbc/npc=22929/greater-shadowfiend
+UPDATE `creature_template` SET `HealthMultiplier`='4.5' WHERE `entry`='22939'; -- Temple Concubine (25151) - https://www.wowhead.com/tbc/npc=22939/temple-acolyte
+UPDATE `creature_template` SET `HealthMultiplier`='10' WHERE `entry`='22946'; -- Shadowmoon War Hound (71810) - https://www.wowhead.com/tbc/npc=22946/shadowmoon-war-hound
+UPDATE `creature_template` SET `HealthMultiplier`='4.5' WHERE `entry`='22955'; -- Charming Courtesan (25151) - https://www.wowhead.com/tbc/npc=22955/charming-patron
+UPDATE `creature_template` SET `HealthMultiplier`='25' WHERE `entry`='22965'; -- Enslaved Servant (179525) - https://www.wowhead.com/tbc/npc=22965/devoted-steward
+UPDATE `creature_template` SET `HealthMultiplier`='8.5' WHERE `entry`='23111'; -- Shadowy Construct (59381) - https://www.wowhead.com/tbc/npc=23111/shadowy-construct
+UPDATE `creature_template` SET `HealthMultiplier`='35' WHERE `entry`='23172'; -- Hand of Gorefiend (258300) - https://www.wowhead.com/tbc/npc=23172/hand-of-gorefiend
+UPDATE `creature_template` SET `HealthMultiplier`='1' WHERE `entry`='23398'; -- Angered Soul Fragment (5589) - https://www.wowhead.com/tbc/npc=23398/angered-soul-fragment
+UPDATE `creature_template` SET `HealthMultiplier`='12' WHERE `entry`='23399'; -- Suffering Soul Fragment (70836) - https://www.wowhead.com/tbc/npc=23399/suffering-soul-fragment
+UPDATE `creature_template` SET `HealthMultiplier`='6' WHERE `entry`='23401'; -- Hungering Soul Fragment (44280) - https://www.wowhead.com/tbc/npc=23401/hungering-soul-fragment
+UPDATE `creature_template` SET `HealthMultiplier`='300' WHERE `entry`='23418'; -- Essence of Suffering (2276400) - https://www.wowhead.com/tbc/npc=23418/essence-of-suffering
+UPDATE `creature_template` SET `HealthMultiplier`='400' WHERE `entry`='23419'; -- Essence of Desire (3035200) - https://www.wowhead.com/tbc/npc=23419/essence-of-desire
+UPDATE `creature_template` SET `HealthMultiplier`='400' WHERE `entry`='23420'; -- Essence of Anger (3035200) - https://www.wowhead.com/tbc/npc=23420/essence-of-anger
+UPDATE `creature_template` SET `HealthMultiplier`='1000' WHERE `entry`='23426'; -- The Illidari Council (6986000) - https://www.wowhead.com/tbc/npc=23426/the-illidari-council#comments:id=318366
+UPDATE `creature_template` SET `HealthMultiplier`='8' WHERE `entry`='23436'; -- Image of Dementia (47224) - https://www.wowhead.com/tbc/npc=23436/image-of-dementia
+UPDATE `creature_template` SET `HealthMultiplier`='1' WHERE `entry`='24722'; -- Fel Crystal (6986) - https://www.wowhead.com/tbc/npc=24722/fel-crystal
+UPDATE `creature_template` SET `HealthMultiplier`='3.915' WHERE `entry`='19884'; -- Bogstrok (1) (27350) - https://www.wowhead.com/tbc/npc=17816/bogstrok
+UPDATE `creature_template` SET `HealthMultiplier`='3.915' WHERE `entry`='20264'; -- Nexus Stalker (1) (28856) - https://www.wowhead.com/tbc/npc=18314/nexus-stalker
+UPDATE `creature_template` SET `HealthMultiplier`='1.35', `MinLevel`= 70, `MaxLevel` = 70 WHERE `entry`='20579'; -- Shattered Hand Archer (1) https://www.wowhead.com/tbc/npc=17427/shattered-hand-archer - strange case
+UPDATE `creature_template` SET `HealthMultiplier`='3.915' WHERE `entry`='20698'; -- Time-Lost Shadowmage (1) (20225) - https://www.wowhead.com/tbc/npc=18320/time-lost-shadowmage
+UPDATE `creature_template` SET `HealthMultiplier`='18.9' WHERE `entry`='20993'; -- Blood Guard Porung (1) (139482) - https://www.wowhead.com/tbc/npc=20923/blood-guard-porung
+UPDATE `creature_template` SET `HealthMultiplier`='1' WHERE `entry`='22300'; -- Spore Strider (1) (5589) - https://www.wowhead.com/tbc/npc=22299/spore-strider
+UPDATE `creature_template` SET `HealthMultiplier`='2' WHERE `entry`='25552'; -- Fel Crystal (1) (13972) - https://www.wowhead.com/tbc/npc=24722/fel-crystal
+UPDATE `creature_template` SET `MinLevel`= 71 WHERE `entry`='20260'; -- Ethereal Spellbinder (1) - https://www.wowhead.com/tbc/npc=18312/ethereal-spellbinder
+
