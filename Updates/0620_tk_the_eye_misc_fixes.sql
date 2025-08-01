f8dd106221ecef0151e67b4f69aa1eca6c80dcdc
Miraco <daaniel743@gmail.com>
2025-08-01 10:11:04 +0200

    First batch of sniffed value changes

--- Updates/0620_tk_the_eye_misc_fixes.sql
+++ Updates/0620_tk_the_eye_misc_fixes.sql
@@ -1,7 +1,39 @@
 -- -----------
 -- Tempest Keep : The Eye
 -- Misc Changes
+--
+-- All those changes are double checked with SniffedDB and tbc/wotlk classic sniffs
 -- -----------
+
+-- Solarium Priest
+-- SniffedDB base_attack_time  1500 (2000 cmangos)
+-- [0] UNIT_FIELD_BASEATTACKTIME: 1500
+-- SniffedDB bounding_radius = 0,766 
+-- SniffedDB combat_reach = 3
+-- [0] UNIT_FIELD_BOUNDINGRADIUS: 0.766
+-- [0] UNIT_FIELD_COMBATREACH: 3
+UPDATE creature_template SET MeleeBaseAttackTime = 1500 WHERE entry = 18806;
+-- SniffedDB bounding_radius = 0,766 (2 cmangos)
+-- SniffedDB combat_reach = 3 (0 cmangos)
+UPDATE creature_model_info SET bounding_radius = 0.766, combat_reach = 2 WHERE modelid IN (19050, 19051, 19052);
+
+-- Solarium Agent 
+-- SniffedDB Level 60-70 (cmangos only 69)
+-- [0] UNIT_FIELD_LEVEL: 70
+UPDATE creature_template SET MaxLevel = 70 WHERE entry = 18925;
+-- SniffedDB bounding_radius = 0.47875 (2 cmangos)
+-- SniffedDB combat_reach = 1.875 (0 cmangos)
+-- [0] UNIT_FIELD_BOUNDINGRADIUS: 0.47875
+-- [0] UNIT_FIELD_COMBATREACH: 1.875
+UPDATE creature_model_info SET bounding_radius = 0.47875, combat_reach = 1.875 WHERE modelid IN (19048, 19049);
+
+-- Kael'thas Sunstrider (19622)
+-- SniffedDB SpeedWalk 1.2
+-- SniffedDB SpeedRun 1.42857
+-- WalkSpeed: 3
+-- RunSpeed: 10
+UPDATE creature_template SET SpeedWalk = 3/2.5, SpeedRun = 10/7 WHERE entry = 19622;
+
 
 -- Bloodwarder Legionnaire
 -- WalkSpeed: 4
@@ -9,51 +41,48 @@
 -- UNIT_FIELD_BOUNDINGRADIUS: 0.6128
 -- UNIT_FIELD_COMBATREACH: 2.4
 UPDATE creature_template SET SpeedWalk = 4/2.5, SpeedRun = 10/7 WHERE entry = 20031;
-UPDATE creature_model_info SET bounding_radius = 2.4, combat_reach = 0.6128 WHERE modelid IN (19386, 19387);
+UPDATE creature_model_info SET bounding_radius = 0.6128, combat_reach = 2.4 WHERE modelid IN (19386, 19387);
 
 -- Bloodwarder Vindicator 20032
 -- WalkSpeed: 2.5
 -- RunSpeed: 10
 -- UNIT_FIELD_BOUNDINGRADIUS: 0.68939996
 -- UNIT_FIELD_COMBATREACH: 2.6999998
--- UNIT_FIELD_DISPLAYID: 21000
 UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 10/7 WHERE entry = 20032;
-UPDATE creature_model_info SET bounding_radius = 2.6999998, combat_reach = 0.68939996 WHERE modelid IN (21000, 19388);
+UPDATE creature_model_info SET bounding_radius = 0.68939996, combat_reach = 2.6999998 WHERE modelid IN (21000, 19388);
 
 -- Astromancer 20033
 -- WalkSpeed: 2.5
 -- RunSpeed: 10
 -- UNIT_FIELD_BOUNDINGRADIUS: 0.6128
 -- UNIT_FIELD_COMBATREACH: 2.4
--- UNIT_FIELD_DISPLAYID: 19391
 UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 10/7 WHERE entry = 20033;
-UPDATE creature_model_info SET bounding_radius = 2.4, combat_reach = 0.6128 WHERE modelid IN (19390, 19391);
+UPDATE creature_model_info SET bounding_radius = 0.6128, combat_reach = 2.4 WHERE modelid IN (19390, 19391);
 
 -- Star Scryer 20034
 -- WalkSpeed: 2.5
 -- RunSpeed: 10
 -- UNIT_FIELD_BOUNDINGRADIUS: 0.68939996
 -- UNIT_FIELD_COMBATREACH: 2.6999998
--- UNIT_FIELD_DISPLAYID: 19392
 UPDATE creature_template SET SpeedWalk = 2.5/2.5, SpeedRun = 10/7 WHERE entry = 20034;
-UPDATE creature_model_info SET bounding_radius = 2.6999998, combat_reach = 0.68939996 WHERE modelid IN (19392, 19393);
+UPDATE creature_model_info SET bounding_radius = 0.68939996, combat_reach = 2.6999998 WHERE modelid IN (19392, 19393);
+
 
 -- Bloodwarder Marshal 20035
 -- WalkSpeed: 4
 -- RunSpeed: 10
 -- UNIT_FIELD_BOUNDINGRADIUS: 0.766
 -- UNIT_FIELD_COMBATREACH: 3
--- UNIT_FIELD_DISPLAYID: 19397
 UPDATE creature_template SET SpeedWalk = 4/2.5, SpeedRun = 10/7 WHERE entry = 20035;
 UPDATE creature_model_info SET bounding_radius = 0.766, combat_reach = 3 WHERE modelid IN (19397, 20978);
+
 
 -- Bloodwarder Squire 20036
 -- WalkSpeed: 4
 -- RunSpeed: 10
 -- UNIT_FIELD_BOUNDINGRADIUS: 0.57449996
 -- UNIT_FIELD_COMBATREACH: 2.25
--- UNIT_FIELD_DISPLAYID: 21001
-UPDATE creature_template SET SpeedWalk = 4/2.5, SpeedRun = 10/7 WHERE entry = 20035;
+UPDATE creature_template SET SpeedWalk = 4/2.5, SpeedRun = 10/7 WHERE entry = 20036;
 UPDATE creature_model_info SET bounding_radius = 0.57449996, combat_reach = 2.25 WHERE modelid IN (19394, 21001);
 
 -- Delete old unused DBscripts
--- Updates/0621_tk_the_eye_spell_lists.sql
+++ Updates/0621_tk_the_eye_spell_lists.sql
@@ -12,9 +12,9 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 
 DELETE FROM `creature_spell_list` WHERE `Id` = 2003101;
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
-(2003101, 1, 15284, 0, -1, 1, 0, 100, 0, 7000, 13000, 17000, 20000, 'Bloodwarder Legionnaire - Cleave - current'),
-(2003101, 2, 34984, 0, -1, 2, 0, 100, 0, 10000, 16000, 18000, 27000, 'Bloodwarder Legionnaire - Whirlwind - self'),
-(2003101, 3, 34984, 0, -1, 2, 0, 100, 0, 8000, 18000, 22000, 24000, 'Bloodwarder Legionnaire - Bloodthirst - self');
+(2003101, 1, 15284, 0, -1, 1, 0, 100, 0, 6000, 22000, 11000, 25000, 'Bloodwarder Legionnaire - Cleave - current'),
+(2003101, 2, 33500, 0, -1, 2, 0, 100, 0, 6000, 14000, 16000, 28000, 'Bloodwarder Legionnaire - Whirlwind - self'),
+(2003101, 3, 35949, 0, -1, 2, 0, 100, 0, 6000, 22000, 16000, 30000, 'Bloodwarder Legionnaire - Bloodthirst - self');
 
 UPDATE `creature_template` SET `SpellList` = 2003101 WHERE `entry` = 20031;
 
@@ -27,13 +27,12 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 
 DELETE FROM `creature_spell_list` WHERE `Id` = 2003201;
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
-(2003201, 1, 37251, 0, -1, 100, 0, 100, 0, 3000, 5000, 6000, 10000, 'Bloodwarder Legionnaire - Hammer of Wrath - random player'),
-(2003201, 2, 37249, 0, -1, 201, 0, 100, 0, 9000, 17000, 9000, 17000, 'Bloodwarder Legionnaire - Flash of Light - missing 50% including self'),
-(2003201, 3, 39078, 0, -1, 3, 0, 100, 0, 5000, 10000, 5000, 10000, 'Bloodwarder Legionnaire - Cleanse - eligible friendly dispel'),
-(2003201, 4, 13005, 0, -1, 1, 0, 100, 0, 3000, 9000, 18000, 24000, 'Bloodwarder Legionnaire - Hammer of Justice - current');
+(2003201, 1, 37251, 0, -1, 100, 0, 100, 0, 3000, 7000, 6000, 10000, 'Bloodwarder Legionnaire - Hammer of Wrath - random player'),
+(2003201, 2, 37249, 0, -1, 201, 0, 100, 0, 2000, 8000, 8000, 16000, 'Bloodwarder Legionnaire - Flash of Light - missing 50% including self'),
+(2003201, 3, 39078, 0, -1, 3, 0, 100, 0, 4000, 12000, 6000, 14000, 'Bloodwarder Legionnaire - Cleanse - eligible friendly dispel'),
+(2003201, 4, 13005, 0, -1, 1, 0, 100, 0, 6000, 19000, 9000, 24000, 'Bloodwarder Legionnaire - Hammer of Justice - current');
 
 UPDATE `creature_template` SET `SpellList` = 2003201 WHERE `entry` = 20032;
-
 
 -- Astromancer
 DELETE FROM `creature_template_spells` WHERE `entry` = 20033;
@@ -44,8 +43,8 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 
 DELETE FROM `creature_spell_list` WHERE `Id` = 2003301;
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
-(2003301, 1, 37110, 0, -1, 1, 0, 100, 0, 14000, 19000, 18000, 28000, 'Astromancer - Fire Blast - current'),
-(2003301, 2, 37109, 0, -1, 1, 0, 100, 0, 8000, 13000, 11000, 16000, 'Astromancer - Fireball Volley - current');
+(2003301, 1, 37110, 0, -1, 1, 0, 100, 0, 10000, 21000, 10000, 25000, 'Astromancer - Fire Blast - current'),
+(2003301, 2, 37109, 0, -1, 1, 0, 100, 0, 6000, 16000, 11000, 20000, 'Astromancer - Fireball Volley - current');
 
 UPDATE `creature_template` SET `SpellList` = 2003301 WHERE `entry` = 20033;
 
@@ -59,10 +58,9 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 
 DELETE FROM `creature_spell_list` WHERE `Id` = 2003401;
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
-(2003401, 1, 37124, 0, -1, 100, 0, 100, 0, 7000, 12000, 22000, 30000, 'Star Scryer - Starfall - random'),
-(2003401, 2, 37126, 0, -1, 1, 0, 100, 0, 13000, 16000, 13000, 25000, 'Star Scryer - Arcane Blast - current'),
-(2003401, 3, 37122, 0, -1, 101, 0, 100, 0, 8000, 16000, 21000, 36000, 'Star Scryer - Domination - random not tank');
-
+(2003401, 1, 37124, 0, -1, 100, 0, 100, 0, 6000, 15000, 22000, 35000, 'Star Scryer - Starfall - random'),
+(2003401, 2, 37126, 0, -1, 1, 0, 100, 0, 11000, 22000, 11000, 25000, 'Star Scryer - Arcane Blast - current'),
+(2003401, 3, 37122, 0, -1, 101, 0, 100, 0, 5000, 18000, 16000, 28000, 'Star Scryer - Domination - random not tank');
 
 UPDATE `creature_template` SET `SpellList` = 2003401 WHERE `entry` = 20034;
 
@@ -76,9 +74,9 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 
 DELETE FROM `creature_spell_list` WHERE `Id` = 2003501;
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
-(2003501, 1, 35949, 0, -1, 2, 0, 100, 0, 8000, 18000, 22000, 24000, 'Bloodwarder Marshal - Bloodthirst - self'),
-(2003501, 2, 34996, 0, -1, 1, 0, 100, 0, 19000, 29000, 22000, 31000, 'Bloodwarder Marshal - Uppercut - current'),
-(2003501, 3, 36132, 0, -1, 2, 0, 100, 0, 10000, 15000, 14000, 17000, 'Bloodwarder Marshal - Whirlwind - self');
+(2003501, 1, 35949, 0, -1, 2, 0, 100, 0, 6000, 25000, 16000, 32000, 'Bloodwarder Marshal - Bloodthirst - self'),
+(2003501, 2, 36132, 0, -1, 2, 0, 100, 0, 6000, 17000, 16000, 32000, 'Bloodwarder Marshal - Whirlwind - self'),
+(2003501, 3, 34996, 0, -1, 1, 0, 100, 0, 17000, 32000, 16000, 30000, 'Bloodwarder Marshal - Uppercut - current'),
 
 UPDATE `creature_template` SET `SpellList` = 2003501 WHERE `entry` = 20035;
 
@@ -91,10 +89,10 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 
 DELETE FROM `creature_spell_list` WHERE `Id` = 2003601;
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
-(2003601, 1, 37255, 0, -1, 100, 0, 100, 0, 3000, 5000, 6000, 10000, 'Bloodwarder Squire - Hammer of Wrath - random player'),
-(2003601, 2, 37254, 0, -1, 206, 0, 100, 0, 9000, 17000, 9000, 17000, 'Bloodwarder Squire - Flash of Light - missing 25% including self'),
-(2003601, 3, 39078, 0, -1, 3, 0, 100, 0, 5000, 10000, 5000, 10000, 'Bloodwarder Squire - Cleanse - eligible friendly dispel'),
-(2003601, 4, 39077, 0, -1, 1, 0, 100, 0, 3000, 9000, 18000, 24000, 'Bloodwarder Squire - Hammer of Justice - current');
+(2003601, 1, 37255, 0, -1, 100, 0, 100, 0, 3000, 8000, 6000, 10000, 'Bloodwarder Squire - Hammer of Wrath - random player'),
+(2003601, 2, 37254, 0, -1, 206, 0, 100, 0, 2000, 8000, 8000, 14000, 'Bloodwarder Squire - Flash of Light - missing 25% including self'),
+(2003601, 3, 39078, 0, -1, 3, 0, 100, 0, 6000, 18000, 6000, 18000, 'Bloodwarder Squire - Cleanse - eligible friendly dispel'),
+(2003601, 4, 39077, 0, -1, 1, 0, 100, 0, 8000, 20000, 14000, 24000, 'Bloodwarder Squire - Hammer of Justice - current');
 
 UPDATE `creature_template` SET `SpellList` = 2003601 WHERE `entry` = 20036;
 
