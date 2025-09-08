1bf55b9cec208a100bd9c6c1cf7783efbfd560b6
Miraco <daaniel743@gmail.com>
2025-09-08 18:24:51 +0200

    DBErrors: delete some unused movement scripts from Tempest Keep : The Eye

---
+++ Updates/0634_tempest_keep_dberrors.sql
@@ -0,0 +1,5 @@
+-- Table `dbscripts_on_creature_movement` contain unused script, id 10121.
+-- Table `dbscripts_on_creature_movement` contain unused script, id 10122.
+-- Table `dbscripts_on_creature_movement` contain unused script, id 10123.
+
+DELETE FROM dbscripts_on_creature_movement WHERE id IN (10121, 10122, 10123);
