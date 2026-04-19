b1193d789d668afda995368da9782fbf8f0c736a
evil-at-wow <evil.at.wow@gmail.com>
2026-04-19 22:40:37 +0200

    Fix chat type for Core Hound (11671) texts.

---
+++ Updates/0707_core_hound_bct.sql
@@ -0,0 +1,6 @@
+
+-- %s collapses and begins to smolder.
+UPDATE broadcast_text SET ChatTypeID = 2 WHERE Id = 7866;
+
+-- %s reignites from the heat of another Core Hound!
+UPDATE broadcast_text SET ChatTypeID = 2 WHERE Id = 7867;
