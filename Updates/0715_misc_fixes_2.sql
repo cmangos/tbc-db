6cc8ae2ae15e76524f78e7fe4b4bdb38a978e450
evil-at-wow <evil-at-wow@users.noreply.github.com>
2026-05-17 15:56:16 +0200

    Fix chat type for Core Hound (11671) texts
    Resolves https://github.com/cmangos/tbc-db/pull/1287

---
+++ Updates/0715_misc_fixes_2.sql
@@ -0,0 +1,8 @@
+-- https://github.com/cmangos/tbc-db/pull/1287
+
+-- %s collapses and begins to smolder.
+UPDATE broadcast_text SET ChatTypeID = 2 WHERE Id = 7866;
+
+-- %s reignites from the heat of another Core Hound!
+UPDATE broadcast_text SET ChatTypeID = 2 WHERE Id = 7867;
+
