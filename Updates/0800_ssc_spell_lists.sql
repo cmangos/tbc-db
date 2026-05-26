074e4102664ccd3471098d74af6ecb0fd1c1330f
Miraco <daaniel743@gmail.com>
2026-05-26 13:40:22 +0200

    Underbog Colossus into spell_list
    update timers to sniffed db

--- Updates/0800_ssc_spell_lists.sql
+++ Updates/0800_ssc_spell_lists.sql
@@ -52,3 +52,26 @@ INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `Combat
 (2133901, 2, 38496, 0, -1, 100, 0, 100, 0, 3000, 12000, 3000, 12000, 'Coilfang Hate-Screamer - Sonic Scream - random');
 
 UPDATE `creature_template` SET `SpellList` = 2133901 WHERE `entry` = 21339;
+
+-- Underbog Colossus
+DELETE FROM `creature_template_spells` WHERE `entry` = 21251;
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (2125101, 2125102, 2125103);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(2125101, 'Serpentshrine Cavern - Underbog Colossus - Set 1', 0, 0),
+(2125102, 'Serpentshrine Cavern - Underbog Colossus - Set 2', 0, 0),
+(2125103, 'Serpentshrine Cavern - Underbog Colossus - Set 3', 0, 0);
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (2125101, 2125102, 2125103);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+-- Set 1 (Phase 1-5)
+(2125101, 1, 39032, 0, -1, 100, 0, 100, 0, 10000, 22000, 11000, 25000, 'Underbog Colossus - Initial Infection - random'),
+(2125101, 2, 38976, 0, -1, 2, 0, 100, 0, 15000, 24000, 20000, 30000, 'Underbog Colossus - Spore Quake - self'),
+-- Set 2 (Phase 6-10)
+(2125102, 1, 39044, 0, -1, 100, 0, 100, 0, 16000, 30000, 25000, 35000, 'Underbog Colossus - Serpentshrine Parasite - random'),
+(2125102, 2, 38971, 0, -1, 101, 0, 100, 0, 13000, 23000, 21000, 30000, 'Underbog Colossus - Acid Geyser - random not tank'),
+-- Set 3 (Phase 6-10)
+(2125103, 1, 39031, 0, -1, 2, 0, 100, 0, 12000, 21000, 12000, 21000, 'Underbog Colossus - Frenzy - self'),
+(2125103, 2, 39015, 0, -1, 1, 0, 100, 0, 4000, 16000, 4000, 16000, 'Underbog Colossus -Antrophic Blow - current');
+
+
