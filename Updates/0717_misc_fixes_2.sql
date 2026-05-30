a194dd6b99b37fea0f2ac08234b29661054406df
AnonXS <anonxs@web.de>
2026-05-30 14:10:55 +0200

    Update [0717] Typo

--- Updates/0717_misc_fixes_2.sql
+++ Updates/0717_misc_fixes_2.sql
@@ -1,6 +1,6 @@
 -- Correct Affect Masks for Vile Poisons Talent
 -- Instant Poison 8192 + Crippling Poison 16384 + Mind-numbing Poison 32768 + Deadly Poison 65536 + Wound Poison 268435456 + Anesthetic Poison 68719476736
-UPDATE `spell_affect` SET `SpellFamilyMask` = (8192,16384,32768,65536,268435456+68719476736) WHERE `effectId` = 0 AND `entry` = 16513; -- 34359861248 (SPELLMOD_DAMAGE)
-UPDATE `spell_affect` SET `SpellFamilyMask` = 65536 WHERE `effectId` = 1 AND `entry` = 16513; -- 65536 (SPELLMOD_DOT)
-UPDATE `spell_affect` SET `SpellFamilyMask` = (8192,16384,32768,65536,268435456+68719476736) WHERE `effectId` = 2 AND `entry` = 16513; -- 268550144 (SPELLMOD_RESIST_DISPEL_CHANCE)
+UPDATE `spell_affect` SET `SpellFamilyMask` = (8192+16384+32768+65536+268435456+68719476736) WHERE `entry` = 16513 AND `effectId` = 0; -- 34359861248 (SPELLMOD_DAMAGE)
+UPDATE `spell_affect` SET `SpellFamilyMask` = 65536 WHERE `entry` = 16513 AND `effectId` = 1; -- 65536 (SPELLMOD_DOT)
+UPDATE `spell_affect` SET `SpellFamilyMask` = (8192+16384+32768+65536+268435456+68719476736) WHERE `entry` = 16513 AND `effectId` = 2; -- 268550144 (SPELLMOD_RESIST_DISPEL_CHANCE)
 
