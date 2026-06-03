af07c91345cb8e49e0154a186b8ed094598d3317
AnonXS <anonxs@web.de>
2026-06-03 04:28:08 +0200

    Update [0082] Fix Targeting for Hellmaw
    Now casts his stuff, though sometimes he seems to skip timer (Availability/Probability issue?)

--- Updates/0082_sl_ambassador_hellmaw.sql
+++ Updates/0082_sl_ambassador_hellmaw.sql
@@ -6,11 +6,11 @@ INSERT INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRange
 (2063601, 'SL - Ambassador Hellmaw - Heroic', 0, 0);
 DELETE FROM creature_spell_list WHERE Id IN(1873101,2063601);
 INSERT INTO creature_spell_list(Id, Position, SpellId, Flags, CombatCondition, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
-('1873101', '0', '33551', '0', '-1', '100', '0', '100', '1','20000','23000','23000','35000', 'Ambassador Hellmaw - Corrosive Acid'),
-('1873101', '1', '33547', '0', '-1', '100', '0', '100', '1','20000','26000','20000','38000', 'Ambassador Hellmaw - Fear'),
-('2063601', '0', '33551', '0', '-1', '100', '0', '100', '1','20000','23000','23000','35000', 'Ambassador Hellmaw - Corrosive Acid'),
-('2063601', '1', '33547', '0', '-1', '100', '0', '100', '1','20000','26000','20000','38000', 'Ambassador Hellmaw - Fear'),
-('2063601', '2', '34970', '0', '-1', '100', '0', '100', '1','180000','180000','180000','180000', 'Ambassador Hellmaw - Enrage');
+('1873101', '0', '33551', '0', '-1', '1', '0', '100', '1','20000','23000','23000','35000', 'Ambassador Hellmaw - Corrosive Acid - current'),
+('1873101', '1', '33547', '0', '-1', '1', '0', '100', '1','20000','26000','20000','38000', 'Ambassador Hellmaw - Fear - current'),
+('2063601', '0', '33551', '0', '-1', '1', '0', '100', '1','20000','23000','23000','35000', 'Ambassador Hellmaw - Corrosive Acid - current'),
+('2063601', '1', '33547', '0', '-1', '1', '0', '100', '1','20000','26000','20000','38000', 'Ambassador Hellmaw - Fear - current'),
+('2063601', '2', '34970', '0', '-1', '0', '0', '100', '1','180000','180000','180000','180000', 'Ambassador Hellmaw - Enrage - none');
 
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10475 WHERE Id IN(17860);
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10476 WHERE Id IN(17861);
@@ -19,5 +19,4 @@ UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10474 WHERE Id IN(17859);
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10478 WHERE Id IN(17864);
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10479 WHERE Id IN(17865);
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10480 WHERE Id IN(17866);
-
 
