2f31fbbd38a17b1a56d368c64938070ac36b4268
AnonXS <anonxs@web.de>
2026-06-03 04:50:16 +0200

    Vendorlist for Eyonix 19531

---
+++ Updates/0722_misc_fixes_2.sql
@@ -0,0 +1,6 @@
+-- Vendorlist for Eyonix 19531
+UPDATE `npc_vendor` SET `slot` = 1 WHERE `item` = 29454 AND `entry` = 19531;
+UPDATE `npc_vendor` SET `slot` = 2 WHERE `item` = 29395 AND `entry` = 19531;
+UPDATE `npc_vendor` SET `slot` = 3 WHERE `item` = 29393 AND `entry` = 19531;
+UPDATE `npc_vendor` SET `slot` = 4 WHERE `item` = 29394 AND `entry` = 19531;
+
