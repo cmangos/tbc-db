993a95d7b7155a097b6d14e7a2200c06b91aee33
Miraco <daaniel743@gmail.com>
2026-06-09 12:12:02 +0200

    Fix typo in [0728]

--- Updates/0728_bonechewer_ravener.sql
+++ Updates/0728_bonechewer_ravener.sql
@@ -1,5 +1,5 @@
 -- Bonechewer Ravener
 -- Patch 2.1 - Bonechewer Raveners on Heroic, damage has been reduced and will now attack slower.
 -- Pre Nerf MeleeBaseAttackTime 1200
--- Nerfed Value according to wotlk and later sniffs is 14000 on heroic.
+-- Nerfed Value according to wotlk and later sniffs is 1400 on heroic.
 UPDATE creature_template SET MeleeBaseAttackTime = 1400 WHERE entry = 18054;
