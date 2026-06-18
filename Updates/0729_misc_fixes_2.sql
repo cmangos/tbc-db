b9ca1c5f811b7c0a623375720f52fcfdb38fdcac
AnonXS <anonxs@web.de>
2026-06-18 15:47:03 +0200

    Vendor Slots for Sylannia 14844

---
+++ Updates/0729_misc_fixes_2.sql
@@ -0,0 +1,8 @@
+-- Vendor Slots for Sylannia 14844
+UPDATE `npc_vendor` SET `slot`= 1 WHERE `entry`=14844 AND `item`=19222;
+UPDATE `npc_vendor` SET `slot`= 2 WHERE `entry`=14844 AND `item`=19221;
+UPDATE `npc_vendor` SET `slot`= 3 WHERE `entry`=14844 AND `item`=19299;
+UPDATE `npc_vendor` SET `slot`= 4 WHERE `entry`=14844 AND `item`=19300;
+UPDATE `npc_vendor` SET `slot`= 5 WHERE `entry`=14844 AND `item`=33234;
+UPDATE `npc_vendor` SET `slot`= 6 WHERE `entry`=14844 AND `item`=33236;
+
