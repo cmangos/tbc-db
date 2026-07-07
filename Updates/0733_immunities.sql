3d255c7ff2919ac576d4e19b685c762b61e736d6
killerwife <killerwife@gmail.com>
2026-07-07 21:34:04 +0200

    One more immunity and fixed comment

--- Updates/0733_immunities.sql
+++ Updates/0733_immunities.sql
@@ -3,7 +3,7 @@ DELETE FROM creature_immunities WHERE entry IN(19513) AND Type=4 AND Value=3; --
 INSERT INTO creature_immunities(Entry, SetId, Type, Value) VALUES
 (19513, 0,  4, 3);
 
-DELETE FROM creature_immunities WHERE entry IN(19735) AND Type=4 AND Value=3; -- Mutate Fear-Shrieker - devouring plague immunity
+DELETE FROM creature_immunities WHERE entry IN(19735) AND Type=4 AND Value=3; -- Tempest-Forge Destroyer - devouring plague immunity
 INSERT INTO creature_immunities(Entry, SetId, Type, Value) VALUES
 (19735, 0,  1, 53);
 
@@ -11,3 +11,6 @@ DELETE FROM creature_immunities WHERE entry IN(19219) AND Type=4 AND Value=3; --
 INSERT INTO creature_immunities(Entry, SetId, Type, Value) VALUES
 (19219, 0,  1, 53);
 
+DELETE FROM creature_immunities WHERE entry IN(19166) AND Type=4 AND Value=3; -- Tempest-Forge Patroller - devouring plague immunity - leech
+INSERT INTO creature_immunities(Entry, SetId, Type, Value) VALUES
+(19166, 0,  1, 53);
