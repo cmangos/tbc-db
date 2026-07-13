0c296b48de9ec3b6aac2353cafa24071b045b03d
Velite12 <45871932+Velite12@users.noreply.github.com>
2026-07-13 02:48:06 -0400

    Update 0738_underbog_fixes.sql

--- Updates/0738_underbog_fixes.sql
+++ Updates/0738_underbog_fixes.sql
@@ -5,3 +5,6 @@ UPDATE `creature_spell_list` SET `Position` = 2 WHERE `Id` = 2016401 AND `Positi
 
 -- Add Enrage at low hp (seems below 30) to Normal Bog Giant
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES ('1772301', '3', '8599', '0', '5364', '0', '0', '100', '1', '0', '0', '600000', '600000', 'Bog Giant - Enrage below 30');
+
+-- Add Enrage at low hp (seems below 15) to Normal Underbog Lord
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES ('1773401', '3', '8599', '0', '1107', '0', '0', '100', '1', '0', '0', '600000', '600000', 'Underbog Lord - Enrage below 15');
