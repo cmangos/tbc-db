5801ad70e0b90a7941f5cc1ede2cd10dc91e56c4
Velite12 <45871932+Velite12@users.noreply.github.com>
2026-08-19 20:51:32 +0200

    Add player pet imp spell list

---
+++ Updates/0005_imp_spell_lists.sql
@@ -0,0 +1,9 @@
+DELETE FROM pet_autocast_spell_list WHERE CreatureEntry=416;
+INSERT INTO pet_autocast_spell_list(CreatureEntry, SpellId, CombatCondition, TargetId, Comments) VALUES
+(416, 6307, -1, 0, 'Imp - Blood Pact Rank 1'),
+(416, 7804, -1, 0, 'Imp - Blood Pact Rank 2'),
+(416, 7805, -1, 0, 'Imp - Blood Pact Rank 3'),
+(416, 11766, -1, 0, 'Imp - Blood Pact Rank 4'),
+(416, 11767, -1, 0, 'Imp - Blood Pact Rank 5'),
+(416, 27268, -1, 0, 'Imp - Blood Pact Rank 6');
+
