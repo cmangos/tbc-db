29030ab2ada5a92e84ec6c9b9ae1ad04e0abe2ab
AnonXS <anonxs@web.de>
2026-09-21 03:33:50 +0200

    Apply Phasing Cleric SpellList 1855701 to Phasing Cleric 18557
    https://github.com/cmangos/tbc-db/commit/992b31a3252e7f870a2d5d402eeffcff46142247#diff-45a2ed4fdd1206ad66fcde301b83594b4543eb5a0b0bc05c3b8265104ecee452R23-R296

---
+++ Updates/0028_18557_list.sql
@@ -0,0 +1,4 @@
+-- Phasing Cleric 18557
+-- https://github.com/cmangos/tbc-db/commit/992b31a3252e7f870a2d5d402eeffcff46142247#diff-45a2ed4fdd1206ad66fcde301b83594b4543eb5a0b0bc05c3b8265104ecee452R23-R296
+UPDATE `creature_template` SET `SpellList` = 1855701 WHERE `entry` = 18557;
+
